cb8f2df235cc35fa61f1b88ff8c40f811b6849a7
Jeff Scherpelz <jeff.scherpelz@blist.com>
2009-01-23 10:01:34 -0800

    Add hover highlight for table rows Add simple logging function to use Firebug console with jQuery
    Reviewed-by: Sam Gibson <sam.gibson@blist.com>

--- public/javascripts/screens/blists-screen.js
+++ public/javascripts/screens/blists-screen.js
@@ -54,6 +54,40 @@ blist.myBlists.rowClickedHandler = function (event)
     }
 }
 
+blist.myBlists.tableMousemoveHandler = function (event)
+{
+    var $target = $(event.target);
+    if ($target.is('tbody') || $target.is('table'))
+    {
+        // If they move the mouse directly on the table or tbody,
+        //  they are not in a row and we should unhighlight them
+        $('td.hover', $target).removeClass('hover');
+    }
+    else if ($target.is('td') && !$target.is('td.hover') &&
+        $target.parent().is('tr.item'))
+    {
+        // If they move over a td that is not already in a hover state,
+        //  and is in an item row, highlight it
+        $target.siblings().andSelf().addClass('hover');
+    }
+}
+
+blist.myBlists.tableMouseoutHandler = function (event)
+{
+    var $target = $(event.target);
+    if ($target.is('tbody') || $target.is('table'))
+    {
+        // If they mouse out of the table or tbody, unhighlight everything
+        $('td.hover', $target).removeClass('hover');
+    }
+    else if ($target.is('td'))
+    {
+        // If they moused out of a cell, unhighlight that row
+        $target.siblings().andSelf().removeClass('hover');
+    }
+}
+
+
 
 /* Functions for info pane related to blists */
 
@@ -97,6 +131,8 @@ $(function ()
 {
     $(window).resize(ns.adjustSize);
     $('#blistList').click(ns.rowClickedHandler);
+    $('#blistList').mousemove(ns.tableMousemoveHandler);
+    $('#blistList').mouseout(ns.tableMouseoutHandler);
     $('#blists').bind(blist.events.ROW_SELECTION, ns.infoPane.rowSelectionHandler);
 
     ns.infoPane.updateSummary();
---
+++ public/javascripts/util/debug.js
@@ -0,0 +1,5 @@
+// extend jQuery with Firebug console logging: http://muzso.hu/2008/03/07/logging-to-the-firebug-console-in-jquery
+jQuery.fn.log = function (msg) {
+  console.log("%s: %o", msg, this);
+  return this;
+};

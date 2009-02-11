161f8c1cfbf574f3a6099e6b4b0dd55af9dfc95b
Pete Stuart <pete.stuart@blist.com>
2009-02-11 12:41:27 -0800

    NOOP: Minor revisions to chrome styling, Fix for IE8 javascript window sizing.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

--- public/javascripts/screens/common-layout.js
+++ public/javascripts/screens/common-layout.js
@@ -3,17 +3,26 @@ var commonNS = blist.namespace.fetch('blist.common');
 // Default function to do generic resizing to fill the screen
 //  If you need more detailed behavior, remove .scrollContent and
 //  implement a separate resize for your screen
+var cachedWinHeight = 0;
 blist.common.adjustSize = function ()
 {
+    // Test to see if the cached window height matches the current window height.
+    // We need to cache this because IE8 will throw itself into an infinite loop if we don't.
+    if ($(window).height() != cachedWinHeight) 
+    {
     $('.scrollContent').each(function ()
     {
         blist.util.sizing.fitWindow($(this));
     });
+        cachedWinHeight = $(window).height();
+    }
 }
-
-$(window).resize(commonNS.adjustSize);
 
 $(function ()
 {
+    $(window).resize(function () 
+    {
+        commonNS.adjustSize();
+    });
     commonNS.adjustSize();
 });

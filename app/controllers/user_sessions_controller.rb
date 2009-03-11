5f25aded5663f04dd17273eec493855c4653e775
Jeff Scherpelz <jeff.scherpelz@blist.com>
2009-03-11 13:36:22 -0700

    NOOP: Styled login page
    Reviewed-by: Pete Stuart <pete.stuart@blist.com>

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,14 +2,18 @@ class UserSessionsController < ApplicationController
   skip_before_filter :require_user
 
   def new
+    @body_id = 'login'
     @user_session = UserSession.new
   end
 
   def create
+    @body_id = 'login'
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      redirect_back_or_default root_url
+      redirect_back_or_default(root_url)
     else
+      flash[:notice] = "Unable to login with that username and password;" +
+        " please try again"
       render :action => :new
     end
   end
@@ -18,6 +22,7 @@ class UserSessionsController < ApplicationController
     if current_user_session
       current_user_session.destroy
     end
-    redirect_to new_user_session_url
+    flash[:notice] = "You have been logged out"
+    redirect_to(new_user_session_url)
   end
 end

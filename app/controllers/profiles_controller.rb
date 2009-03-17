697ab8f854a4653c9dd4aef3d47cc4d23ba830b1
Jeff Scherpelz <jeff.scherpelz@blist.com>
2009-03-17 13:14:12 -0700

    NOOP: Fill out display for profile page * Friendly display for country & state * Display list of public blists & filters for the user * Proper pluralization of 'item' for multi-select info pane * Correct last opened date for filter list
    Reviewed-by: Pete Stuart <pete.stuart@blist.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,12 @@
 class ProfilesController < ApplicationController
   def show
+    @body_id = 'profileBody'
+    @public_blists = current_user.public_blists.sort do |a,b|
+      if a.viewCount == b.viewCount
+        a.name <=> b.name
+      else
+        b.viewCount <=> a.viewCount
+      end
+    end
   end
 end
---
+++ public/javascripts/screens/profile-screen.js
@@ -0,0 +1,8 @@
+var profileNS = blist.namespace.fetch('blist.profile');
+
+/* Initial start-up calls, and setting up bindings */
+
+$(function ()
+{
+    $('#profile .publicBlists table.gridList').blistListHoverItems();
+});

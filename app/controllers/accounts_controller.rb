53e45aba890cc505ff6926ba792a8889e55b2339
Pete Stuart <pete.stuart@blist.com>
2009-03-17 09:32:49 -0700

    NOOP: My account page - added editable panels, forms, basic javascript.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,15 @@ class AccountsController < ApplicationController
   def show
     @user = User.find(current_user.id)
   end
+  
+  def update
+    @user = User.find(current_user.id)
+    @user.email = params[:email] || @user.email
+    @user.password = params[:password_new] || @user.password
+    
+    respond_to do |format|
+      format.html { redirect_to(account_url) }
+      format.data   { render :json => @user.to_json }
+    end
+  end
 end
---
+++ public/javascripts/screens/account-screen.js
@@ -0,0 +1,42 @@
+$(function ()
+{
+    $(".sectionShow p").hover(
+        function() { $(this).addClass("hover"); },
+        function() { $(this).removeClass("hover"); }
+    );
+
+    $(".sectionShow p, .sectionShow a.showAction").click(function(event)
+    {
+        event.preventDefault();
+        $(this).closest(".sectionShow").slideUp("fast");
+        $(this).closest(".listSection").find(".sectionEdit").slideDown("fast");
+    });
+
+    $(".sectionClose a").click(function(event)
+    {
+        event.preventDefault();
+        $(this).closest(".sectionEdit").slideUp("fast");
+        $(this).closest(".listSection").find(".sectionShow").slideDown("fast");
+    });
+    
+    $(".sectionEdit form").submit(function(event)
+    {
+        $this = $(this);
+        event.preventDefault();
+        
+        var requestData = $.param($this.find(":input"));
+        requestData += "&" + form_authenticity_token + "=" + encodeURIComponent(request_forgery_protection_token);
+        $.ajax({
+            url: window.location.path,
+            type: "PUT",
+            dataType: "json",
+            data: requestData,
+            success: function(responseData, textStatus) {
+                $(".dataEmail").text(responseData.data.email);
+                
+                $this.closest(".sectionEdit").slideUp("fast");
+                $this.closest(".listSection").find(".sectionShow").slideDown("fast");
+            }
+        });
+    });
+});

1b805afcfa21d2a796adff258ae463da0092f7e0
Pete Stuart <pete.stuart@blist.com>
2009-03-18 10:33:16 -0700

    NOOP: Added form field validation for my account forms.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

--- public/javascripts/screens/account-screen.js
+++ public/javascripts/screens/account-screen.js
@@ -30,24 +30,53 @@ $(function ()
         $(this).closest(".listSection").find(".sectionShow").slideDown("fast");
     });
     
-    $(".sectionEdit form").submit(function(event)
-    {
-        $this = $(this);
-        event.preventDefault();
-        
-        var requestData = $.param($this.find(":input"));
-        requestData += "&" + form_authenticity_token + "=" + encodeURIComponent(request_forgery_protection_token);
-        $.ajax({
-            url: window.location.path,
-            type: "PUT",
-            dataType: "json",
-            data: requestData,
-            success: function(responseData, textStatus) {
-                $(".dataEmail").text(responseData.data.email);
-                
-                $this.closest(".sectionEdit").slideUp("fast");
-                $this.closest(".listSection").find(".sectionShow").slideDown("fast");
-            }
-        });
+    // Form validation.
+    $.validator.setDefaults({
+        submitHandler: function(form)
+        {
+            $form = $(form);
+            
+            var requestData = $.param($form.find(":input"));
+            requestData += "&" + form_authenticity_token + "=" + encodeURIComponent(request_forgery_protection_token);
+            $.ajax({
+                url: $form.attr("action"),
+                type: "PUT",
+                dataType: "json",
+                data: requestData,
+                success: function(responseData, textStatus) {
+                    $(".dataEmail").text(responseData.data.email);
+                    
+                    $form.closest(".sectionEdit").slideUp("fast");
+                    $form.closest(".listSection").find(".sectionShow").slideDown("fast");
+                }
+            });
+        }
     });
+    
+    // Email form.
+    $(".emailSection form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            email_confirm: {
+                required: true,
+                equalTo: "#email"
+            },
+            email_password: "required",
+        }
+    });
+    // Password form.
+    $(".passwordSection form").validate({
+        rules: {
+            password_old: "required",
+            password_new: "required",
+            password_confirm: {
+                required: true,
+                equalTo: "#password_new"
+            }
+        }
+    });
+    
 });

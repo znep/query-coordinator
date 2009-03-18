9b0e7c56ee1e9853ecf8c75c6798687f816c03c8
Jeff Scherpelz <jeff.scherpelz@blist.com>
2009-03-18 09:33:12 -0700

    NOOP: Add sidebar boxes on profile page
    Reviewed-by: Pete Stuart <pete.stuart@blist.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,23 @@
 class ProfilesController < ApplicationController
   def show
     @body_id = 'profileBody'
-    @public_blists = current_user.public_blists.sort do |a,b|
+    @all_owned_views = View.find().reject {|v| v.owner.id != current_user.id}
+    @public_views = @all_owned_views.reject {|v| !v.is_public?}.sort do |a,b|
       if a.viewCount == b.viewCount
         a.name <=> b.name
       else
         b.viewCount <=> a.viewCount
       end
     end
+    @public_view_count = @public_views.inject(0) {|sum,v| sum + v.viewCount}
+
+    @private_blists = @all_owned_views.find_all {|v|
+      !v.is_public? && v.flag?('default')}
+    @private_filters = @all_owned_views.find_all {|v|
+      !v.is_public? && !v.flag?('default')}
+    @shared_blists = @all_owned_views.find_all {|v|
+      v.is_shared? && v.flag?('default')}
+
+    @contacts = Contact.find();
   end
 end

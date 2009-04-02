db84139c445df1d3c00bf702d3634ae7be48c42e
Pete Stuart <pete.stuart@blist.com>
2009-04-02 16:24:28 -0700

    NOOP: Fixed cliptext plugin so it uses parent to determine height.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

--- public/javascripts/widgets/clipped-text.js
+++ public/javascripts/widgets/clipped-text.js
@@ -2,7 +2,7 @@ blist.namespace.fetch('blist.widget.clippedText');
 
 /* This function takes an element with text, and will clip the
  * text to fit, using an ellipse on the end if it is clipped.
- * The element passed is assumed to have display of block, have
+ * The element passed must be within a parent who has a display of block, have
  * fixed height, have auto width (i.e., a containing width
  * equivalent to the current width), and the title is the full
  * content (or if there is no title, it will be set to the full text)
@@ -15,7 +15,7 @@ blist.widget.clippedText.clipElement = function ($elem)
         $elem.text($elem.attr('title'));
     }
 
-    var origHeight = $elem.height();
+    var origHeight = $elem.parent().height();
     var origDisplay = $elem.css('display');
     $elem.css('display', 'inline');
     if ($elem.height() > origHeight)

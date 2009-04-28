44efb8db5778092ce4ba1a40e9b78a31ee4ca0ed
Pete Stuart <pete.stuart@blist.com>
2009-04-27 17:09:16 -0700

    FEATURE: Hooked up filters sidebar in discovery.
    QA:
    * In both Top 100 tab or All blists tabs, clicking category filters the list to show only that category.
    * In both tabs, under "Show blists", "In my network", "Public only" and "Private only" should show only when a user is logged in, and clicking on them should filter the list to show the appropriate data set.
    * Known issues:
    	* In both tabs, under "Show blists", both "Most comments" and "Most favorited" will fail.
    	* When there are only a few or no items in the list, the white box does not extend to the bottom of the page.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

--- app/controllers/discovers_controller.rb
+++ app/controllers/discovers_controller.rb
@@ -1,5 +1,5 @@
 class DiscoversController < SwfController
-  skip_before_filter :require_user, :only => [:show]
+  skip_before_filter :require_user, :only => [:show, :filter]
 
   def show
     @body_class = 'discover'
@@ -8,11 +8,45 @@ class DiscoversController < SwfController
     opts = Hash.new
     opts['limit'] = 10
     @all_views = View.find(opts, true)
-
     @popular_views = View.find_popular()
+    
     @carousel_views = View.find_featured()
     @network_views = View.find_recent(5, true)
 
+  end
+
+  def filter
+    type = params[:type]
+    filter = params[:filter]
+    
+    opt_string = type == "POPULAR" ? "sortBy=POPULAR&" : ""
+    opt_string += filter.to_param
+    @filtered_views = View.find_filtered(opt_string)
+    
+    tab_title = type == "POPULAR" ? "Popular" : "All"
+    if (filter[:inNetwork])
+      tab_title += " #{t(:blists_name)} in my network"
+    elsif (filter[:publicOnly])
+      tab_title += " Public #{t(:blists_name)}"
+    elsif (filter[:privateOnly])
+      tab_title += " Private #{t(:blists_name)}"
+    elsif (filter[:category])
+      tab_title += " #{filter[:category]} #{t(:blists_name)}"
+    end
+    
+    respond_to do |format|
+      format.html { redirect_to(discover_url(params)) }
+      format.data { 
+        render(:partial => "discovers/view_list_tab", 
+          :locals => 
+          {
+            :tab_title => tab_title, 
+            :views => @filtered_views, 
+            :type => type
+          })
+      }
+    end
+    
   end
 
   def swf
@@ -24,5 +58,7 @@ class DiscoversController < SwfController
 
     render(:template => "discovers/show_swf")
   end
+  
+  
 
 end

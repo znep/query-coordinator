7dd97772f9b4657f8f2cee4b0c3f8f673dc84960
Pete Stuart <pete.stuart@blist.com>
2009-04-29 13:43:05 -0700

    NOOP: Limit fake community results to 10.
    Reviewed-by: Chris Metcalf <chris.metcalf@blist.com>

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -5,10 +5,10 @@ class CommunitiesController < SwfController
     @body_class = 'community'
     @show_search_form = false
     
-    @carousel_members = User.find();
-    @top_members = User.find();
-    @top_uploaders = User.find();
-    @all_members = User.find();
+    @carousel_members = User.find({ :limit => 10 });
+    @top_members = User.find({ :limit => 10 });
+    @top_uploaders = User.find({ :limit => 10 });
+    @all_members = User.find({ :limit => 10 });
   end
 
 end

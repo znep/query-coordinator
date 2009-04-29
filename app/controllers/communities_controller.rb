2a25e45895b635ed191949251aad6cd4bc801bce
Pete Stuart <pete.stuart@blist.com>
2009-04-29 10:30:24 -0700

    FEATURE: Added community page.
    QA: No real data yet. Styling only. Do not test.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -3,6 +3,12 @@ class CommunitiesController < SwfController
 
   def show
     @body_class = 'community'
+    @show_search_form = false
+    
+    @carousel_members = User.find();
+    @top_members = User.find();
+    @top_uploaders = User.find();
+    @all_members = User.find();
   end
 
 end
---
+++ public/javascripts/widgets/simple-tab-navigate.js
@@ -0,0 +1,67 @@
+(function($) {
+
+$.fn.simpleTabNavigate = function(options) {
+    // check if a navigator for this list was already created
+    var tabNavigator = $(this[0]).data("tabNavigator");
+    if (tabNavigator) {
+        return tabNavigator;
+    }
+    
+    tabNavigator = new $.simpleTabNavigator( options, this[0] );
+    $(this[0]).data("tabNavigator", tabNavigator);
+    
+    return tabNavigator;
+};
+
+$.simpleTabNavigator = function(options, list) {
+    this.settings = $.extend({}, $.simpleTabNavigator.defaults, options);
+    this.currentList = list;
+    this.init();
+};
+$.extend($.simpleTabNavigator, {
+    defaults: {
+        activationClass : "active",
+        tabSelector: "li",
+        tabMap: {
+            "tabPopular" : "#discoverTabPopular",
+            "tabAll" : "#discoverTabAll"
+        },
+        allPanelsSelector : ".tabContentContainer",
+        switchCompleteCallback: function(){}
+    },
+    prototype: {
+        init: function() {
+            var tabNavigator = this;
+            // Enumerate the tabs..
+            $(tabNavigator.currentList).find(tabNavigator.settings.tabSelector).each(function() {
+                var $li = $(this);
+                // Wire up the click event for the tab itself for activation
+                $li.find("a").click(function(event) {
+                    event.preventDefault();
+                    tabNavigator.activateTab($(this).closest(tabNavigator.settings.tabSelector));
+                });
+            });
+        },
+        activateTab: function(tab) {
+            var tabNavigator = this;
+            var $tab = $(tab);
+            var $tabLink = $tab.find("a");
+            var $panel = $(tabNavigator.settings.tabMap[$tab.attr("id")]);
+
+            $(tabNavigator.currentList).find(tabNavigator.settings.tabSelector).each(function() {
+                $(this).removeClass(tabNavigator.settings.activationClass);
+            });
+            $tab.addClass(tabNavigator.settings.activationClass);
+
+            $(tabNavigator.settings.allPanelsSelector).each(function() { 
+                $(this).removeClass(tabNavigator.settings.activationClass); 
+            });
+
+            $panel.addClass(tabNavigator.settings.activationClass);
+
+            tabNavigator.settings.switchCompleteCallback($tab);
+        }
+    }
+});
+
+})(jQuery);

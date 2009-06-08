a8e7f5afee26a9e6c47d784a37800ee1f98b12d3
Pete Stuart <pete.stuart@socrata.com>
2009-06-08 14:30:33 -0700

    NOOP: Checkpoint: Added new modal dialog layout and hooks in main layout and common javascript files. Added outline for invitations modal.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@blist.com>

---
+++ app/controllers/invitation_controller.rb
@@ -0,0 +1,17 @@
+class InvitationController < ApplicationController
+  
+  def invite
+    render(:layout => "modal_dialog")
+  end
+  
+  def create
+    
+  end
+  
+  def show
+  end
+  
+  def accept
+  end
+  
+end

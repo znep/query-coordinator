bdf242d693498148318190b53358a91200444b61
Paul Paradise <paul.paradise@socrata.com>
2009-07-14 15:28:14 -0700

    SPARTA: Filter out sensitive AWS credential info from Hoptoad
    This prevents us from sending things like our EC2 credentials to
    Hoptoad.
    
    Reviewed-by: Chris Metcalf <chris.metcalf@socrata.com>

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,10 @@
 HoptoadNotifier.configure do |config|
   config.api_key = '2aa9cf5b8e41f462f46fe1cfd07aed69'
+  config.environment_filters << "AWS_ACCESS_KEY_ID"
+  config.environment_filters << "AWS_ACCESS"
+  config.environment_filters << "AWS_ACCOUNT_NUMBER"
+  config.environment_filters << "AWS_SECRET_ACCESS_KEY"
+  config.environment_filters << "AWS_SECRET"
+  config.environment_filters << "EC2_CERT"
+  config.environment_filters << "EC2_PRIVATE_KEY"
 end

cf95ebcdf61340d3e9efe39e372b24dfed820b3a
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2009-07-27 13:53:48 -0700

    BUG 998847: Fix crash on reload when Flash grid is loaded * Don't create new datasetGrid() if readGrid is not present
    QA:
    * Make sure the bug is fixed
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/screens/blist-grid-screen.js
+++ public/javascripts/screens/blist-grid-screen.js
@@ -785,8 +785,9 @@ $(function ()
 
     window.onbeforeunload = function ()
     {
+        var $grid = $('#readGrid');
         if (!blist.widgets.visualization.isVisualization &&
-            $('#readGrid').datasetGrid().isTempView)
+            $grid.length > 0 && $grid.datasetGrid().isTempView)
         {
             return 'You will lose your temporary filter.';
         }

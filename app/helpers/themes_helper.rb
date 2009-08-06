d34f500e62907ea7692b81c69bddce7aeada49e6
Chris Metcalf <chris.metcalf@socrata.com>
2009-08-05 18:27:51 -0700

    WIP

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -12,6 +12,27 @@ module ThemesHelper
     def carousel_bg
         box :h => 235, :r => 5, :bc => 'ffffff', :fc => :carousel_bg, :s => 'h'
     end
+    
+    def expanded_list_bg
+      # TODO: This should eventually be a gradient
+      box :h => 1, :w => 150, :fc => :highlight_color
+    end
+
+    def bottom_left_dark_bg
+      box :w => 18, :h => 18, :r => 9, :rw => 9, :ry => 9, :bc => "ececec", :fc => :highlight_color
+    end
+
+    def summary_bottom_right_bg
+      box :w => 18, :h => 18, :fc => "cacaca", :bc => "ececec", :r => 9, :rx => 9, :rw => 9, :ry => 9, :ew => 1, :ec => :highlight_color
+    end
+
+    def background_info_tab_top_left_sprites
+      # TODO: We need the ability to set dim/bright sprites, and to orient them SW->NE
+    end
+
+    def background_info_tab_top_right_sprites
+      # TODO: We need the ability to set dim/bright sprites, and to orient them SW->NE      
+    end
 
     def box(options)
         ui_url :box, options

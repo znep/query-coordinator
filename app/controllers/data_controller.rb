8fab4a4552f9114e3fcd2fce0ca209e63e7bc35f
Chris Metcalf <chris.metcalf@socrata.com>
2009-08-20 22:02:40 -0700

    BUG: Attempting to fix a caching issue on the discover homepage
    QA: Make sure that the discover page shows proper data for the Socrata theme.
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -18,14 +18,25 @@ class DataController < ApplicationController
     agency_mcache_key = (@agency_id.nil? ? "" : "-agency-#{@agency_id}")
 
     unless @all_views_rendered = read_fragment("discover-tab-all#{agency_mcache_key}")
-      @all_views_total = View.find({ :limit => PAGE_SIZE, :agencyId => @agency_id, :count => true }, true).count
-      @all_views = View.find({ :limit => PAGE_SIZE, :agencyId => @agency_id }, true)
+      opts = {:limit => PAGE_SIZE}
+      if !@agency_id.nil?
+        opts.merge!({:agencyId => @agency_id})
+      end
+      
+      @all_views_total = View.find(opts.merge({:count => true }), true).count
+      @all_views = View.find(opts, true)
+      
       # TODO: Tags should also allow filtering by org
       @all_views_tags = Tag.find({ :method => "viewsTags", :limit => 5 })
     end
     unless @popular_views_rendered = read_fragment("discover-tab-popular#{agency_mcache_key}")
+      opts = {:top100 => true, :limit => PAGE_SIZE, :page => 1}
+      if !@agency_id.nil?
+        opts.merge!({:agencyId => @agency_id})
+      end
+      
       @popular_views_total = 100
-      @popular_views = View.find_filtered({ :top100 => true, :limit => PAGE_SIZE, :agencyId => @agency_id, :page => 1 })
+      @popular_views = View.find_filtered(opts)
       @popular_views_tags = Tag.find({ :method => "viewsTags", :top100 => true, :limit => 5 })
     end
     unless @carousel_views_rendered = read_fragment("discover-carousel")

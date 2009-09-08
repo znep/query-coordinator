55f4d5f0038223358b969aafac95119ddce18ad8
Paul Paradise <paul.paradise@socrata.com>
2009-09-08 16:33:33 -0700

    NOOP: Move RPX functionality into its own controller
    This commit just moves the "return URL" for RPX from being part of the
    UserSessionsController to being in it's own controller (RpxController).
    It'll be important later, when we want to add custom logic for
    login/signup via RPX.
    
    QA: Do not test yet.
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

---
+++ app/controllers/rpx_controller.rb
@@ -0,0 +1,25 @@
+class RpxController < ApplicationController
+  ssl_required :return_token
+  skip_before_filter :require_user
+  protect_from_forgery :except => [:return_token]
+
+
+  def return_token
+    return (redirect_to login_path) unless params[:token]
+
+    rpx_authentication = RpxAuthentication.new(params[:token])
+    if (rpx_authentication.existing_account?)
+      user_session = UserSession.rpx(rpx_authentication)
+      redirect_back_or_default(home_path)
+    else
+      flash[:rpx_user] = rpx_authentication.user
+      redirect_to signup_url
+    end
+  end
+
+  def signup
+  end
+
+  def login
+  end
+end

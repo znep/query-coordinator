8c221bd305e082bacc9d374d072c94c75a4becca
Clint Tseng <clint.tseng@socrata.com>
2009-10-12 14:48:51 -0700

    NOOP: Once again, Git failed to notice changes to an untracked file.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/util/urls.js
+++ public/javascripts/util/urls.js
@@ -1,23 +1,13 @@
 (function($) {
-    var stringToUrl = function(str)
-    {
-        str = str.replace(/\s+/ig, '-').replace(/[^a-zA-Z0-9_\-]/ig, '-').replace(/\-+/ig, '-');
-        if (str.length === 0)
-        {
-            return '-';
-        }
-        return str.slice(0, 50);
-    };
-
     $.generateProfileUrl = function(user)
     {
-        return "/profile/" + stringToUrl(user.displayName) + "/" + user.id;
+        return "/profile/" + $.urlSafe(user.displayName) + "/" + user.id;
     };
 
     $.generateViewUrl = function(view)
     {
-        return "/" + stringToUrl(view.category || "dataset") + 
-               "/" + stringToUrl(view.name) +
+        return "/" + $.urlSafe(view.category || "dataset") + 
+               "/" + $.urlSafe(view.name) +
                "/" + view.id
     };
 })(jQuery);

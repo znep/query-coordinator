278089bfdb0fe116326d3661dee458ee1a272cff
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2009-11-13 10:45:09 -0800

    BUG 1718023: Fix crash when editing empty picklist
    QA:
    * Make sure bug is fixed
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

--- public/javascripts/data/editors/picklist-editor.js
+++ public/javascripts/data/editors/picklist-editor.js
@@ -36,9 +36,12 @@
                 var editObj = this;
                 editObj._valuesList = [ { id: 'null', label: '(Blank)'} ];
 
-                $.each(editObj.column.options, function(id, v)
-                    { if (!v.deleted) { editObj._valuesList.push(
-                        {id: id, label: v.text, icon: v.icon}); } });
+                if (editObj.column.options !== undefined)
+                {
+                    $.each(editObj.column.options, function(id, v)
+                            { if (!v.deleted) { editObj._valuesList.push(
+                                {id: id, label: v.text, icon: v.icon}); } });
+                }
                 editObj.setFullSize();
                 editObj.$dom().addClass('blist-combo-wrapper')
                     .addClass('combo-container');

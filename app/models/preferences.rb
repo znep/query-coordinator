f8be4fd54b370c5bde085e44449d7f68b94cb5f9
Chris Metcalf <chris.metcalf@socrata.com>
2010-03-22 13:01:36 -0700

    NOOP: Removing the now-longer-used Preferences model, since its counterpart in the core server is now gone.
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- app/models/preferences.rb
+++
@@ -1,9 +0,0 @@
-class Preferences < Model
-  attr_accessor :data
-
-  # since this is always really a key-value store and not a
-  # 'real object' might as well add a convenience helper.
-  def [](key)
-    self.data[key]
-  end
-end

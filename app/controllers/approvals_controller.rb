e7520b6c44963904c197d72dce405f5e53210f80
Aiden Scandella <aiden.scandella@socrata.com>
2010-04-12 17:31:47 -0700

    BUG 893: Able to access approvals page without proper credentials.
    QA: Retest
    
    Reviewed-by: Chris Metcalf <chris.metcalf@socrata.com>

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,5 +1,6 @@
 class ApprovalsController < ApplicationController
   before_filter { |c| c.require_module! :publisher_comment_moderation }
+  before_filter :require_domain_member
 
   def show
   end

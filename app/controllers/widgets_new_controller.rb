4ba8b1fe5f144722d2462c0e9933a8ed68a48fbe
Clint Tseng <clint@dontexplain.com>
2010-06-19 13:56:14 -0700

    BUG: When falling back to another theme, also fallback the css.
    QA: Ignore.
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

--- app/controllers/widgets_new_controller.rb
+++ app/controllers/widgets_new_controller.rb
@@ -15,17 +15,21 @@ class WidgetsNewController < ApplicationController
 
     begin
       if params[:customization_id] == 'default'
+        @theme_id = 'default'
         @theme = WidgetCustomization.default_theme(1)
       else
+        @theme_id = params[:customization_id]
         @theme = WidgetCustomization.find(params[:customization_id]).customization
       end
     rescue CoreServer::ResourceNotFound
       begin
-        @theme = WidgetCustomization.find(CurrentDomain.default_widget_customization_id).customization
+        @theme_id = CurrentDomain.default_widget_customization_id
+        @theme = WidgetCustomization.find(@theme_id).customization
       rescue CoreServer::CoreServerError => e
         # unless people are mucking around with the db directly, this
         # should never happen (only if a domain has no default customization),
         # but let's be safe.
+        @theme_id = 'default'
         @theme = WidgetCustomization.default_theme(1)
       end
     end

a3ed88d71e1ee22171a96645083bd359493a271d
Michael Chui <michael.chui@socrata.com>
2010-06-23 15:14:43 -0700

    BUG 1648: Ensuring featureSet is queried for when not present; removing nulls from value set; using correct precision for integer values.
    QA: Retest bug 1648.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/widgets/maps/heatmap-map.js
+++ public/javascripts/widgets/maps/heatmap-map.js
@@ -31,13 +31,12 @@
                         new dojo.Color([50,50+(20*i),50,0.8])
                     );
                 }
+            }
 
-                doQueries(mapObj);
-            }
+            if (_.isUndefined(mapObj._featureSet))
+            { doQueries(mapObj); }
             else
-            {
-                addFeatureSetToMap(mapObj, null);
-            }
+            { addFeatureSetToMap(mapObj, null); }
         }
     });
 
@@ -95,9 +94,10 @@
             if (!$.isArray(e.value)) return parseFloat(e.value);
 
             var quantityPrecision = 0;
-            e.value = _.reduce(e.value, 0.0, function(m, v)
+            e.value = _.reduce(_.compact(e.value), 0.0, function(m, v)
                 {
-                    var precision = v.length-v.lastIndexOf('.')-1;
+                    var precision = v.indexOf('.') > -1
+                                    ? v.length-v.lastIndexOf('.')-1 : 0;
                     quantityPrecision = quantityPrecision > precision
                                     ? quantityPrecision : precision;
                     return m + parseFloat(v);

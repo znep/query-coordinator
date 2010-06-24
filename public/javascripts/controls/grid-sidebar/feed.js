7962264874672e10f6b471e59db2b1abbbb566d5
Clint Tseng <clint.tseng@socrata.com>
2010-06-23 17:46:12 -0700

    FEATURE/CHECKPOINT: v4-chrome: The feed should now render. It does not yet filter, and none of the actions on it are hooked up yet.
    QA: Make sure the feed looks visually okay. Nothing you click on in it will work yet though.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

---
+++ public/javascripts/controls/grid-sidebar/feed.js
@@ -0,0 +1,63 @@
+(function($)
+{
+    var $feed, feedData, comments, views;
+
+    var renderFeed = function()
+    {
+        $feed.removeClass('loading');
+
+        $feed.feedList({
+            comments: comments,
+            views: views
+        });
+    };
+
+    var pendingRequests = 3;
+    $.Tache.Get({
+        url: '/views/' + blist.display.view.id + '/comments.json',
+        dataType: 'json',
+        success: function(responseData)
+        {
+            comments = responseData;
+            if (--pendingRequests === 0)
+                renderFeed();
+        }
+    });
+
+    $.Tache.Get({ url: '/views.json',
+        data: { method: 'getByTableId', tableId: blist.display.view.tableId },
+        dataType: 'json', contentType: 'application/json',
+        success: function(responseData)
+        {
+            views = responseData;
+            if (--pendingRequests === 0)
+                renderFeed();
+        }
+    });
+
+    var config =
+    {
+        name: 'feed',
+        title: 'Feed',
+        subtitle: 'The feed shows you the discussion and activity around a dataset',
+        sections: [
+            {
+                customContent: {
+                    template: 'feedList',
+                    directive: {},
+                    data: {},
+                    callback: function($elem)
+                    {
+                        $elem.addClass('loading');
+                        $feed = $elem;
+                        if (--pendingRequests === 0)
+                            renderFeed();
+                    }
+                }
+            }
+        ]
+    };
+
+    $.gridSidebar.registerConfig(config);
+
+})(jQuery);

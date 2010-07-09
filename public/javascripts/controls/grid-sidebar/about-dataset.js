a1a734d72465e30d1a5178f7b5d5272870eb1503
Aiden Scandella <aiden.scandella@socrata.com>
2010-07-09 16:05:02 -0700

    FEATURE: Flag / message dataset owner on v4-chrome
    QA:
    * Under the about pane of new datasets page, on the bottom
      should be a new section, 'Actions' if you're not logged in
      as the dataset owner.
    * These should work the same as on the old datasets page,
      just a little prettier.
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/controls/grid-sidebar/about-dataset.js
+++ public/javascripts/controls/grid-sidebar/about-dataset.js
@@ -35,6 +35,130 @@
                             event.preventDefault();
                             $('.statsPopupModal').jqmShow();
                         });
+
+                        // Build the email subject for flag/message
+                        var contactPurposeChange = function(event)
+                        {
+                            var $select = $sect.find('#contactPurpose');
+
+                            var type = $select.val();
+                            var subject = '';
+
+                            if ($.isBlank(type)) { return; }
+
+                            if (type == 'other')
+                            {
+                                subject = 'A visitor has sent you a message about your "' +
+                                      blist.display.view.name + '" ' +
+                                      blist.configuration.strings.company + ' dataset';
+                            }
+                            else
+                            {
+                                subject = 'Your dataset "' + blist.display.view.name + '" has been flagged ';
+                                switch (type)
+                                {
+                                    case 'copyright_violation':
+                                        subject += 'for copyright violation';
+                                        break;
+                                    case 'offensive_content':
+                                        subject += 'for offensive content';
+                                        break;
+                                    case 'spam':
+                                        subject += 'as potential spam';
+                                        break;
+                                    case 'personal_information':
+                                        subject += 'for containing personal information';
+                                        break;
+                                }
+                            }
+                            $sect.find('#contactSubject').val(subject);
+                            $sect.find('#contactBody').focus();
+                        };
+
+                        // Swap out links for form and back, show 'required' hint
+                        var toggleContactActions = function()
+                        {
+                            $sect.find('.contactOwnerForm').slideToggle()
+                                .find('#contactBody')
+                                    .focus().end().end()
+                                .find('.contactOwnerLinks')
+                                    .slideToggle().end();
+                            $('#gridSidebar_about').find('.scrollContent > div.required').toggleClass('hide');
+                        };
+
+                        $.live('#gridSidebar_about .contactButton',
+                            'click', function(event)
+                        {
+                            var $this = $(this);
+
+                            // Grab the form from its template
+                            if ($sect.find('.contactOwnerForm').length === 0)
+                            {
+                                $this.closest('.formSection').after($.renderTemplate('aboutDataset_contact'));
+
+                                var $form = $sect.find('.contactOwnerForm');
+
+                                $form.validate({
+                                    rules: {
+                                        'type'   : 'required',
+                                        'subject': 'required',
+                                        'message': 'required',
+                                        'from_address': {'required': true, 'email': true}
+                                    },
+                                    messages: {
+                                        'type'   : 'You must select a purpose for this message.',
+                                        'subject': 'You must select a purpose for this message above.',
+                                        'message': 'The message must have a body.',
+                                        'from_address': 'Your email address is required.'
+                                    }
+                                });
+
+                                $sect.find('#contactPurpose')
+                                    .change(contactPurposeChange)
+                                    .uniform();
+
+                                $form.submit(function(event)
+                                {
+                                    event.preventDefault();
+
+                                    if ($form.valid())
+                                    {
+                                        $.ajax({
+                                            url: $form.attr('action'),
+                                            data: $form.serialize(),
+                                            error: function(request, textStatus, errorThrown) {
+                                                $sect.find('.flash')
+                                                  .removeClass('notice').addClass('error')
+                                                  .text('There was an error sending feedback for this dataset. Please retry later.').fadeIn();
+                                            },
+                                            success: function(response) {
+                                                _.defer(function() {
+                                                    $sect.find('.flash')
+                                                      .removeClass('error').addClass('notice')
+                                                      .text('The dataset owner has been notified.').fadeIn();
+                                                    toggleContactActions();
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+
+                                $('#gridSidebar_about .sendContactButton').click(function(event)
+                                {
+                                    event.preventDefault();
+                                    $form.submit();
+                                });
+                            }
+
+                            toggleContactActions();
+
+                            // Pre-populate message subject
+                            if (!_.isUndefined($this.attr('data-select')))
+                            {
+                               $('#contactPurpose').val($this.attr('data-select'));
+                               contactPurposeChange();
+                            }
+                        });
                     }
                 }
             }

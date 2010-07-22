d84c89d51ac558b5b5c155e9bd4943c741b2f450
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-07-22 10:38:11 -0700

    FEATURE: Add API pane to Export sidebar * Refactored widget API content to share with sidebar
    QA:
    * Make sure there is an API pane under the Export tab
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

---
+++ public/javascripts/controls/grid-sidebar/api.js
@@ -0,0 +1,24 @@
+(function($)
+{
+    var config = {
+        name: 'export.api',
+        priority: 10,
+        title: 'API',
+        subtitle: 'Access this ' + blist.dataset.getTypeName(blist.display.view) +
+            ' via SODA',
+        sections: [
+            {
+                customContent: {
+                    template: 'apiContentWrapper',
+                    directive: {
+                        'p@class+': 'pClass'
+                    },
+                    data: { pClass: 'sectionContent' }
+                }
+            }
+        ]
+    };
+
+    $.gridSidebar.registerConfig(config);
+
+})(jQuery);

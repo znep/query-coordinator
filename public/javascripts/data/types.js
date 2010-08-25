40be53ea622b2bea9b1333be6e194122c51dca82
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-08-25 16:10:55 -0700

    BUG 2125: Give phones column totals
    QA:
    * Make sure bug is fixed
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

--- public/javascripts/data/types.js
+++ public/javascripts/data/types.js
@@ -1030,6 +1030,7 @@ blist.namespace.fetch('blist.data.types');
             filterRender: renderFilterPhone,
             filterText: true,
             sortable: true,
+            aggregates: nonNumericAggs,
             alignment: alignment,
             convertableTypes: ['text'],
             filterable: true,

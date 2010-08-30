724b00e9cb1a046513c1c66d81b261653709a2dc
Clint Tseng <clint.tseng@socrata.com>
2010-08-30 10:55:36 -0700

    NOOP: Removing shoddy version check in preinit for Bundler 1.0.0.
    Reviewed-by: Aiden Scandella <aiden.scandella@socrata.com>

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -4,12 +4,5 @@ rescue LoadError
   # Fallback on doing the resolve at runtime
   require "rubygems"
   require "bundler"
-  major, minor, rev = Bundler::VERSION.split(".").map { |v| v.to_i }
-  if minor < 9 || rev <= 5 
-    raise RuntimeError, "Bundler incompatible.\n" +
-      "Your bundler version is incompatible with Rails 2.3 and an unlocked bundle.\n" +
-      "Run `gem install bundler` to upgrade or `bundle lock` to lock."
-  else
-    Bundler.setup
-  end
+  Bundler.setup
 end

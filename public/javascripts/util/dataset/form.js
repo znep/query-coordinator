54742d4682836d2e73c8ad72304983bb0adfdb59
Clint Tseng <clint@dontexplain.com>
2010-09-08 21:27:56 -0700

    BUG: summary view throwing exception for form module in dataset model.
    QA: Should resolve v4 moderation issues for demo user on demo.test
    
    To-be-
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/util/dataset/form.js
+++ public/javascripts/util/dataset/form.js
@@ -4,6 +4,9 @@ Dataset.modules['form'] =
 {
     _checkValidity: function()
     {
+        // summary view gives us no columns; validity is irrelevant in that case anyway.
+        this.visibleColumns = this.visibleColumns || [];
+
         return _.any(this.visibleColumns, function(c)
             { return !_.include(['tag', 'nested_table'], c.dataTypeName); });
     }

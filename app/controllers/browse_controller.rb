2ec514a51906971eed8f8080e8c82af23245d150
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-09-16 13:43:11 -0700

    NOOP: Sort working for browse * Show sort options in select * Show/hide time slices for appropriate sort options * Switch to using search service instead of view finding * Initial Javascript for handling sort boxes
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -4,8 +4,17 @@ module BrowseController
 
 protected
   def process_browse!
-    opts = {:limit => 10, :page => 1}
-    @views = View.find(opts, true)
+    @opts = {:limit => 10, :page => 1}
+
+    if !params[:sort].nil?
+      @opts[:sortBy] = params[:sort]
+    end
+
+    # Terrible hack; but search service needs _something_ non-null; so we'll
+    # search for everything!
+    @opts[:q] = "''"
+
+    @view_results = SearchResult.search('views', @opts)[0]
   end
 
 end
---
+++ public/javascripts/screens/browse.js
@@ -0,0 +1,51 @@
+$(function()
+{
+    var opts = {};
+    if (!$.isBlank(window.location.search))
+    {
+        _.each(window.location.search.slice(1).split('&'), function(p)
+        {
+            var s = p.split('=');
+            opts[s[0]] = s[1];
+        });
+    }
+
+    var $browse = $('.browseSection');
+    $browse.find('select').uniform();
+    $browse.find('select.hide').each(function()
+    {
+        var $t = $(this);
+        $t.removeClass('hide');
+        $t.closest('.uniform').addClass('hide');
+    });
+
+    var $sortType = $browse.find('select.sortType');
+    var $sortPeriod = $browse.find('select.sortPeriod');
+    var showHideSortPeriod = function()
+    {
+        _.defer(function()
+        {
+            $sortPeriod.closest('.uniform').toggleClass('hide',
+                !$sortType.find('option:selected').hasClass('timePeriod'));
+        });
+    };
+    $sortType.change(showHideSortPeriod)
+        .keypress(showHideSortPeriod).click(showHideSortPeriod);
+
+    var doSort = function()
+    {
+        _.defer(function()
+        {
+            opts.sort = $sortType.val();
+            if ($sortPeriod.is(':visible'))
+            { opts.sortPeriod = $sortPeriod.val(); }
+            else
+            { delete opts.sortPeriod; }
+            // Reset page
+            delete opts.page;
+            window.location = window.location.pathname + '?' +
+                _.map(opts, function(v, k) { return k + '=' + v; }).join('&');
+        });
+    };
+    $sortType.add($sortPeriod).change(doSort);
+});

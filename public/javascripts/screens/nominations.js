27beede022ba55cb6f33dd369e67baeee5d7ee8b
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-09-24 13:54:58 -0700

    NOOP: Switch to rendering nominations in JS * This allows us to immediately render a just-submitted item
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/screens/nominations.js
+++ public/javascripts/screens/nominations.js
@@ -1,5 +1,63 @@
 $(function()
 {
+    var friendlyStatus = function(nom)
+    {
+        if (nom.status == 'pending' &&
+            (new Date(nom.createdAt * 1000) > Date.parse('7 days ago')))
+        { return 'new'; }
+        else if (nom.status == 'pending')
+        { return 'open'; }
+        return nom.status;
+    };
+
+    var $tbody = $('.nominationsList .gridList tbody');
+    var addNomination = function(n, beginning)
+    {
+        if (!(n.user instanceof User)) { n.user = new User(n.user); }
+        var $newItem = $.renderTemplate('nominationItem', n,
+        {
+            '.user a@href': function(n) { return n.context.user.getProfileUrl(); },
+            '.user a@title': 'user.displayName!',
+            '.user img@src': function(n)
+                { return n.context.user.getProfileImageUrl('small'); },
+            '.user img@alt': 'user.displayName!',
+            '.user .userName': 'user.displayName!',
+            '.details .title': 'title!',
+            '.details .submitTime .fullTime': function(n)
+                { return new Date(n.context.createdAt * 1000).format('F d, Y'); },
+            '.details .submitTime .relativeTime': function(n)
+                { return blist.util.humaneDate
+                    .getFromDate(n.context.createdAt * 1000); },
+            '.details .description': 'description!',
+            '.details .description@class+': function(n)
+                { return $.isBlank(n.context.description) ? 'hide' : ''; },
+            '.attachments li':
+            {
+                'a<-attachments':
+                {
+                    'a': 'a.filename!',
+                    'a@title': 'a.filename!',
+                    'a@href': '/api/nominations/#{id}/attachments/#{a.id}'
+                }
+            },
+            '.rating': 'netVotes',
+            '.rating@class+': function(n)
+                {
+                    return n.context.netVotes > 0 ? 'positive' :
+                        n.context.netVotes < 0 ? 'negative' : '';
+                },
+            '.status': function(n)
+                { return friendlyStatus(n.context).capitalize(); },
+            '.status@class+': function(n)
+                { return friendlyStatus(n.context).toLowerCase(); }
+        }).find('tr.item')
+
+        if (beginning) { $tbody.prepend($newItem); }
+        else { $tbody.append($newItem); }
+    };
+
+    _.each(blist.nominations.items, function(n) { addNomination(n); });
+
     var $dialog = $('.nominateDialog');
     $('.nominateLink').click(function(e)
     {
@@ -33,6 +91,7 @@ $(function()
             {
                 $dialog.find('.loadingSpinner, .loadingOverlay').addClass('hide');
                 $dialog.jqmHide();
+                addNomination(resp, true);
             }});
     };
 

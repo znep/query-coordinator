08a6cfa61e6b02c82977a8e6ea37db08041eaad9
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-09-27 16:24:39 -0700

    NOOP: Allow non-Cassandra devs to load profile
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -55,11 +55,13 @@ class ProfileController < ApplicationController
       @body_class = 'community'
     end
 
+    @stat_displays = []
     # TODO/v4: Remove the module check, this is just for backwards compat
     # Also, we can probably make these _views vars local, not @ accessible
     unless !CurrentDomain.module_available?(:new_datasets_page) || \
         @view_summary_cached = read_fragment( \
-          app_helper.cache_key('profile-view-summary', @current_state))
+          app_helper.cache_key('profile-view-summary', @current_state)) || \
+          params[:_oldViews] == 'true'
 
       base_req = {:limit => 1, :for_user => @user.id}
       stats = [
@@ -71,7 +73,6 @@ class ProfileController < ApplicationController
         {:params => {:limitTo => 'calendars'}, :name => 'Calendars'},
         {:params => {:limitTo => 'forms'}, :name => 'Forms'}
       ]
-      @stat_displays = []
       CoreServer::Base.connection.batch_request do
         stats.each do |s|
           SearchResult.search('views', base_req.merge(s[:params]), true)

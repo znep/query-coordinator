d03ff4eab1c2a7ef7ba814e429de27012acfdf83
Clint Tseng <clint.tseng@socrata.com>
2010-09-30 18:52:34 -0700

    NOOP: Unhide sidebar on home; hide create dataset button.
    QA: Self-explanatory.
    
    Reviewed-by: May Peria <may.peria@socrata.com>

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,8 +5,6 @@ class HomepageController < ApplicationController
   def show
     # move to /browse on interaction
     @base_url = browse_path
-    # hide some of the chrome on this page
-    @facets = {}
 
     @no_results_text = 'No Datasets Yet'
     process_browse!

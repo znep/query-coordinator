8c78e20d7e3e5728c20f89a8e407b6e4d361d6b2
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-10-08 14:41:24 -0700

    NOOP: Add pagination to fat row * Make sass shared between page & fat row render type
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/controls/fatrow-render-type.js
+++ public/javascripts/controls/fatrow-render-type.js
@@ -22,15 +22,28 @@
     {
         defaults:
         {
+            pageSize: 20,
+            view: null
         },
 
         prototype:
         {
             init: function ()
             {
-                var currentObj = this;
-                var $domObj = currentObj.$dom();
-                $domObj.data("fatrowRenderType", currentObj);
+                var frObj = this;
+                var $domObj = frObj.$dom();
+                $domObj.data("fatrowRenderType", frObj);
+
+                frObj._curPageIndex = 0;
+                frObj._totalPages = null;
+                hookUpNavigation(frObj);
+                frObj.settings.view.bind('row_count_change', function()
+                    { updateNavigation(frObj); })
+                    .bind('query_change', function()
+                    {
+                        frObj._curPageIndex = 0;
+                        renderCurrentPage(frObj);
+                    });
             },
 
             $dom: function()
@@ -39,7 +52,105 @@
                 { this._$dom = $(this.currentDom); }
                 return this._$dom;
             },
+
+            $list: function()
+            {
+                if (!this._$list)
+                { this._$list = this.$dom().find('.rowList'); }
+                return this._$list;
+            },
+
+            $nav: function()
+            {
+                if (!this._$nav)
+                { this._$nav = this.$dom().find('.navigation'); }
+                return this._$nav;
+            },
+
+            displayPage: function(index)
+            {
+                var frObj = this;
+
+                var pageCount = Math.ceil(frObj.settings.view.totalRows /
+                        frObj.settings.pageSize);
+                if (index < 0)
+                { index = pageCount + index + 1; }
+                if (index >= pageCount)
+                { index = pageCount - 1; }
+
+                frObj._curPageIndex = index;
+                renderCurrentPage(frObj);
+                updateNavigation(frObj);
+            }
         }
     });
+
+    var renderCurrentPage = function(frObj)
+    {
+        $.debug('rendering page ' + frObj._curPageIndex);
+    };
+
+    var hookUpNavigation = function(frObj)
+    {
+        frObj.$nav().find('.button').click(function(e)
+        {
+            e.preventDefault();
+            var $a = $(this);
+            if ($a.hasClass('disabled')) { return; }
+
+            switch ($.hashHref($a.attr('href')))
+            {
+                case 'start':
+                    frObj.displayPage(0);
+                    break;
+                case 'end':
+                    frObj.displayPage(-1);
+                    break;
+                case 'previous':
+                    frObj.displayPage(frObj._curPageIndex - 1);
+                    break;
+                case 'next':
+                    frObj.displayPage(frObj._curPageIndex + 1);
+                    break;
+            }
+        });
+
+        frObj.$nav().find('.page a').click(function(e)
+        {
+            e.preventDefault();
+            var $a = $(this);
+            if ($a.parent().hasClass('active')) { return; }
+            frObj.displayPage($a.data('pageNum'));
+        });
+    };
+
+    var updateNavigation = function(frObj)
+    {
+        var pageCount = Math.ceil(frObj.settings.view.totalRows /
+            frObj.settings.pageSize);
+
+        frObj.$nav().find('.start, .previous')
+            .toggleClass('disabled', frObj._curPageIndex <= 0);
+        frObj.$nav().find('.end, .next')
+            .toggleClass('disabled', frObj._curPageIndex >= pageCount - 1);
+
+        if (pageCount != frObj._totalPages)
+        {
+            var $templLink = frObj.$nav().find('.page:first');
+            frObj.$nav().find('.page:not(:first)').remove();
+            var $newSet = $();
+            for (var i = 0; i < pageCount; i++)
+            {
+                var $newL = $templLink.clone(true).removeClass('active');
+                $newL.find('a').text(i + 1).data('pageNum', i);
+                $newSet = $newSet.add($newL);
+            }
+            $templLink.replaceWith($newSet);
+            frObj._totalPages = pageCount;
+        }
+
+        frObj.$nav().find('.page').removeClass('active');
+        frObj.$nav().find('.page').eq(frObj._curPageIndex).addClass('active');
+    };
 
 })(jQuery);

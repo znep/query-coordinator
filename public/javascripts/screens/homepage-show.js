d6a94b74c349e0b76e013a49ac46dce615db59ae
Clint Tseng <clint.tseng@socrata.com>
2010-10-25 18:56:28 -0700

    FEATURE: Awesome animations on the homepage stories. (more to come later perhaps)
    QA: Make sure the animation renders alright in all
    the browsers when you change pages on the stories.
    
    To-be-
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/screens/homepage-show.js
+++ public/javascripts/screens/homepage-show.js
@@ -15,6 +15,8 @@
             $this.hide();
         });
 
+        var $storiesContainer = $('.storiesContainer');
+
         // set up the pager as appropriate
         var $storyTexts = $('.storyTexts').children();
         var $storyImages = $('.storyImages').children();
@@ -28,6 +30,14 @@
         var $storyPagers = $('.storyPager a');
         $storyPagers.filter(':first-child').addClass('selected');
 
+        // generate slices for the animation
+        _(30).times(function() { $storiesContainer.append('<div class="slice"></div>'); });
+        var $slices = $storiesContainer.children('.slice');
+        $slices.css('z-index', 1)
+               .css('position', 'absolute')
+               .css('background-repeat', 'no-repeat')
+               .hide();
+
         $storyPagers.click(function(event)
         {
             event.preventDefault();
@@ -37,17 +47,62 @@
 
             var idxToActivate = $this.prevAll().length + 1;
 
+            // animate text
             $storyTexts.filter(':visible').fadeOut();
             $storyTexts.filter(':nth-child(' + idxToActivate + ')').fadeIn();
 
+            // animate images
+            var storiesContainerWidth = $storiesContainer.width();
             var $lastImage = $storyImages.filter(':visible');
-            $storyImages.filter(':nth-child(' + idxToActivate + ')')
-                .css('z-index', 1)
-                .fadeIn('slow', function()
+            var $nextImage = $storyImages.filter(':nth-child(' + idxToActivate + ')');
+
+            // measure next image's width since we'll need it
+            $nextImage.show();
+            var nextImageWidth = $nextImage.children('img').width();
+            $nextImage.hide();
+
+            // set up slices
+            $slices.css('background-color', $nextImage.css('background-color'))
+                   .css('background-image', 'url(' + $nextImage.children('img').attr('src') + ')')
+                   .css('opacity', 0);
+            $slices.width(storiesContainerWidth / 30);
+            $slices.height($storiesContainer.height());
+            $slices.each(function(i)
+            {
+                var $slice = $(this);
+
+                var targetBgOffset,
+                    targetPosition = (storiesContainerWidth / 30) * i;
+                if ($storiesContainer.attr('data-orientation') == 'right')
+                { targetBgOffset = targetPosition; }
+                else
+                { targetBgOffset = storiesContainerWidth - nextImageWidth - targetPosition; }
+
+                $slice.css('left', targetPosition)
+                      .css('background-position', targetBgOffset + 'px 0');
+
+                $slice.css('top', 100 * ((i % 2 === 0) ? -1 : 1))
+                      .hide();
+
+                setTimeout(function()
                 {
-                    $lastImage.hide();
-                    $(this).css('z-index', '');
-                });
+                    $slice
+                        .show()
+                        .animate({ top: 0 }, 400)
+                        .animate({ opacity: 1 }, {
+                            duration: 700,
+                            complete: function()
+                            {
+                                if (i == ($slices.length - 1))
+                                {
+                                    $slices.hide();
+                                    $nextImage.show();
+                                    $lastImage.hide();
+                                }
+                            },
+                            queue: false });
+                }, i * 40);
+            });
 
             $storyPagers.removeClass('selected');
             $this.addClass('selected');

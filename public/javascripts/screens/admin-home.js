05ac2d943e2de250087a2a5b24c276581656427d
Clint Tseng <clint.tseng@socrata.com>
2010-11-11 13:24:14 -0800

    FEATURE: Sorting the table of stories.
    QA: The title and date should sort.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/screens/admin-home.js
+++ public/javascripts/screens/admin-home.js
@@ -11,6 +11,18 @@ $(function()
             $row.slideUp().remove();
         }
     });
+
+    $('.storiesList.gridList').combinationList({
+        headerContainerSelector: '.gridListWrapper',
+        initialSort: [[2, 1]],
+        scrollableBody: false,
+        selectable: false,
+        sortGrouping: false,
+        sortHeaders: {1: {sorter: 'text'}, 2: {sorter: 'numeric'}},
+        sortTextExtraction: function(node) {
+            return $(node).find('.cellInner').text();
+        }
+    });
 });
 
 

df7b0199c27a24cd27144b9582f3d34a253d4e9e
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2010-11-11 17:34:32 -0800

    NOOP: Just a big rearrangement of code (rich display editor)
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/screens/dataset-edit-rr.js
+++ public/javascripts/screens/dataset-edit-rr.js
@@ -1,7 +1,8 @@
 var editRRNS = blist.namespace.fetch('blist.editRR');
 
-(function($) {
-    // Resizing
+// ------------ Resizing -------------
+editRRNS.initResizing = function()
+{
     var adjustSizes = function()
     {
         // match page height
@@ -17,108 +18,12 @@ var editRRNS = blist.namespace.fetch('blist.editRR');
     };
     adjustSizes();
     $(window).resize(adjustSizes);
-
-    editRRNS.$container = $('#layoutContainer');
-    editRRNS.$renderArea = editRRNS.$container.find('.renderArea');
+};
 
 
-    editRRNS.$dropIndicator = $.tag({tagName: 'span', 'class': 'dropIndicator'});
-
-    // Handle placing an item during dragging
-    var itemDragging = function(event, ui)
-    {
-        if ($.isBlank(editRRNS.$dropItem)) { return; }
-
-        var contOffset = editRRNS.$dropItem.offset();
-        var curX = ui.offset.left;
-        var curY = ui.offset.top;
-
-        editRRNS.$dropBeforeItem = null;
-        var $fields = editRRNS.$dropItem.children('.fieldItem').each(function()
-        {
-            var $t = $(this);
-            if (curX < $t.offset().left + $t.width() / 2)
-            {
-                editRRNS.$dropBeforeItem = $t;
-                return false;
-            }
-        });
-
-        editRRNS.$dropItem.append(editRRNS.$dropIndicator);
-
-        if (!$.isBlank(editRRNS.$dropBeforeItem))
-        {
-            editRRNS.$dropIndicator.css('left',
-                editRRNS.$dropBeforeItem.offset().left - contOffset.left - 1);
-        }
-        else
-        {
-            if ($fields.length < 1) { editRRNS.$dropIndicator.css('left', 0); }
-            else
-            {
-                var $lastField = $fields.eq($fields.length - 1);
-                editRRNS.$dropIndicator.css('left',
-                    $lastField.offset().left - contOffset.left +
-                        $lastField.outerWidth(true) - 1);
-            }
-        }
-    };
-
-
-    // Hook up actions for an item in the layout area
-    var enableFieldItem = function($item)
-    {
-        $item.draggable({
-            appendTo: $('.mainContainer'),
-            containment: $('.mainContainer'),
-            helper: 'clone',
-            opacity: 0.8,
-            revert: 'invalid',
-            start: function() { $(this).addClass('itemDragging'); },
-            stop: function() { $(this).removeClass('itemDragging'); },
-            drag: itemDragging
-        });
-    };
-
-
-    // Hook up drop acceptance
-    var makeDroppable = function($row)
-    {
-        $row.droppable({accept: '.fieldItem',
-            activeClass: 'inDrag',
-            hoverClass: 'dragOver',
-            over: function() { editRRNS.$dropItem = $(this); },
-            out: function()
-            {
-                editRRNS.$dropItem = null;
-                editRRNS.$dropIndicator.remove();
-            },
-            drop: function(event, ui)
-            {
-                editRRNS.$dropItem = null;
-                editRRNS.$dropIndicator.remove();
-
-                var $cont = $(this);
-                var $item = ui.draggable;
-                if ($item.closest('.renderArea').length < 1)
-                { $item = $item.clone().removeClass('ui-draggable itemDragging'); }
-
-                if ($.isBlank(editRRNS.$dropBeforeItem))
-                { $cont.append($item); }
-                else
-                {
-                    editRRNS.$dropBeforeItem.before($item);
-                    editRRNS.$dropBeforeItem = null;
-                }
-
-                enableFieldItem($item);
-                renderCurrentRow();
-                updateConfig();
-            }});
-    };
-
-
-    // Sidebar
+// --------------- Sidebar ---------------
+editRRNS.initSidebar = function()
+{
     var sortFunc = function(c)
     {
         // Sort all the visible columns first, so start the sort string
@@ -158,7 +63,7 @@ var editRRNS = blist.namespace.fetch('blist.editRR');
                 callback: function($sect)
                 {
                     $sect.find('.fieldItem').each(function()
-                        { enableFieldItem($(this)); });
+                        { editRRNS.enableFieldItem($(this)); });
                 }
             }
         }]
@@ -172,113 +77,237 @@ var editRRNS = blist.namespace.fetch('blist.editRR');
     });
 
     editRRNS.sidebar.show('palette');
+};
 
 
-    // Do row load and render
-    var renderCurrentRow = function()
+
+// ---------- Drag / Drop -------------
+
+// Handle placing an item during dragging
+editRRNS.itemDragging = function(event, ui)
+{
+    if ($.isBlank(editRRNS.$dropItem)) { return; }
+
+    var contOffset = editRRNS.$dropItem.offset();
+    var curX = ui.offset.left;
+    var curY = ui.offset.top;
+
+    editRRNS.$dropBeforeItem = null;
+    var $fields = editRRNS.$dropItem.children('.fieldItem').each(function()
     {
-        blist.dataset.getRows(editRRNS.navigation.currentPage(), 1, function(rows)
+        var $t = $(this);
+        if (curX < $t.offset().left + $t.width() / 2)
         {
-            if (rows.length == 1)
-            { editRRNS.richRenderer.renderRow(editRRNS.$renderArea, rows[0]); }
-        });
-    };
+            editRRNS.$dropBeforeItem = $t;
+            return false;
+        }
+    });
 
+    editRRNS.$dropItem.append(editRRNS.$dropIndicator);
 
-    // Pull and update the new config
-    var updateConfig = function()
+    if (!$.isBlank(editRRNS.$dropBeforeItem))
     {
-        var conf = {columns: []};
-        var processColumn = function($col, parConf)
+        editRRNS.$dropIndicator.css('left',
+            editRRNS.$dropBeforeItem.offset().left - contOffset.left - 1);
+    }
+    else
+    {
+        if ($fields.length < 1) { editRRNS.$dropIndicator.css('left', 0); }
+        else
         {
-            var c = {rows: []};
-            $col.children('.richLine').each(function()
+            var $lastField = $fields.eq($fields.length - 1);
+            editRRNS.$dropIndicator.css('left',
+                $lastField.offset().left - contOffset.left +
+                    $lastField.outerWidth(true) - 1);
+        }
+    }
+};
+
+
+// Hook up actions for an item in the layout area
+editRRNS.enableFieldItem = function($item)
+{
+    $item.draggable({
+        appendTo: $('.mainContainer'),
+        containment: $('.mainContainer'),
+        helper: 'clone',
+        opacity: 0.8,
+        revert: 'invalid',
+        start: function() { $(this).addClass('itemDragging'); },
+        stop: function() { $(this).removeClass('itemDragging'); },
+        drag: editRRNS.itemDragging
+    });
+};
+
+
+// Hook up drop acceptance
+editRRNS.makeDroppable = function($row)
+{
+    $row.droppable({accept: '.fieldItem',
+        activeClass: 'inDrag',
+        hoverClass: 'dragOver',
+        over: function() { editRRNS.$dropItem = $(this); },
+        out: function()
+        {
+            editRRNS.$dropItem = null;
+            editRRNS.$dropIndicator.remove();
+        },
+        drop: function(event, ui)
+        {
+            editRRNS.$dropItem = null;
+            editRRNS.$dropIndicator.remove();
+
+            var $cont = $(this);
+            var $item = ui.draggable;
+            if ($item.closest('.renderArea').length < 1)
+            { $item = $item.clone().removeClass('ui-draggable itemDragging'); }
+
+            if ($.isBlank(editRRNS.$dropBeforeItem))
+            { $cont.append($item); }
+            else
             {
-                var r = {};
-                var $r = $(this);
-                if ($r.children('.richColumn').length > 0)
+                editRRNS.$dropBeforeItem.before($item);
+                editRRNS.$dropBeforeItem = null;
+            }
+
+            editRRNS.enableFieldItem($item);
+            editRRNS.renderCurrentRow();
+            editRRNS.updateConfig();
+        }});
+};
+
+
+// ------------- Layout -----------------
+
+// Do row load and render
+editRRNS.renderCurrentRow = function()
+{
+    blist.dataset.getRows(editRRNS.navigation.currentPage(), 1, function(rows)
+    {
+        if (rows.length == 1)
+        { editRRNS.richRenderer.renderRow(editRRNS.$renderArea, rows[0]); }
+    });
+};
+
+// Pull and update the new config
+editRRNS.updateConfig = function()
+{
+    var conf = {columns: []};
+    var processColumn;
+    processColumn = function($col, parConf)
+    {
+        var c = {rows: []};
+        $col.children('.richLine').each(function()
+        {
+            var r = {};
+            var $r = $(this);
+            if ($r.children('.richColumn').length > 0)
+            {
+                r.columns = [];
+                $r.children('.richColumn').each(function()
+                { processColumn($(this), r); });
+            }
+            else
+            {
+                r.fields = [];
+                $r.children('.fieldItem').each(function()
                 {
-                    r.columns = [];
-                    $r.children('.richColumn').each(function()
-                    { processColumn($(this), r); });
-                }
-                else
-                {
-                    r.fields = [];
-                    $r.children('.fieldItem').each(function()
+                    var $f = $(this);
+                    var f = {};
+                    if ($f.hasClass('columnData'))
                     {
-                        var $f = $(this);
-                        var f = {};
-                        if ($f.hasClass('columnData'))
-                        {
-                            f.type = 'columnData';
-                            f.tableColumnId = $f.data('tcId');
-                        }
-                        else if ($f.hasClass('columnLabel'))
-                        {
-                            f.type = 'columnLabel';
-                            f.tableColumnId = $f.data('tcId');
-                        }
-                        else if ($f.hasClass('staticLabel'))
-                        {
-                            f.type = 'label';
-                            if (!$f.hasClass('defaultData'))
-                            { f.text = $f.text(); }
-                        }
-                        r.fields.push(f);
-                    });
-                }
-                c.rows.push(r);
-            });
-            parConf.columns.push(c);
-        };
-
-        editRRNS.$renderArea.children('.richColumn').each(function()
-        { processColumn($(this), conf); });
-
-        var md = $.extend(true, {richRendererConfigs: {}}, blist.dataset.metadata);
-        md.richRendererConfigs[editRRNS.renderType] = conf;
-        blist.dataset.update({metadata: md});
+                        f.type = 'columnData';
+                        f.tableColumnId = $f.data('tcId');
+                    }
+                    else if ($f.hasClass('columnLabel'))
+                    {
+                        f.type = 'columnLabel';
+                        f.tableColumnId = $f.data('tcId');
+                    }
+                    else if ($f.hasClass('staticLabel'))
+                    {
+                        f.type = 'label';
+                        if (!$f.hasClass('defaultData'))
+                        { f.text = $f.text(); }
+                    }
+                    r.fields.push(f);
+                });
+            }
+            c.rows.push(r);
+        });
+        parConf.columns.push(c);
     };
 
+    editRRNS.$renderArea.children('.richColumn').each(function()
+    { processColumn($(this), conf); });
 
+    var md = $.extend(true, {richRendererConfigs: {}}, blist.dataset.metadata);
+    md.richRendererConfigs[editRRNS.renderType] = conf;
+    blist.dataset.update({metadata: md});
+};
+
+editRRNS.renderCurrentLayout = function()
+{
+    editRRNS.richRenderer.renderLayout();
+    editRRNS.$renderArea.find('.richItem, .richLabel').addClass('fieldItem');
+    editRRNS.$renderArea.find('.staticLabel:empty').addClass('defaultData')
+        .text('(Static text)');
+    editRRNS.$renderArea.find('.fieldItem').each(function()
+        { editRRNS.enableFieldItem($(this)); });
+    editRRNS.$renderArea.find('.richLine').each(function()
+        { editRRNS.makeDroppable($(this)); });
+};
+
+editRRNS.resetConfig = function()
+{
+    var config = ((blist.dataset.metadata || {}).richRendererConfigs ||
+        {})[editRRNS.renderType] || {columns: [{rows: [{}]}]};
+    editRRNS.richRenderer.setConfig(config);
+    editRRNS.renderCurrentLayout();
+};
+
+editRRNS.initLayout = function()
+{
     // Hook up rendering
     editRRNS.renderType = 'fatRow';
-
-    var renderCurrentLayout = function()
-    {
-        editRRNS.richRenderer.renderLayout();
-        editRRNS.$renderArea.find('.richItem, .richLabel').addClass('fieldItem');
-        editRRNS.$renderArea.find('.staticLabel:empty').addClass('defaultData')
-            .text('(Static text)');
-        editRRNS.$renderArea.find('.fieldItem').each(function()
-            { enableFieldItem($(this)); });
-        editRRNS.$renderArea.find('.richLine').each(function()
-            { makeDroppable($(this)); });
-    };
 
     editRRNS.richRenderer = editRRNS.$renderArea.richRenderer({
         defaultItem: '(Data for #{column.name})',
         view: blist.dataset });
 
-    var resetConfig = function()
-    {
-        var config = ((blist.dataset.metadata || {}).richRendererConfigs ||
-            {})[editRRNS.renderType] || {columns: [{rows: [{}]}]};
-        editRRNS.richRenderer.setConfig(config);
-        renderCurrentLayout();
-    };
-    resetConfig();
+    editRRNS.resetConfig();
 
 
     // Hook up navigation
     editRRNS.navigation = editRRNS.$container.find('.navigation')
-        .bind('page_changed', renderCurrentRow)
+        .bind('page_changed', editRRNS.renderCurrentRow)
         .navigation({pageSize: 1, view: blist.dataset});
-    renderCurrentRow();
+    editRRNS.renderCurrentRow();
+
+    // Handle switching types
+    $('#renderTypeOptions').pillButtons();
+    $('#renderTypeOptions a').click(function(e)
+    {
+        e.preventDefault();
+        var rt = $.urlParam($(this).attr('href'), 'defaultRender');
+        if (rt == 'richList') { rt = 'fatRow'; }
+
+        $('#renderTypeOptions li a').removeClass('active');
+        $('#renderTypeOptions li .' + rt.toLowerCase()).addClass('active');
+
+        editRRNS.$container.removeClass('fatRowRenderType pageRenderType')
+            .addClass(rt + 'RenderType');
+
+        editRRNS.renderType = rt;
+        editRRNS.resetConfig();
+        editRRNS.renderCurrentRow();
+    });
+};
 
 
-    // Unsaved view stuff
+// -------------- Unsaved View -----------
+editRRNS.initSaving = function()
+{
     blist.dataset.bind('set_temporary',
         function() { $('body').addClass('unsavedView'); });
     blist.dataset.bind('clear_temporary',
@@ -307,29 +336,25 @@ var editRRNS = blist.namespace.fetch('blist.editRR');
         e.preventDefault();
         blist.dataset.reload(function()
         {
-            renderCurrentLayout();
-            renderCurrentRow();
+            editRRNS.renderCurrentLayout();
+            editRRNS.renderCurrentRow();
         });
     });
+};
 
 
-    // Handle switching types
-    $('#renderTypeOptions').pillButtons();
-    $('#renderTypeOptions a').click(function(e)
-    {
-        e.preventDefault();
-        var rt = $.urlParam($(this).attr('href'), 'defaultRender');
-        if (rt == 'richList') { rt = 'fatRow'; }
 
-        $('#renderTypeOptions li a').removeClass('active');
-        $('#renderTypeOptions li .' + rt.toLowerCase()).addClass('active');
+(function($) {
+    editRRNS.$container = $('#layoutContainer');
+    editRRNS.$renderArea = editRRNS.$container.find('.renderArea');
 
-        editRRNS.$container.removeClass('fatRowRenderType pageRenderType')
-            .addClass(rt + 'RenderType');
+    editRRNS.$dropIndicator = $.tag({tagName: 'span', 'class': 'dropIndicator'});
 
-        editRRNS.renderType = rt;
-        resetConfig();
-        renderCurrentRow();
-    });
+    editRRNS.initResizing();
 
+    editRRNS.initSidebar();
+
+    editRRNS.initLayout();
+
+    editRRNS.initSaving();
 })(jQuery);

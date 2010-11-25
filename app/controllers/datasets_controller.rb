28ef03074921c8c0f1063a0cf12777bfd33fd295
Clint Tseng <clint@dontexplain.com>
2010-11-24 20:47:19 -0800

    BUG: Fix NPE's.
    QA: Should fix unauthed user 500's on grid page.
    
    Reviewed-by: Nate Peter Emerson <nate.emerson@socrata.com>

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -302,8 +302,8 @@ class DatasetsController < ApplicationController
     @view = get_view(params[:id])
     return if @view.nil?
 
-    unless @current_user.has_right?('create_datasets') &&
-      @view.has_rights?('update_view')
+    unless !@current_user.nil? || (@current_user.has_right?('create_datasets') &&
+      @view.has_rights?('update_view'))
       return render_forbidden
     end
   end

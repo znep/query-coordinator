ed5963c47f7edd94d6541df28456235ddde18d1d
Clint Tseng <clint.tseng@socrata.com>
2010-12-27 12:59:32 -0800

    BUG: Popup Select control doesn't actually work because BT is dumb.
    QA: Ignore.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/controls/popup-select.js
+++ public/javascripts/controls/popup-select.js
@@ -1,5 +1,12 @@
 ;(function($)
 {
+    // unfortunately, beautytips is fucking stupid and reaps/recreates
+    // the tip from source each time it's created, so we have to keep
+    // track of everything separately.
+    var eventsBound = false;
+    var popups = {};
+    var items = {};
+
     $.fn.popupSelect = function(options)
     {
         var opts = $.extend({}, $.fn.popupSelect.defaults, options);
@@ -8,9 +15,11 @@
         {
             var $this = $(this);
 
+            var popupId = _.uniqueId();
             var $popup = $.tag({
                 tagName: 'div',
                 'class': 'popupSelectContainer',
+                'data-listid': popupId,
                 contents: [{
                     tagName: 'p',
                     'class': 'popupSelectPrompt',
@@ -24,24 +33,23 @@
             $list = $popup.find('.popupSelectList');
             _.each(opts.choices, function(choice)
             {
+                var itemId = _.uniqueId();
+                items[itemId] = choice;
+
                 var $item = $.tag({
                     tagName: 'li',
+                    'class': [ 'none', // so icons default to blank
+                               { value: 'checked', onlyIf: _.include($.arrayify(opts.selectedItem), choice) } ],
+                    'data-itemid': itemId,
                     contents: [{
-                        tagName: 'div',
-                        'class': [ 'selectedIcon',
-                                   'none',
-                                   { value: 'checked', onlyIf: choice === opts.selectedItem } ],
-                        contents: {
-                            tagName: 'span',
-                            'class': 'icon'
-                        }
+                        tagName: 'span',
+                        'class': 'icon selectedIcon'
                     }, {
                         tagName: 'div',
                         'class': 'contentWrapper',
                         contents: opts.renderer(choice)
                     }]
                 });
-                $item.data('popupSelect-choice', choice);
                 $list.append($item);
             });
 
@@ -52,30 +60,43 @@
                 trigger: 'click'
             });
 
-            $list.children().click(function(event)
+            popups[popupId] = {
+                opts: opts,
+                tip: tip
+            };
+
+            if (!eventsBound)
             {
-                var $item = $(this);
-                var $selectedIcon = $item.find('.selectedIcon');
-
-                if (opts.selectCallback($item.data('popupSelect-choice'),
-                                       !$selectedIcon.hasClass('checked')))
+                $.live('.popupSelectList li', 'click', function(event)
                 {
-                    if (opts.multiselect)
-                    {
-                        $selectedIcon.toggleClass('checked');
-                    }
-                    else
-                    {
-                        $list.find('.selectedIcon').removeClass('checked');
-                        $selectedIcon.addClass('checked');
-                    }
-                }
+                    var $item = $(this);
+                    var $popup = $item.closest('.popupSelectContainer');
 
-                if (opts.dismissOnClick)
-                {
-                    tip.hide();
-                }
-            });
+                    var data = popups[$popup.attr('data-listid')];
+                    var opts = data.opts;
+                    var tip = data.tip;
+
+                    if (opts.selectCallback(items[$item.attr('itemid')],
+                                                 !$item.hasClass('checked')))
+                    {
+                        if (opts.multiselect)
+                        {
+                            $item.toggleClass('checked');
+                        }
+                        else
+                        {
+                            $item.siblings().removeClass('checked');
+                            $item.addClass('checked');
+                        }
+                    }
+
+                    if (opts.dismissOnClick)
+                    {
+                        tip.hide();
+                    }
+                });
+                eventsBound = true;
+            }
         });
     };
 

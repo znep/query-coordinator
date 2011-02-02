7c689a5ab09df1e7d96d711b5d5e5db3f1b81523
Clint Tseng <clint@dontexplain.com>
2011-02-01 19:03:00 -0800

    NOOP: And of course there shouldn't be html in a js file
    QA: Ignore.
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/domains/data.baltimorecity.gov.js
+++ public/javascripts/domains/data.baltimorecity.gov.js
@@ -1,10 +1,7 @@
-<!-- Zoho Feedback Widget -->
-<script type="text/javascript" charset="utf-8">
 var scheme = document.location.protocol;
 var hostURL = scheme + "//" + "discussions.zoho.com";
 document.write(unescape("%3Cscript src='" + hostURL + "/js/discussions.feedbackwidget.js' type='text/javascript'%3E%3C/script%3E")); //No I18N
-</script>
-<script type="text/javascript">
+
 var zdFBWSettings = {};
 zdFBWSettings.alignment = "fbLeft";
 zdFBWSettings.tabColor = "#7FBF5D";
@@ -13,4 +10,4 @@ zdFBWSettings.fbURL = "http://discuss.baltimorecity.gov/fbw?fbwId=47048000000006
 zdFBWSettings.defaultDomain = "discussions.zoho.com";
 zdFBWSettings.display = "popout";
 var zdFBW = new ZDiscussions.loadZDFeedbackTab;
-</script>
+

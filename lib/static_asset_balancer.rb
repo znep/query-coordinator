95a2c1101abb32933a9c8e7afffee65d6e833f97
Aiden Scandella <aiden.scandella@socrata.com>
2011-05-17 13:33:33 -0700

    NOOP: removing dead code path from unused code * Step 3: Profit?
    QA: No
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- lib/static_asset_balancer.rb
+++ lib/static_asset_balancer.rb
@@ -5,13 +5,10 @@ class StaticAssetBalancer
   end
 
   def call(source, request)
-    # TODO: Remove this hack once we deprecate the ThemesController/v3
-    unless source =~ /^\/stylesheets\/theme\//
-      if request && request.ssl?
-        "#{request.protocol}#{@hosts[source.hash % @count]}"
-      else
-        "http://#{@hosts[source.hash % @count]}"
-      end
+    if request && request.ssl?
+      "#{request.protocol}#{@hosts[source.hash % @count]}"
+    else
+      "http://#{@hosts[source.hash % @count]}"
     end
   end
 end

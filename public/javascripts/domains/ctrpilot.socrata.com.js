6b491436c5c85d385165aa1b72f414472df3f2ff
Aiden Scandella <aiden.scandella@socrata.com>
2011-06-01 16:22:55 -0700

    NOOP: Updates for world bank * Hook up social links * Use JS to set the active nav menu item
    QA: No
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- public/javascripts/domains/ctrpilot.socrata.com.js
+++ public/javascripts/domains/ctrpilot.socrata.com.js
@@ -6,13 +6,49 @@ $(function()
         $(this).siblings('.widgetContent').toggle();
     });
 
-    // social dropdown
-    var $social = $('.worldBankSocialLinks .share');
-    var $socialDropdown = $('.worldBankSocialLinks .socialDropdown');
-    $social.click(function(event)
+    var title = document.title;
+    if (title.indexOf('|') > -1)
+    {
+        title = title.substring(0, title.indexOf('|') - 1);
+    }
+    $('.worldBankSocialLinks .templatedLink').each(function()
+    {
+        $(this).attr('href',
+            $(this).attr('href')
+                .replace('{title}', title)
+                .replace('{location}', self.document.location));
+    });
+    $('.worldBankSocialLinks .printLink').click(function(event)
     {
         event.preventDefault();
-        $socialDropdown.toggle();
-        $social.toggleClass('active');
+        window.print();
     });
+
+    var patterns = {
+        '/facet/countries': 'countries',
+        '/page/organization': 'organization',
+        '/page/funds': 'funds',
+        '/page/faqs': 'faqs',
+        '/browse': 'browse'
+    };
+    var match = 'home';
+    for (var pattern in patterns)
+    {
+        if (window.location.pathname.indexOf(pattern) == 0)
+        {
+            match = patterns[pattern];
+            break;
+        }
+    }
+    $('.worldBankNav .textNav li.' + match).addClass('active');
+
+    // // social dropdown
+    // var $social = $('.worldBankSocialLinks .share');
+    // var $socialDropdown = $('.worldBankSocialLinks .socialDropdown');
+    // $social.click(function(event)
+    // {
+    //     event.preventDefault();
+    //     $socialDropdown.toggle();
+    //     $social.toggleClass('active');
+    // });
 });

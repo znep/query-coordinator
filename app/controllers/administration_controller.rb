068c8b41e083149977f71c391d640c5557bfbc77
Clint Tseng <clint@dontexplain.com>
2011-06-21 18:01:11 -0700

    Revert "NOOP: Update to accomodate core server role change."
    This reverts commit fa370a9e43b09e7d111f276ef7055da8b7cd1789.

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -976,8 +976,8 @@ private
     story.customization = customization
   end
 
-  def find_privileged_users
-    User.find :method => 'findPrivilegedUsers'
+  def find_privileged_users(level=1)
+    User.find :method => 'usersWithRole', :role => level
   end
 
   def redirect_federation(message = nil)

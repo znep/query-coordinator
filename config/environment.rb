eea0b611fa1ae5c465d74b663d4cd479dbbcfdc1
Paul Paradise <paul.paradise@socrata.com>
2011-07-13 14:54:26 -0700

    BUG: YAML parsing on Ruby 1.9.2-p180 is broken, resulting in blist.secureUrl being wrong.
    QA: This will likely fix some things with inline login
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+YAML::ENGINE.yamler = 'syck'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')

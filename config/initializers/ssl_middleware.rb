9ad534ac75b05bde0014a9cb4c33a3928f731d23
Clint Tseng <clint.tseng@socrata.com>
2011-08-03 17:20:32 -0700

    BUG: Fixing inline signup for non-https sites.
    QA: Test inline signup for a site that does not
    have https-everywhere enabled. demo.test should be
    fine for this. Make sure that once you hit signup,
    the comment you were trying to post actually posts.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- config/initializers/ssl_middleware.rb
+++ config/initializers/ssl_middleware.rb
@@ -5,4 +5,4 @@ Rails.configuration.middleware.insert_after CoreServerConnectionMiddleware, "Cur
 Rails.configuration.middleware.insert_after CurrentDomainMiddleware, Rack::SslEnforcer,
   :https_port => APP_CONFIG['ssl_port'],
   :http_port => APP_CONFIG['http_port'],
-  :only => [/^\/login$/, /^\/login\//, /^\/signup/, /^\/accounts.json/, /^\/account\/add_rpx_token/, /^\/profile\/.*\/update_account/, /^\/oauth/]
+  :only => [/^\/login$/, /^\/login\//, /^\/signup$/, /^\/signup\//, /^\/accounts.json/, /^\/account\/add_rpx_token/, /^\/profile\/.*\/update_account/, /^\/oauth/]

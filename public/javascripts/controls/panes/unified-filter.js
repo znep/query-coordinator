afe9102a0c00f1b2083057b36e369135f31df83e
Clint Tseng <clint.tseng@socrata.com>
2011-10-27 10:16:58 -0700

    BUG 5273: Never meant to check this in anyway.
    QA: Retest.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/controls/panes/unified-filter.js
+++ public/javascripts/controls/panes/unified-filter.js
@@ -1643,8 +1643,7 @@
                 var column = dataset.columnForTCID(metadata.tableColumnId[dataset.publicationGroup]);
                 var columnDefinition = {
                     type: 'column',
-                    //columnId: column.id
-                    columnFieldName: column.fieldName
+                    columnId: column.id
                 };
 
                 if (!_.isUndefined(metadata.subcolumn))
@@ -1751,8 +1750,8 @@
                         (ds.query.filterCondition !== rootCondition))
                     {
                         // we're not the default filter, need to push this on
-                        /*columnDefinition.columnId =
-                            ds.columnForTCID(metadata.tableColumnId[ds.publicationGroup]).id;*/
+                        columnDefinition.columnId =
+                            ds.columnForTCID(metadata.tableColumnId[ds.publicationGroup]).id;
 
                         var dsCondition = $.extend({}, condition);
                         dsCondition.children = $.extend(true, [], children);

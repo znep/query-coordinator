278608e7a77566f1daea4bd0210a07cfeec17aaf
Clint Tseng <clint.tseng@socrata.com>
2011-11-04 16:03:37 -0700

    NOOP: Bump Rails environment version, not that it matters.
    QA: Ignore.
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 YAML::ENGINE.yamler = 'syck'
 
 # Bootstrap the Rails environment, frameworks, and default configuration

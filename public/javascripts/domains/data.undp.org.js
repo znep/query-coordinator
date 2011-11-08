1a2b731548a2b135d890e4b4c4c70cfd2e91618d
Clint Tseng <clint.tseng@socrata.com>
2011-11-08 14:04:25 -0800

    THEME: More updates for UNDP JS; this time breadcrumb.

--- public/javascripts/domains/data.undp.org.js
+++ public/javascripts/domains/data.undp.org.js
@@ -19,4 +19,45 @@ $(function() {
             match = patterns[pattern];
 
     $('.leftNav li[data-page=' + match + ']').addClass('active');
+
+    var generateBreadcrumb = function(text, url)
+    {
+        return $.tag({
+            tagName: 'li',
+            contents: (url ? {
+                tagName: 'a',
+                href: url,
+                contents: text
+            } : text)
+        });
+    };
+
+    if (blist.sidebarHidden)
+    {
+        $('.breadcrumbs li:last-child')
+            .replaceWith(generateBreadcrumb('Open Data', '/'));
+
+        // we're probably on a grid page
+        if ($.subKeyDefined(blist.dataset, 'metadata.custom_fields.Scope.Country'))
+        {
+            // we're probably on a country page
+            $('.breadcrumbs li:last-child')
+                .after(generateBreadcrumb($.htmlEscape(blist.dataset.name)))
+                .after(generateBreadcrumb('Data By Country/Unit', '/page/projects'));
+        }
+        else
+        {
+            // we're on some other dataset
+            $('.breadcrumbs li:last-child')
+                .after(generateBreadcrumb($.htmlEscape(blist.dataset.name)));
+        }
+    }
+    else if (match == 'country_unit')
+    {
+        // we're on the projects page
+        $('.breadcrumbs li:last-child')
+            .replaceWith(generateBreadcrumb('Open Data', '/'));
+        $('.breadcrumbs li:last-child')
+            .after(generateBreadcrumb('Data By Country/Unit'));
+    }
 });

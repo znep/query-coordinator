5c156df5c89cdb4c305bb31cde6d466b4d834978
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2012-01-18 15:01:45 -0800

    FEATURE: Add Menu component to Canvas2 * Renders a menu control * Does stringSubstitution * Can hook up to a context and repeat across rows * Also moved controls/menu.js to common code, since it is fairly widely used
    QA: No
    
    To-be-
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

---
+++ public/javascripts/component/content/menu.js
@@ -0,0 +1,69 @@
+$.component.Component.extend('Menu', 'content', {
+    _needsOwnContext: true,
+
+    _render: function() {
+        if (!this._super.apply(this, arguments)) { return false; }
+
+        var cObj = this;
+        var doRender = function()
+        {
+            var createMenu = function(menuOpts)
+            { cObj.$contents.addClass('menu').menu(menuOpts); }
+
+            var opts = {};
+            _.each(['Class', 'Contents', 'Title'], function(v)
+                {
+                    if (!$.isBlank(cObj._properties['button' + v]))
+                    {
+                        opts['menuButton' + v] =
+                            cObj._stringSubstitute(cObj._properties['button' + v]);
+                    }
+                });
+
+            if (_.isArray(cObj._properties.contents))
+            {
+                opts.contents = cObj._stringSubstitute(cObj._properties.contents);
+                createMenu(opts);
+            }
+            else if ($.isPlainObject(cObj._properties.contents) &&
+                    !$.isBlank(cObj._dataContext))
+            {
+                opts.contents = [];
+                switch (cObj._dataContext.type)
+                {
+                    case 'dataset':
+                        cObj._dataContext.dataset.getRows(0, 100,
+                            function(rows)
+                            {
+                                var columnMap = this.columnMap = {};
+                                _.each(cObj._dataContext.dataset.visibleColumns,
+                                    function(c) { columnMap[c.id] = c.fieldName; });
+                                _.each(rows, function(r)
+                                    {
+                                        var entity = {};
+                                        _.each(columnMap, function(to, from)
+                                        {
+                                            entity[to] = r[from];
+                                            if (entity[to] == undefined)
+                                            entity[to] = null;
+                                        });
+                                        opts.contents.push(cObj._stringSubstitute(cObj._properties.contents, entity));
+                                    });
+                                createMenu(opts);
+                            });
+                        break;
+                }
+            }
+        }
+        if (!cObj._updateDataSource(cObj._properties, doRender))
+        { doRender(); }
+
+        return true;
+    },
+
+    _propWrite: function(properties)
+    {
+        this._super(properties);
+        if (!_.isEmpty(properties)) { this._render(); }
+    }
+});

9d4fbb08f336bb8da19507f2fbf081b7ed3af539
Clint Tseng <clint.tseng@socrata.com>
2012-02-17 09:55:54 -0800

    BUG: Missed tooltip hovers for localization in browse.
    QA: The type icons on the right have tooltips; they
    should still render. If Chris gets translations they
    should be in italian on the italian site.
    
    Reviewed-by: chris.metcalf@socrata.com

--- app/models/displays/calendar.rb
+++ app/models/displays/calendar.rb
@@ -1,2 +1,5 @@
 class Displays::Calendar < Displays::Base
+  def name
+    I18n.t('core.view_types.calendar')
+  end
 end
--- app/models/displays/chart.rb
+++ app/models/displays/chart.rb
@@ -2,4 +2,8 @@ class Displays::Chart < Displays::Base
   def type
     'chart'
   end
+
+  def name
+    I18n.t('core.view_types.chart')
+  end
 end

51f48e2094ec0e4acc370218abad83604b7adf81
Michael Chui <michael.chui@socrata.com>
2012-02-27 10:33:03 -0800

    NOOP: Remove toGoogle and toBing, since we don't convert into those formats anymore.
    Reviewed-by: Michael Chui <michael.chui@socrata.com>

--- public/javascripts/util/dataset/map.js
+++ public/javascripts/util/dataset/map.js
@@ -1,62 +1,6 @@
 (function(){
 
 Dataset.map = {};
-Dataset.map.toGoogle = {
-    polygon: function(geometry)
-    {
-        return _.map(geometry.rings, function(ring, r)
-        {
-            return _.map(ring, function(point, p)
-            {
-                var point = geometry.getPoint(r, p);
-                if (point.spatialReference.wkid == 102100)
-                { point = esri.geometry.webMercatorToGeographic(point); }
-                return new google.maps.LatLng(point.y, point.x);
-            });
-        });
-    },
-    extent: function(extent)
-    {
-        var sw = new esri.geometry.Point(extent.xmin, extent.ymax,
-            extent.spatialReference);
-        var ne = new esri.geometry.Point(extent.xmax, extent.ymin,
-            extent.spatialReference);
-        if (extent.spatialReference.wkid == 102100)
-        {
-            sw = esri.geometry.webMercatorToGeographic(sw);
-            ne = esri.geometry.webMercatorToGeographic(ne);
-        }
-        return new google.maps.LatLngBounds(
-            new google.maps.LatLng(sw.y, sw.x),
-            new google.maps.LatLng(ne.y, ne.x));
-    }
-};
-Dataset.map.toBing = {
-    polygon: function(geometry)
-    {
-        // Bing does not support multiple-ring Polygons. As a result, we're picking
-        // the largest ring and using that as the polygon's definition.
-    //    var largestRing = _.max(geometry.rings, function(ring) { return ring.length; });
-    //    var r = _.indexOf(geometry.rings, largestRing);
-
-        // Bing's documentation flouts their inability to close polygons, so let's do
-        // it for them.
-    //    if (largestRing[largestRing.length - 1] != largestRing[0])
-    //    { largestRing.push(largestRing[0]); }
-        return _.map(geometry.rings, function(ring, r)
-        {
-            if (ring[ring.length - 1] != ring[0])
-            { ring.push(ring[0]); }
-            return new Microsoft.Maps.Polygon(_.map(ring, function(point, p)
-            {
-                var point = geometry.getPoint(r, p);
-                if (point.spatialReference.wkid == 102100)
-                { point = esri.geometry.webMercatorToGeographic(point); }
-                return new Microsoft.Maps.Location(point.y, point.x);
-            }))
-        });
-    }
-};
 
 Dataset.map.isValid = function(view, displayFormat)
 {

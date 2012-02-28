99db4222aa8d6b89f64b95be1971281676a742d1
Aiden Scandella <aiden.scandella@socrata.com>
2012-02-27 18:30:15 -0800

    BUG: Commenting broken, probably others
    * Need to deep merge into blist.translations!!
    
    QA: You can post comments on datasets
    
    Reviewed-by: Chris Metcalf <chris.metcalf@socrata.com>

--- public/javascripts/util/load-translations.js
+++ public/javascripts/util/load-translations.js
@@ -3,6 +3,6 @@
     var $translations = $('div.translations');
     blist.translations = {};
     $translations.each(function()
-    { $.extend(blist.translations, JSON.parse($(this).html())); });
+    { $.extend(true, blist.translations, JSON.parse($(this).html())); });
     blist.locale = $translations.attr('data-locale');
 })(jQuery);

263ed323af902d565cd9c07bd50a2e9ede00d2e0
Aiden Scandella <aiden.scandella@socrata.com>
2012-02-29 17:28:23 -0800

    NOOP: Silence our middlewares in backtraces
    * Makes backtraces a lot a bit easier to look at
    
    QA: No, logging only
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -1,7 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
-# Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rails_patches/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /core_server/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /socrata_cookie_store/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /health_check/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /locale_middleware/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /current_domain/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /log_referer/ }
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!

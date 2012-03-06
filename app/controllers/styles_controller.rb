40541b7da45bd6dfb4afa30c14b9e8be86524a4d
Aiden Scandella <aiden.scandella@socrata.com>
2012-03-05 20:43:44 -0800

    BUG: Redirecting users to secure stylesheets
    * We probably shouldn't redirect to a stylesheet
    
    QA: You shouldn't get redirected to any stylesheets
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -2,7 +2,8 @@ require 'tmpdir'
 require 'digest/md5'
 
 class StylesController < ApplicationController
-  skip_before_filter :require_user, :set_user, :set_meta, :sync_logged_in_cookie
+  skip_before_filter :require_user, :set_user, :set_meta,
+    :redirect_logged_in_users_to_https, :sync_logged_in_cookie
 
   def individual
     if params[:stylesheet].present? && params[:stylesheet].match(/^(\w|-|\.)+$/)

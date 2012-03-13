b4856485c67f839dee1ce21539fb91d5f8633f69
Aiden Scandella <aiden.scandella@socrata.com>
2012-03-13 14:10:06 -0700

    BUG: Fix our middlewares
    * Rack now expects an array for the body of the response
    
    Reviewed-by: Paul Paradise <paul.paradise@socrata.com>

--- lib/health_check_middleware.rb
+++ lib/health_check_middleware.rb
@@ -27,11 +27,11 @@ class HealthCheckMiddleware
     rescue Exception => e
       # Yes, I mean 200. If the CS is failing its health check,
       # we should already know about it
-      return [200, {}, "ERROR\nError fetching core server health status:\n#{e.message}"]
+      return [200, {}, ["ERROR\n", "Error fetching core server health status\n", e.message]]
     end
 
     if health.nil? || !health.is_a?(Hash)
-      return [500, {}, "ERROR\nCore server returned malformed health check:\n'#{health.inspect}'"]
+      return [500, {}, ["ERROR\n", "Core server returned malformed health check:\n", health.inspect]]
     end
 
     # Only check to see if requests are past expiration on /health-check requests
@@ -40,7 +40,7 @@ class HealthCheckMiddleware
     @request_log.delete_if { |l| (now - l.last) > @@WINDOW_SECONDS }
 
     if @request_log.empty?
-      return [200, {}, "WARNING\nNo requests in window."]
+      return [200, {}, ["WARNING\nNo requests in window."]]
     end
 
     errors = @request_log.select { |r| (r.first / 100) == 5 }
@@ -50,9 +50,9 @@ class HealthCheckMiddleware
     status = "Core server says: '#{health['status']}'"
 
     if failometer < @@ERROR_THRESHOLD
-      [200, {}, "OK\n#{percentage}% failure.\n#{status}"]
+      [200, {}, ["OK\n", "#{percentage}% failure.\n", status]]
     else
-      [500, {}, "ERROR\n#{percentage}% failure.\n#{status}"]
+      [500, {}, ["ERROR\n", "#{percentage}% failure.\n", status]]
     end
   end
 

7d90762b1abdf93c4a132389075d97e6fdf43110
Clint Tseng <clint.tseng@socrata.com>
2012-05-07 19:28:30 -0700

    THEME: JS changes for HSTP.
    QA: Make sure modal dialogs still work at all. They should.

--- public/javascripts/controls/modal-dialogs.js
+++ public/javascripts/controls/modal-dialogs.js
@@ -1,30 +1,36 @@
-$(function()
+(function($){$(function()
 {
-    // Set up modals
-    $('.modalDialog, #modals > *').jqm({
-        trigger: false,
-        modal: true,
-        onShow: function(jqm)
-        {
-            $('.menu').trigger('menu-close');
-            if (!_.isUndefined(blist.events))
-            { $(document).trigger(blist.events.MODAL_SHOWN); }
-            jqm.w.fadeIn('slow');
-            jqm.o.fadeIn('slow');
-        },
-        onHide: function(jqm)
-        {
-            jqm.w.fadeOut('slow');
-            jqm.o.fadeOut('slow', function()
+    $.fn.socrataJqm = function()
+    {
+        this.jqm({
+            trigger: false,
+            modal: true,
+            onShow: function(jqm)
             {
+                $('.menu').trigger('menu-close');
                 if (!_.isUndefined(blist.events))
-                { $(document).trigger(blist.events.MODAL_HIDDEN); }
-            });
-        }
-    });
+                { $(document).trigger(blist.events.MODAL_SHOWN); }
+                jqm.w.fadeIn('slow');
+                jqm.o.fadeIn('slow');
+            },
+            onHide: function(jqm)
+            {
+                jqm.w.fadeOut('slow');
+                jqm.o.fadeOut('slow', function()
+                {
+                    if (!_.isUndefined(blist.events))
+                    { $(document).trigger(blist.events.MODAL_HIDDEN); }
+                });
+            }
+        });
+    };
+
+    // Set up modals
+    $('.modalDialog, #modals > *').socrataJqm();
     $.live('a.jqmClose', 'click', function(event)
     {
         event.preventDefault();
         $(this).closest('.modalDialog').jqmHide();
     });
-});
+
+});})(jQuery);

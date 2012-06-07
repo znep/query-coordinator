ff650fdb31be154b20a609feeda9aec91fa6aad6
Clint Tseng <clint.tseng@socrata.com>
2012-06-07 12:02:16 -0700

    NOOP: D3/Raphael IE8 compat.
    * Added passthroughs for native array functions.
    * Fix up CSSStyleDeclaration where possible.
    * Still haven't looked at IE7.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/util/compat.js
+++ public/javascripts/util/compat.js
@@ -77,23 +77,49 @@
     }
 
     // prototype defs to make CSSStyleDeclaration sane in ie8-
-    if (!_.isFunction(CSSStyleDeclaration.prototype.getProperty))
+    if (CSSStyleDeclaration)
     {
-        CSSStyleDeclaration.prototype.getProperty = function(a) {
-            return this.getAttribute(a);
-        };
+        if (!_.isFunction(CSSStyleDeclaration.prototype.getProperty))
+        {
+            CSSStyleDeclaration.prototype.getProperty = function(a) {
+                return this.getAttribute(a);
+            };
+        }
+        if (!_.isFunction(CSSStyleDeclaration.prototype.setProperty))
+        {
+            CSSStyleDeclaration.prototype.setProperty = function(a, b) {
+                return this.setAttribute(a, b);
+            };
+        }
+        if (!_.isFunction(CSSStyleDeclaration.prototype.removeProperty))
+        {
+            CSSStyleDeclaration.prototype.removeProperty = function(a) {
+                return this.removeAttribute(a);
+            };
+        }
     }
-    if (!_.isFunction(CSSStyleDeclaration.prototype.setProperty))
+    else
     {
-        CSSStyleDeclaration.prototype.setProperty = function(a, b) {
-            return this.setAttribute(a, b);
-        };
+        // ruh oh.
     }
-    if (!_.isFunction(CSSStyleDeclaration.prototype.removeProperty))
-    {
-        CSSStyleDeclaration.prototype.removeProperty = function(a) {
-            return this.removeAttribute(a);
-        };
+
+    // support getComputedStyle in IE
+    // from: http://snipplr.com/view/13523/
+    if (!window.getComputedStyle) {
+        window.getComputedStyle = function(el, pseudo) {
+            this.el = el;
+            this.getPropertyValue = function(prop) {
+                var re = /(\-([a-z]){1})/g;
+                if (prop == 'float') prop = 'styleFloat';
+                if (re.test(prop)) {
+                    prop = prop.replace(re, function () {
+                        return arguments[2].toUpperCase();
+                    });
+                }
+                return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+            }
+            return this;
+        }
     }
 
 })(jQuery);

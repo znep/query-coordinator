387b97825a2826e37b2073db78d71f2c070d922e
Michael Chui <michael.chui@socrata.com>
2012-08-24 11:57:06 -0700

    FEATURE: Added explanations of restrictions on password as per Marc.
    QA: On the Signup Page and in Edit Account Settings, there are now
    fields marked "Restrictions apply". Hovering over these should result in tooltips.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- public/javascripts/screens/profile-screen-edit.js
+++ public/javascripts/screens/profile-screen-edit.js
@@ -221,5 +221,9 @@
             }
         });
     });
+
+    var $content = $('<p>Your password must be <b>between 8 and 40 characters</b> and satisfy <b>three of the following four criteria</b>:</p><p></p><ul><li>&bullet; contain a digit</li><li>&bullet; contain a lowercase letter</li><li>&bullet; contain an uppercase letter</li><li>&bullet; contain a non-alphanumeric symbol</li></ul>');
+
+    $('.passwordHint').socrataTip({ content: $content });
 });
 

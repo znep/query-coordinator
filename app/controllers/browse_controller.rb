036aaf33b36fee80639e9f4b00ebb2824f4cfb59
John Kew <john.kew@socrata.com>
2012-08-27 14:01:44 -0700

    FEATURE: Cache embedded facets. Note; this adds caching to a feature which was originally     a hack and which may be rewritten. The cached, embedded facets will have no dataset controls     which require user permissions.
    QA: Test the world bank canvas page, or pages which include an embedded iframe for the catalog
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,4 +1,5 @@
 class BrowseController < ApplicationController
+  include ApplicationHelper
   prepend_before_filter :check_chrome, :only => :show
   skip_before_filter :require_user
   skip_before_filter :disable_frame_embedding, :only => :embed
@@ -13,28 +14,36 @@ class BrowseController < ApplicationController
   end
 
   def embed
-    browse_options = {
-      browse_in_container: true,
-      suppress_dataset_creation: true,
-      rel_type: 'external'
-    }
+    cache_params = {'domain' => CurrentDomain.cname,
+                    'domain_updated' => CurrentDomain.default_config_updated_at,
+                    'locale' => I18n.locale,
+                    'params' => Digest::MD5.hexdigest(params.sort.to_json)}
+    @cache_key = cache_key("canvas-embedded-browse-#{params[:action]}", cache_params)
+    @cached_fragment = read_fragment(@cache_key)
+    if @cached_fragment.nil?
+      browse_options = {
+          browse_in_container: true,
+          embedded_browse_iframe_hack: true,
+          suppress_dataset_creation: true,
+          rel_type: 'external'
+      }
+      # Mimic the 'default' selection for facets
+      browse_options.merge!(params[:defaults].deep_symbolize_keys) if params[:defaults].present?
+      if params[:suppressed_facets].is_a? Hash
+        params[:suppressed_facets] =
+            params[:suppressed_facets].map { |k, v| k if v }.flatten
+      end
 
-    # Mimic the 'default' selection for facets
-    browse_options.merge!(params[:defaults].deep_symbolize_keys) if params[:defaults].present?
-    if params[:suppressed_facets].is_a? Hash
-      params[:suppressed_facets] =
-        params[:suppressed_facets].map{ |k,v| k if v }.flatten
+      browse_options[:facets] = [
+          view_types_facet,
+          custom_facets,
+          categories_facet,
+          topics_facet,
+          federated_facet
+      ]
+
+      @processed_browse = process_browse(request, browse_options)
     end
-
-    browse_options[:facets] = [
-      view_types_facet,
-      custom_facets,
-      categories_facet,
-      topics_facet,
-      federated_facet 
-    ]
-
-    @processed_browse = process_browse(request, browse_options)
     render :layout => 'embedded_browse'
   end
 end

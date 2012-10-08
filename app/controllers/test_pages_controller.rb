eff321432d550ab0ba986b5f14914b21ffe87b76
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2012-10-08 16:04:11 -0700

    NOOP: Fix test_page/cf w/ debug stuff
    QA: No, dev only
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

--- app/controllers/test_pages_controller.rb
+++ app/controllers/test_pages_controller.rb
@@ -41,6 +41,7 @@ class TestPagesController < ApplicationController
   def cf
     Canvas2::DataContext.reset
     Canvas2::Util.set_params(params)
+    Canvas2::Util.set_debug(true)
     Canvas2::Util.set_env({
       domain: CurrentDomain.cname,
       renderTime: Time.now.to_i,

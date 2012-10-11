6fb0eb0fed1d94e2b86a45fe3fd4e949e17aa062
Michael Chui <michael.chui@socrata.com>
2012-10-10 23:29:05 -0700

    BUG 6753: Changing items from datasets to ds summaries. * Untested.
    QA: Retest.
    
    Reviewed-by: Michael Chui <michael.chui@socrata.com>

--- public/javascripts/controls/panes/map-config.2.js
+++ public/javascripts/controls/panes/map-config.2.js
@@ -119,8 +119,8 @@
             value = $.trim(value);
             var query = blist.util.patterns.UID.test(value)
                 ? value : 'name:' + value;
-            Dataset.search({ q: query, nofederate: true, limit: 20 },
-                function(data) { callback(data.views) });
+            Dataset.search({ q: query, nofederate: true, limit: 20 }, function(data)
+            { callback(_.map(data.views, function(v) { return { id: v.id, name: v.name }; })) });
         },
         noResultsMessage: 'No results were found. Note: This search only ' +
             'matches full words.',
@@ -136,7 +136,8 @@
             {
                 $field.data('uid', dataset.id);
                 makeStatic(dataset.name);
-                modifySection.call(cpObj, dataset, $field);
+                Dataset.lookupFromViewId(dataset.id, function(ds)
+                { modifySection.call(cpObj, ds, $field); });
             }
         });
 

60dbc4c736f799e5655a34361e54f71834554524
John Kew <john.kew@socrata.com>
2012-12-12 17:06:46 -0800

    FEATURE: Prepend all frontent logs with domain and request uuid
    QA: None
    
    Reviewed-by: jeff.scherpelz@socrata.com

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,8 @@ module Frontend
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
+    config.log_tags = [:domain, :uuid]
+
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible

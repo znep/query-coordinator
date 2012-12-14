812f98e1e0722a4e65f3c74bfe0ceae9eb037dbf
Dan Rathbone <dan.rathbone@socrata.com>
2012-12-13 17:36:48 -0800

    BUG: style fixes per Kevin's request
    QA: look at the api management pages
    
    Reviewed-by: john.kew@socrata.com

--- public/javascripts/controls/panes/api-foundry.js
+++ public/javascripts/controls/panes/api-foundry.js
@@ -16,7 +16,30 @@
                         directive: {
                             'p@class+': 'pClass'
                         },
-                        data: { pClass: 'sectionContent' }
+                        data: { pClass: 'sectionContent' },
+                        callback: function($s)
+                        {
+                            var cpObj = this;
+                            cpObj._startProcessing();
+                            cpObj._$section = $s;
+                            cpObj._view.getRelatedViews(
+                                function(v)
+                                {
+                                    cpObj._finishProcessing();
+                                    cpObj._viewList = v;
+                                    var apis = _.filter(v, function(view){return view.displayName === "API";});
+                                    if (apis.length > 0){
+                                      var ea = $("#existingAPIs");
+                                      ea.show();
+                                      _.each(apis, function(api){
+                                        var button = $('<a href="/api_foundry/manage/' + api.id + '"'
+                                                      + ' class="manageapi toolbarButton button"'
+                                                       +'> Manage API: ' + api.name + '</a>');
+                                        ea.after(button);
+                                      });
+                                    }
+                                });
+                        }
                     }
                 }
             ];

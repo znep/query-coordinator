d5743d906f0232241b2702843ef44399f6599588
Clint Tseng <clint@dontexplain.com>
2013-03-03 00:30:19 -0500

    BUG: Apparently it's "publicly", not "publically". Reversing.
    Reviewed-by: Lilia Gutnik <lilia.gutnik@socrata.com>

--- public/javascripts/screens/govstat/goals/markup.js
+++ public/javascripts/screens/govstat/goals/markup.js
@@ -196,7 +196,7 @@ blist.namespace.fetch('blist.govstat').markup = {
                                 _: 'option',
                                 value: 'true',
                                 selected: goal.get('is_public') === true,
-                                contents: 'publically'
+                                contents: 'publicly'
                             }, {
                                 _: 'option',
                                 value: 'false',

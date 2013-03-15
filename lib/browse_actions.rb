ef770b4a1dc0d40f0ee2c61cb46523b8a5b1fddd
Clint Tseng <clint@dontexplain.com>
2013-03-15 14:02:16 -0700

    BUG: Browse UTF: Not all params are strings.
    QA: Ignore.
    
    Reviewed-by: paul.paradise@socrata.com

--- lib/browse_actions.rb
+++ lib/browse_actions.rb
@@ -170,7 +170,13 @@ protected
     @request_params = request.params
 
     # make our params all safe
-    @request_params.each{ |_, v| v.force_encoding('iso-8859-1').encode!('utf-8') }
+    @request_params.each do |_, v|
+      if v.is_a? Array
+        v.each{ |elem| elem.force_encoding('iso-8859-1').encode!('utf-8') }
+      else
+        v.force_encoding('iso-8859-1').encode!('utf-8')
+      end
+    end
 
     # grab our catalog configuration first
     catalog_config = CurrentDomain.configuration('catalog')

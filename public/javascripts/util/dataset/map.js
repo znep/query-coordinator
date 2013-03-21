a7a5bce249be3c1f53d0fc7254e0c88d41342b0f
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2013-03-21 10:43:33 -0700

    NOOP: Optimized fixing IDs in convertLegacy maps
    QA: No visible change
    
    Reviewed-by: Michael Chui <michael.chui@socrata.com>

--- public/javascripts/util/dataset/map.js
+++ public/javascripts/util/dataset/map.js
@@ -216,8 +216,8 @@ Dataset.modules['map'] =
                 return vdResult;
             };
 
-            if (!fixIds(view) && !_.any(view.displayFormat.viewDefinitions, function(vd)
-                { return vd.uid == 'self'; }))
+            if (!_.any(view.displayFormat.viewDefinitions, function(vd)
+                        { return vd.uid == 'self'; }) && !fixIds(view))
             {
                 view.getParentView(function(parView)
                 {

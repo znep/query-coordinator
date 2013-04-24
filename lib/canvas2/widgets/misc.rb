06a09a7dc554d7bf12c89cf749537cffc851d30f
Clint Tseng <clint.tseng@socrata.com>
2013-04-24 09:33:04 -0700

    BUG 10368: DataRenderer component referencing unavailable path helper.
    QA: Should be not 404'ing anymore.
    
    Reviewed-by: jeff.scherpelz@socrata.com

--- lib/canvas2/widgets/misc.rb
+++ lib/canvas2/widgets/misc.rb
@@ -118,6 +118,8 @@ module Canvas2
   end
 
   class DataRenderer < CanvasWidget
+    include Rails.application.routes.url_helpers
+
     def initialize(props, parent = nil, resolver_context = nil)
       @needs_own_context = true
       super(props, parent, resolver_context)

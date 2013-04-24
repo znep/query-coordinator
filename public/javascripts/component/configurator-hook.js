37ea3ce25d97eae7576c983fefbd389d217bab07
Clint Tseng <clint.tseng@socrata.com>
2013-04-23 18:04:10 -0700

    NOOP: Load translations for dataslate designer dynamically.
    QA: Slate designer sidebar should still have proper text.
    
    Reviewed-by: jeff.scherpelz@socrata.com

--- public/javascripts/component/configurator-hook.js
+++ public/javascripts/component/configurator-hook.js
@@ -33,7 +33,8 @@ $(function() {
             blist.util.assetLoading.loadAssets(
                 {javascripts: [{assets: 'configurator'}, {assets: 'shared-editors'}],
                     stylesheets: stylesheets,
-                    templates: ['grid_sidebar'], modals: ['configurator_settings', 'select_dataset']},
+                    templates: ['grid_sidebar'], modals: ['configurator_settings', 'select_dataset'],
+                    translations: ['screens.ds.grid_sidebar']},
             function()
             {
                 $('.socrata-page').loadingSpinner().showHide(false);

24cc164d93b11dbab2b8e1131b40abc3d7bdc735
Clint Tseng <clint.tseng@socrata.com>
2013-05-01 18:19:05 -0700

    FEATURE: Last-ish pass on GovStat leafChart for now.
    * No longer requires leafChart=nextgen.
    * Still renders if it has data but no projection.
    * Now shows a marker for the baseline value.
    * Relayout numeric metrics for goal.
    * Chart lives behind goal until expand.
    * Still works in Firefox!
    * Now works in IE8!
    
    QA: See above.
    
    Reviewed-by: jeff.scherpelz@socrata.com

--- public/javascripts/screens/govstat/goal-page.js
+++ public/javascripts/screens/govstat/goal-page.js
@@ -8,27 +8,27 @@
 
     var nextgen = blist.namespace.fetch('blist.nextgen');
 
-    if (nextgen.leafChart === true)
-    {
-        var goal = new govstatNS.models.Goal({ id: $('.goalUid').text() });
-        goal.fetch({
-            success: function()
-            {
-                if (!prepareChart(goal, 0, $('.goalBox .chartArea .content-wrapper'), $('.titleContainer').css('border-color')))
-                {
-                    $('.goalBox .chartArea').remove();
-                }
-            },
-            error: function()
+    var goal = new govstatNS.models.Goal({ id: $('.goalUid').text() });
+    goal.fetch({
+        success: function()
+        {
+            if (!prepareChart(goal, 0, $('.goalBox .chartArea'), $('.titleContainer').css('border-top-color')))
             {
                 $('.goalBox .chartArea').remove();
             }
-        });
-    }
+        },
+        error: function()
+        {
+            $('.goalBox .chartArea').remove();
+        }
+    });
 
     // main code for initializing and rendering a chart
-    var prepareChart = function(goal, idx, $container, color)
+    var chartHeight = function() { return $('.chartArea .meter').height(); };
+    var prepareChart = function(goal, idx, $wrapper, color)
     {
+        var $container = $wrapper.children('.content-wrapper'); // todo: ew. clean up.
+
         // get our data
         var metric = goal.get('metrics').at(idx);
         if ($.isBlank(metric)) { return false; } // we don't have anything to show here
@@ -36,13 +36,15 @@
         var computedValues = metric.get('computed_values');
         if ($.isBlank(computedValues)) { return false; } // we still don't have anything to show
 
+        var hasBaseline = _.isNumber(computedValues.baseline_value);
+
+        var hasProjection = !$.isBlank(computedValues.projected_values);
+        var projection = computedValues.projected_value;
+
         var dataSeries = computedValues.values;
-        var projectionSeries = computedValues.projected_values;
+        var projectionSeries = computedValues.projected_values || [];
 
         if ($.isBlank(dataSeries)) { return false; } // stilllll nothing interesting to show.
-        if ($.isBlank(projectionSeries)) { return false; } // and again.
-
-        var projection = computedValues.projected_value;
 
         // time formats
         var timeParse = d3.time.format('%Y-%m-%dT%H:%M:%S.000').parse;
@@ -59,10 +61,12 @@
         // scales
         var xScale = d3.time.scale().domain([ startDate, endDate ]);
 
-        var allYValues = _.pluck(dataSeries
-                .concat(projectionSeries)
-                .concat([ { y: computedValues.target_value } ]),
-            'y');
+        var allYPoints = dataSeries
+           .concat(projectionSeries)
+           .concat([ { y: computedValues.target_value } ]);
+        if (hasBaseline) { allYPoints.push({ y: computedValues.baseline_value }); }
+
+        var allYValues = _.pluck(allYPoints, 'y');
         var yScale = d3.scale.linear().domain([ d3.min(allYValues), d3.max(allYValues) ]);
         var yScaleInv = yScale.copy();
 
@@ -77,7 +81,7 @@
         var dataSeriesArea = d3.svg.area()
             .interpolate('linear')
             .x(function(d) { return xScale(d.x); })
-            .y0(function() { return $container.height(); })
+            .y0(function() { return chartHeight(); })
             .y1(function(d) { return yScale(d.y); });
 
         svg.append('svg:path').attr('class', 'dataSeriesArea');
@@ -98,28 +102,31 @@
         dataSeriesLinePath.data([ dataSeries ]);
         if (color) { dataSeriesLinePath.style('stroke', color); };
 
-        // projection line shadow
-        var projectionLineShadow = d3.svg.line()
-            .interpolate('linear')
-            .x(function(d) { return xScale(d.x) })
-            .y(function(d) { return yScale(d.y) });
+        if (hasProjection)
+        {
+            // projection line shadow
+            var projectionLineShadow = d3.svg.line()
+                .interpolate('linear')
+                .x(function(d) { return xScale(d.x) })
+                .y(function(d) { return yScale(d.y) });
 
-        svg.append('svg:path').attr('class', 'projectionLineShadow');
+            svg.append('svg:path').attr('class', 'projectionLineShadow');
 
-        var projectionLineShadowPath = svg.select('path.projectionLineShadow');
-        projectionLineShadowPath.data([ projectionSeries ]);
+            var projectionLineShadowPath = svg.select('path.projectionLineShadow');
+            projectionLineShadowPath.data([ projectionSeries ]);
 
-        // projection line
-        var projectionLine = d3.svg.line()
-            .interpolate('linear')
-            .x(function(d) { return xScale(d.x) })
-            .y(function(d) { return yScale(d.y) });
+            // projection line
+            var projectionLine = d3.svg.line()
+                .interpolate('linear')
+                .x(function(d) { return xScale(d.x) })
+                .y(function(d) { return yScale(d.y) });
 
-        svg.append('svg:path').attr('class', 'projectionLine');
+            svg.append('svg:path').attr('class', 'projectionLine');
 
-        var projectionLinePath = svg.select('path.projectionLine');
-        projectionLinePath.data([ projectionSeries ]);
-        projectionLinePath.classed('progress-' + (computedValues.progress || 'none'), true);
+            var projectionLinePath = svg.select('path.projectionLine');
+            projectionLinePath.data([ projectionSeries ]);
+            projectionLinePath.classed('progress-' + (computedValues.progress || 'none'), true);
+        }
 
         // meter
         var computeProgress;
@@ -145,6 +152,14 @@
         var last = function(arr)
         {
             return arr[arr.length - 1];
+        };
+
+        var text = function(f)
+        {
+            return function(d)
+            {
+                $(this).text(f(d));
+            };
         }
 
         // process date ticks
@@ -181,7 +196,7 @@
         {
             // plot areas
             var width = $container.width();
-            var height = $container.height();
+            var height = chartHeight();
 
             // update scales
             xScale.range([ 0, width ]);
@@ -202,9 +217,12 @@
                 .css('width', width - xScale(last(dataSeries).x))
                 .css('height', yScaleInv(last(dataSeries).y));
 
-            // projectionSeries
-            svg.select('path.projectionLineShadow').attr('d', projectionLineShadow);
-            svg.select('path.projectionLine').attr('d', projectionLine);
+            if (hasProjection)
+            {
+                // projectionSeries
+                svg.select('path.projectionLineShadow').attr('d', projectionLineShadow);
+                svg.select('path.projectionLine').attr('d', projectionLine);
+            }
 
             // y ticks
             var yTicks = yScale.ticks(height / 50);
@@ -212,10 +230,10 @@
                 .data(yTicks)
                 .enter().append('div')
                     .classed('yTick', true)
-                    .style('top', function(d) { return yScale(d) + 'px'; })
+                    .style('bottom', function(d) { return yScaleInv(d) + 'px'; })
                     .append('div')
                         .classed('bubble', true)
-                        .text(function(d) { return $.commaify(d); })
+                        .each(text(function(d) { return $.commaify(d); }))
                         .append('div')
                             .classed('tip', true);
 
@@ -228,7 +246,7 @@
                 .enter().append('div')
                     .classed('xTick', true)
                     .classed('bubble', true)
-                    .text(function(d) { return timeFormat(d); })
+                    .each(text(function(d) { return timeFormat(d); }))
                     .append('div')
                         .classed('tip', true);
             xTickDivs
@@ -239,27 +257,56 @@
                 .find('.rangeTop').css('height', yScale(computedValues.target_value)).end()
                 .find('.rangeBottom').css('height', yScaleInv(computedValues.target_value));
 
-            // projection marker
-            $container.find('.projectionMarker')
-                .addClass('progress-' + (computedValues.progress || 'none'))
-                .css('top', yScale(last(projectionSeries).y));
+            if (hasProjection)
+            {
+                // projection marker
+                $container.find('.projectionMarker')
+                    .show()
+                    .addClass('progress-' + (computedValues.progress || 'none'))
+                    .css('bottom', yScaleInv(last(projectionSeries).y));
+            }
+            else
+            {
+                $container.find('.projectionMarker').hide();
+            }
 
             // current marker
             $container.find('.currentMarker')
                 .addClass('progress-' + computeProgress(last(dataSeries).y))
-                .css('top', yScale(last(dataSeries).y));
+                .css('bottom', yScaleInv(last(dataSeries).y));
 
             // target marker
             $container.find('.targetMarker')
-                .css('top', yScale(computedValues.target_value));
+                .css('bottom', yScaleInv(computedValues.target_value));
+
+            if (hasBaseline)
+            {
+                // baseline marker
+                $container.find('.baselineMarker')
+                    .css('bottom', yScaleInv(computedValues.baseline_value));
+            }
 
             // no tip, do you eat em?
             $container.find('.nowTip')
                 .css('left', xScale(last(dataSeries).x));
+
+
+            // post-render massage for ie8/r2d3
+            $container.find('.goalUid').prev().addClass('r2d3Chart');
         };
 
         render();
         $(window).on('resize', render);
+
+
+        // pulldown/up
+        $container.find('.pull').on('click', function()
+        {
+            $wrapper.toggleClass('collapsed');
+            $(this)
+                .toggleClass('down')
+                .toggleClass('up');
+        });
 
         return true;
     };

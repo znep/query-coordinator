4aacd0ce4f89d839d64c31bda3de17b16d3ed161
Clint Tseng <clint.tseng@socrata.com>
2013-05-19 15:49:15 -0700

    NOOP: Tweak Odysseus wire format / render.
    Reviewed-by: clint.tseng@socrata.com

--- app/controllers/odysseus_controller.rb
+++ app/controllers/odysseus_controller.rb
@@ -1,8 +1,11 @@
 class OdysseusController < ApplicationController
-  layout 'odysseus'
-
   def index
-    @contents = Net::HTTP.get(URI::HTTP.build({ host: ODYSSEUS_URI.host, port: ODYSSEUS_URI.port, path: request.path }))
+    contents = JSON.parse(Net::HTTP.get(URI::HTTP.build({ host: ODYSSEUS_URI.host, port: ODYSSEUS_URI.port, path: request.path })))
+
+    @title = contents['title'] || ''
+    @style_packages = contents['styles'] || []
+    @script_packages = contents['scripts'] || []
+    @contents = contents['markup']
   end
 end
 

e1ee9c465641af8f61df0d6b1742f0988bdf25ec
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2013-06-24 17:37:04 -0700

    FEATURE 10374: Permissions for pages * New button & dialog in configurator * Switch between three permissions, with descriptions * Save/Cancel * Default new pages/reports to Private
    QA: When you create a new Report, it should by default be Private. This
    means only you (or a domain administrator) can view it. There should be
    a new Permissions button (next to Settings) when editing a Report (or
    Page). It should show the current permission level, and allow you to
    change it. Public means any user, including unsigned users, can view it.
    Shared to Site (also called Domain Private) means any user who has some
    kind of role on this domain can view it. Private is described above.
    
    Reviewed-by: Clint Tseng <clint.tseng@socrata.com>

--- app/models/page.rb
+++ app/models/page.rb
@@ -220,8 +220,7 @@ class Page < Model
 
   def self.create(attributes, custom_headers = {})
     # Status should eventually start as unpublished
-    attributes = {:content => { type: 'Container', id: 'pageRoot' }}.
-      merge(attributes)
+    attributes = {content: { type: 'Container', id: 'pageRoot' }}.merge(attributes)
     path = "/pages.json"
     parse(CoreServer::Base.connection.
                  create_request(path, attributes.to_json, custom_headers))

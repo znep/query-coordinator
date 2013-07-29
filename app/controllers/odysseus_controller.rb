c54f5da97dbfbd0ab740ee51bfd52c370b5d6058
Clint Tseng <clint.tseng@socrata.com>
2013-07-28 20:33:51 -0700

    NOOP: Passthrough X-Socrata-Host and Cookie to Odysseus.
    Reviewed-by: robert.macomber@socrata.com

--- app/controllers/odysseus_controller.rb
+++ app/controllers/odysseus_controller.rb
@@ -1,6 +1,13 @@
 class OdysseusController < ApplicationController
   def index
-    contents = JSON.parse(Net::HTTP.get(URI::HTTP.build({ host: ODYSSEUS_URI.host, port: ODYSSEUS_URI.port, path: request.path })))
+    uri = URI::HTTP.build({ host: ODYSSEUS_URI.host, port: ODYSSEUS_URI.port, path: request.path })
+    req = Net::HTTP::Get.new(uri.request_uri)
+
+    req['X-Socrata-Host'] = req['Host'] = CurrentDomain.cname
+    req['Cookie'] = request.headers['Cookie']
+
+    res = Net::HTTP.start(uri.host, uri.port){ |http| http.request(req) }
+    contents = JSON.parse(res.body)
 
     @title = contents['title'] || ''
     @style_packages = contents['styles'] || []

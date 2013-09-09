znep/query-coordinator

commit 26fd5849c11045967a1fbddbad51bc3092dbdb8c

NOOP: Do not request #getCommentLocations unless cell_comments is enabled.
Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com>

public/javascripts/controls/table/table.js
@@ -4213,8 +4213,11 @@
                 if (!$.isBlank(curView))
                 { curView.unbind(null, null, table); }
                 curView = model.view;
+
                 // Request comment indicators
-                model.view.getCommentLocations();
+                // This will only be false if the cell comment module is enabled.
+                if ($.deepGet(blist.sidebarHidden, 'feed', 'cellFeed') === false)
+                { model.view.getCommentLocations(); }
 
                 model.view.bind('row_change', function(rows)
                         { updateRows(rows); }, table)

commit 46f380c59e1abeafe82801cb3b3f48e8cf358ad6

BUG CORE-208: SDP preview loads blank.
Regression by 26fd5849c11045967a1fbddbad51bc3092dbdb8c

QA: Bug should be fixed. Cell commenting should still work.

Reviewed-by: Michael Chui <michael.chui@socrata.com>

public/javascripts/controls/table/table.js
@@ -4216,7 +4216,7 @@
 
                 // Request comment indicators
                 // This will only be false if the cell comment module is enabled.
-                if ($.deepGet(blist.sidebarHidden, 'feed', 'cellFeed') === false)
+                if ($.deepGet(blist, 'sidebarHidden', 'feed', 'cellFeed') === false)
                 { model.view.getCommentLocations(); }
 
                 model.view.bind('row_change', function(rows)
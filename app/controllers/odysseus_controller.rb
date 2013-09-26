385a17b5ef474c4f9f1860fd653a5bddb61de4d3
Clint Tseng <clint.tseng@socrata.com>
2013-09-25 21:43:15 -0700

    FEATURE: Lift blanket user auth requirement on Odysseus; listen for response code.
    QA: Make a goal or dashboard public, and try to view it anonymously.
    
    To-be-
    Reviewed-by: jeff.scherpelz@socrata.com

--- app/controllers/odysseus_controller.rb
+++ app/controllers/odysseus_controller.rb
@@ -1,4 +1,6 @@
 class OdysseusController < ApplicationController
+  skip_before_filter :require_user
+
   def index
     uri = URI::HTTP.build({ host: ODYSSEUS_URI.host, port: ODYSSEUS_URI.port, path: request.path })
     req = Net::HTTP::Get.new(uri.request_uri)
@@ -7,16 +9,27 @@ class OdysseusController < ApplicationController
     req['Cookie'] = request.headers['Cookie']
 
     res = Net::HTTP.start(uri.host, uri.port){ |http| http.request(req) }
-    contents = JSON.parse(res.body)
 
-    # TEMPORARY HACK for edmonton
-    @suppress_govstat = true if request.path =~ /edmonton-dash$/
+    if res.code == '400'
+      render_error(400)
+    elsif res.code == '401'
+      require_user
+    elsif res.code == '403'
+      render_403
+    elsif res.code == '404'
+      render_404
+    else
+      contents = JSON.parse(res.body)
 
-    @title = contents['title'] || ''
-    @style_packages = contents['styles'] || []
-    @script_packages = contents['scripts'] || []
-    @objects = contents['objects']
-    @contents = contents['markup']
+      # TEMPORARY HACK for edmonton
+      @suppress_govstat = true if request.path =~ /edmonton-dash$/
+
+      @title = contents['title'] || ''
+      @style_packages = contents['styles'] || []
+      @script_packages = contents['scripts'] || []
+      @objects = contents['objects']
+      @contents = contents['markup']
+    end
   end
 end
 

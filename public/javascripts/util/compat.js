86e2611ac20704ce4c99edfca878f9a9a6fec161
Clint Tseng <clint.tseng@socrata.com>
2013-09-27 02:55:05 -0700

    NOOP: Add a compat shim for Object.create for IE8.
    Reviewed-by: moz.illa@socrata.com

--- public/javascripts/util/compat.js
+++ public/javascripts/util/compat.js
@@ -122,6 +122,26 @@
         }
     }
 
+    // support Object.create in IE
+    // from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+    if (!Object.create)
+    {
+        Object.create = (function()
+        {
+            function F(){}
+
+            return function(o)
+            {
+                if (arguments.length != 1) {
+                    throw new Error('Object.create implementation only accepts one parameter.');
+                }
+                F.prototype = o;
+                return new F();
+            }
+        })()
+    }
+}
+
 })(jQuery);
 
 $(function()

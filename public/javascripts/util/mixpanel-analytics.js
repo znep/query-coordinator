4f4225a55c7e905667c8346b205ba35575d7ae3d
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2013-11-04 12:34:40 -0800

    BUG ONCALL-99: Fix bare rendering.
    QA: Not sure if this will fix the thumbnailer, but it will fix a script
    error that may be causing it on a bare page. So QA should really consist of
    making sure that all these still work (I changed code behind these):
    - Dataset page
    - SDP/widget
    
    Reviewed-by: jeff.scherpelz@socrata.com

--- public/javascripts/util/mixpanel-analytics.js
+++ public/javascripts/util/mixpanel-analytics.js
@@ -1,126 +1,121 @@
-//Track entering certain pages
-$(document).ready(function()
+if (mixpanel)
 {
-    //TODO make sure user properties are loaded, otherwise it will always look like the users is not logged in
-    //update the meta properties
-    //$.mixpanelMeta();
-    //mixpanel.track('Entered Page', {'Page type': jQuery.metrics.determine_page_type()});
-});
-
-mixpanel.delegate_links = function (parent, selector, event_name, getProperties)
-{
-    $(parent || document.body).on('click', selector, function (event)
+    //Track entering certain pages
+    $(document).ready(function()
     {
-        try
-        {
-            //get the specific properties for the event
-            var properties = _.isFunction(getProperties) ? getProperties(event.currentTarget) : {};
-
-            var willOpenInNewTab = event.which === 2 || event.metaKey || event.currentTarget.target === '_blank';
-            properties.url = event.currentTarget.href;
-
-            var isDefaultPrevented = event.isDefaultPrevented();
-            var callback = function()
-            {
-                if (!willOpenInNewTab && !isDefaultPrevented)
-                {
-                    window.location = properties.url;
-                }
-            }
-
-            if (!willOpenInNewTab)
-            {
-                event.preventDefault();
-            }
-            //update the meta properties
-            $.mixpanelMeta();
-
-            //Track!
-            mixpanel.track(event_name, properties, callback);
-        }
-        catch(e)
-        {
-            if (!isDefaultPrevented)
-            { window.location = event.currentTarget.href; }
-            throw e;
-        }
+        //TODO make sure user properties are loaded, otherwise it will always look like the users is not logged in
+        //update the meta properties
+        //$.mixpanelMeta();
+        //mixpanel.track('Entered Page', {'Page type': jQuery.metrics.determine_page_type()});
     });
-};
 
-//HEADER
-mixpanel.delegate_links("#siteHeader", "a", "Clicked Header Item",
-    function(element)
+    mixpanel.delegate_links = function (parent, selector, event_name, getProperties)
     {
-        var linkType = (element.title != '') ? element.title : element.text;
-        return { 'Header Item Type': linkType };
-    }
-);
+        $(parent || document.body).on('click', selector, function (event)
+        {
+            try
+            {
+                //get the specific properties for the event
+                var properties = _.isFunction(getProperties) ? getProperties(event.currentTarget) : {};
 
-//FOOTER
-mixpanel.delegate_links("#siteFooter", "a", "Clicked Footer Item",
-    function(element)
-    {
-        var linkType = (element.title != '') ? linkType = element.title : element.text;
-        return { 'Footer Item Type': linkType };
-    }
-);
+                var willOpenInNewTab = event.which === 2 || event.metaKey || event.currentTarget.target === '_blank';
+                properties.url = event.currentTarget.href;
 
-//CATALOG
-//Featured Views
-mixpanel.delegate_links(".featuredViews .featuredView", "a", "Clicked Featured View");
+                var isDefaultPrevented = event.isDefaultPrevented();
+                var callback = function()
+                {
+                    if (!willOpenInNewTab && !isDefaultPrevented)
+                    {
+                        window.location = properties.url;
+                    }
+                }
 
-//Catalog results
-mixpanel.delegate_links(".gridList .titleLine", "a", "Clicked Catalog Result",
-    function(element)
-    {
-        var linkNo = parseFloat($(element).closest('.item').find('.index .value').text());
-        var page = $(element).closest('.browseList').find('.pagination .active').text();
-        var pageNo = (page=='')? 1 : parseFloat(page);
-        return { 'Result Number': linkNo, 'Page Number': pageNo };
-    }
-);
+                if (!willOpenInNewTab)
+                {
+                    event.preventDefault();
+                }
+                //update the meta properties
+                $.mixpanelMeta();
 
+                //Track!
+                mixpanel.track(event_name, properties, callback);
+            }
+            catch(e)
+            {
+                if (!isDefaultPrevented)
+                { window.location = event.currentTarget.href; }
+                throw e;
+            }
+        });
+    };
 
-//SEARCH FACETS
-//View Types/Categories/Topics
-mixpanel.delegate_links(".facetSection", "a", "Used Search Facets",
-    function(element)
-    {
-        facetType = $(element).closest('.facetSection').find('> .title').text();
-        var linkName = element.text;
-        return { 'Facet Type': facetType, 'Facet Type Name': linkName };
-    }
-);
+    //HEADER
+    mixpanel.delegate_links("#siteHeader", "a", "Clicked Header Item",
+        function(element)
+        {
+            var linkType = (element.title != '') ? element.title : element.text;
+            return { 'Header Item Type': linkType };
+        }
+    );
 
-//SIDEBAR TRACKING
-mixpanel.delegate_links("#sidebarOptions", "a", "Clicked Sidebar Option",
-    function(element)
-    {
-        return {'Sidebar Name': element.title};
-    }
-);
+    //FOOTER
+    mixpanel.delegate_links("#siteFooter", "a", "Clicked Footer Item",
+        function(element)
+        {
+            var linkType = (element.title != '') ? linkType = element.title : element.text;
+            return { 'Footer Item Type': linkType };
+        }
+    );
 
-//Panes in sidebar (Needs a delegated .on since they are not present in the DOM from the beginning)
-mixpanel.delegate_links('#gridSidebar', 'a.headerLink', "Clicked Pane in Sidebar",
-    function(element)
-    {
-        return {'Pane Name': element.text};
-    }
-);
+    //CATALOG
+    //Featured Views
+    mixpanel.delegate_links(".featuredViews .featuredView", "a", "Clicked Featured View");
 
-//Render Type Options
-mixpanel.delegate_links("#renderTypeOptions", "a", "Changed Render Type Options",
-    function(element)
-    {
-        return {'Render Type': element.title};
-    }
-);
-
-
-
-
-
-
+    //Catalog results
+    mixpanel.delegate_links(".gridList .titleLine", "a", "Clicked Catalog Result",
+        function(element)
+        {
+            var linkNo = parseFloat($(element).closest('.item').find('.index .value').text());
+            var page = $(element).closest('.browseList').find('.pagination .active').text();
+            var pageNo = (page=='')? 1 : parseFloat(page);
+            return { 'Result Number': linkNo, 'Page Number': pageNo };
+        }
+    );
 
 
+    //SEARCH FACETS
+    //View Types/Categories/Topics
+    mixpanel.delegate_links(".facetSection", "a", "Used Search Facets",
+        function(element)
+        {
+            facetType = $(element).closest('.facetSection').find('> .title').text();
+            var linkName = element.text;
+            return { 'Facet Type': facetType, 'Facet Type Name': linkName };
+        }
+    );
 
+    //SIDEBAR TRACKING
+    mixpanel.delegate_links("#sidebarOptions", "a", "Clicked Sidebar Option",
+        function(element)
+        {
+            return {'Sidebar Name': element.title};
+        }
+    );
+
+    //Panes in sidebar (Needs a delegated .on since they are not present in the DOM from the beginning)
+    mixpanel.delegate_links('#gridSidebar', 'a.headerLink', "Clicked Pane in Sidebar",
+        function(element)
+        {
+            return {'Pane Name': element.text};
+        }
+    );
+
+    //Render Type Options
+    mixpanel.delegate_links("#renderTypeOptions", "a", "Changed Render Type Options",
+        function(element)
+        {
+            return {'Render Type': element.title};
+        }
+    );
+
+}

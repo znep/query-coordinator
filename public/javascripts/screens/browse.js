e34939fe1cb3fb7dc0953a3a8327d2c967c9e981
Jeff Scherpelz <jeff.scherpelz@socrata.com>
2013-12-02 16:40:11 -0800

    BUG CORE-1069: Hide catalog 'Make Private/Public' button if public is inherited
    QA: You should no longer see the catalog button to change permissions if
    the view is based on a public dataset
    
    Reviewed-by: Michael Chui <michael.chui@socrata.com>

--- public/javascripts/screens/browse.js
+++ public/javascripts/screens/browse.js
@@ -109,8 +109,12 @@ $(function()
                 '.permissions.button': function(v)
                     { return $.t('controls.browse.actions.permissions.change_button.' + (v.context.isPublic() ? 'public' : 'private') + '_html'); },
                 '.permissions.button@class+': function(v)
-                    { return v.context.hasRight('update_view') &&
-                        !v.context.isFederated() ? '' : 'hide'; },
+                    {
+                        var publicGrant = _.detect(v.context.grants || [], function(grant)
+                            { return _.include(grant.flags || [], 'public'); });
+                        return v.context.hasRight('update_view') && !v.context.isFederated() &&
+                            (!publicGrant || !publicGrant.inherited) ? '' : 'hide';
+                    },
                 '.delete.button@class+': function(v)
                     { return v.context.hasRight('delete_view') &&
                         !v.context.isFederated() ? '' : 'hide'; },

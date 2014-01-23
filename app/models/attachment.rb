97236e56cdbf43389c333eb07d2a6ad5349c0a5e
Michael Chui <michael.chui@socrata.com>
2014-01-23 11:36:40 -0800

    BUG CORE-1304: Add filename parameter so that the right filename is saved.
    QA: Retest.
    
    Reviewed-by: Giacomo Ferrari <giacomo.ferrari@socrata.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < Model
   def href(view_id)
-    data['assetId'].present? ? "/api/views/#{view_id}/files/#{assetId}?download=true" : "/api/assets/#{blobId}?download=true"
+    data['assetId'].present? ? "/api/views/#{view_id}/files/#{assetId}?download=true&filename=#{data['filename']}" : "/api/assets/#{blobId}?download=true"
   end
   
   def displayName

dd5b15c3fab5f9c95aeb5c9a92a6404761f64131
Randy Antler <randy.antler@socrata.com>
2014-01-23 12:30:14 -0800

    Explicitly set the configuration state prior to initializing the Dataset

--- app/models/displays/base.rb
+++ app/models/displays/base.rb
@@ -65,15 +65,15 @@ class Displays::Base
     def render_inline_setup_js(target_dom_id, context, debug = false)
       # Set common base variables communicating display configuration to JS
       js = <<END
+blist.namespace.fetch('blist.configuration');
+blist.configuration.development = #{Rails.env.development?};
+blist.configuration.useSoda2 = #{CurrentDomain.module_enabled?(:use_soda2)};
 #{render_dataset_setup_js}
 blist.assets = {libraries: #{debug ? ASSET_MAP.debug_javascripts : ASSET_MAP.javascripts}, stylesheets: #{@@app_helper.stylesheet_assets.to_json}};
 $(function()
 {
     blist.$container = $('##{target_dom_id}');
 });
-blist.namespace.fetch('blist.configuration');
-blist.configuration.development = #{Rails.env.development?};
-blist.configuration.useSoda2 = #{CurrentDomain.module_enabled?(:use_soda2)};
 END
       # When our JS is bundled, tinymce can't figure out where to load components
       # from; so we have to tell it before it loads up

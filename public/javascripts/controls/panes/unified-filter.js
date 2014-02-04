fc058332c9ac4349ad462f02c77378394bc5f1f3
Michael Chui <michael.chui@socrata.com>
2014-02-03 16:01:56 -0800

    BUG CORE-1414: column.renderType.subColumns is an object, not an array.
    QA: Retest.
    
    Reviewed-by: Jeff Scherpelz <jeff.scherpelz@socrata.com

--- public/javascripts/controls/panes/unified-filter.js
+++ public/javascripts/controls/panes/unified-filter.js
@@ -760,7 +760,7 @@
                 '.subcolumnName@class+': function()
                     {
                         return (!$.isBlank(column.renderType.subColumns) &&
-                            column.renderType.subColumns.length > 0) ? '' : 'hide';
+                            _.size(column.renderType.subColumns) > 0) ? '' : 'hide';
                     },
                 '.operator': function()
                     { return getOperatorName(column, metadata.subcolumn, metadata.operator); }
@@ -847,7 +847,7 @@
                 '.columnInfo@title': 'column.description!',
                 '.subcolumnName': function() { return (((column.renderType.subColumns || {})
                         [metadata.subcolumn] || {}).title || '').toLowerCase(); },
-                '.subcolumnName@class+': function() { return (!$.isBlank(column.renderType.subColumns) && column.renderType.subColumns.length > 0) ? '' : 'hide' },
+                '.subcolumnName@class+': function() { return (!$.isBlank(column.renderType.subColumns) && _.size(column.renderType.subColumns) > 0) ? '' : 'hide' },
                 '.operator': function()
                     { return getOperatorName(column, metadata.subcolumn, metadata.operator); }
             });

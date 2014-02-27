90a2b83f36094be00459eb5391661a0600e5b592
Michael Chui <michael.chui@socrata.com>
2014-02-27 14:47:46 -0800

    NOOP ONCALL-737: Move to using momentjs and time_ago_in_words for humane dates.
    Reviewed-by: Randy Antler <randy.antler@socrata.com>

--- app/helpers/humane_date_helper.rb
+++ app/helpers/humane_date_helper.rb
@@ -1,81 +1,13 @@
 module HumaneDateHelper
-  HUMANE_DATE_GRANULARITY = {
-    :minute => 0,
-    :hour => 3,
-    :day => 6,
-    :week => 9,
-    :month => 12,
-    :year => 15,
-    :century => 18
-  }
-  HUMANE_DATE_TIME_FORMATS = [
-    [60, 'just now'],
-    [120, '1 minute ago', '1 minute from now'], # 60*2
-    [3600, 'minutes', 60], # 60*60, 60
-    [3600, 'this hour'], # 60*60, 60
-    [7200, '1 hour ago', '1 hour from now'], # 60*60*2
-    [86400, 'hours', 3600], # 60*60*24, 60*60
-    [86400, 'today'], # 60*60*24, 60*60
-    [172800, 'yesterday', 'tomorrow'], # 60*60*24*2
-    [604800, 'days', 86400], # 60*60*24*7, 60*60*24
-    [604800, 'this week'], # 60*60*24*7, 60*60*24
-    [1209600, 'last week', 'next week'], # 60*60*24*7*4*2
-    [2419200, 'weeks', 604800], # 60*60*24*7*4, 60*60*24*7
-    [2419200, 'this month'], # 60*60*24*7*4, 60*60*24*7
-    [4838400, 'last month', 'next month'], # 60*60*24*7*4*2
-    [29030400, 'months', 2419200], # 60*60*24*7*4*12, 60*60*24*7*4
-    [29030400, 'this year'], # 60*60*24*7*4*12, 60*60*24*7*4
-    [58060800, 'last year', 'next year'], # 60*60*24*7*4*12*2
-    [2903040000, 'years', 29030400], # 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    [2903040000, 'this century'], # 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    [5806080000, 'last century', 'next century'] # 60*60*24*7*4*12*100*2
-  ]
+  extend ActionView::Helpers::DateHelper
 
-  def self.duration_in_words(from_date, to_date=Time.now, granularity=HUMANE_DATE_GRANULARITY[:day])
-    raise "Invalid time duration" unless to_date > from_date
-
-    duration = (to_date - from_date).to_i
-
-    humane_date((to_date - duration.seconds).to_i)
-  end
-
-  def self.humane_date(epoch_secs, granularity = HUMANE_DATE_GRANULARITY[:minute])
+  def self.humane_date(epoch_secs)
     if epoch_secs.nil? || epoch_secs == 0
       return 'None'
     end
-    dt = Time.now.tv_sec
-    seconds = (dt - epoch_secs)
-    token = 'ago'
-    list_choice = 1
+    other = Time.at(epoch_secs)
+    token = Time.now > other ? 'ago' : 'from now'
 
-    if (seconds < 0)
-      seconds = seconds.abs
-      token = 'from now'
-      list_choice = 2
-    end
-    
-    start = granularity
-    stop = HUMANE_DATE_TIME_FORMATS.length - 1
-    for i in start..stop do 
-      format = HUMANE_DATE_TIME_FORMATS[i]
-      
-      if (seconds < format[0])
-        if (format.length < 3)
-          out = format[1]
-        elsif (format[2].kind_of?(String))
-          out = format[list_choice]
-        else
-          out = (seconds/format[2]).floor.to_s + ' ' + format[1] + ' ' + token
-        end
-        break
-      end
-    end
-    
-    # overflow for centuries
-    if (seconds > 5806080000)
-        out = (seconds / 2903040000).floor + ' centuries ' + token
-    end
-    
-    out
+    "#{time_ago_in_words(other)} #{token}"
   end
 end
--- public/javascripts/util/humane-date.js
+++ public/javascripts/util/humane-date.js
@@ -22,46 +22,29 @@
 
 var humaneUtilNS = blist.namespace.fetch('blist.util.humaneDate');
 
-blist.util.humaneDate.MINUTE = 0;
-blist.util.humaneDate.HOUR = 3;
-blist.util.humaneDate.DAY = 6;
-blist.util.humaneDate.WEEK = 9;
-blist.util.humaneDate.MONTH = 12;
-blist.util.humaneDate.YEAR = 15;
-blist.util.humaneDate.CENTURY = 18;
+_.each(['minute', 'hour', 'day', 'week', 'month', 'year'], function(duration)
+{
+    blist.util.humaneDate[duration.toUpperCase()] = {
+        duration: moment.duration(1, duration).as('milliseconds'),
+        translation_key: 'current_' + duration + '_future'
+    };
+});
 
 blist.util.humaneDate.getFromDate = function (date_obj, granularity)
 {
-    if (granularity === undefined)
-    {
-        granularity = blist.util.humaneDate.MINUTE;
-    }
+    var date = moment(date_obj);
 
-    if (_.isNumber(date_obj)) { date_obj = new Date(date_obj); }
+    if (!date.isValid())
+    { return $.t('core.forms.none'); } // Yes, I'm cheating.
+    if (_.isUndefined(granularity))
+    { return date.fromNow(); }
 
-    var dt = new Date();
-    var seconds = (dt - date_obj) / 1000;
-    var isFuture = false;
+    var now = moment(), abs_diff = Math.abs(date.diff(now));
 
-    if (seconds < 0)
-    {
-        seconds = Math.abs(seconds);
-        isFuture = true;
-    }
-
-    var i = granularity;
-    var format = humaneUtilNS.timeFormats[i++];
-    while (format)
-    {
-        if (seconds < format[0])
-        {
-            return $.t('core.date_time.' + format[1] + (isFuture ? '_future' : '_past'),
-                    { time: Math.floor(seconds / (format[2] || 1)) });
-        }
-        format = humaneUtilNS.timeFormats[i++];
-    }
-
-    return $.t('core.date_time.unknown');
+    if (granularity.duration >= abs_diff)
+    { return $.t('core.date_time.' + granularity.translation_key); }
+    else
+    { return date.fromNow(); }
 };
 
 blist.util.humaneDate.getFromISO = function (date_str)
@@ -70,31 +53,6 @@ blist.util.humaneDate.getFromISO = function (date_str)
     return humaneUtilNS.getFromDate(new Date(time));
 };
 
-blist.util.humaneDate.timeFormats = [
-    [60, 'current_minute'],
-    [120, 'single_minute'], // 60*2
-    [3600, 'minutes', 60], // 60*60, 60
-    [3600, 'current_hour'], // 60*60, 60
-    [7200, 'single_hour'], // 60*60*2
-    [86400, 'hours', 3600], // 60*60*24, 60*60
-    [86400, 'current_day'], // 60*60*24, 60*60
-    [172800, 'single_day'], // 60*60*24*2
-    [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-    [604800, 'current_week'], // 60*60*24*7, 60*60*24
-    [1209600, 'single_week'], // 60*60*24*7*4*2
-    [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-    [2419200, 'current_month'], // 60*60*24*7*4, 60*60*24*7
-    [4838400, 'single_month'], // 60*60*24*7*4*2
-    [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-    [29030400, 'current_year'], // 60*60*24*7*4*12, 60*60*24*7*4
-    [58060800, 'single_year'], // 60*60*24*7*4*12*2
-    [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    [2903040000, 'current_century'], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    [5806080000, 'single_century'], // 60*60*24*7*4*12*100*2
-    [Number.MAX_VALUE, 'centuries', 2903040000], // ALL THE TIME!!!, 60*60*24*7*4*12*100
-];
-
-
 // If jQuery is included in the page, adds a jQuery plugin to handle it as well
 if ( typeof jQuery != "undefined" )
 {
@@ -102,10 +60,10 @@ if ( typeof jQuery != "undefined" )
     {
         return this.each(function()
                 {
-                    var date = humaneUtilNS.getFromISO(this.title);
-                    if ( date )
+                    var date = moment(this.title);
+                    if ( date.isValid() )
                     {
-                        jQuery(this).text( date );
+                        jQuery(this).text( date.fromNow() );
                     }
                 });
     };

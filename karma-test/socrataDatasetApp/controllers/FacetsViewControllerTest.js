5d1dc5690aac32de720e9b118003c27982470306
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-05-14 16:33:50 -0700

    NOOP: dataCards: Test for FacetsViewController.
    Reviewed-by: Randy Antler <randy.antler@socrata.com>

---
+++ karma-test/socrataDatasetApp/controllers/FacetsViewControllerTest.js
@@ -0,0 +1,43 @@
+describe("FacetsViewController", function() {
+  var createController;
+  beforeEach(module('socrataDatasetApp'));
+
+  it('should provide a correct facet set', inject(function($controller, $rootScope, $q, ViewFacet) {
+    //TODO parametrize controller on breaks, if needed.
+    function withFacets(facetsArray) {
+      var scope = $rootScope.$new();
+      var fakeViewId = 'fooo-baar';
+      function MockView(viewId) {
+        expect(viewId).to.equal(fakeViewId);
+        this.getFacetsAsync = _.constant($q.when(facetsArray));
+      };
+
+      var controller = $controller('FacetsViewController', {
+        $scope: scope,
+          viewId: fakeViewId,
+          focusedFacet: '',
+          View: MockView
+      });
+      scope.$apply();
+      expect(scope.view).to.be.instanceof(MockView);
+      expect(scope.facets).to.equal(facetsArray); // note ref equality.
+
+      return {
+        scope: scope,
+        controller: controller
+      };
+    };
+
+    var facets = [];
+    var scope = withFacets(facets).scope;
+    expect(scope.facets).to.be.empty;
+
+    facets = [new ViewFacet('asdf-fdsa')]
+    scope = withFacets(facets).scope;
+    expect(scope.facets).to.deep.equal(facets);
+
+    facets = _.map(_.times(10, _.uniqueId), function(id) { return new ViewFacet(id); })
+    scope = withFacets(facets).scope;
+    expect(scope.facets).to.deep.equal(facets);
+  }));
+});

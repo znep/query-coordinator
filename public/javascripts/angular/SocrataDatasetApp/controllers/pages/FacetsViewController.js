0f4859f6fd0e88fc51dee227ed2bad9bdfe5ab4a
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-05-14 14:58:58 -0700

    BUG: dataCards: Prettier placeholder cards-view and facets-view.
    Reviewed-by: Randy Antler <randy.antler@socrata.com>

--- public/javascripts/angular/SocrataDatasetApp/controllers/pages/FacetsViewController.js
+++ public/javascripts/angular/SocrataDatasetApp/controllers/pages/FacetsViewController.js
@@ -3,4 +3,7 @@ angular.module('socrataDatasetApp.controllers')
     function($scope, $location, View, viewId, focusedFacet) {
       $scope.view = new View(viewId);
       $scope.focusedFacet = focusedFacet;
+      $scope.view.getFacetsAsync().then(function(facets) {
+        $scope.facets = facets;
+      });
     });

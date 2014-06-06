201eee6135c8907096cde32833a73799be086796
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-06-06 11:23:39 -0700

    FEATURE: First pass of QFB/header/cards scroll behavior.

---
+++ public/javascripts/angular/dataCards/directives/stickyHeader.js
@@ -0,0 +1,44 @@
+angular.module('dataCards.directives').directive('stickyHeader', function(AngularRxExtensions) {
+  return {
+    restrict: 'A',
+    link: function($scope, element, attrs) {
+      function addWindowBindings() {
+        $(window).resize(onResize);
+        $(window).scroll(onScroll);
+        element.on('$destroy', function() {
+          removeWindowBindings();
+        });
+      };
+
+      function removeWindowBindings() {
+        $(window).off('resize', onResize);
+        $(window).off('resize', onScroll);
+      };
+
+      function onResize() {
+        $scope.$apply(function() {
+          pollLayout();
+        });
+      };
+
+      function onScroll() {
+        $scope.$apply(function() {
+          pollLayout();
+        });
+      };
+
+      function pollLayout() {
+        var windowScrollTop = $(document).scrollTop();
+        var windowHeight = $(window).height();
+        var headerPositionTop = element.offset().top;
+        var headerHeight = element.height();
+
+        $scope.headerIsStuck = windowScrollTop >= headerPositionTop;
+        $scope.availableContentHeight = headerHeight + Math.max(0, headerPositionTop - windowScrollTop);
+      };
+
+      addWindowBindings();
+      pollLayout();
+    }
+  }
+});

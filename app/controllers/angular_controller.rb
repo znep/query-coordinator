6289bd412aa69de4ffb937495e594ee3aad1059e
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-06-09 15:13:13 -0700

    FEATURE: Hide datacards behind feature flag

--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -1,8 +1,13 @@
 class AngularController < ActionController::Base
+  include ActionControllerExtensions
 
   layout 'angular'
 
   def serve_app
+    raise 'Need an app parameter' unless request[:app]
+
+    app_feature_flag = "app-#{request[:app]}"
+    render_error(404) unless FeatureFlags.derive(nil, request)[app_feature_flag]
   end
 
 end

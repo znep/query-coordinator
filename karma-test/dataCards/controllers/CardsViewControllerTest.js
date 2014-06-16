84c7a00522666e1d2029511b80ebcbe27b37bcde
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-06-16 15:12:49 -0700

    NOOP: Code cleanups.
    Reviewed-by: randy.antler@socrata.com
    QA: none

--- karma-test/dataCards/controllers/CardsViewControllerTest.js
+++ karma-test/dataCards/controllers/CardsViewControllerTest.js
@@ -195,4 +195,29 @@ describe("CardsViewController", function() {
     expect(scope.cardLinesBySizeGroup[2]).to.have.length(1); // One row in this group.
     expect(scope.cardSizeNamesInDisplayOrder).to.deep.equal(['1', '2']);
   });
+
+  describe('card layout', function() {
+    it('should provide an classForScreenPosition implementation that is left-weighted', function() {
+      var controllerHarness = makeController();
+      var cfsp = controllerHarness.scope.classForScreenPosition;
+      expect(cfsp).to.exist;
+
+      /* args are cardIndex, cardsInLine */
+      var left = 'onLeft';
+      var right = 'onRight';
+      expect(cfsp(0, 1)).to.equal(left);
+
+      expect(cfsp(0, 2)).to.equal(left);
+      expect(cfsp(1, 2)).to.equal(right);
+
+      expect(cfsp(0, 3)).to.equal(left);
+      expect(cfsp(1, 3)).to.equal(left);
+      expect(cfsp(2, 3)).to.equal(right);
+
+      expect(cfsp(0, 4)).to.equal(left);
+      expect(cfsp(1, 4)).to.equal(left);
+      expect(cfsp(2, 4)).to.equal(right);
+      expect(cfsp(3, 4)).to.equal(right);
+    });
+  });
 });
--- public/javascripts/angular/dataCards/controllers/pages/CardsViewController.js
+++ public/javascripts/angular/dataCards/controllers/pages/CardsViewController.js
@@ -89,7 +89,11 @@ angular.module('dataCards.controllers').controller('CardsViewController',
       };
     });
 
-    $scope.isCardOnRightSideOfScreen = function(cardIndex, numCardsInLine) {
-      return cardIndex >= Math.floor(numCardsInLine/2);
+    // Given a card's position in its line and the number of cards in its line,
+    // returns either the string 'onRight' or 'onLeft'. If the card is in the middle,
+    // 'onLeft' is returned.
+    $scope.classForScreenPosition = function(cardIndex, numCardsInLine) {
+      var onRight = cardIndex >= Math.ceil(numCardsInLine/2);
+      return onRight ? 'onRight' : 'onLeft';
     };
   });

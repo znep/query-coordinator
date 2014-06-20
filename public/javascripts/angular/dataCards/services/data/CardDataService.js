ef2b4babffb57ad72054db6dc09016cdbf6dedc9
Randy Antler <randy.antler@socrata.com>
2014-06-20 14:27:16 -0700

    NOOP: Remove localhost from URL for demo

--- public/javascripts/angular/dataCards/services/data/CardDataService.js
+++ public/javascripts/angular/dataCards/services/data/CardDataService.js
@@ -6,7 +6,7 @@ angular.module('dataCards.services').factory('CardDataService', function($q, $ht
       if (fieldName == 'location') {
         return $q.when([]);
       }
-      var url = 'https://localhost:9443/api/id/{1}.json?$query=select {0} as name, count(*) as value group by {0} order by count(*) desc limit 50'.format(fieldName, datasetId);
+      var url = '/api/id/{1}.json?$query=select {0} as name, count(*) as value group by {0} order by count(*) desc limit 50'.format(fieldName, datasetId);
       return $http.get(url, { cache: true }).then(function(response) {
         return _.map(response.data, function(item) {
           return { name: item.name, value: Number(item.value) };
@@ -19,7 +19,7 @@ angular.module('dataCards.services').factory('CardDataService', function($q, $ht
       if (fieldName == 'location') {
         return $q.when([]);
       }
-      var url = 'https://localhost:9443/api/id/{1}.json?$query=select {0} as name, count(*) as value group by {0} order by count(*) desc limit 50'.format(fieldName, datasetId);
+      var url = '/api/id/{1}.json?$query=select {0} as name, count(*) as value group by {0} order by count(*) desc limit 50'.format(fieldName, datasetId);
       return $http.get(url, { cache: true }).then(function(response) {
         return _.map(response.data, function(item) {
           return { name: item.name, value: Number(item.value) };

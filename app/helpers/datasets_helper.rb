993ae5bde0a9142fe00b6d2568f6796cd4bb41a9
Michael Chui <michael.chui@socrata.com>
2014-06-26 14:41:53 -0700

    NOOP CORE-2418: Remove map_publish.
    Reviewed-by: Michael Chui <michael.chui@socrata.com>

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -375,7 +375,7 @@ module DatasetsHelper
 
   def hide_map_create?
     view.is_unpublished? || view.is_alt_view? && !view.available_display_types.include?('map') ||
-      view.is_grouped? || !module_available?(:map_publish) || view.new_backend?
+      view.is_grouped? || view.new_backend?
   end
 
   def hide_cell_feed?

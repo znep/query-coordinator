587f2098cb236a4bf823c8e33f2c719462d98568
Chris Laidlaw <chris.laidlaw@socrata.com>
2014-07-09 16:53:07 -0700

    Final updates for CORE-2547 and CORE-2397 after escaping test harness dependency hell.

--- public/javascripts/angular/dataCards/services/data/CardDataService.js
+++ public/javascripts/angular/dataCards/services/data/CardDataService.js
@@ -31,10 +31,8 @@ angular.module('dataCards.services').factory('CardDataService', function($q, $ht
     // prepare for live GeoJSON data.
     getChoroplethRegions: function(fieldName, datasetId, shapefileId) {
       shapefileId = DeveloperOverrides.dataOverrideForShapefile(shapefileId) || shapefileId;
-      var url = '/api/views/{0}/rows.geojson'.format(shapefileId);
-//      url = '/datasets/geojson/ward_geojson.json';
+      url = '/datasets/geojson/ward_geojson.json';
       return $http.get(url, { cache: true }).then(function(response) {
-        console.log(response.data);
         return response.data;
       });
     },

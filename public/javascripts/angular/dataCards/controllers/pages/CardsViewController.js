28b7548d6585138f6925e0114d4e6741926e2056
Chris Laidlaw <chris.laidlaw@socrata.com>
2014-07-24 10:50:21 -0700

    FEATURE #1.11 Download dataset as CSV
    * Add functinality to derive dataset CSV download link from 4x4.
    * Update cards-view.html template to accept new CSV download link.
    
    Reviewed-by: giacomo.ferrari@socrata.com
    
    QA: None.

--- public/javascripts/angular/dataCards/controllers/pages/CardsViewController.js
+++ public/javascripts/angular/dataCards/controllers/pages/CardsViewController.js
@@ -54,7 +54,16 @@ angular.module('dataCards.controllers').controller('CardsViewController',
     $scope.bindObservable('pageName', page.observe('name').map(function(name) {
       return _.isUndefined(name) ? 'Untitled' : name;
     }));
+
     $scope.bindObservable('pageDescription', page.observe('description'));
+
+    $scope.bindObservable('datasetCSVDownloadURL', page.observe('dataset').map(function(dataset) {
+      if (dataset && dataset.hasOwnProperty('id')) {
+        return '/api/views/{0}/rows.csv?accessType=DOWNLOAD'.format(dataset.id);
+      } else {
+        return '#';
+      }
+    }));
 
     $scope.bindObservable('rowsOfCardsBySize', rowsOfCardsBySize);
     $scope.bindObservable('cardSizeNamesInDisplayOrder', rowsOfCardsBySize.map(function(sizedCards) {

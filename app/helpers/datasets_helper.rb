c2634afb753d3b967fd8005620540acfefc93ac3
Michael Chui <michael.chui@socrata.com>
2014-08-15 13:18:30 -0700

    BUG CORE-2877: Remove API Foundry from ESRI maps.
    QA: Load an ESRI map. API Foundry section should no longer be in sidebar.
    
    Reviewed-by: Michael Chui <michael.chui@socrata.com>

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -349,7 +349,7 @@ module DatasetsHelper
   def hide_api_foundry?
     !module_enabled?(:api_foundry) || (!view.is_blist? && !view.is_api?) ||
       !view.is_published? || !view.has_rights?('update_view') || !view.can_publish? ||
-      view.new_backend?
+      view.new_backend? || view.is_arcgis?
   end
 
   # Note: This controls visibility of columnOrder, not to be confused with the aptly named "manage.columnOrder" config. :-/

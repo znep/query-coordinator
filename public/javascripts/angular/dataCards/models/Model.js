b519a2c826d6b67586cdfdfa4212d48011bcc9e3
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-09-18 18:44:57 -0700

    NOOP: Improving some comments.

--- public/javascripts/angular/dataCards/models/Model.js
+++ public/javascripts/angular/dataCards/models/Model.js
@@ -84,11 +84,17 @@ angular.module('dataCards.models').factory('Model', function(ModelHelper) {
   };
 
   // Observes the given property.
+  // Will throw an exception if that property
+  // hasn't been defined on this Model.
   Model.prototype.observe = function(propertyName) {
     this._assertProperty(propertyName);
     return this._propertyTable[propertyName];
   };
 
+  // Sets the named property on this model to the given
+  // value.
+  // Will throw an exception if that property
+  // hasn't been defined on this Model.
   Model.prototype.set = function(propertyName, value) {
     this._assertProperty(propertyName);
     this._propertyTable[propertyName] = value;
@@ -99,19 +105,57 @@ angular.module('dataCards.models').factory('Model', function(ModelHelper) {
     });
   };
 
+  // Gets the current value of the named property on this model.
+  // Will throw an exception if that property
+  // hasn't been defined on this Model.
+  //
+  // NOTE: It is inadvisable to use this function other than
+  // in a few specific cases, such as reacting to a discrete
+  // user input event with a well-defined timing relationship
+  // to this Model. Strongly consider using observe() instead.
   Model.prototype.getCurrentValue = function(propertyName) {
     this._assertProperty(propertyName);
     return this._propertyTable[propertyName].value;
   };
 
+  // A sequence of all writes on this Model. This includes
+  // values written by defaults, lazy or otherwise.
+  // Writes are represented by objects of this form:
+  // {
+  //   model: <this model>,
+  //   property: <string name of changed property>,
+  //   newValue: <new value of the property>
+  // }
   Model.prototype.observeWrites = function() {
     return this._writes;
   };
 
+  // A sequence of all sets on this Model. Does NOT
+  // include any form of defaults, lazy or otherwise.
+  // In other words, only those sets coming from Model.set().
+  //
+  // Sets are represented by objects of this form:
+  // {
+  //   model: <this model>,
+  //   property: <string name of changed property>,
+  //   newValue: <new value of the property>
+  // }
   Model.prototype.observeSets = function() {
     return this._sets;
   };
 
+  // A sequence of all sets on this Model and its children.
+  // Children of a Model M are defined as all Models which
+  // are directly assigned to one of M's properties, or are
+  // in an array assigned to one of M's properties.
+  //
+  // In other words, this returns a merged sequence of all
+  // child models' observeSets().
+  //
+  // Sets are represented in the same way as in observeSets.
+  //
+  // Note that this does not include values from defaults,
+  // lazy or otherwise, just like observeSets.
   Model.prototype.observeSetsRecursive = function() {
     return Rx.Observable.merge(this._recursiveSets, this._sets);
   };

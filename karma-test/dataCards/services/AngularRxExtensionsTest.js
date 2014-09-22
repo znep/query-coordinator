0ec19236a0e16478d076c564c4b5a5591d81ec04
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-09-22 13:59:12 -0700

    NOOP: scope.bindObservable: Add (optional) onError and onCompleted handlers.

--- karma-test/dataCards/services/AngularRxExtensionsTest.js
+++ karma-test/dataCards/services/AngularRxExtensionsTest.js
@@ -46,62 +46,98 @@ describe('Angular RX Extensions', function() {
     $scope.$apply();
   });
 
-  it('should reflect changes inside and outside digest-apply cycles', function() {
-    //The Enrichment Center regrets to inform you that this next test is impossible. Make no attempt to solve it.
+  describe('bindObservable', function() {
+    it('should reflect changes inside and outside digest-apply cycles', function() {
+      //The Enrichment Center regrets to inform you that this next test is impossible. Make no attempt to solve it.
 
-    var $scope = _$rootScope.$new(true);
-    _extensions.install($scope);
+      var $scope = _$rootScope.$new(true);
+      _extensions.install($scope);
 
-    var testSubjectName = new Rx.Subject();
-    var testSubjectPromisedCake = new Rx.Subject();
-    var testSubjectCandescing = new Rx.Subject();
+      var testSubjectName = new Rx.Subject();
+      var testSubjectPromisedCake = new Rx.Subject();
+      var testSubjectCandescing = new Rx.Subject();
 
-    $scope.bindObservable('testSubjectName', testSubjectName);
-    $scope.bindObservable('testSubjectPromisedCake', testSubjectPromisedCake);
-    $scope.bindObservable('cake', testSubjectPromisedCake.combineLatest(testSubjectCandescing, function(promised, candescing) {
-      return promised && candescing; // All RX technologies remain safely operational up to 4000 degrees Kelvin.
-    }));
+      $scope.bindObservable('testSubjectName', testSubjectName);
+      $scope.bindObservable('testSubjectPromisedCake', testSubjectPromisedCake);
+      $scope.bindObservable('cake', testSubjectPromisedCake.combineLatest(testSubjectCandescing, function(promised, candescing) {
+        return promised && candescing; // All RX technologies remain safely operational up to 4000 degrees Kelvin.
+      }));
 
-    expect($scope).not.to.have.property('testSubjectName');
-    expect($scope).not.to.have.property('testSubjectPromisedCake');
-    expect($scope).not.to.have.property('cake');
+      expect($scope).not.to.have.property('testSubjectName');
+      expect($scope).not.to.have.property('testSubjectPromisedCake');
+      expect($scope).not.to.have.property('cake');
 
-    $scope.$apply(function() {
-      testSubjectName.onNext('Chell');
+      $scope.$apply(function() {
+        testSubjectName.onNext('Chell');
+      });
+      expect($scope).to.have.property('testSubjectName').that.equals('Chell');
+      expect($scope).not.to.have.property('testSubjectPromisedCake');
+      expect($scope).not.to.have.property('cake');
+
+      testSubjectPromisedCake.onNext(true);
+      expect($scope).to.have.property('testSubjectName').that.equals('Chell');
+      expect($scope).to.have.property('testSubjectPromisedCake').that.is.true;
+      expect($scope).not.to.have.property('cake');
+
+      testSubjectCandescing.onNext(true);
+      expect($scope).to.have.property('testSubjectName').that.equals('Chell');
+      expect($scope).to.have.property('testSubjectPromisedCake').that.is.true;
+      expect($scope).to.have.property('cake').that.is.true;
     });
-    expect($scope).to.have.property('testSubjectName').that.equals('Chell');
-    expect($scope).not.to.have.property('testSubjectPromisedCake');
-    expect($scope).not.to.have.property('cake');
 
-    testSubjectPromisedCake.onNext(true);
-    expect($scope).to.have.property('testSubjectName').that.equals('Chell');
-    expect($scope).to.have.property('testSubjectPromisedCake').that.is.true;
-    expect($scope).not.to.have.property('cake');
+    it('should apply the value returned by onError', function() {
+      var $scope = _$rootScope.$new(true);
+      _extensions.install($scope);
+      var testSubject= new Rx.Subject();
 
-    testSubjectCandescing.onNext(true);
-    expect($scope).to.have.property('testSubjectName').that.equals('Chell');
-    expect($scope).to.have.property('testSubjectPromisedCake').that.is.true;
-    expect($scope).to.have.property('cake').that.is.true;
-  });
+      $scope.bindObservable('testProperty', testSubject, function(err) {
+        return 'error:'+err;
+      });
 
-  it('should reject bad arguments', function() {
-    var $scope = _$rootScope.$new(true);
-    _extensions.install($scope);
+      testSubject.onError('fakeError');
+      expect($scope.testProperty).to.equal('error:fakeError');
+    });
 
-    expect(function() {
-      $scope.bindObservable();
-    }).to.throw();
+    it('should apply the value returned by onCompleted', function() {
+      var $scope = _$rootScope.$new(true);
+      _extensions.install($scope);
+      var testSubject= new Rx.Subject();
 
-    expect(function() {
-      $scope.bindObservable('string');
-    }).to.throw();
+      $scope.bindObservable('testProperty', testSubject, undefined, function() {
+        return 'last value';
+      });
 
-    expect(function() {
-      $scope.bindObservable(123, new Rx.Subject());
-    }).to.throw();
+      testSubject.onCompleted();
+      expect($scope.testProperty).to.equal('last value');
+    });
 
-    expect(function() {
-      $scope.bindObservable('string', 'string');
-    }).to.throw();
+    it('should reject bad arguments', function() {
+      var $scope = _$rootScope.$new(true);
+      _extensions.install($scope);
+
+      expect(function() {
+        $scope.bindObservable();
+      }).to.throw();
+
+      expect(function() {
+        $scope.bindObservable('string');
+      }).to.throw();
+
+      expect(function() {
+        $scope.bindObservable(123, new Rx.Subject());
+      }).to.throw();
+
+      expect(function() {
+        $scope.bindObservable('string', 'string');
+      }).to.throw();
+
+      expect(function() {
+        $scope.bindObservable('prop', new Rx.Subject(), 1);
+      }).to.throw();
+
+      expect(function() {
+        $scope.bindObservable('prop', new Rx.Subject(), function(){}, {});
+      }).to.throw();
+    });
   });
 });

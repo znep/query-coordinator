ab148c43526ece1a40ae4b61c76db79456036686
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-09-29 19:21:10 -0700

    NOOP: bindFromModel directive.
    QA: none.
    Reviewed-by: randy.antler@socrata.com

---
+++ karma-test/dataCards/directives/bindFromModelTest.js
@@ -0,0 +1,77 @@
+describe('bindFromModel directive', function() {
+  var Model, $compile, $rootScope;
+  beforeEach(module('dataCards'));
+  beforeEach(inject(function($injector) {
+    Model = $injector.get('Model');
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
+  }));
+
+  it('should throw with invalid keypaths.', function() {
+    expect(function() {
+      $compile('<div bind-from-model></div>')($rootScope.$new());
+    }).to.throw();
+    expect(function() {
+      $compile('<div bind-from-model="foo"></div>')($rootScope.$new());
+    }).to.throw();
+  });
+
+  it('should throw an exception if binding against a non-existing property', function() {
+    var scope = $rootScope.$new();
+    var el = $compile('<div bind-from-model="myModel.propA"></div>')(scope);
+
+    scope.myModel = new Model();
+
+    expect(function() {
+      scope.$digest();
+    }).to.throw(/propA/);
+    
+  });
+
+  it('should reflect the value of a single model property in the scope', function() {
+    var scope = $rootScope.$new();
+    var el = $compile('<div bind-from-model="myModel.propA"></div>')(scope);
+
+    var model = new Model();
+    model.defineObservableProperty('propA', 5);
+    scope.myModel = model;
+    scope.$digest();
+
+    // Remember the bindings happen on the inner scope.
+    expect(el.scope().propA).to.equal(5);
+    model.set('propA', 10);
+    expect(el.scope().propA).to.equal(10);
+  });
+
+  it('should reflect the value of multiple model properties in the scope', function() {
+    var scope = $rootScope.$new();
+    var el = $compile('<div bind-from-model="myModel.propA, myModel.propB"></div>')(scope);
+
+    var model = new Model();
+    model.defineObservableProperty('propA', 5);
+    model.defineObservableProperty('propB', 'a');
+    scope.myModel = model;
+    scope.$digest();
+
+    // Remember the bindings happen on the inner scope.
+    expect(el.scope().propA).to.equal(5);
+    expect(el.scope().propB).to.equal('a');
+  });
+
+  it('should reflect the value of multiple model properties on multiple models in the scope', function() {
+    var scope = $rootScope.$new();
+    var el = $compile('<div bind-from-model="myModelA.propA, myModelB.propB"></div>')(scope);
+
+    var modelA = new Model();
+    var modelB = new Model();
+    modelA.defineObservableProperty('propA', 50);
+    modelB.defineObservableProperty('propB', 'fifty');
+    scope.myModelA = modelA;
+    scope.myModelB = modelB;
+    scope.$digest();
+
+    // Remember the bindings happen on the inner scope.
+    expect(el.scope().propA).to.equal(50);
+    expect(el.scope().propB).to.equal('fifty');
+  });
+});

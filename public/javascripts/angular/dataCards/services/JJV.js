938a153a7fdab37210d8c9bd282a0f6de3678b1c
Jerjou Cheng <jerjou.cheng@socrata.com>
2014-10-10 17:48:58 -0700

    NOOP: Add comments to SortedTileLayout.
    Reviewed-by: giacomo.ferrari@socrata.com

--- public/javascripts/angular/dataCards/services/JJV.js
+++ public/javascripts/angular/dataCards/services/JJV.js
@@ -1,3 +1,14 @@
 angular.module('dataCards.services').factory('JJV', function() {
-  return window.jjv();
+  var jjv = window.jjv();
+  /**
+   * The card model was storing levels as Strings, but telling jjv they were integers.
+   */
+  jjv.addTypeCoercion('integer', function(s) {
+    if (_.isString(s)) {
+      return parseInt(s, 10);
+    }
+    return s;
+  });
+
+  return jjv;
 });
--- public/javascripts/angular/dataCards/services/SortedTileLayout.js
+++ public/javascripts/angular/dataCards/services/SortedTileLayout.js
@@ -1,27 +1,44 @@
-// SortedTileLayout is a facility to group a list of tiles into tiers, each tier being
-// composed of a list of lines having a particular number of columns.
-// If the number of items does not evenly divide into the number of columns
-// for a given tier, the items are rebalanced evenly across lines.
+/**
+ * SortedTileLayout is a facility to group a list of tiles into tiers, each tier being
+ * composed of a list of lines having a particular number of columns.  If the number of
+ * items does not evenly divide into the number of columns for a given tier, the items are
+ * rebalanced evenly across lines.
+ */
 angular.module('dataCards.services').factory('SortedTileLayout', function() {
   var defaultOptions = {
+    // Key: The name of the tier
+    // Value: The max number of items per row in that tier
     tiers: {
-      '1': 2,
-      '2': 3,
-      '3': 4,
-      '4': 1
+      1: 2,
+      2: 3,
+      3: 4,
+      4: 1
     },
     tierAccessor: _.property('cardSize')
   };
 
 
-  var splitRows = function(maxPerRow, items) {
-    if (maxPerRow > 4) { throw new Error("Only up to four columns supported for this layout."); }
+  /**
+   * Splits the given items into rows, where each row can contain at max maxPerRow items.
+   *
+   * @param {number} maxPerRow The maximum number of items one row can hold. No greater
+   * than 4 right now.
+   * @param {object[]} items An array of items.
+   *
+   * @return {object[][]} An array of rows, where each row is an array of items.
+   */
+  function splitRows(maxPerRow, items) {
+    if (maxPerRow > 4) {
+      throw new Error("Only up to four columns supported for this layout.");
+    }
 
     var count = items.length;
+    if (count <= maxPerRow) {
+      return [items];
+    }
+
     var numFullRows = Math.floor(count / maxPerRow);
     var remainder = count % maxPerRow;
-
-    if (count <= maxPerRow) { return [items]; }
 
     var outputRows = [];
     if (remainder === 0) {
@@ -29,52 +46,80 @@ angular.module('dataCards.services').factory('SortedTileLayout', function() {
         outputRows.push(items.slice(i * maxPerRow, (i + 1) * maxPerRow));
       }
     } else {
+      // Try to fill out the firstRow (that's not full) by borrowing from the full rows.
+      // 'gap' is the number of items to borrow from the other rows, to get the firstRow
+      // relatively full without becoming full (ie if we become full by making the
+      // rows below us non-full, that'd look top-heavy. Stay non-full, because /someone/
+      // has to)
       var gap = maxPerRow - remainder - 1;
-      var reducedRows = Math.min(numFullRows, gap);
 
-      var firstRow = _.first(items, remainder + reducedRows);
+      // Borrow one item from each of the full rows until we fill out the firstRow.
+      var numItemsBorrowed = Math.min(numFullRows, gap);
 
-      var restRows = _.rest(items, remainder + reducedRows);
+      var firstRowLength = remainder + numItemsBorrowed
+      var firstRow = _.first(items, firstRowLength);
+      var restRows = _.rest(items, firstRowLength);
 
-      var reducedRowItems = _.first(restRows, (maxPerRow - 1) * reducedRows);
-      var fullRowItems = _.rest(restRows, (maxPerRow - 1) * reducedRows);
+      // We borrowed one item from each of the reduced rows, so now their length is one
+      // less.
+      var reducedRowLength = maxPerRow - 1;
+      var numReducedRowItems = reducedRowLength * numItemsBorrowed;
+      var reducedRowItems = _.first(restRows, numReducedRowItems);
+      var fullRowItems = _.rest(restRows, numReducedRowItems);
 
       outputRows.push(firstRow);
 
-      for(var i=0; i<reducedRows; i++) {
-        outputRows.push(reducedRowItems.slice(i * (maxPerRow - 1), (i + 1) * (maxPerRow - 1)));
+      // Slice up the reduced rows data into its rows of length numItemsBorrowed
+      for(var i=0; i<numItemsBorrowed; i++) {
+        outputRows.push(reducedRowItems.slice(
+          i * reducedRowLength,
+          (i + 1) * reducedRowLength));
       }
 
-      for(var i=0; i<numFullRows - reducedRows; i++) {
+      // Now fill up
+      for(var i = 0, fullRowsRemaining = numFullRows - numItemsBorrowed;
+          i < fullRowsRemaining;
+          i++) {
         outputRows.push(fullRowItems.slice(i * maxPerRow, (i + 1) * maxPerRow));
       }
     }
 
     return outputRows;
-  };
+  }
 
-  // Constructor. Options has any of these optional keys:
-  // tiers: Hash. Key is arbitrary tier name. Value is the natural column count for that tier.
-  // tierAccessor. Function. Given an item passed to doLayout, returns the tier name.
-  //               The tier name must be in the tiers hash.
+  /**
+   * @param {object} options has any of these optional keys:
+   * @property {object} tiers Key is arbitrary tier name. Value is the natural column
+   *   count for that tier.
+   * @property {function} tierAccessor Given an item passed to doLayout, returns the tier
+   *   name. The tier name must be in the tiers hash.
+   * @constructor
+   */
   function SortedTileLayout(options) {
-    if (!_.isUndefined(options) && !_.isObject(options)) { throw new Error('Options object expected.') };
+    if (!_.isUndefined(options) && !_.isObject(options)) {
+      throw new Error('Options object expected.');
+    }
 
     this.options = $.extend({}, defaultOptions, options);
   };
 
-  // Given an enumeration of item objects, returns an object whose keys
-  // are the tier names, and the corresponding values are arrays
-  // of lines (which are themselves arrays containing objects
-  // from the items enumeration).
+  /**
+   * Given an enumeration of item objects, returns an object whose keys are the tier
+   * names, and the corresponding values are arrays of lines (which are themselves arrays
+   * containing objects from the items enumeration).
+   *
+   * @param {object[]} items An enumeration of item objects.
+   *
+   * @return {object} Where the key is the tierId, and the value is an array of rows.
+   */
   SortedTileLayout.prototype.doLayout = function(items) {
-    var self = this;
+    // tiers is a hash of tierId -> itemsInThatTier[]
     var tiers = _.groupBy(items, this.options.tierAccessor);
 
-    var out = _.reduce(tiers, function(acc, tierItems, tierId) {
-      acc[tierId] = splitRows(self.options.tiers[tierId], tierItems);
-      return acc;
-    }, {});
+    var out = {};
+    _.forOwn(tiers, function(tierItems, tierId) {
+      out[tierId] = splitRows(this.options.tiers[tierId], tierItems);
+    }, this);
 
     return out;
   };

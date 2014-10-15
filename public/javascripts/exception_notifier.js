b9d0b0e31459c74ec48d2436a05972c3a4f32851
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2014-10-15 16:39:02 -0700

    FEATURE: Log console errors to airbrake. QA: none Reviewed-by: randy.antler@socrata.com

--- public/javascripts/exception_notifier.js
+++ public/javascripts/exception_notifier.js
@@ -1,45 +1,81 @@
-angular.module('exceptionNotifier', []).
-  factory('$exceptionHandler', function ($log) {
-    return function(exception, cause) {
-      var stackTrace;
-      var exceptionInformation;
+(function() {
+  function pushExceptionToAirbrake(exception, cause) {
+    var stackTrace;
+    var exceptionInformation;
 
-      try {
-        stackTrace = new StackTrace().fromError(exception).join("\n");
-      } catch(stackTraceError) { }
+    try {
+      stackTrace = new StackTrace().fromError(exception).join("\n");
+    } catch(stackTraceError) { }
 
-      exceptionInformation = { error: exception, context: {} };
+    exceptionInformation = { error: exception, context: {} };
 
-      if (cause) {
-        exceptionInformation.context.cause = cause;
-      }
-
-      if (!stackTrace && exception.stack) {
-        stackTrace = exception.stack;
-      }
-
-      if (stackTrace) {
-        exceptionInformation.context.stackTrace = stackTrace;
-      }
-
-      Airbrake.push(exceptionInformation);
-      $log.error(exception);
-    };
-  }).
-  factory('httpErrorInterceptor', function ($q) {
-    return {
-      responseError: function responseError(rejection) {
-        Airbrake.push({
-          error: new Error('HTTP response error'),
-          context: {
-            config: rejection.config,
-            status: rejection.status
-          }
-        });
-        return $q.reject(rejection);
-      }
+    if (cause) {
+      exceptionInformation.context.cause = cause;
     }
-  }).
-  config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('httpErrorInterceptor');
-  }]);
+
+    if (!stackTrace && exception.stack) {
+      stackTrace = exception.stack;
+    }
+
+    if (stackTrace) {
+      exceptionInformation.context.stackTrace = stackTrace;
+    }
+
+    Airbrake.push(exceptionInformation);
+  };
+
+  angular.module('exceptionNotifier', []).
+    factory('$exceptionHandler', function () {
+      return function(exception, cause) {
+        pushExceptionToAirbrake(exception, cause);
+        $log.error(exception);
+      };
+    }).
+    factory('httpErrorInterceptor', function ($q) {
+      return {
+        responseError: function responseError(rejection) {
+          Airbrake.push({
+            error: new Error('HTTP response error'),
+            context: {
+              config: rejection.config,
+              status: rejection.status
+            }
+          });
+          return $q.reject(rejection);
+        }
+      }
+    }).
+    config(function($httpProvider, $provide) {
+      // Decorate the logger to log errors to Airbrake.
+      $provide.decorator('$log', function($delegate) {
+        var airbrakeLogger = Object.create($delegate);
+        airbrakeLogger.error = function logErrorToAirbrake() {
+          $delegate.error.apply($delegate, arguments);
+
+          // Use the logged exception if possible.
+          var loggedException = _.find(arguments, function(arg) {
+            return arg instanceof Error;
+          });
+
+          var nonExceptionArgs = _.filter(arguments, function(arg) {
+            return !(arg instanceof Error);
+          });
+
+          if (loggedException) {
+            pushExceptionToAirbrake(
+              loggedException,
+              nonExceptionArgs.join(', ')
+            );
+          } else {
+            pushExceptionToAirbrake(
+              new Error(nonExceptionArgs.join(', '))
+            );
+          }
+        };
+
+        return airbrakeLogger;
+      });
+
+      $httpProvider.interceptors.push('httpErrorInterceptor');
+    });
+})();

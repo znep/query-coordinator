b2f83aeb2e1d34764162845167672180c82b6b74
Ragan Webber <ragan.webber@socrata.com>
2014-11-21 08:42:08 -0800

    FEATURE: CORE-2521 Add aggregation selection tests
    * Add new aggregationChooser test suite
    * Add test case in CardDataService test suite to exercise aggregation
    
    QA: Nothing to test.

---
+++ karma-test/dataCards/directives/aggregationChooserTest.js
@@ -0,0 +1,241 @@
+describe('<aggregation-chooser/>', function() {
+  var scope;
+  var $window;
+  var testHelpers;
+  var rootScope;
+  var Model;
+  var Page;
+  var AngularRxExtensions;
+  var $q;
+
+  beforeEach(function() {
+    module('/angular_templates/dataCards/aggregationChooser.html');
+    module('socrataCommon.services');
+    module('dataCards.directives');
+    module('dataCards.services');
+    module('dataCards.models');
+    module('test');
+  });
+
+  beforeEach(function() {
+    module(function($provide) {
+      var mockCardDataService = {
+        getData: function(){ return $q.when([]);},
+        getChoroplethRegions: function() { return {then: _.noop}; },
+        getRowCount: function() { return {then: _.noop}; },
+        getTimelineDomain: function() { return {then: _.noop}; }
+      };
+      $provide.value('CardDataService', mockCardDataService);
+
+      $provide.value('Card', _.noop);
+    });
+  });
+
+  beforeEach(inject(function($injector) {
+    $window = $injector.get('$window');
+    testHelpers = $injector.get('testHelpers');
+    rootScope = $injector.get('$rootScope');
+    Model = $injector.get('Model');
+    Page = $injector.get('Page');
+    AngularRxExtensions = $injector.get('AngularRxExtensions');
+    $q = $injector.get('$q');
+  }));
+
+  afterEach(function(){
+    testHelpers.TestDom.clear();
+  });
+
+  var DEFAULT_ROW_DISPLAY_UNIT = 'unique row unit';
+
+  function createDatasetModel(options) {
+    options = options || {};
+    _.defaults(options, {
+      rowDisplayUnit: DEFAULT_ROW_DISPLAY_UNIT,
+      columns: {
+        // Define some columns of different types, so we can create different types of cards
+        statBar_column: {
+          name: 'statBar_column',
+          title: 'test column title',
+          description: 'test column description',
+          logicalDatatype: 'amount',
+          physicalDatatype: 'number',
+          importance: 2
+        },
+        statBar_column2: {
+          name: 'statBar_column2',
+          title: 'second test column title',
+          description: 'second test column description',
+          logicalDatatype: 'amount',
+          physicalDatatype: 'number',
+          importance: 2
+        }
+      }
+    })
+    var datasetModel = new Model();
+    datasetModel.id = 'rant-lerz';
+    datasetModel.fieldName = 'ward';
+    datasetModel.defineObservableProperty('rowDisplayUnit', options.rowDisplayUnit);
+    datasetModel.defineObservableProperty('columns', options.columns);
+
+    return datasetModel;
+  }
+
+  function createPageModel(datasetModel, options) {
+    options = options || {};
+    _.defaults(options, {
+      primaryAmountField: null,
+      primaryAggregation: null
+    });
+    var pageModel = new Page('asdf-fdsa');
+    pageModel.set('dataset', datasetModel);
+    pageModel.set('baseSoqlFilter', null);
+    pageModel.set('primaryAmountField', options.primaryAmountField);
+    pageModel.set('primaryAggregation', options.primaryAggregation);
+    pageModel.set('cards', []);
+
+    return pageModel;
+  }
+
+  function createModels(options) {
+    options = options || {};
+    var datasetModel = createDatasetModel({ rowDisplayUnits: options.rowDisplayUnits, columns: options.columns });
+    var pageModel = createPageModel(datasetModel, {
+      primaryAmountField: options.primaryAmountField,
+      primaryAggregation: options.primaryAggregation
+    });
+
+    return {
+      dataset: datasetModel,
+      page: pageModel
+    };
+  }
+
+  function createElement(html, baseScope) {
+    var directive;
+    var element = angular.element(html);
+
+    inject(function($compile, $rootScope) {
+      scope = $rootScope.$new();
+      _.extend(scope, baseScope);
+      directive = $compile(element)(scope);
+      scope.$digest();
+    });
+    return directive;
+  }
+
+  function generateFakeMouseMove(clientX, clientY, target) {
+    var ev = document.createEvent('HTMLEvents');
+    ev.initEvent('mousemove', true, true);
+    ev.clientX = clientX;
+    ev.clientY = clientY;
+    ev.target = target;
+    return ev;
+  };
+
+  it('should exist when created', function() {
+    var models = createModels();
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    expect(subjectUnderTest.is('aggregation-chooser')).to.be.true;
+    expect(subjectUnderTest.find('.aggregation-chooser-static-label').text().toLowerCase()).to.contain(DEFAULT_ROW_DISPLAY_UNIT);
+    expect(subjectUnderTest.find('.aggregation-chooser-trigger').text().toLowerCase()).to.contain(DEFAULT_ROW_DISPLAY_UNIT);
+  });
+
+  it('should toggle visibility when clicked', function() {
+    var models = createModels();
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    testHelpers.fireMouseEvent(subjectUnderTest.find('.aggregation-chooser-trigger')[0], 'click');
+    expect(subjectUnderTest.isolateScope().panelActive).to.be.true;
+    expect(subjectUnderTest.find('.tool-panel-main').hasClass('active')).to.be.true;
+    testHelpers.fireMouseEvent(subjectUnderTest.find('.aggregation-chooser-trigger')[0], 'click');
+    expect(subjectUnderTest.isolateScope().panelActive).to.be.false;
+    expect(subjectUnderTest.find('.tool-panel-main').hasClass('active')).to.be.false;
+  });
+
+  it('should close when clicked outside of it', function() {
+    var models = createModels();
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    testHelpers.TestDom.append(subjectUnderTest);
+    rootScope.$apply(function() {
+      subjectUnderTest.isolateScope().panelActive = true;
+    });
+    testHelpers.fireMouseEvent($('#test-root')[0], 'click');
+    expect(subjectUnderTest.isolateScope().panelActive).to.be.false;
+    expect(subjectUnderTest.find('.tool-panel-main').hasClass('active')).to.be.false;
+  });
+
+  it('should highlight when options are hovered', function() {
+    var models = createModels({ primaryAggregation: 'sum', primaryAmountField: 'statBar_column'});
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    testHelpers.TestDom.append(subjectUnderTest);
+    rootScope.$apply(function() {
+      subjectUnderTest.isolateScope().panelActive = true;
+    });
+    var body = document.getElementsByTagName('body')[0];
+    var hoverTarget = subjectUnderTest.find('.aggregation-functions [data-aggregation-type="count"]');
+    testHelpers.fireMouseEvent(hoverTarget[0], 'mousemove');
+    expect(subjectUnderTest.find('.aggregation-functions [data-aggregation-type="count"]').hasClass('active')).to.be.true;
+    expect(subjectUnderTest.find('.aggregation-columns [data-aggregation-type="count"]').hasClass('active')).to.be.true;
+  });
+
+  it('should set attributes on the Page model correctly', function() {
+    var models = createModels();
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    rootScope.$apply(function() {
+      subjectUnderTest.isolateScope().panelActive = true;
+    });
+    expect(models.page.getCurrentValue('primaryAggregation')).to.equal(null);
+    expect(models.page.getCurrentValue('primaryAmountField')).to.equal(null);
+    testHelpers.fireMouseEvent(subjectUnderTest.find('[data-aggregation-type="sum"]')[0], 'click');
+    expect(models.page.getCurrentValue('primaryAggregation')).to.equal('sum');
+    expect(models.page.getCurrentValue('primaryAmountField')).to.equal('statBar_column');
+    testHelpers.fireMouseEvent(subjectUnderTest.find('[data-column-id="statBar_column2"]')[0], 'click');
+    expect(models.page.getCurrentValue('primaryAggregation')).to.equal('sum');
+    expect(models.page.getCurrentValue('primaryAmountField')).to.equal('statBar_column2');
+    testHelpers.fireMouseEvent(subjectUnderTest.find('[data-aggregation-type="count"]')[0], 'click');
+    expect(models.page.getCurrentValue('primaryAggregation')).to.equal('count');
+    expect(models.page.getCurrentValue('primaryAmountField')).to.equal(null);
+  });
+
+  it('should select the appropriate aggregation function if one is present', function() {
+    var models = createModels({ primaryAggregation: 'sum', primaryAmountField: 'statBar_column' });
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    expect(subjectUnderTest.find('[data-aggregation-type="sum"]').hasClass('active')).to.be.true;
+    expect(subjectUnderTest.find('[data-column-id="statBar_column"]').hasClass('active')).to.be.true;
+  });
+
+  it('should display a flyout for invalid selections', function() {
+    var models = createModels();
+    var subjectUnderTest = createElement(
+      '<aggregation-chooser page="page"></aggregation-chooser>',
+      { page: models.page }
+    );
+    testHelpers.TestDom.append(subjectUnderTest);
+    rootScope.$apply(function() {
+      subjectUnderTest.isolateScope().panelActive = true;
+    });
+    var body = document.getElementsByTagName('body')[0];
+    var hoverTarget = subjectUnderTest.find('.aggregation-columns .disabled');
+    testHelpers.fireMouseEvent(hoverTarget[0], 'mousemove');
+    var flyout = $('#uber-flyout');
+    expect(flyout.length).to.equal(1);
+    expect(flyout.text().toLowerCase()).to.contain('this column cannot be used with a');
+  });
+});
--- karma-test/dataCards/services/data/CardDataServiceTest.js
+++ karma-test/dataCards/services/data/CardDataServiceTest.js
@@ -63,6 +63,16 @@ describe("CardDataService", function() {
       $httpBackend.flush();
     });
 
+    it('should pass through the aggregation options', function() {
+      $httpBackend.expectGET(toUriRegex('/api/id/{0}.json?$query=select fakeNumberColumn as name, sum(fakeNumberColumn) as value  group by fakeNumberColumn order by sum(fakeNumberColumn) desc limit 200'.format(fake4x4)));
+      $httpBackend.expectGET(toUriRegex('/api/id/{0}.json?$query=select fakeNumberColumn as name, sum(fakeNumberColumn) as value where MAGICAL_WHERE_CLAUSE group by fakeNumberColumn order by sum(fakeNumberColumn) desc limit 200'.format(fake4x4)));
+
+      CardDataService.getData('fakeNumberColumn', fake4x4, null, { aggregation: 'sum', field: 'fakeNumberColumn' });
+      CardDataService.getData('fakeNumberColumn', fake4x4, 'MAGICAL_WHERE_CLAUSE', { aggregation: 'sum', field: 'fakeNumberColumn' });
+
+      $httpBackend.flush();
+    });
+
     it('should reject the promise on 404', function(done) {
       fakeDataRequestHandler.respond(404, []);
       assertReject(CardDataService.getData('fakeNumberColumn', fake4x4), done);

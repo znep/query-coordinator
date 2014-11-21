ab6a54441b0c385644c1cd61550946d2702aec28
Chris Laidlaw <chris.laidlaw@socrata.com>
2014-11-21 13:55:52 -0800

    BUG: Make protocol buffer requests work for point maps in IE9
    * Add atob and btoa polyfill

--- public/javascripts/util/mapbox-vector-tiles.js
+++ public/javascripts/util/mapbox-vector-tiles.js
@@ -3,6 +3,71 @@
 
   /****************************************************************************
    *
+   * atob and btoa
+   *
+   */
+
+  var object = typeof exports != 'undefined' ? exports : this; // #8: web workers
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+  function InvalidCharacterError(message) {
+    this.message = message;
+  }
+  InvalidCharacterError.prototype = new Error;
+  InvalidCharacterError.prototype.name = 'InvalidCharacterError';
+
+  // encoder
+  // [https://gist.github.com/999166] by [https://github.com/nignag]
+  object.btoa || (
+  object.btoa = function (input) {
+    var str = String(input);
+    for (
+      // initialize result and counter
+      var block, charCode, idx = 0, map = chars, output = '';
+      // if the next str index does not exist:
+      //   change the mapping table to "="
+      //   check if d has no fractional digits
+      str.charAt(idx | 0) || (map = '=', idx % 1);
+      // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
+      output += map.charAt(63 & block >> 8 - idx % 1 * 8)
+    ) {
+      charCode = str.charCodeAt(idx += 3/4);
+      if (charCode > 0xFF) {
+        throw new InvalidCharacterError("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      }
+      block = block << 8 | charCode;
+    }
+    return output;
+  });
+
+  // decoder
+  // [https://gist.github.com/1020396] by [https://github.com/atk]
+  object.atob || (
+  object.atob = function (input) {
+    var str = String(input).replace(/=+$/, '');
+    if (str.length % 4 == 1) {
+      throw new InvalidCharacterError("'atob' failed: The string to be decoded is not correctly encoded.");
+    }
+    for (
+      // initialize result and counters
+      var bc = 0, bs, buffer, idx = 0, output = '';
+      // get next character
+      buffer = str.charAt(idx++);
+      // character found in table? initialize bit storage and add its ascii value;
+      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+        // and if not first of each 4 characters,
+        // convert the first 8 bits to one ascii character
+        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+    ) {
+      // try to find character in table (0-63, not found => -1)
+      buffer = chars.indexOf(buffer);
+    }
+    return output;
+  });
+
+
+  /****************************************************************************
+   *
    * ieee754
    *
    */
@@ -2146,7 +2211,7 @@
     _eventHandlers: {},
 
     useBase64Fallback: function() {
-      return false;
+      return true;
     },
 
     style: function(feature) {
@@ -2327,7 +2392,7 @@
       if (!this.options.url) return;
 
       if (self.useBase64Fallback()) {
-        url = url.replace('{y}.pbf', '{y}.b64pbf');
+        url = url.replace('{y}.pbf', '{y}.bpbf');
       }
 
       url = url.replace("{z}", ctx.zoom).replace("{x}", ctx.tile.x).replace("{y}", ctx.tile.y);
@@ -2336,15 +2401,31 @@
 
       xhr.onload = function() {
 
+        var response;
+        var byteLength;
+        var arrayBuffer;
+        var i;
+
         if (xhr.status == "200") {
 
-          if (self.useBase64Fallback()) {
-            xhr.response = atob(xhr.response);
+          if (typeof xhr.response === 'undefined' && xhr.responseText) {
+            response = xhr.responseText;
+          } else if (!xhr.response) {
+            return;
+          } else {
+            response = xhr.response;
           }
 
-          if(!xhr.response) return;
-
-          var arrayBuffer = new Uint8Array(xhr.response);
+          if (self.useBase64Fallback()) {
+            response = atob(response);
+            byteLength = response.length;
+            arrayBuffer = new Uint8Array(byteLength);
+            for (i = 0; i < byteLength; i++) {
+              arrayBuffer[i] = response.charCodeAt(i);
+            }
+          } else {
+            arrayBuffer = new Uint8Array(xhr.response);
+          }
 
           var buf = new Protobuf(arrayBuffer);
           var vt = new VectorTile(buf);
@@ -2377,7 +2458,12 @@
       }
 // end Allow Custom Headers
 
-      xhr.responseType = 'arraybuffer';
+      if (self.useBase64Fallback()) {
+//        xhr.setRequestHeader('Content-Transfer-Encoding', 'base64');
+        //xhr.responseType = 'application/octet-stream';
+      } else {
+        xhr.responseType = 'arraybuffer';
+      }
 
       xhr.send();
 

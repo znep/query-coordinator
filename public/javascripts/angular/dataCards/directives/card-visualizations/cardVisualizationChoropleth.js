f657db475622242130a915fad457a0932879c5e2
Chris Laidlaw <chris.laidlaw@socrata.com>
2014-11-24 11:20:41 -0800

    FEATURE Support new NYC shapefile "hak8-5bvb"

--- public/javascripts/angular/dataCards/directives/card-visualizations/cardVisualizationChoropleth.js
+++ public/javascripts/angular/dataCards/directives/card-visualizations/cardVisualizationChoropleth.js
@@ -146,7 +146,8 @@
             '8fjz-g95m': 'tract',
             '2q28-58m6': 'tractce',
             '86dh-mgvd': 'geoid10',
-            '8thk-xhvj': 'name'
+            '8thk-xhvj': 'name',
+            'hak8-5bvb': 'coundist'
           };
 
           if (shapefile) {

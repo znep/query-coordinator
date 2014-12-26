1a1d6cbbf2b27b5cb4cacbdd44ecaca1ac001ba7
Michael Chui <michael.chui@socrata.com>
2014-12-26 11:45:10 -0800

    requirements change: login pages only, and text change

--- public/javascripts/screens/all-screens.js
+++ public/javascripts/screens/all-screens.js
@@ -178,13 +178,14 @@
             }
         });
 
-        if (blist.feature_flags.domain_decommissioning) {
+        if (blist.configuration.show_domain_decommissioning_message) {
             $('#noticeContainer').append(
-                $.tag2({ _: 'div', className: 'flash notice maintenanceNotice', contents: [
+                $.tag2({ _: 'div', className: 'flash notice maintenanceNotice decommissionMessage', contents: [
                     { _: 'a', href: '#', className: 'close', contents: [
                         { _: 'span', className: 'icon', contents: 'close' }]
                     },
-                    'We are shutting down this site, effective February 1, 2015'
+                    { _: 'p', contents: [ 'Effective February 1, 2015 this Socrata demonstration site (', { _: 'a', href: 'https://opendata.socrata.com', contents: 'https://opendata.socrata.com' }, ') will no longer be available. We encourage all users to immediately remove all data data prior to the February 1, 2015 site shut down.'] },
+                    { _: 'p', contents: ['Additional questions please contact', { _: 'a', href: 'mailto:support@socrata.com', contents: 'support@socrata.com' }]}
                 ] })
             );
         }

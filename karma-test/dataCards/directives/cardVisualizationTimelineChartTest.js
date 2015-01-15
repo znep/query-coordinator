69fbde76935401f73b62c4cf82c9fb8543c9cbef
Jerjou Cheng <jerjou.cheng@socrata.com>
2015-01-15 13:02:23 -0800

    CORE-4101: Factor out helper function, so timelineChartTest can use it. Add tests.

---
+++ karma-test/dataCards/directives/cardVisualizationTimelineChartTest.js
@@ -0,0 +1,62 @@
+describe("A Timeline Chart Card Visualization", function() {
+  var testHelpers, $q, timelineChartVisualizationService, _$provide;
+
+  beforeEach(module('/angular_templates/dataCards/cardVisualizationTimelineChart.html'));
+
+  beforeEach(module('dataCards'));
+  beforeEach(module('dataCards.directives'));
+  beforeEach(module(function($provide) {
+    _$provide = $provide;
+  }));
+  beforeEach(inject(function($injector) {
+    testHelpers = $injector.get('testHelpers');
+    $q = $injector.get('$q');
+    timelineChartVisualizationService = $injector.get('timelineChartVisualizationService');
+    var mockCardDataService = {
+      getTimelineDomain: function(){ return $q.when([]);},
+      getTimelineData: function(){ return $q.when([]);}
+    };
+    _$provide.value('CardDataService', mockCardDataService);
+    testHelpers.mockDirective(_$provide, 'timelineChart');
+  }));
+
+  afterEach(function(){
+    testHelpers.TestDom.clear();
+  });
+
+  describe('transformChartDataForRendering', function() {
+    it('should add min/max for dates and values, and mean for value', function() {
+      var numValues = 30;
+      var datumCount = _.range(numValues);
+      var dates = _.map(datumCount, function(i) { return moment(new Date(2014, 0, i + 1)); });
+      var unfilteredValues = _.map(datumCount, function(i) { return 100 * i; });
+      var filteredValues = _.map(unfilteredValues, function(val) { return val / 2; });
+
+      var transformed = timelineChartVisualizationService.transformChartDataForRendering(
+        _.map(datumCount, function(i) {
+          return {
+            date: dates[i],
+            total: unfilteredValues[i],
+            filtered: filteredValues[i]
+          }
+        })
+      );
+
+      expect(1 * transformed.minDate).to.equal(1 * new Date(2014, 0, 1));
+      expect(1 * transformed.maxDate).to.equal(1 * new Date(2014, 0, 30));
+      expect(transformed.minValue).to.equal(0);
+      expect(transformed.maxValue).to.equal(100 * (numValues - 1));
+      expect(transformed.meanValue).to.equal(100 * (numValues - 1) / 2);
+      expect(transformed.values.length).to.equal(numValues);
+      for (var i = 0; i < numValues; i++) {
+        expect(1 * transformed.values[i].date).to.equal(1 * dates[i]);
+        expect(transformed.values[i].filtered).to.equal(filteredValues[i]);
+        expect(transformed.values[i].unfiltered).to.equal(unfilteredValues[i]);
+      }
+      expect(transformed.labelUnit).to.equal('day');
+    });
+  });
+
+  // TODO: MOAR TESTS
+});
+

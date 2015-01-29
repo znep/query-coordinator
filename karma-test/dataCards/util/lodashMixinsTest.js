6a46317dcfe4d82cab5764cfdcd148b568cbd3d9
Ragan Webber <ragan.webber@socrata.com>
2015-01-29 13:03:54 -0800

    NOOP: Add lodash mixins for deep object traversal

--- karma-test/dataCards/util/lodashMixinsTest.js
+++ karma-test/dataCards/util/lodashMixinsTest.js
@@ -4,6 +4,14 @@
 
   describe('lo-dash mixins', function() {
 
+    var testObject = {
+      foo: {
+        bar: {
+          baz: 'test value'
+        }
+      }
+    };
+
     describe('isPresent', function() {
 
       it('should return true for "present" values', function() {
@@ -23,6 +31,39 @@
         expect(_.isPresent(undefined)).to.equal(false);
         expect(_.isPresent(NaN)).to.equal(false);
       });
+
+    });
+
+    describe('getPathOrElse', function() {
+      it('should return the value if the object path exists', function() {
+        expect(_.getPathOrElse(testObject, 'foo.bar.baz')).to.equal('test value');
+        expect(_.getPathOrElse(testObject, 'foo.bar')).to.eql({ baz: 'test value' });
+      });
+
+      it('should return the "else" argument if the object path does not exist', function() {
+        expect(_.getPathOrElse(testObject, 'foo.bar.blah', 'else case')).to.equal('else case');
+        expect(_.getPathOrElse({}, 'foo.bar', 'else case')).to.equal('else case');
+      });
+
+      it('should return undefined if the object path does not exist and there is no "else" argument', function() {
+        expect(_.getPathOrElse(testObject, 'foo.bar.blah')).to.be.undefined;
+      });
+
+    });
+
+    describe('isPathDefined', function() {
+
+      it('should return true if the object path exists', function() {
+        expect(_.isPathDefined(testObject, 'foo')).to.be.true;
+        expect(_.isPathDefined(testObject, 'foo.bar')).to.be.true;
+        expect(_.isPathDefined(testObject, 'foo.bar.baz')).to.be.true;
+      });
+
+      it('should return false if the object path does not exist', function() {
+        expect(_.isPathDefined(testObject, 'far')).to.be.false;
+        expect(_.isPathDefined(testObject, 'foo.baz')).to.be.false;
+        expect(_.isPathDefined(testObject, '')).to.be.false;
+      })
 
     });
 

d18a5052a9a66b5be406404d74531f9fe4754ef7
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-02-18 16:01:29 -0800

    NOOP: Bump timeout for richTextEditorTests. Sadface IE9.

--- karma-test/dataCards/directives/richTextEditorTest.js
+++ karma-test/dataCards/directives/richTextEditorTest.js
@@ -5,6 +5,10 @@
   var testHelpers;
 
   describe('Rich text editor', function() {
+    // For some reason the editor takes a long time to come online.
+    // We need to investigate why when we integrate richTextEditor.
+    this.timeout(5000);
+
     var jqueryFx;
     beforeEach(function() {
       module('socrataCommon.directives');

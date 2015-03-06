6f0b9f255808478135098572f3b2f188ca139107
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-03-06 14:05:08 -0800

    Schema no longer incorrectly requires fred property on system columns.
    Schema no longer validates system columns against any requirements.

--- public/javascripts/angular/dataCards/services/SchemaDefinitions.js
+++ public/javascripts/angular/dataCards/services/SchemaDefinitions.js
@@ -73,7 +73,7 @@ angular.module('dataCards.services').factory('SchemaDefinitions', function() {
           'columns': {
             'type': 'object',
             'patternProperties': {
-              '.*': {
+              '(:@)?([a-z][a-z_0-9\-]*)': {
                 'type': 'object',
                 'properties': {
                   'title': {

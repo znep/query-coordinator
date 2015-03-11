5cabff7e370ae8c11b83949c3c64314fbda30223
Clint Tseng <clint.tseng@socrata.com>
2015-03-11 16:27:33 -0700

    BUG: Fix up the revert button copy to match the AC.
    QA: should resolve the 13.17 mismatch.
    
    Reviewed-by: karin.hellman@socrata.com

--- public/javascripts/angular/dataCards/directives/revertButton.js
+++ public/javascripts/angular/dataCards/directives/revertButton.js
@@ -17,7 +17,7 @@
            'revert-btn',
            function() {
              if (scope.pageHasChanges) {
-               return '<div class="flyout-title">Click to revert all changes since you last saved</div>';
+               return '<div class="flyout-title">Click to undo all changes since you last saved</div>';
              } else {
                return '<div class="flyout-title">No changes to be reverted</div>';
              }

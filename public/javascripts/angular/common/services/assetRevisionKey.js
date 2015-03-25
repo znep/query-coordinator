9beebd778e16fac7bdfd55349227a16ddf5ab04c
Ragan Webber <ragan.webber@socrata.com>
2015-03-25 13:52:21 -0700

    BUG: CORE-3869 - Don't recycle RegExp objects
    * This fixes an issue where calling `test` on the same RegExp object
      multiple times will produce undesirable results
    * See the note under 'Description' [here](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test)

--- public/javascripts/angular/common/services/assetRevisionKey.js
+++ public/javascripts/angular/common/services/assetRevisionKey.js
@@ -4,25 +4,26 @@
   var REVISION_KEY_PATTERNS = [
     'angular_templates/'
   ];
-  var REVISION_KEY_MATCHER = new RegExp('({0})'.format(REVISION_KEY_PATTERNS.join('|')), 'ig');
-  var PATHNAME_MATCHER = new RegExp('^/');
 
   function AssetRevisionKeyHttpInterceptor(ServerConfig) {
     var assetRevisionKey = ServerConfig.get('assetRevisionKey');
-
-    return {
-      request: function(config) {
-        if (_.isPresent(assetRevisionKey)) {
+    if (_.isPresent(assetRevisionKey)) {
+      return {
+        request: function(config) {
+          var REVISION_KEY_MATCHER = new RegExp('({0})'.format(REVISION_KEY_PATTERNS.join('|')), 'ig');
+          var PATHNAME_MATCHER = new RegExp('^/');
           var urlOrPathname = config.url;
           if (REVISION_KEY_MATCHER.test(urlOrPathname)) {
             var url = PATHNAME_MATCHER.test(urlOrPathname) ? $.baseUrl(urlOrPathname) : new URL(urlOrPathname);
             url.searchParams.set('assetRevisionKey', assetRevisionKey);
             config.url = url.href;
           }
+          return config;
         }
-        return config;
-      }
-    };
+      };
+    } else {
+      return {};
+    }
   }
 
   angular.module('socrataCommon.services').

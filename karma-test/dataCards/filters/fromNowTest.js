5c5a8c1a337283de96f121014e4a79449f97f59b
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-03-26 16:15:30 -0700

    I swear I saw this pass last night without these modificaitons. Ferrari was there.
    Anyway, since we are no longer wrapping moment as a service we can get rid of all the dependency-injected crud.

--- karma-test/dataCards/filters/fromNowTest.js
+++ karma-test/dataCards/filters/fromNowTest.js
@@ -1,37 +1,25 @@
 describe('fromNow filter', function() {
+  'use strict';
+
   var FROM_NOW_VALUE = '12 days ago';
+  var TWELVE_DAYS_AGO_DATE = moment().subtract(12, 'day').toISOString();
   var fromNow;
   var isValidStub;
   var fromNowStub;
 
   beforeEach(function() {
-    isValidStub = sinon.stub();
-    fromNowStub = sinon.stub();
-    module('socrataCommon.filters', function($provide) {
-      $provide.value('moment', function() {
-        return {
-          isValid: isValidStub,
-          fromNow: fromNowStub
-        }
-      });
-    });
+    module('socrataCommon.filters');
+
     inject(function(fromNowFilter) {
       fromNow = fromNowFilter;
     });
   });
 
   it('should take a date and return a moment.fromNow value', function() {
-    isValidStub.returns(true);
-    fromNowStub.returns(FROM_NOW_VALUE);
-    expect(fromNow(new Date())).to.equal(FROM_NOW_VALUE);
-    expect(isValidStub.callCount).to.equal(1);
-    expect(fromNowStub.callCount).to.equal(1);
+    expect(fromNow(TWELVE_DAYS_AGO_DATE)).to.equal(FROM_NOW_VALUE);
   });
 
   it('should return an empty string for an invalid date', function() {
-    isValidStub.returns(false);
-    fromNowStub.returns('ignored');
     expect(fromNow('invalid')).to.equal('');
-    expect(fromNowStub.callCount).to.equal(0);
   });
 });
--- public/javascripts/angular/common/filters/fromNow.js
+++ public/javascripts/angular/common/filters/fromNow.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
 
-  function FromNowFilter(moment) {
+  function FromNowFilter() {
     return function(input) {
-      var momentObj = moment(input);
+      var momentObj = moment(input, moment.ISO_8601);
       if (momentObj.isValid()) {
         return momentObj.fromNow();
       } else {

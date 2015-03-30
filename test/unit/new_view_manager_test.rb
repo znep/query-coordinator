d9a4846274e23e30d502faccfb78b40850199332
Randy Antler <randy.antler@socrata.com>
2015-03-30 11:59:22 -0700

    Change error logic to handle differing behaviors for different error classes Handle additional core error case of ResourceNotFound Change report_error to only pass valid airbrake parameters Move private methods into private section Remove unused code Change single-letter variable names Add tests coverage for exception cases
    Signed-off-by: Randy Antler <randy.antler@socrata.com>

--- test/unit/new_view_manager_test.rb
+++ test/unit/new_view_manager_test.rb
@@ -26,12 +26,12 @@ class NewViewManagerTest < Test::Unit::TestCase
       elsif url == '/views/niew-veww/publication.json?accessType=WEBSITE'
         published = true
       end
-    end.then.returns('{"id": "niew-veww"}')
+    end.returns('{"id": "niew-veww"}')
     connection_stub.expects(:update_request).times(1).with do |url, payload|
       assert_equal('/views/niew-veww.json', url)
       payload = JSON.parse(payload).with_indifferent_access
       assert_equal('opendata_url', payload[:metadata][:accessPoints][:new_view])
-    end.then.returns('{}')
+    end.returns('{}')
 
     CoreServer::Base.stubs(connection: connection_stub)
 
@@ -43,6 +43,14 @@ class NewViewManagerTest < Test::Unit::TestCase
     assert(!published)
   end
 
+  def test_create_raises_when_view_not_created
+    new_view_manager.stubs(:create_new_view => nil)
+    assert_raises(NewViewManager::NewViewNotCreatedError) do
+      new_view_manager.create('title', 'description')
+    end
+  end
+
+
   def test_update
     connection_stub = mock
     connection_stub.expects(:update_request).times(1).with do |url, payload|
@@ -50,18 +58,50 @@ class NewViewManagerTest < Test::Unit::TestCase
       payload = JSON.parse(payload).with_indifferent_access
       assert_equal(payload[:name], 'new name')
       assert_equal(payload[:description], 'new description')
-    end.then.returns('{}')
+    end.returns('{}')
 
     CoreServer::Base.stubs(connection: connection_stub)
 
     new_view_manager.update('asdf-asdf', 'new name', 'new description')
   end
 
+  def test_update_does_not_raise_when_reporting_core_errors
+    connection_stub = mock
+    connection_stub.expects(:update_request).times(1).with do |url, payload|
+      assert_equal('/views/asdf-asdf.json', url)
+      payload = JSON.parse(payload).with_indifferent_access
+      assert_equal(payload[:name], 'new name')
+      assert_equal(payload[:description], 'new description')
+    end.raises(CoreServer::Error)
+
+    CoreServer::Base.stubs(connection: connection_stub)
+
+    assert_nothing_raised do
+      new_view_manager.update('asdf-asdf', 'new name', 'new description')
+    end
+  end
+
+  def test_update_does_not_raise_when_reporting_core_resource_not_found
+    connection_stub = mock
+    connection_stub.expects(:update_request).times(1).with do |url, payload|
+      assert_equal('/views/asdf-asdf.json', url)
+      payload = JSON.parse(payload).with_indifferent_access
+      assert_equal(payload[:name], 'new name')
+      assert_equal(payload[:description], 'new description')
+    end.raises(CoreServer::ResourceNotFound.new(View.new))
+
+    CoreServer::Base.stubs(connection: connection_stub)
+
+    assert_nothing_raised do
+      new_view_manager.update('asdf-asdf', 'new name', 'new description')
+    end
+  end
+
   def test_fetch
     connection_stub = mock
     connection_stub.expects(:get_request).times(1).with do |url|
       assert_equal('/views/asdf-asdf.json', url)
-    end.then.returns('{"body": 1}')
+    end.returns('{"body": 1}')
 
     CoreServer::Base.stubs(connection: connection_stub)
 
@@ -73,7 +113,7 @@ class NewViewManagerTest < Test::Unit::TestCase
     connection_stub = mock
     connection_stub.expects(:get_request).times(1).with do |url|
       assert_equal('/views/asdf-asdf.json', url)
-    end.then.raises(CoreServer::CoreServerError.new(nil, 'authentication_required', 'msg'))
+    end.raises(CoreServer::CoreServerError.new(nil, 'authentication_required', 'msg'))
 
     CoreServer::Base.stubs(connection: connection_stub)
 
@@ -86,7 +126,7 @@ class NewViewManagerTest < Test::Unit::TestCase
     connection_stub = mock
     connection_stub.expects(:get_request).times(1).with do |url|
       assert_equal('/views/asdf-asdf.json', url)
-    end.then.raises(CoreServer::CoreServerError.new(nil, 'permission_denied', 'msg'))
+    end.raises(CoreServer::CoreServerError.new(nil, 'permission_denied', 'msg'))
 
     CoreServer::Base.stubs(connection: connection_stub)
 
@@ -95,14 +135,54 @@ class NewViewManagerTest < Test::Unit::TestCase
     end
   end
 
+  def test_fetch_raises_view_not_found_when_core_resource_not_found
+    connection_stub = mock
+    connection_stub.expects(:get_request).times(1).with do |url|
+      assert_equal('/views/asdf-asdf.json', url)
+    end.raises(CoreServer::ResourceNotFound.new(View.new))
+
+    CoreServer::Base.stubs(connection: connection_stub)
+
+    assert_raises(NewViewManager::ViewNotFound) do
+      new_view_manager.fetch('asdf-asdf')
+    end
+  end
+
   def test_delete
     connection_stub = mock
     connection_stub.expects(:delete_request).times(1).with do |url, payload|
       assert_equal('/views/asdf-asdf.json', url)
-    end.then.returns('{}')
+    end.returns('{}')
 
     CoreServer::Base.stubs(connection: connection_stub)
 
     new_view_manager.delete('asdf-asdf')
   end
+
+  def test_delete_does_not_raise_when_core_raises
+    connection_stub = mock
+    connection_stub.expects(:delete_request).times(1).with do |url, payload|
+      assert_equal('/views/asdf-asdf.json', url)
+    end.raises(CoreServer::Error)
+
+    CoreServer::Base.stubs(connection: connection_stub)
+
+    assert_nothing_raised do
+      new_view_manager.delete('asdf-asdf')
+    end
+  end
+
+  def test_delete_does_not_raise_when_core_resource_not_found
+    connection_stub = mock
+    connection_stub.expects(:delete_request).times(1).with do |url, payload|
+      assert_equal('/views/asdf-asdf.json', url)
+    end.raises(CoreServer::ResourceNotFound.new(View.new))
+
+    CoreServer::Base.stubs(connection: connection_stub)
+
+    assert_nothing_raised do
+      new_view_manager.delete('asdf-asdf')
+    end
+  end
+
 end

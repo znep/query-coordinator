9eba1c8d43e26b5d9f1686b70d63124f14a52b06
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-03-31 15:34:08 -0700

    Make Rails embed page and dataset metadata (the latter including 'pages for dataset') in on the page at render time.
    Update tests.

--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -1,6 +1,21 @@
 class AngularController < ActionController::Base
   include ActionControllerExtensions
+  include CommonMetadataMethods
+  include CommonMetadataTransitionMethods
+  include UserAuthMethods
   include UnminifiedAssetsHelper
+
+  class AuthenticationRequired < RuntimeError; end
+  class UnauthorizedPageMetadataRequest < RuntimeError; end
+  class PageMetadataNotFound < RuntimeError; end
+  class UnauthorizedDatasetMetadataRequest < RuntimeError; end
+  class DatasetMetadataNotFound < RuntimeError; end
+  class UnknownRequestError < RuntimeError; end
+
+  before_filter :hook_auth_controller
+
+  helper_method :current_user
+  helper_method :current_user_session
 
   layout 'angular'
 
@@ -9,7 +24,226 @@ class AngularController < ActionController::Base
   end
 
   def serve_app
-    raise 'Need an app parameter' unless request[:app]
+    raise 'The "app" parameter is required.' unless request[:app]
+
+    # First fetch the current user's profile.
+    @current_user = current_user
+
+    # Then fetch the page metadata.
+    begin
+      @page_metadata = fetch_page_metadata(params[:id])
+    rescue AuthenticationRequired
+      return redirect_to_login
+    rescue UnauthorizedPageMetadataRequest
+      return render_403
+    rescue PageMetadataNotFound
+      return render_404
+    rescue UnknownRequestError => error
+      error_class = 'PageMetadataRequestFailure'
+      error_message = "Could not serve app: encountered unknown error " \
+        "fetching page metadata for page id #{params[:id]}: #{error.to_s}"
+      report_error(error_class, error_message)
+      return render_500
+    end
+
+    # Then ensure that there is a dataset id.
+    unless @page_metadata[:datasetId].present?
+      error_class = 'NoDatasetId'
+      error_message = "Could not serve app: page metadata does not include " \
+        "a dataset id: #{@page_metadata.inspect}"
+      report_error(error_class, error_message)
+    end
+
+    # Then fetch the dataset metadata.
+    begin
+      @dataset_metadata = fetch_dataset_metadata(@page_metadata[:datasetId])
+    rescue AuthenticationRequired
+      return redirect_to_login
+    rescue UnauthorizedDatasetMetadataRequest
+      return render_403
+    rescue DatasetMetadataNotFound
+      return render_404
+    rescue UnknownRequestError => error
+      error_class = 'DatasetMetadataRequestFailure'
+      error_message = "Could not serve app: encountered unknown error " \
+        "fetching dataset metadata for dataset id " \
+        "#{@page_metadata[:datasetId]}: #{error.to_s}"
+      report_error(error_class, error_message)
+      return render_500
+    end
+
+    # Finally fetch the dataset's pages.
+    begin
+      @dataset_metadata[:pages] = fetch_pages_for_dataset(@page_metadata[:datasetId])
+    rescue AuthenticationRequired
+      return redirect_to_login
+    rescue UnauthorizedDatasetMetadataRequest
+      return render_403
+    rescue DatasetMetadataNotFound
+      return render_404
+    rescue UnknownRequestError => error
+      error_class = 'PagesForDatasetRequestFailure'
+      error_message = "Could not serve app: encountered unknown error " \
+        "fetching pages for dataset with id " \
+        "#{@page_metadata[:datasetId]}: #{error.to_s}"
+      report_error(error_class, error_message)
+      return render_500
+    end
   end
 
+  private
+
+  def fetch_page_metadata(page_id)
+
+    if inherit_catalog_lens_permissions?
+      # Grab permissions from core.
+      begin
+        permissions = fetch_permissions(params[:id])
+      rescue NewViewManager::ViewAuthenticationRequired
+        raise AuthenticationRequired.new
+      rescue NewViewManager::ViewAccessDenied
+        raise UnauthorizedPageMetadataRequest.new
+      rescue NewViewManager::ViewNotFound
+        raise PageMetadataNotFound.new
+      rescue => error
+        raise UnknownRequestError.new error.to_s
+      end
+    end
+
+    result = phidippides.fetch_page_metadata(
+      params[:id],
+      :request_id => request_id,
+      :cookies => forwardable_session_cookies
+    )
+
+    if result[:status] != '200'
+      case result[:status]
+        when '401'
+          raise AuthenticationRequired.new
+        when '403'
+          raise UnauthorizedPageMetadataRequest.new
+        when '404'
+          raise PageMetadataNotFound.new
+        else
+          raise UnknownRequestError.new result[:body].to_s
+      end
+    end
+
+    page_metadata = result[:body]
+
+    if inherit_catalog_lens_permissions?
+      page_metadata[:permissions] = permissions if page_metadata
+    end
+
+    page_metadata
+  end
+
+  def fetch_dataset_metadata(dataset_id)
+
+    if inherit_catalog_lens_permissions?
+      # Grab permissions from core.
+      begin
+        permissions = fetch_permissions(dataset_id)
+      rescue NewViewManager::ViewAuthenticationRequired
+        raise AuthenticationRequired.new
+      rescue NewViewManager::ViewAccessDenied
+        raise UnauthorizedDatasetMetadataRequest.new
+      rescue NewViewManager::ViewNotFound
+        raise DatasetMetadataNotFound.new
+      rescue => error
+        raise UnknownRequestError.new error.to_s
+      end
+    end
+
+    result = phidippides.fetch_dataset_metadata(
+      dataset_id,
+      :request_id => request_id,
+      :cookies => forwardable_session_cookies
+    )
+
+    if result[:status] != '200'
+      case result[:status]
+        when '401'
+          raise AuthenticationRequired.new
+        when '403'
+          raise UnauthorizedDatasetMetadataRequest.new
+        when '404'
+          raise DatasetMetadataNotFound.new
+        else
+          raise UnknownRequestError.new result[:body].to_s
+      end
+    end
+
+    # This is temporary, but constitutes a rolling migration.
+    # Eventually we can check that every extant dataset metadata
+    # blob has a 'defaultPage' property and remove this migration
+    # step.
+    phidippides.migrate_dataset_metadata_to_v1(result)
+
+    # Moving forward, we also compute and insert two card type mapping
+    # properties on columns before we send them to the front-end.
+    # This method call will check the metadata transition phase
+    # internally and just pass through if it is not set to '3'.
+    phidippides.set_default_and_available_card_types_to_columns!(result)
+
+    dataset_metadata = result[:body]
+
+    if inherit_catalog_lens_permissions?
+      dataset_metadata[:permissions] = permissions if dataset_metadata && result[:status] =~ /^20[0-9]$/
+    end
+
+    add_table_column_to_dataset_metadata!(dataset_metadata)
+
+    dataset_metadata
+  end
+
+  def fetch_pages_for_dataset(dataset_id)
+
+    result = phidippides.fetch_pages_for_dataset(
+      dataset_id,
+      :request_id => request_id,
+      :cookies => forwardable_session_cookies
+    )
+
+    if result[:status] != '200'
+      case result[:status]
+        when '401'
+          raise AuthenticationRequired.new
+        when '403'
+          raise UnauthorizedDatasetMetadataRequest.new
+        when '404'
+          raise DatasetMetadataNotFound.new
+        else
+          raise UnknownRequestError.new result[:body].to_s
+      end
+    end
+
+    result[:body]
+  end
+
+  def add_table_column_to_dataset_metadata!(dataset_metadata)
+    table_column = {
+      :availableCardTypes => ['table'],
+      :defaultCardType => 'table',
+      :name => 'Data Table',
+      :description => '',
+      :fred => '*',
+      :physicalDatatype => '*'
+    }
+    dataset_metadata[:columns]['*'] = table_column
+
+    dataset_metadata
+  end
+
+  def redirect_to_login
+    redirect_to '/login?referer_redirect=1'
+  end
+
+  def report_error(error_class, error_message)
+    Airbrake.notify(
+      :error_class => error_class,
+      :error_message => error_message
+    )
+    Rails.logger.error(error_message)
+  end
 end

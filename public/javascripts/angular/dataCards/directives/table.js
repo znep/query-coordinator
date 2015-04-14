38f2294f5b749be69903f70ab3accfe2bf23c513
Andrew Harrison <andrew.harrison@socrata.com>
2015-04-14 15:38:40 -0700

    CORE-4752: Implement one-off currency (USD) formatting

--- public/javascripts/angular/dataCards/directives/table.js
+++ public/javascripts/angular/dataCards/directives/table.js
@@ -410,7 +410,6 @@
                     }
 
                   } else if (cellType === 'timestamp' || cellType === 'floating_timestamp') {
-                    cellText = '';
                     // Don't instantiate moment at all if we can avoid it.
                     if (_.isPresent(cellContent)) {
                       var time = moment(cellContent);
@@ -425,6 +424,22 @@
                         }
                       }
                     }
+                  } else if (cellType === 'money') {
+                    // TODO: use accountingjs to support non-US formats
+                    var dollarAmount = parseFloat(cellContent);
+                    if (_.isFinite(dollarAmount)) {
+                      var isNegativeDollarAmount = dollarAmount < 0;
+                      cellText = Math.abs(Math.round(dollarAmount * 100)).toString(); // positive cents
+                      cellText = ('00' + cellText).slice(Math.min(-cellText.length, -3)); // pad zeroes
+                      cellText = cellText.replace(/(\d+)(\d{2})$/, '$1.$2'); // "divide" to get dollars
+                      cellText = $.commaify(cellText).replace(/(\d)/, '$$$1'); // commaify, dollar sign
+                      if (isNegativeDollarAmount) {
+                        cellText = '-' + cellText; // restore negative sign
+                      }
+                    }
+
+                    // Fallback to escaped content if invalid amount
+                    cellText = cellText || _.escape(cellContent);
                   } else {
                     cellText = _.escape(cellContent);
                   }

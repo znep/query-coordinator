5e7130de0bf8c77097ff1c974fa3b212df69dc5a
bjorn <tie.372@gmail.com>
2015-04-15 09:25:18 -0700

    CORE-4627: Initial version;
    Functional, but probably overkill.

--- public/javascripts/angular/dataCards/controllers/pages/CardsViewController.js
+++ public/javascripts/angular/dataCards/controllers/pages/CardsViewController.js
@@ -138,7 +138,8 @@
     ServerConfig,
     $http,
     Schemas,
-    PageHelpersService
+    PageHelpersService,
+    localStorageService
   ) {
 
     AngularRxExtensions.install($scope);
@@ -620,6 +621,18 @@
       $scope.page.set('cards', _.without($scope.cardModels, cardModel));
     });
 
+    var mobileWarningClosed = (localStorageService.get('mobileWarningClosed') !== null);
+    var isMobile = navigator.userAgent.match(/Mobi/);
+
+    $scope.mobileWarningState = {
+      'show': isMobile && !mobileWarningClosed
+    };
+
+    $scope.$watch('mobileWarningState.show', function(newValue) {
+      if (newValue === false) {
+        localStorageService.set('mobileWarningClosed', true);
+      }
+    });
 
     // Set up flyout handlers.
 

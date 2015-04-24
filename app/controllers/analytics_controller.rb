b36f87782816fd5cd87a09e779daa313a0162bb7
Michael Hotan <michael.hotan@socrata.com>
2015-04-24 16:28:45 -0700

    Added returning 400

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -74,7 +74,7 @@ class AnalyticsController < ApplicationController
   end
 
   def render_metric_error(reason)
-    Rails.logger.error "Analytics Controller Metric Error: #{reason}"
+    Rails.logger.error "Analytics Controller Metric Error: #{reason}. Returning 400."
     render :json => reason.to_json, :status => 400
   end
 end

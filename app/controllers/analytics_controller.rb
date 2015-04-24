454b152cd4cd8441407631b2f5a57a7225d31b2d
Michael Hotan <michael.hotan@socrata.com>
2015-04-24 16:25:54 -0700

    Added logger errors in analytics controller

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -74,6 +74,7 @@ class AnalyticsController < ApplicationController
   end
 
   def render_metric_error(reason)
+    Rails.logger.error "Analytics Controller Metric Error: #{reason}"
     render :json => reason.to_json, :status => 400
   end
 end

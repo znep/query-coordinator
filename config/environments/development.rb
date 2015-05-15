727027fc5d102ca560ae54e130426e61dccc8611
Courtney Spurgeon <courtney.spurgeon@socrata.com>
2015-05-14 18:04:14 -0700

    [CHART-27] Finalizing redirect logic - If we have a matching domain on the frontend we will not need a frontend_uri parameter - I propose waiting until some level of integration test to test authorization itself. Comfy controls the inclusion of the module.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -41,5 +41,5 @@ Rails.application.configure do
 
   # Core configuration
   config.core_service_uri = "http://localhost:8080"
-  config.frontend_uri = "https://local.dev:9443"
+
 end
--- config/initializers/cms_pages_auth.rb
+++ config/initializers/cms_pages_auth.rb
@@ -1,8 +1,6 @@
 module CmsPagesAuth
   def authenticate
-    if Rails.env.development?
-      redirect_path = "#{Rails.application.config.frontend_uri}"
-    end
-    redirect_to "#{redirect_path}/login?referer_redirect=1" unless current_user
+    # If no current_user, send to main login page
+    redirect_to "/login?referer_redirect=1&return_to=#{request.path}" unless current_user
   end
 end

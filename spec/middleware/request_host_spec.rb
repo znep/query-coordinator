95857c87082bfe2b4fef3f49a827a2126bf1e8f6
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-05-18 16:11:01 -0700

    NOOP: Test whitespace.

--- spec/middleware/request_host_spec.rb
+++ spec/middleware/request_host_spec.rb
@@ -1,23 +1,25 @@
 require 'request_host'
 
 describe RequestHost do
-  let(:environment_hash_without_x_socrata_host) {
-   {
-   "REQUEST_METHOD"=>"GET",
-   "REQUEST_URI"=>"http://domain.in.host.header.com/some_path?query=value",
-   "SCRIPT_NAME"=>"/some_path",
-   "SERVER_NAME"=>"domain.in.host.header.com",
-   "SERVER_PORT"=>"80",
-   "HTTP_HOST"=>"domain.in.host.header.com",
-   "REQUEST_PATH"=>"/stories/admin",
-   "ORIGINAL_FULLPATH"=>"/stories/admin",
-   "ORIGINAL_SCRIPT_NAME"=>"/stories",
-   }
-  }
+  let(:environment_hash_without_x_socrata_host) do
+    {
+      "REQUEST_METHOD"=>"GET",
+      "REQUEST_URI"=>"http://domain.in.host.header.com/some_path?query=value",
+      "SCRIPT_NAME"=>"/some_path",
+      "SERVER_NAME"=>"domain.in.host.header.com",
+      "SERVER_PORT"=>"80",
+      "HTTP_HOST"=>"domain.in.host.header.com",
+      "REQUEST_PATH"=>"/stories/admin",
+      "ORIGINAL_FULLPATH"=>"/stories/admin",
+      "ORIGINAL_SCRIPT_NAME"=>"/stories",
+    }
+  end
 
-  let(:environment_hash_with_x_socrata_host) {
-    environment_hash_without_x_socrata_host.merge("HTTP_X_SOCRATA_HOST" => "domain.in.x.socrata.host.header.com")
-  }
+  let(:environment_hash_with_x_socrata_host) do
+    environment_hash_without_x_socrata_host.merge(
+      "HTTP_X_SOCRATA_HOST" => "domain.in.x.socrata.host.header.com"
+    )
+  end
 
   let(:blank_env) { {} }
 
@@ -33,18 +35,28 @@ describe RequestHost do
 
     context 'when the X-Socrata-Host header is set' do
       it 'replaces the existing SERVER_NAME with the contents of the header' do
-        expect(app).to receive(:call).with(hash_including('SERVER_NAME' => 'domain.in.x.socrata.host.header.com'))
+        expect(app).to receive(:call).with(
+          hash_including('SERVER_NAME' => 'domain.in.x.socrata.host.header.com')
+        )
+
         subject.call(environment_hash_with_x_socrata_host.deep_dup)
       end
 
       it 'replaces the existing HTTP_HOST with the contents of the header' do
-        expect(app).to receive(:call).with(hash_including('HTTP_HOST' => 'domain.in.x.socrata.host.header.com'))
+        expect(app).to receive(:call).with(
+          hash_including('HTTP_HOST' => 'domain.in.x.socrata.host.header.com')
+        )
+
         subject.call(environment_hash_with_x_socrata_host.deep_dup)
       end
 
       context 'with a valid REQUEST_URI' do
         it 'rewrites the existing REQUEST_URI use X-Socrata-Host as the domain' do
-          expect(app).to receive(:call).with(hash_including('REQUEST_URI' => 'http://domain.in.x.socrata.host.header.com/some_path?query=value'))
+          expect(app).to receive(:call).with(
+            hash_including(
+              'REQUEST_URI' => 'http://domain.in.x.socrata.host.header.com/some_path?query=value'
+            )
+          )
           subject.call(environment_hash_with_x_socrata_host.deep_dup)
         end
       end
@@ -55,24 +67,35 @@ describe RequestHost do
           environment_hash_with_bad_request_uri = environment_hash_with_x_socrata_host.merge(
             'REQUEST_URI' => '0.0.0.0/something bad'
           )
-          expect { subject.call(environment_hash_with_bad_request_uri) }.to raise_error(URI::InvalidURIError)
+          expect do
+            subject.call(environment_hash_with_bad_request_uri)
+          end.to raise_error(URI::InvalidURIError)
         end
       end
     end
 
     context 'when the X-Socrata-Host header is not set' do
       it 'keeps the existing SERVER_NAME' do
-        expect(app).to receive(:call).with(hash_including('SERVER_NAME' => 'domain.in.host.header.com'))
+        expect(app).to receive(:call).with(
+          hash_including('SERVER_NAME' => 'domain.in.host.header.com')
+        )
+
         subject.call(environment_hash_without_x_socrata_host.deep_dup)
       end
 
       it 'keeps the existing HTTP_HOST' do
-        expect(app).to receive(:call).with(hash_including('HTTP_HOST' => 'domain.in.host.header.com'))
+        expect(app).to receive(:call).with(
+          hash_including('HTTP_HOST' => 'domain.in.host.header.com')
+        )
+
         subject.call(environment_hash_without_x_socrata_host.deep_dup)
       end
 
       it 'keeps the existing REQUEST_URI' do
-        expect(app).to receive(:call).with(hash_including('REQUEST_URI' => 'http://domain.in.host.header.com/some_path?query=value'))
+        expect(app).to receive(:call).with(
+          hash_including('REQUEST_URI' => 'http://domain.in.host.header.com/some_path?query=value')
+        )
+
         subject.call(environment_hash_without_x_socrata_host.deep_dup)
       end
     end

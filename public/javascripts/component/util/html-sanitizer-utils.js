97359f7fc6b46fe729c290b0139d6188d1740f48
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-05-22 16:24:07 -0700

    BUG STAT-849: Rich text editor: Strip extra newlines prior to markdownifying.

--- public/javascripts/component/util/html-sanitizer-utils.js
+++ public/javascripts/component/util/html-sanitizer-utils.js
@@ -6,7 +6,20 @@
 
     // Move trailing spaces inside elements to just outside them. Ref: STAT-647
     var fixEmbeddedSpacesInTags = function(html)
-    { return html.replace(/( |&nbsp;)(<\/\w+>)/, '$2 '); };
+    {
+      return html.replace(/( |&nbsp;)(<\/\w+>)/, '$2 ');
+    };
+
+    // The typical use case for sanitizeHtmlPermissive is to clean DOM
+    // subtrees destined for HTML2Markdown. Newline characters tend to
+    // confuse HTML2Markdown, so strip them out. In cases we care about,
+    // this shouldn't affect the visuals.
+    //
+    // Ref: STAT-849
+    var removeNewlineChars = function(html)
+    {
+      return html.replace(/\n/g, '');
+    };
 
     //Sanitizes the given HTML using a moderate whitelist, allowing tags and
     // attributes expected from Markdown rendering.
@@ -37,7 +50,7 @@
 
         var outputArray = [];
         permissiveHtmlSanitizer(fixEmbeddedSpacesInTags(inputHtml), outputArray);
-        return outputArray.join('');
+        return removeNewlineChars(outputArray.join(''));
     },
 
     // Strip all HTML from the input except for span and div tags.

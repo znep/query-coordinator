62674f04765e425386e0b080a39cd1100e902af0
Courtney Spurgeon <courtney.spurgeon@socrata.com>
2015-06-08 15:16:52 -0700

    [CHART-44] Refactor so show and edit routes can share contexts, and adding additional test cases

--- spec/routing/stories_routing_spec.rb
+++ spec/routing/stories_routing_spec.rb
@@ -1,45 +1,119 @@
 require 'rails_helper'
 
-RSpec.describe 'published stories routing', type: :routing do
-  it 'supports vanity text' do
-    four_by_four = '52my-2pac'
-    vanity_text = 'let_me_tell_you_a_story'
+RSpec.describe 'stories routing', type: :routing do
+  let(:four_by_four) { '52my-2pac' }
+  let(:vanity_text) { 'let_me_tell-you-a-story' }
 
-    # NOTE: Should be /stories/s/#{vanity_text}/#{four_by_four},
-    # but RSpec routing tests seem to ignore relative_url_root.
-    expect(get: "/s/#{vanity_text}/#{four_by_four}").to route_to(
-      controller: 'stories',
-      action: 'show',
-      four_by_four: four_by_four,
-      vanity_text: vanity_text
-    )
-  end
+  # NOTE: Should be /stories/s/#{vanity_text}/#{four_by_four},
+  # but RSpec routing tests seem to ignore relative_url_root.
+  let(:stories_vanity_show_route_url) { "/s/#{vanity_text}/#{four_by_four}" }
+  let(:stories_vanity_edit_route_url) { "/s/#{vanity_text}/#{four_by_four}/edit" }
+  let(:stories_show_route_url) { "/s/#{four_by_four}" }
+  let(:stories_edit_route_url) { "/s/#{four_by_four}/edit" }
 
-  it 'requires a 4x4 when using a vanity URL' do
-    bad_four_by_four = 'not-afourbyfour'
+  describe 'stories show and edit routes' do
 
-    # NOTE: Should be /stories/s/vanity_text/#{bad_four_by_four},
-    # but RSpec routing tests seem to ignore relative_url_root.
-    expect(get: "/s/vanity_text/#{bad_four_by_four}").to_not be_routable
-  end
+    context 'with vanity text' do
 
-  it 'supports no vanity text' do
-    four_by_four = '52my-2pac'
+      it 'routes to show' do
+        expect(get: stories_vanity_show_route_url).to route_to(
+          controller: 'stories',
+          action: 'show',
+          four_by_four: four_by_four,
+          vanity_text: vanity_text
+        )
+      end
 
-    # NOTE: Should be /stories/s/#{four_by_four},
-    # but RSpec routing tests seem to ignore relative_url_root.
-    expect(get: "/s/#{four_by_four}").to route_to(
-      controller: 'stories',
-      action: 'show',
-      four_by_four: four_by_four
-    )
-  end
+      it 'routes to edit' do
+        expect(get: stories_vanity_edit_route_url).to route_to(
+          controller: 'stories',
+          action: 'edit',
+          four_by_four: four_by_four,
+          vanity_text: vanity_text
+        )
+      end
 
-  it 'requires a 4x4 when using a non-vanity URL' do
-    bad_four_by_four = 'really_im_not_a_fourbyfour'
+      context 'when vanity text contains strange characters' do
+        let(:vanity_text) { 'A+a!B_79~()+abd' }
+        let(:four_by_four) { '1234-0987' }
 
-    # NOTE: Should be /stories/s/#{bad_four_by_four},
-    # but RSpec routing tests seem to ignore relative_url_root.
-    expect(get: "/s/#{bad_four_by_four}").to_not be_routable
-  end
+        it 'routes to show' do
+          expect(get: stories_vanity_show_route_url).to route_to(
+            controller: 'stories',
+            action: 'show',
+            four_by_four: four_by_four,
+            vanity_text: vanity_text
+          )
+        end
+
+        it 'routes to edit' do
+          expect(get: stories_vanity_edit_route_url).to route_to(
+            controller: 'stories',
+            action: 'edit',
+            four_by_four: four_by_four,
+            vanity_text: vanity_text
+          )
+        end
+      end
+
+      context 'when the 4x4 is too long' do
+        let(:four_by_four) { 'not-afourbyfour' }
+
+        it 'does not route to show' do
+          expect(get: stories_vanity_show_route_url).to_not be_routable
+        end
+
+        it 'does not route to edit' do
+          expect(get: stories_vanity_edit_route_url).to_not be_routable
+        end
+      end
+
+      context 'when the 4x4 has no dash' do
+        let(:four_by_four) { 'nota_4x4x' }
+
+        it 'does not route to show' do
+          expect(get: stories_vanity_show_route_url).to_not be_routable
+        end
+
+        it 'does not route to edit' do
+          expect(get: stories_vanity_edit_route_url).to_not be_routable
+        end
+      end
+    end # end with vanity text tests
+
+
+    context 'without vanity text' do
+
+      it 'routes to show' do
+        expect(get: stories_show_route_url).to route_to(
+          controller: 'stories',
+          action: 'show',
+          four_by_four: four_by_four
+        )
+      end
+
+      it 'routes to edit' do
+        expect(get: stories_edit_route_url).to route_to(
+          controller: 'stories',
+          action: 'edit',
+          four_by_four: four_by_four
+        )
+      end
+
+      context 'with an invalid 4x4' do
+        let(:four_by_four) { 'really_im_not_a_fourbyfour' }
+
+        it 'does not route to show' do
+          expect(get: stories_show_route_url).to_not be_routable
+        end
+
+        it 'does not route to edit' do
+          expect(get: stories_edit_route_url).to_not be_routable
+        end
+      end
+
+    end # end without vanity text tests
+
+  end # end show and edit routes
+
 end

4ddae0e20855e16a869bba9a276592b198d23d3b
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-06-15 16:05:12 -0700

    got into an invalid state. Replace it with a version created by a fresh db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615204044) do
+ActiveRecord::Schema.define(version: 20150615204029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blocks", id: false, force: :cascade do |t|
-    t.integer  "block_id"
+  create_table "blocks", force: :cascade do |t|
     t.string   "layout",     null: false
     t.json     "components", null: false
     t.string   "created_by", null: false
@@ -26,11 +25,10 @@ ActiveRecord::Schema.define(version: 20150615204044) do
     t.datetime "deleted_at"
   end
 
-  create_table "draft_stories", id: false, force: :cascade do |t|
-    t.integer  "draft_story_id"
-    t.string   "id",             limit: 9,              null: false
-    t.integer  "block_ids",                default: [], null: false, array: true
-    t.string   "created_by",                            null: false
+  create_table "draft_stories", force: :cascade do |t|
+    t.string   "four_by_four", limit: 9,              null: false
+    t.integer  "block_ids",              default: [], null: false, array: true
+    t.string   "created_by",                          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
@@ -38,19 +36,18 @@ ActiveRecord::Schema.define(version: 20150615204044) do
   end
 
   add_index "draft_stories", ["created_by"], name: "index_draft_stories_on_created_by", using: :btree
-  add_index "draft_stories", ["id"], name: "index_draft_stories_on_id", using: :btree
+  add_index "draft_stories", ["four_by_four"], name: "index_draft_stories_on_four_by_four", using: :btree
 
-  create_table "published_stories", id: false, force: :cascade do |t|
-    t.integer  "published_story_id"
-    t.string   "id",                 limit: 9,              null: false
-    t.integer  "block_ids",                    default: [], null: false, array: true
-    t.string   "created_by",                                null: false
+  create_table "published_stories", force: :cascade do |t|
+    t.string   "four_by_four", limit: 9,              null: false
+    t.integer  "block_ids",              default: [], null: false, array: true
+    t.string   "created_by",                          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
   end
 
   add_index "published_stories", ["created_by"], name: "index_published_stories_on_created_by", using: :btree
-  add_index "published_stories", ["id"], name: "index_published_stories_on_id", using: :btree
+  add_index "published_stories", ["four_by_four"], name: "index_published_stories_on_four_by_four", using: :btree
 
 end

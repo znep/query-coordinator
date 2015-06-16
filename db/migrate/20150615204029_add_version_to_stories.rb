dde0fc367eb11b748108e3a5396b815f8545ae0c
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-06-16 10:01:57 -0700

    Update migration (and many tests) to reflect four-by-four column name change to .

--- db/migrate/20150615204029_add_version_to_stories.rb
+++ db/migrate/20150615204029_add_version_to_stories.rb
@@ -1,12 +1,20 @@
 class AddVersionToStories < ActiveRecord::Migration
 
   def up
+
+    rename_column :draft_stories, :four_by_four, :uid
+    rename_column :published_stories, :four_by_four, :uid
+
     change_table :draft_stories do |t|
       t.string :version
     end
   end
 
   def down
+
+    rename_column :draft_stories, :uid, :four_by_four
+    rename_column :published_stories, :uid, :four_by_four
+
     change_table :draft_stories do |t|
       t.remove :version
     end
--- spec/support/story_as_json_shared.rb
+++ spec/support/story_as_json_shared.rb
@@ -4,8 +4,12 @@ shared_examples 'has_story_as_json' do
 
   describe '#as_json' do
 
-    it "filters out 'id'" do
-      expect(subject.as_json).to_not include('id')
+    it "filters out the internal id" do
+      expect(subject.as_json['id']).to_not eq(subject.id)
+    end
+
+    it "exposes the four by four as 'id'" do
+      expect(subject.as_json['id']).to eq(subject.uid)
     end
 
     it "filters out 'block_ids'" do

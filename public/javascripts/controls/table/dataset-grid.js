8bb5f1dd7b7c55d9663e3530acf2c6685c1cb828
Trey Cordova <trey.cordova@socrata.com>
2015-06-23 11:03:50 -0700

    Update comment to reflect changes;

--- public/javascripts/controls/table/dataset-grid.js
+++ public/javascripts/controls/table/dataset-grid.js
@@ -442,7 +442,7 @@
 
         var action;
         var rowId;
-        // If # isn't present, but ends with /\d+ then it is a row URL
+        // If # isn't present, but ends with /\d+ or /row-.+ then it is a row URL
         if (hashIndex < 0 && (!$.isBlank(href.match(/\/\d+$/)) || !$.isBlank(href.match(/\/row-/))))
         {
             action = 'view-row';

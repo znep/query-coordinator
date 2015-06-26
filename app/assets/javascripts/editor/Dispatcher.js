9268e863c91daa34f6fa95a117fc6218c26a1eeb
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-06-26 15:29:41 -0700

    ENG: Add Dispatcher.

---
+++ app/assets/javascripts/editor/Dispatcher.js
@@ -0,0 +1,59 @@
+;var Dispatcher = (function() {
+  'use strict';
+
+  /**
+   * @constructor
+   */
+  function Dispatcher() {
+    var _callbacks = [];
+
+    /**
+     * Public methods
+     */
+
+    /**
+     * Dispatches a payload to all registered callbacks.
+     *
+     * @param {anything} payload
+     */
+    this.dispatch = function(payload) {
+      _callbacks.forEach(function(callback) {
+        callback.callback(payload);
+      });
+    };
+
+    /**
+     * Registers a callback to be invoked with every dispatched payload. Returns
+     * a token that can be used with `.unregister()`.
+     *
+     * @param {function} callback
+     * @return {string}
+     */
+    this.register = function(callback) {
+      var id = _.uniqueId('dispatcher-registration-id::');
+
+      if (!_.isFunction(callback)) {
+        throw new Error('`callback` parameter must be a function, was: ' + callback);
+      }
+
+      _callbacks.push({
+        id: id,
+        callback: callback
+      });
+
+      return id;
+    };
+
+    /**
+     * Removes a callback based on its token.
+     *
+     * @param {string} id
+     */
+    this.unregister = function(id) {
+      _.remove(_callbacks, 'id', id);
+    };
+  };
+
+  return Dispatcher;
+})();
+

ab1b1c57721595f0dbdad91172b2a9f7ee92a354
Randy Antler <randy.antler@socrata.com>
2015-07-10 08:34:05 -0700

    CORE-4478 Pull memcached host out of ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Frontend::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store, 'memcache', { :namespace => 'webapp' }
+  config.cache_store = :mem_cache_store, ENV['MEMCACHED_HOST'] || 'memcache', { :namespace => 'webapp' }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"

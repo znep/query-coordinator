a54424d2ac50e57844b95e98a1bf1ab5b9ff74bd
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-07-14 14:33:31 -0700

    ENG: Stack stack traces.

--- lib/core_server.rb
+++ lib/core_server.rb
@@ -154,7 +154,7 @@ class CoreServer
   end
 
   def self.report_error(error, message)
-    Rails.logger.error("#{error.class}: #{error} (on #{message}):\n\n#{error.backtrace}")
+    Rails.logger.error("#{error.class}: #{error} (on #{message}):\n\n#{error.backtrace.join('\n')}")
     Airbrake.notify_or_ignore(
       error,
       error_message: message

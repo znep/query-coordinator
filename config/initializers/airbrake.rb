67c726028e6716529889ff38aed8b3c306555be3
Stan Rawrysz <stan.rawrysz@socrata.com>
2015-07-14 21:36:21 -0700

    ENG: Add configurable environment name for airbrake errors

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,6 @@
 if ENV['AIRBRAKE_API_KEY'].present?
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
+    config.environment_name = ENV['AIRBRAKE_ENVIRONMENT_NAME'] || Rails.env
   end
 end

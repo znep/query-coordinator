ff39f9d72ff5aece077a066ff8718077ddff5b6a
Stan Rawrysz <stan.rawrysz@socrata.com>
2015-07-23 16:58:45 -0700

    Improve readability for string formatter

--- app/assets/javascripts/editor/StringExtensions.js
+++ app/assets/javascripts/editor/StringExtensions.js
@@ -1,15 +1,17 @@
-(function(){
+(function () {
   'use strict';
 
   // Included from the frontend repo. Ex:
   // "Hello {0}".format('myName')
   // => "Hello myName"
-  String.prototype.format = function() {
-    var txt = this,
-    i = arguments.length;
-    while (i--) {
+  String.prototype.format = function () {
+    var txt = this;
+    var i;
+
+    for (i = 0; i < arguments.length; i++) {
       txt = txt.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
     }
+
     return txt;
   };
 })();
--- spec/karma/StringExtensions.spec.js
+++ spec/karma/StringExtensions.spec.js
@@ -2,6 +2,6 @@ describe('String.prototype.format', function() {
   'use strict';
 
   it('should insert correctly via index', function() {
-    expect('{0}, {1}, {2}, {3}, {4}'.format(1, 2, 3, 4, 5)).to.equal('1, 2, 3, 4, 5');
+    expect('{0}, {1}, {2}, {3}, {4}'.format(1, "2", 3, 4, "five")).to.equal('1, 2, 3, 4, five');
   });
 });

dd609932be0343dbf94c5ccaffca3fd53b25b367
Courtney Spurgeon <courtney.spurgeon@socrata.com>
2015-07-24 10:29:52 -0700

    [CHART-80] Adapt dataset picker to be embeddable in Storyteller - Add checks in case parent page doesn't contain window.blist - Add way to fire 'onDatasetSelect' without having window.blist.common.selectedDataset

--- public/javascripts/screens/browse-select-dataset.js
+++ public/javascripts/screens/browse-select-dataset.js
@@ -1,4 +1,5 @@
 (function($)
+
 {
 
 if (_.isUndefined(window.parent))
@@ -6,7 +7,6 @@ if (_.isUndefined(window.parent))
     throw "Not in an iFrame! Not sure what you're expecting.";
 }
 
-var commonNS = window.parent.blist.namespace.fetch('blist.common');
 
 $(function()
 {
@@ -16,17 +16,29 @@ $(function()
         contents: [ $.t('controls.common.dataset_picker.button') ]
     }, true));
 
-    $('.chooseDatasetButton').click(function(event)
-    {
-        event.preventDefault();
 
-        if (_.isFunction(commonNS.selectedDataset))
-        {
-            commonNS.selectedDataset(blist.browse.getDS($(this)));
-        }
-        else
-        {
-            throw "Can't find the blist.common.selectedDataset handler in the parent!"
+    $('.chooseDatasetButton').click(function(event) {
+        event.preventDefault();
+        var chosenDataset = blist.browse.getDS($(this));
+
+        if (_.isFunction(window.frameElement.onDatasetSelect)) {
+            // Use this to fire a `onDatasetSelect` function in the parent window
+            // when a dataset is selected (if you do not have blist.common in the
+            // parent window context).
+            // Add an attribute to the iframe element that is the function to be called
+            // Ex: $('iframe')[0].onDatasetSelect = function(datasetObj) {}
+            // Context: This was added for Storyteller
+            window.frameElement.onDatasetSelect(chosenDataset)
+        } else {
+            // Default behavior when embedded in an environment with blist.common
+            // and the frameElement.onDatasetSelect is not defined
+            var commonNS = window.parent.blist.namespace.fetch('blist.common');
+
+            if (_.isFunction(commonNS.selectedDataset)) {
+                commonNS.selectedDataset(chosenDataset);
+            } else {
+                throw "Can't find the blist.common.selectedDataset handler in the parent!"
+            }
         }
     });
 });

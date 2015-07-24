085f3cb8426e2d7d36f68c4c1d51e556bb5e3f0c
Stan Rawrysz <stan.rawrysz@socrata.com>
2015-07-24 12:38:41 -0700

    ENG: CR feedback

--- spec/karma/StringExtensions.spec.js
+++ spec/karma/StringExtensions.spec.js
@@ -2,6 +2,6 @@ describe('String.prototype.format', function() {
   'use strict';
 
   it('should insert correctly via index', function() {
-    expect('{0}, {1}, {2}, {3}, {4}'.format(1, "2", 3, 4, "five")).to.equal('1, 2, 3, 4, five');
+    expect('{0}, {1}, {2}, {3}, {4}'.format(1, '2', 3, 4, 'five')).to.equal('1, 2, 3, 4, five');
   });
 });

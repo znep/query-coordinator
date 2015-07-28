f9af4b23900a2ed9f44ea374a2d6b76ff9ededf4
bjorn <tie.372@gmail.com>
2015-07-27 17:15:43 -0700

    CORE-6244: CR improvements;

--- config/environment.rb
+++ config/environment.rb
@@ -66,8 +66,15 @@ module Frontend
     # Disable rack-cache, as we have ATS and it's entirely redundant. Also, failing randomly.
     config.action_dispatch.rack_cache = nil
 
-    config.log_file = (ENV['RAILS_LOG_FILE'] == 'STDOUT') ? STDOUT : ENV['RAILS_LOG_FILE']
-    config.log_file ||= (Rails.env == 'development') ? STDOUT : "log/#{Rails.env}.log"
+    # Set the log file as follows:
+    #  - If the RAILS_LOG_FILE environment variable is set, use it.
+    #  - Otherwise, use STDOUT for development and log/#{Rails.env}.log otherwise.
+    if ENV['RAILS_LOG_FILE'].to_s.upcase == 'STDOUT' || Rails.env.development?
+      config.log_file = STDOUT
+    else
+      config.log_file = (ENV['RAILS_LOG_FILE'].blank?) ? "log/#{Rails.env}.log" : ENV['RAILS_LOG_FILE']
+    end
+
     config.logger = ActiveSupport::TaggedLogging.new(Logger.new(config.log_file))
     config.logger.level = Logger::DEBUG
   end

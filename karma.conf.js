dff085fa6fd81a527f7132cc2f0aa0443fd8ff8b
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-08-04 15:45:48 -0700

    Add additional uitilities. Update tests.
    * Rename package payload from `socrata-utils.js` to `socrata.utils.js`.
    * Add new utility functions.
    * Add tests for new uitility functions.
    * Bump version.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/lodash/lodash.js',
-      'socrata-utils.js',
+      'socrata.utils.js',
       'karma/**/*spec.js'
     ],
 

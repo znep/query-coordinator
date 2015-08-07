19dad401b7ab8fa784a38320d1ef7ba7ca6349c6
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-08-07 12:25:01 -0700

    Use `socrata-utils` and `socrata-visualizations` in story editor app.

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -10,13 +10,18 @@
 
 //= require jquery2
 //= require lodash
+//= require d3
 //= require squire-rte/build/squire
 //= require eventEmitter/EventEmitter
 //= require eventie/eventie
 //= require unipointer/unipointer
 //= require unidragger/unidragger
 //= require jQuery-sidebar/src/jquery.sidebar
-//= require socrata-utils/socrata-utils
+//= require socrata-utils/socrata.utils
+//= require socrata-visualizations/socrata.visualizations.DataProvider
+//= require socrata-visualizations/socrata.visualizations.SoqlDataProvider
+//= require socrata-visualizations/socrata.visualizations.Visualization
+//= require socrata-visualizations/socrata.visualizations.Column
 //= require editor/init
 //= require editor/storytellerUtils
 //= require editor/SimpleEventEmitter

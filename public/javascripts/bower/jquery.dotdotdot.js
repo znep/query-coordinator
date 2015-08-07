a1ed1ec559523684b5eb5ae47d5e8a1b7eebb980
Zachary Elliott <zelliottm@gmail.com>
2015-08-07 10:40:17 -0700

    CORE-6435: Re-add dylan's fix from 5/8/2015

--- public/javascripts/bower/jquery.dotdotdot.js
+++ public/javascripts/bower/jquery.dotdotdot.js
@@ -589,6 +589,15 @@
 
 		return n;
 	}
+
+	// SOCRATA - dylan.bussone@socrata.com - 5/8/2015
+	// Strip newLines from the textNode so that the ellipses is not on the next line.
+	function trimAndRemoveLineBreaks( $el )
+	{
+		$el.children('br').remove();
+ 		$el.html($el.html().trim());
+	}
+
 	function findLastTextNode( $el, $top, excludeCurrent )
 	{
 		var e = $el && $el[ 0 ], p;
@@ -602,6 +611,7 @@
 				}
 				if ( $.trim( $el.text() ) )
 				{
+					trimAndRemoveLineBreaks($el);
 					return findLastTextNode( $el.contents().last(), $top );
 				}
 			}

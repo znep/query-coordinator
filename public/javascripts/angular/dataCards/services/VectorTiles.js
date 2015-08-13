22db5c83a8c59714d4b24da7a2874991f9bc383c
Kelsie Haakenson <kelsie.haakenson@socrata.com>
2015-08-13 11:40:50 -0700

    CORE-6477/CORE-6480: Dense data flyout appears properly and click disabled
    Fixed one line renaming issue that didn't make it into earlier changes (Constants variable not reflecting name change was undefined, so denseData always evaluated to false).

--- public/javascripts/angular/dataCards/services/VectorTiles.js
+++ public/javascripts/angular/dataCards/services/VectorTiles.js
@@ -831,7 +831,7 @@
               injectTileInfo(e);
 
               // Determine if click should be disabled (when data is dense or flannel would be full)
-              var denseData = e.tile.totalPoints >= Constants.FEATURE_MAP_MAX_POINT_LIMIT;
+              var denseData = e.tile.totalPoints >= Constants.FEATURE_MAP_POINTS_PER_TILE_LIMIT;
               var manyRows = _.sum(e.points, 'count') > Constants.FLANNEL_ROW_CONTENT_LIMIT;
               if (!denseData && !manyRows) {
                 highlightClickedPoints(e.points);

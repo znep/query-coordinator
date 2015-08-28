38fbbca9438062eccb2c47933bc2fc912ff60179
Michael Chui <michael.chui@socrata.com>
2015-08-28 13:54:25 -0700

    some minor improvements, including cr feedback

--- tools/verify_compression.rb
+++ tools/verify_compression.rb
@@ -4,12 +4,21 @@ require 'yaml'
 require 'yui/compressor'
 require 'action_view'
 include ActionView::Helpers::NumberHelper
+require 'rainbow/ext/string'
 
 FRONTEND = `git rev-parse --show-toplevel`.chop
 TMP_DIR  = File.expand_path(File.join(FRONTEND, '..', '..', 'tmp'))
 YUI_PATH = `gem contents yui-compressor`.split($\).detect { |name| name.end_with? '.jar' }
 
-git_command = ARGV.first ? "git show --name-only #{ARGV.first}" : "git diff --staged --name-only"
+git_command = if ARGV.first
+                # Intended for discovering problematic SHAs.
+                # Argument can be any git pointer: SHA, tag, or branch.
+                "git show --name-only #{ARGV.first}"
+              else
+                # Intended as a pre-commit hook.
+                # Looks at staged but un-commited files.
+                "git diff --staged --name-only"
+              end
 
 changed_js = IO.popen("cd #{FRONTEND} && #{git_command}") do |f|
   f.readlines.collect do |line|
@@ -26,21 +35,20 @@ end
 
 packages.each do |package, libraries|
   print "[verify compression] "
-  buffer = ''
-  libraries.each do |lib|
-    buffer << File.read(File.join(FRONTEND, lib))
-  end
+  buffer = libraries.inject('') { |buf, lib| buf << File.read(File.join(FRONTEND, lib)) }
 
   begin
-    (data_length = YUI::JavaScriptCompressor.new.compress(buffer).length) > 0
-    puts "Compression of #{package}.js worked fine. [#{number_to_human_size(data_length)}]"
+    data_length = YUI::JavaScriptCompressor.new.compress(buffer).length
+    puts "Compression of #{package}.js worked fine.".color(:green) + " [#{number_to_human_size(data_length)}]"
   rescue YUI::Compressor::RuntimeError => e
-    puts "Compression failed. Overwriting #{TMP_DIR}/#{package}.js"
+    puts "Compression failed. Overwriting #{TMP_DIR}/#{package}.js".color(:red)
 
+    # No longer appears necessary to re-run the compressor in order to dump errors into the console.
+    # Un-comment the below lines if you don't see any upon failure.
     concatted_file = File.join(TMP_DIR, "#{package}.js")
-    minified_file  = File.join(TMP_DIR, "#{package}.min.js")
+    #minified_file  = File.join(TMP_DIR, "#{package}.min.js")
     File.open(concatted_file, 'w') do |f| f.write(buffer) end
-    system("java -jar #{YUI_PATH} #{concatted_file} -o #{minified_file}")
+    #system("java -jar #{YUI_PATH} #{concatted_file} -o #{minified_file}")
 
     exit 1
   end

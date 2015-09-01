fdafd82529b9598a965faab1734aed72fd8839e7
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-09-01 15:52:06 -0700

    Update package definitions to get build working.

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
       'bower_components/leaflet/dist/leaflet.css',
       'bower_components/lodash/lodash.js',
       'bower_components/socrata-utils/socrata.utils.js',
-      'bower_components/vector-tile-js/dist/vectortile.js',
+      'bower_components/vector-tile/dist/vectortile.js',
       'socrata.visualizations.DataProvider.js',
       'socrata.visualizations.MetadataProvider.js',
       'socrata.visualizations.SoqlDataProvider.js',

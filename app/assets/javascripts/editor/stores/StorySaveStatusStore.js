148c593c336ae8e779bd5a9afddfef2be554cd49
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-09-05 22:26:53 -0700

    ENG: Cleanups

--- app/assets/javascripts/editor/stores/StorySaveStatusStore.js
+++ app/assets/javascripts/editor/stores/StorySaveStatusStore.js
@@ -33,8 +33,7 @@
 
         case Constants.STORY_CREATE:
         case Constants.STORY_SAVED:
-          // Story load.
-          // Let StoryStore deal with this action, then remember the initial story state.
+          // Let StoryStore deal with this action, then remember the initial or updated story state.
           storyteller.dispatcher.waitFor([ storyteller.storyStore.getDispatcherToken() ]);
           _lastSavedSerializedStory = storyteller.storyStore.serializeStory(forStoryUid);
           _saveInProgress = false;

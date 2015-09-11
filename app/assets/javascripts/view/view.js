645c018879a29ccec0530b140d8101b93fecf2d5
Trey Cordova <trey.cordova@socrata.com>
2015-09-11 12:25:57 -0700

    Cleaned up all of the JavaScript and SCSS;
    - Made an erb template for presentation mode buttons;

--- app/assets/javascripts/view/view.js
+++ app/assets/javascripts/view/view.js
@@ -26,103 +26,112 @@ $(document).on('ready', function() {
     );
   });
 
-  //
-  // Don't breathe this.
-  //
+  /**
+   * @class PresentationMode
+   * @description
+   * Adds functionality to the presentation mode template.
+   * This includes:
+   * - Taking a preview-mode story and showing blocks one at a time.
+   * - Paging between individual blocks
+   * - Resizing the content to fit appropriately into the screen size.
+   */
+  function PresentationMode() {
+    var pageable;
+    var blacklist = ['spacer', 'horizontal-rule'];
+    var presenting = false;
+    var presentationNavigation = document.querySelector('.presentation-navigation');
+    var blocks = Array.prototype.slice.call(document.querySelectorAll('.block'), 0);
+    var index = 0;
 
-  var pageable;
-  var blacklist = ['spacer', 'horizontal-rule'];
-  var presenting = false;
-  var presentationNavigation = document.querySelector('.presentation-navigation');
-  var blocks = Array.prototype.slice.call(document.querySelectorAll('.block'), 0);
-  var index = 0;
+    blocks.forEach(function(block) {
+      if (notBlacklisted(block)) {
+        block.setAttribute('data-page-index', index++);
+      }
+    });
 
-  blocks.forEach(function(block) {
-    if (notBlacklisted(block)) {
-      block.setAttribute('data-page-index', index++);
+    pageable = Array.prototype.slice.call(document.querySelectorAll('.block[data-page-index]'), 0);
+
+    document.documentElement.addEventListener('keyup', pageOrClose);
+    document.querySelector('.presentation-next').addEventListener('click', pageNext);
+    document.querySelector('.presentation-previous').addEventListener('click', pagePrevious);
+    document.querySelector('.presentation-mode').addEventListener('click', enablePresentationMode);
+    document.querySelector('.linear-mode').addEventListener('click', enableLinearMode);
+
+    function enablePresentationMode(event) {
+      presenting = true;
+
+      event.target.setAttribute('disabled', 'disabled');
+      document.querySelector('.linear-mode').removeAttribute('disabled');
+
+      blocks.forEach(function(block) {
+        block.classList.toggle('hidden', block !== pageable[0]);
+      });
+
+      presentationNavigation.classList.remove('hidden');
     }
-  });
 
-  pageable = Array.prototype.slice.call(document.querySelectorAll('.block[data-page-index]'), 0);
+    function enableLinearMode(event) {
+      presenting = false;
 
-  document.documentElement.addEventListener('keyup', pageOrClose);
-  document.querySelector('.presentation-next').addEventListener('click', pageNext);
-  document.querySelector('.presentation-previous').addEventListener('click', pagePrevious);
-  document.querySelector('.presentation-mode').addEventListener('click', enablePresentationMode);
-  document.querySelector('.linear-mode').addEventListener('click', enableLinearMode);
+      event.target.setAttribute('disabled', 'disabled');
+      document.querySelector('.presentation-mode').removeAttribute('disabled');
 
-  function enablePresentationMode(event) {
-    presenting = true;
+      blocks.forEach(function(block) {
+        block.classList.remove('hidden');
+      });
 
-    event.target.setAttribute('disabled', 'disabled');
-    document.querySelector('.linear-mode').removeAttribute('disabled');
+      presentationNavigation.classList.add('hidden');
+      document.body.scrollTop = 0;
+    }
 
-    blocks.forEach(function(block) {
-      block.classList.toggle('hidden', block !== pageable[0]);
-    });
+    function pageNext() {
+      var visible = document.querySelector('.block:not(.hidden)');
+      var nextIndex = parseInt(visible.getAttribute('data-page-index'), 10) + 1;
+      var nextVisible = document.querySelector('.block[data-page-index="' + nextIndex + '"]');
 
-    presentationNavigation.classList.remove('hidden');
-  }
+      nextVisible = nextVisible ? nextVisible : document.querySelector('.block[data-page-index="0"]');
 
-  function enableLinearMode(event) {
-    presenting = false;
+      visible.classList.add('hidden');
+      nextVisible.classList.remove('hidden');
+    }
 
-    event.target.setAttribute('disabled', 'disabled');
-    document.querySelector('.presentation-mode').removeAttribute('disabled');
+    function pagePrevious() {
+      var visible = document.querySelector('.block:not(.hidden)');
+      var previousIndex = parseInt(visible.getAttribute('data-page-index'), 10) - 1;
+      var previousVisible = document.querySelector('.block[data-page-index="' + previousIndex + '"]');
 
-    blocks.forEach(function(block) {
-      block.classList.remove('hidden');
-    });
+      previousVisible = previousVisible ? previousVisible : document.querySelector('.block[data-page-index="' + (pageable.length - 1) + '"]');
 
-    presentationNavigation.classList.add('hidden');
-    document.body.scrollTop = 0;
-  }
+      visible.classList.add('hidden');
+      previousVisible.classList.remove('hidden');
+    }
 
-  function pageNext() {
-    var visible = document.querySelector('.block:not(.hidden)');
-    var nextIndex = parseInt(visible.getAttribute('data-page-index'), 10) + 1;
-    var nextVisible = document.querySelector('.block[data-page-index="' + nextIndex + '"]');
+    function pageOrClose(event) {
+      var key = event.charCode || event.keyCode;
 
-    nextVisible = nextVisible ? nextVisible : document.querySelector('.block[data-page-index="0"]');
-
-    visible.classList.add('hidden');
-    nextVisible.classList.remove('hidden');
-  }
-
-  function pagePrevious() {
-    var visible = document.querySelector('.block:not(.hidden)');
-    var previousIndex = parseInt(visible.getAttribute('data-page-index'), 10) - 1;
-    var previousVisible = document.querySelector('.block[data-page-index="' + previousIndex + '"]');
-
-    previousVisible = previousVisible ? previousVisible : document.querySelector('.block[data-page-index="' + (pageable.length - 1) + '"]');
-
-    visible.classList.add('hidden');
-    previousVisible.classList.remove('hidden');
-  }
-
-  function pageOrClose(event) {
-    var key = event.charCode || event.keyCode;
-
-    if (presenting) {
-      switch (key) {
-        case 27:
-          document.querySelector('.linear-mode').click();
-          break;
-        case 37:
-          pagePrevious();
-          break;
-        case 39:
-          pageNext();
-          break;
+      if (presenting) {
+        switch (key) {
+          case 27:
+            document.querySelector('.linear-mode').click();
+            break;
+          case 37:
+            pagePrevious();
+            break;
+          case 39:
+            pageNext();
+            break;
+        }
       }
     }
+
+    function notBlacklisted(element) {
+      element = element.querySelector('.component-container > .component');
+      var classes = Array.prototype.slice.call(element.classList, 0);
+      return classes.every(function(value) {
+        return blacklist.indexOf(value.replace('component-', '')) === -1;
+      });
+    }
   }
 
-  function notBlacklisted(element) {
-    element = element.querySelector('.component-container > .component');
-    var classes = Array.prototype.slice.call(element.classList, 0);
-    return classes.every(function(value) {
-      return blacklist.indexOf(value.replace('component-', '')) === -1;
-    });
-  }
+  var presentationMode = new PresentationMode();
 });

bae49398e0f53392c5ef3b8fb121e7cdb0fd1643
Trey Cordova <trey.cordova@socrata.com>
2015-09-16 15:27:28 -0700

    CHART-137: Removed magic chart block from inspiration list;
    - Lint fixes;

--- app/assets/javascripts/editor/Constants.js
+++ app/assets/javascripts/editor/Constants.js
@@ -1,37 +1,41 @@
-var Constants = {
+(function(root) {
+  'use strict';
 
-  // Configuration
-  YOUTUBE_URL_PATTERNS: [
-    /youtu\.be\/([^#\&\?]{11})/,  // youtu.be/<id>
-    /\?v=([^#\&\?]{11})/,         // ?v=<id>
-    /\&v=([^#\&\?]{11})/,         // &v=<id>
-    /embed\/([^#\&\?]{11})/,      // embed/<id>
-    /\/v\/([^#\&\?]{11})/         // /v/<id>
-  ],
+  var Constants = {
+    // Configuration
+    YOUTUBE_URL_PATTERNS: [
+      /youtu\.be\/([^#\&\?]{11})/,  // youtu.be/<id>
+      /\?v=([^#\&\?]{11})/,         // ?v=<id>
+      /\&v=([^#\&\?]{11})/,         // &v=<id>
+      /embed\/([^#\&\?]{11})/,      // embed/<id>
+      /\/v\/([^#\&\?]{11})/         // /v/<id>
+    ],
 
-  HISTORY_MAX_UNDO_COUNT: 99,
+    HISTORY_MAX_UNDO_COUNT: 99,
 
-  SOQL_DATA_PROVIDER_NAME_ALIAS: 'COLUMN_ALIAS_GUARD_NAME',
-  SOQL_DATA_PROVIDER_VALUE_ALIAS: 'COLUMN_ALIAS_GUARD_VALUE',
+    SOQL_DATA_PROVIDER_NAME_ALIAS: 'COLUMN_ALIAS_GUARD_NAME',
+    SOQL_DATA_PROVIDER_VALUE_ALIAS: 'COLUMN_ALIAS_GUARD_VALUE',
 
-  SOCRATA_VISUALIZATION_DESTROY: 'SOCRATA_VISUALIZATION_DESTROY',
+    SOCRATA_VISUALIZATION_DESTROY: 'SOCRATA_VISUALIZATION_DESTROY',
 
-  API_PREFIX_PATH: '/stories/api/v1/',
+    API_PREFIX_PATH: '/stories/api/v1/',
 
-  // The `name` column of the `lenses` table is defined as:
-  //
-  //   name character varying(255)
-  //
-  // Here 255 is the maxiumum allowed length, not the maxiumum character
-  // count.
-  CORE_VIEW_NAME_MAX_LENGTH: 254,
+    // The `name` column of the `lenses` table is defined as:
+    //
+    //   name character varying(255)
+    //
+    // Here 255 is the maxiumum allowed length, not the maxiumum character
+    // count.
+    CORE_VIEW_NAME_MAX_LENGTH: 254,
 
-  WINDOW_RESIZE_RERENDER_DELAY: 200,
+    WINDOW_RESIZE_RERENDER_DELAY: 200,
 
-  // Breakpoints for Window Width
-  WINDOW_SIZE_BREAK_LARGE: 1200,
-  WINDOW_SIZE_BREAK_MEDIUM: 768,
+    // Breakpoints for Window Width
+    WINDOW_SIZE_BREAK_LARGE: 1200,
+    WINDOW_SIZE_BREAK_MEDIUM: 768,
 
-  DEFAULT_VISUALIZATION_HEIGHT: 300
-};
+    DEFAULT_VISUALIZATION_HEIGHT: 300
+  };
 
+  root.Constants = Constants;
+})(window);

88f53341d5693cfec19c08a3a216d8d24ec6c741
Trey Cordova <trey.cordova@socrata.com>
2015-09-24 10:05:40 -0700

    CHART-169: Generalized blocks_with_images to search type;

--- app/models/concerns/block_operations.rb
+++ app/models/concerns/block_operations.rb
@@ -7,17 +7,17 @@ module BlockOperations
       Block.for_story(self)
     end
 
-    def blocks_with_images
+    def blocks_with_component_type(type)
       blocks = Block.for_story(self)
       blocks.select do |block|
         block.components.any? do |component|
-          component['type'] == 'image'
+          component['type'] == type
         end
       end
     end
 
     def block_images
-      blocks = blocks_with_images
+      blocks = blocks_with_component_type('image')
       images = blocks.map do |block|
         block.components.map do |component|
           component['value']['url']

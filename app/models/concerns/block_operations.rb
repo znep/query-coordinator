994a53490e0d336adba438066eb87475d8b9a3f4
Trey Cordova <trey.cordova@socrata.com>
2015-09-23 17:25:40 -0700

    CHART-169: Add basic sharing;

--- app/models/concerns/block_operations.rb
+++ app/models/concerns/block_operations.rb
@@ -6,5 +6,24 @@ module BlockOperations
     def blocks
       Block.for_story(self)
     end
+
+    def blocks_with_images
+      blocks = Block.for_story(self)
+      blocks.select do |block|
+        block.components.any? do |component|
+          component['type'] == 'image'
+        end
+      end
+    end
+
+    def block_images
+      blocks = blocks_with_images
+      images = blocks.map do |block|
+        block.components.map do |component|
+          component['value']['url']
+        end
+      end
+      images.flatten
+    end
   end
 end

2cfd4d54616e1bddc071129acd819d3b5ddcb9f8
Stan Rawrysz <stan.rawrysz@socrata.com>
2015-09-30 09:22:28 -0700

    Add index on components json field in blocks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925172221) do
+ActiveRecord::Schema.define(version: 20150930160915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150925172221) do
     t.datetime "updated_at"
     t.datetime "deleted_at"
   end
+
+  add_index "blocks", ["components"], name: "index_blocks_on_components", using: :gin
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false

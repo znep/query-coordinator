a2b1bd70838762848b18452a8c796f128e1df138
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-10-02 16:06:18 -0700

    Clean up tests.

--- spec/features/xss_protection_spec.rb
+++ spec/features/xss_protection_spec.rb
@@ -1,230 +1,124 @@
 require 'rails_helper'
 
+shared_examples 'a secure application' do
+  describe 'View mode' do
+    before do
+      visit "/s/magic-thing/#{attack_story_uid}"
+    end
+    it 'should not evaluate JS' do
+      expect(page.evaluate_script('window.xssFailure')).to eq(nil)
+    end
+  end
+
+  describe 'Preview mode' do
+    before do
+      visit "/s/magic-thing/#{attack_story_uid}/preview"
+    end
+    it 'should not evaluate JS' do
+      expect(page.evaluate_script('window.xssFailure')).to eq(nil)
+    end
+  end
+
+  describe 'Edit mode' do
+    before do
+      visit "/s/magic-thing/#{attack_story_uid}/edit"
+    end
+    it 'should not evaluate JS' do
+      expect(page.evaluate_script('window.xssFailure')).to eq(nil)
+    end
+  end
+end
+
 # This entire test is attempting to prove a negative.
 # Tread lightly. This was TDDd and was verified to fail before
 # the fixes were put in place.
 RSpec.describe 'XSS protection', type: :feature, js: true do
+  let(:view_metadata) { {} }
+
   before do
     stub_logged_in_user
+
+    story = FactoryGirl.build(:draft_story, uid: attack_story_uid) do |story|
+      story.block_ids = story_blocks.map(&:id)
+    end
+    story.save!
+
+    published_story = PublishedStory.from_draft_story(story)
+    published_story.created_by = '11'
+    published_story.save!
+
+    stub_core_view(attack_story_uid, view_metadata)
   end
 
-  # A selection of HTML-based attacks.
-  # Many of these come from
-  # https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
-  let(:html_component_attacks) do
-    [
-      '<script>window.xssFailure = "simple script tag injection"</script>',
-      '<img src="javascript:window.xssFailure = \'img src script injection\'">',
-      '<IMG """><SCRIPT>window.xssFailure = "malformed IMG tag script injection"</SCRIPT>">',
-      '<<SCRIPT>window.xssFailure = "extraneous open brackets script injection";//<</SCRIPT>',
-      '<BODY onload="window.xssFailure=\'body.onload event injection\'">',
-      '<DIV STYLE="width: expression(window.xssFailure=\'style expression script injection\');">',
-      '<!--[if gte IE 4]>\n<SCRIPT>window.xssFailure = "ie comment conditional script injection"</SCRIPT>\n<![endif]-->'
-    ]
-  end
 
   describe 'attacks against the html component' do
-    let!(:attack_story_uid) do
-      story = FactoryGirl.build(:draft_story, uid: 'html-doom') do |story|
-        html_component_attacks.each do |attack_markup|
-          story.block_ids.push(FactoryGirl.create(
-            :block,
-            { components: [ { type: 'html', value: attack_markup } ] }
-          ).id)
-        end
-      end
-      story.save!
-
-      published_story = PublishedStory.from_draft_story(story)
-      published_story.created_by = '11'
-      published_story.save!
-
-      stub_core_view(story.uid)
-
-      story.uid
+    # A selection of HTML-based attacks.
+    # Many of these come from
+    # https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
+    let(:html_component_attacks) do
+      [
+        '<script>window.xssFailure = "simple script tag injection"</script>',
+        '</script><script>window.xssFailure="premature script close"</script>',
+        '<img src="javascript:window.xssFailure = \'img src script injection\'">',
+        '<IMG """><SCRIPT>window.xssFailure = "malformed IMG tag script injection"</SCRIPT>">',
+        '<<SCRIPT>window.xssFailure = "extraneous open brackets script injection";//<</SCRIPT>',
+        '<BODY onload="window.xssFailure=\'body.onload event injection\'">',
+        '<DIV STYLE="width: expression(window.xssFailure=\'style expression script injection\');">',
+        '<!--[if gte IE 4]>\n<SCRIPT>window.xssFailure = "ie comment conditional script injection"</SCRIPT>\n<![endif]-->'
+      ]
     end
 
-    describe 'View mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(html_component_attacks.length)
-
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
+    let(:story_blocks) do
+      html_component_attacks.map do |attack_markup|
+        FactoryGirl.create(
+          :block,
+          { components: [ { type: 'html', value: attack_markup } ] }
+        )
       end
     end
 
-    describe 'Preview mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/preview"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(html_component_attacks.length)
+    let(:attack_story_uid) { 'html-doom' }
 
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
-
-    describe 'Edit mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/edit"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(html_component_attacks.length)
-
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
+    it_behaves_like 'a secure application'
   end
 
   describe 'attacks against the youtube component' do
-    let!(:attack_story_uid) do
+    let(:story_blocks) do
       evil_youtube_value = {
         id: '"><script>window.xssFailure=true</script>'
       }
 
-      story = FactoryGirl.build(:draft_story, uid: 'ytub-doom') do |story|
-        story.block_ids.push(FactoryGirl.create(
+      [
+        FactoryGirl.create(
           :block,
           { components: [ { type: 'youtube.video', value: evil_youtube_value } ] }
-        ).id)
-      end
-      story.save!
-
-      published_story = PublishedStory.from_draft_story(story)
-      published_story.created_by = '11'
-      published_story.save!
-
-      stub_core_view(story.uid)
-
-      story.uid
+        )
+      ]
     end
 
-    describe 'View mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(1)
+    let(:attack_story_uid) { 'ytub-doom' }
 
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
-
-    describe 'Preview mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/preview"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(1)
-
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
-
-    describe 'Edit mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/edit"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(1)
-
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
+    it_behaves_like 'a secure application'
   end
 
   describe 'attacks against the story metadata' do
-    let!(:attack_story_uid) do
+    let(:view_metadata) {
       evil_title = '"><script>window.xssFailure="title injection"</script>'
       evil_description = '"><script>window.xssFailure="description injection"</script>'
 
-      story = FactoryGirl.build(:draft_story, uid: 'titl-doom')
-      story.save!
-
-      published_story = PublishedStory.from_draft_story(story)
-      published_story.created_by = '11'
-      published_story.save!
-
-      stub_core_view(
-        story.uid,
+      {
         name: evil_title,
         description: evil_description
-      )
+      }
+    }
 
-      story.uid
+    let(:story_blocks) do
+      []
     end
 
-    describe 'View mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}"
-      end
-      it 'should not evaluate JS' do
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
+    let(:attack_story_uid) { 'mtda-doom' }
 
-    describe 'Preview mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/preview"
-      end
-      it 'should not evaluate JS' do
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
-
-    describe 'Edit mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/edit"
-      end
-      it 'should not evaluate JS' do
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
+    it_behaves_like 'a secure application'
   end
 
-
-  describe 'attacks against story JSON' do
-    let!(:attack_story_uid) do
-      evil_json = '</script><script>window.xssFailure=true</script>'
-
-      story = FactoryGirl.build(:draft_story, uid: 'json-doom') do |story|
-        story.block_ids.push(FactoryGirl.create(
-          :block,
-          { components: [ { type: 'html', value: evil_json } ] }
-        ).id)
-      end
-      story.save!
-
-      published_story = PublishedStory.from_draft_story(story)
-      published_story.created_by = '11'
-      published_story.save!
-
-      stub_core_view(story.uid)
-
-      story.uid
-    end
-
-    describe 'Edit mode' do
-      before do
-        visit "/s/magic-thing/#{attack_story_uid}/edit"
-      end
-      it 'should not evaluate JS' do
-        # This assertion is really to make sure the page is fully loaded.
-        expect(page.all('.block').length).to eq(1)
-
-
-        expect(page.evaluate_script('window.xssFailure')).to eq(nil)
-      end
-    end
-  end
-
-  
 end

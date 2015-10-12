281a998cb5ff4377a981396fe754f03c15d7fe68
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2015-10-12 11:04:00 -0700

    Increase shape limit for mondara maps

--- public/javascripts/controls/maps/mondara-map.js
+++ public/javascripts/controls/maps/mondara-map.js
@@ -147,7 +147,7 @@
           'line-opacity: 0.5;' +
         '}'
       );
-      var tileUrl = '/tiles/' + layerName + '/the_geom/${z}/${x}/${y}.png?$limit=50000&$style=' + style;
+      var tileUrl = '/tiles/' + layerName + '/the_geom/${z}/${x}/${y}.png?$limit=80000&$style=' + style;
       return new OpenLayers.Layer.XYZ(layerName, tileUrl, layerOpts);
     },
 

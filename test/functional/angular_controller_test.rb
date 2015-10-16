2a38da13a8780973a3863fd9d522977bf99b95a5
Randy Antler <randy.antler@socrata.com>
2015-10-16 12:17:01 -0700

    CORE-7122 BUG Fix raising incorrect exception -- Fix bad test

--- test/functional/angular_controller_test.rb
+++ test/functional/angular_controller_test.rb
@@ -284,7 +284,7 @@ class AngularControllerTest < ActionController::TestCase
 
     should 'redirect to the 403 page if the dataset is private' do
       PageMetadataManager.any_instance.stubs(:show).returns(v1_page_metadata)
-      AngularController.any_instance.stubs(:fetch_dataset_metadata).raises(CommonMetadataMethods::UnauthorizedDatasetMetadataRequest)
+      Phidippides.any_instance.stubs(:fetch_dataset_metadata => { status: '403' })
       NewViewManager.any_instance.stubs(:fetch).returns({})
 
       get :data_lens, :id => '1234-1234', :app => 'dataCards'

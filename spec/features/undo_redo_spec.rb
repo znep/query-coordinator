b5a427060cca131bca7ae7d8d4b6276c2f914109
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2015-11-04 21:18:06 -0800

    CHART-222: more undo-redo tests
    * Exercise some problems that were previously fixed

--- spec/features/undo_redo_spec.rb
+++ spec/features/undo_redo_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'os'
 
 # This test verifies the integration of HistoryStore with the rest of the system.
 # This integration has proven problematic, because the system needs to understand
@@ -20,6 +21,7 @@ RSpec.describe 'undo/redo', type: :feature, js: true do
     stub_logged_in_user
     stub_core_view('hasb-lock')
     visit '/s/magic-thing/hasb-lock/edit'
+    @original_story_json = story_json
     @undo_btn = page.find('.undo-btn')
     @redo_btn = page.find('.redo-btn')
   end
@@ -124,5 +126,125 @@ RSpec.describe 'undo/redo', type: :feature, js: true do
     expect(block_count).to eq(initial_block_count)
     expect_button_disabled(@undo_btn)
     expect_button_enabled(@redo_btn)
+  end
+
+  describe 'rich text editor' do
+
+    before do
+      @blocks = page.all('.user-story .block-edit')
+      @heading_block = @blocks.first
+      @squire_frame = @heading_block.find('iframe')
+    end
+
+    context 'when user types a single letter' do
+      it 'undoes and redoes' do
+        type_key_into_squire(@squire_frame, 'x')
+        undo_then_verify_story_returns_to_original_content
+      end
+    end
+
+    context 'when pasting from another block' do
+      it 'undoes and redoes' do
+        # Select all contents in the second block. Couldn't get ctrl-a (select all) to work.
+        # So bruteforce selection with normal DOM APIs.
+        page.evaluate_multiline_script("
+          var iframeWindow = $('iframe').eq(1)[0].contentWindow; // Select second iframe in page.
+          var selection = iframeWindow.getSelection();
+          selection.removeAllRanges();
+          selection.selectAllChildren(iframeWindow.document.body);
+        ")
+
+        copy_current_selection(@blocks[1].all('iframe')[0])
+        paste_clipboard_into_squire(@squire_frame)
+
+        undo_then_verify_story_returns_to_original_content
+      end
+    end
+
+    context 'when pasting complex things' do
+      # The intent of this test is to verify that Squire's fixing-up of complex
+      # clipboard contents does not count as a user action. If there is a regression
+      # in this area, the Squire fixups will blow away the redo stack which will
+      # break this test.
+      it 'can redo after multiple edits and redos' do
+        copy_html_to_clipboard('<section><div><p><b>complex</b><i>things</i><p></div></section>')
+        paste_clipboard_into_squire(@squire_frame)
+        @story_after_paste_json = story_json
+
+        type_key_into_squire(@squire_frame, 'x')
+        @story_after_key_press_json = story_json
+
+        expect(@story_after_paste_json).to_not eq(@original_story_json)
+        expect(@story_after_key_press_json).to_not eq(@story_after_paste_json)
+
+        @undo_btn.click
+        expect(story_json).to eq(@story_after_paste_json)
+        @undo_btn.click
+        expect(story_json).to eq(@original_story_json)
+
+        @redo_btn.click
+        expect(story_json).to eq(@story_after_paste_json)
+        @redo_btn.click
+        expect(story_json).to eq(@story_after_key_press_json)
+      end
+    end
+
+    def undo_then_verify_story_returns_to_original_content
+      dirty_story_json = story_json
+      expect(dirty_story_json).to_not eq(@original_story_json)
+
+      @undo_btn.click
+      expect(story_json).to eq(@original_story_json)
+
+      @redo_btn.click
+      expect(story_json).to eq(dirty_story_json)
+    end
+  end
+
+  def story_json
+    page.evaluate_script(
+      "storyteller.storyStore.serializeStory(storyteller.userStoryUid)"
+    )
+  end
+
+  def copy_html_to_clipboard(html)
+    page.evaluate_multiline_script("
+      var $clipboardContentSource = $('<div id=\"clipboard-content-source\"></div>');
+      $clipboardContentSource.append('#{html}');
+      $(document.body).append($clipboardContentSource);
+      window.getSelection().removeAllRanges();
+      window.getSelection().selectAllChildren($clipboardContentSource[0]);
+    ")
+    copy_current_selection
+    page.evaluate_script('$("#clipboard-content-source").remove()')
+  end
+
+  def type_key_into_squire(squire_frame, key)
+    with_squire_body_element(squire_frame) do |squire_body|
+      squire_body.native.send_key(key)
+    end
+  end
+
+  def paste_clipboard_into_squire(squire_frame)
+    with_squire_body_element(squire_frame) do |squire_body|
+      squire_body.native.send_keys([os_control_key, 'v'])
+    end
+  end
+
+  # Copy the current selection from the page (by default)
+  # or from the given iframe's selection (if an iframe is passed
+  # as an argument).
+  def copy_current_selection(source_element=page.find('body'))
+    source_element.native.send_keys([os_control_key, 'c'])
+  end
+
+  def os_control_key
+    OS.mac? ? :command : :control
+  end
+
+  def with_squire_body_element(iframe)
+    within_frame(iframe) do
+      yield(find('body'))
+    end
   end
 end

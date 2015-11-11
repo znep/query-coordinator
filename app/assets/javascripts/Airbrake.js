016b9006e00cb78bf605458692336055ecbbf9bd
Trey Cordova <trey.cordova@socrata.com>
2015-11-11 14:44:01 -0800

    ENG: Documentation and minor cleanup;

--- app/assets/javascripts/Airbrake.js
+++ app/assets/javascripts/Airbrake.js
@@ -4,19 +4,31 @@
 
   var storyteller = window.storyteller;
 
+  /**
+   * @class Airbrake
+   * @description
+   * A small wrapper class for airbrake-client-js. Exposes a `.notify`
+   * method for elected errors. This class will notify on unexpected
+   * errors that surface through the window error event.
+   * @param {Object} options - Class and airbrake-client-js options.
+   * @param {String} options.environment - Describes the expected environment, such as 'development'.
+   * @param {String} options.projectKey - A valid Airbrake project key.
+   * @param {String} options.projectId - A valid Airbrake project id.
+   */
   function Airbrake(options) {
     var _self = this;
     var _airbrake;
-    var _options = options;
+    var _environment = options.environment;
+    var _airbrakeOptions = _.omit(options, 'environment');
 
-    build();
+    setup();
     attachEvents();
 
-    function build() {
-      if (_options.projectKey !== null) {
-        _airbrake = new airbrakeJs.Client(_options);
+    function setup() {
+      if (_airbrakeOptions.projectKey !== null) {
+        _airbrake = new airbrakeJs.Client(_airbrakeOptions);
         _airbrake.addFilter(function(notice) {
-          notice.context.environment = _options.environment;
+          notice.context.environment = _environment;
           return notice;
         });
       }
@@ -28,6 +40,14 @@
       });
     }
 
+    /**
+     * @function notify
+     * @description
+     * Exposes an error through Airbrake, if it is available.
+     * In either case, it will `console.error` the argument passed
+     * into this function.
+     * @param {Any} error - Anything that should be logged in Airbrake/console.
+     */
     this.notify = function(error) {
       if (!_.isUndefined(_airbrake)) {
         _airbrake.notify(error);

1bea9825ec2620401b15012399096043c61b9b95
John Krah <john.krah@socrata.com>
2015-11-13 16:59:54 -0800

    permit unicode category/name in url

--- spec/lib/resource_constraint_spec.rb
+++ spec/lib/resource_constraint_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'rails_helper'
 require_relative '../../test/test_helper'
 require_relative '../../lib/constraints/resource_constraint'
@@ -7,11 +9,15 @@ describe Constraints::ResourceConstraint do
   describe '#matches?' do
 
     subject(:constraint) { described_class.new }
-    let(:request_with_valid_id) { double('Request', path_parameters: {id: '1234-five'}) }
-    let(:request_with_invalid_id) { double('Request', path_parameters: {id: '_123456-seven?'}) }
-    let(:request_with_missing_id) { double('Request', path_parameters: {}) }
+    let(:request_with_valid_id) { double('Request', path_parameters: {id: '1234-five', category: 'fun', view_name: 'something interesting'}) }
+    let(:request_with_invalid_id) { double('Request', path_parameters: {id: '_123456-seven?', category: 'fun', view_name: 'something interesting'}) }
+    let(:request_with_missing_id) { double('Request', path_parameters: {category: 'fun', view_name: 'something interesting'}) }
+    let(:request_with_missing_category) { double('Request', path_parameters: {id: '1234-five', view_name: 'something interesting'}) }
+    let(:request_with_missing_viewname) { double('Request', path_parameters: {id: '1234-five', category: 'fun'}) }
+    let(:request_with_unicode_category) { double('Request', path_parameters: {id: '1234-five', category: '愛', view_name: 'motherly love'}) }
+    let(:request_with_unicode_viewname) { double('Request', path_parameters: {id: '1234-five', category: 'love', view_name: 'الرسم-البياني-الأمومي'}) }
 
-    it 'is accepted with a valid 4x4 id parameter' do
+    it 'is accepted with a valid 4x4 id parameter, category, and viewname' do
       expect(constraint.matches?(request_with_valid_id)).to be_truthy
     end
 
@@ -19,8 +25,24 @@ describe Constraints::ResourceConstraint do
       expect(constraint.matches?(request_with_missing_id)).to be_falsy
     end
 
+    it 'is not accepted with a missing category' do
+      expect(constraint.matches?(request_with_missing_category)).to be_falsy
+    end
+
+    it 'is not accepted with a missing viewname' do
+      expect(constraint.matches?(request_with_missing_viewname)).to be_falsy
+    end
+
     it 'is not accepted with an invalid 4x4 id parameter' do
       expect(constraint.matches?(request_with_invalid_id)).to be_falsy
+    end
+
+    it 'is accepted with unicode category parameter' do
+      expect(constraint.matches?(request_with_unicode_category)).to be_truthy
+    end
+
+    it 'is accepted with unicode name parameter' do
+      expect(constraint.matches?(request_with_unicode_viewname)).to be_truthy
     end
 
   end

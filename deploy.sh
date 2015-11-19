e4ae14eb2f012fa4e59e73595de692b277ae0c7b
Trey Cordova <trey.cordova@socrata.com>
2015-11-18 23:50:57 -0800

    More robust changes check in gh-pages;

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,19 @@
 if [[ -z $(git status -s) ]]; then
   bundle exec middleman build
   git checkout gh-pages
+
   ls | grep -v "build" | xargs -o rm -rf
   mv build/* .
   rm -rf build/
-  git add -A
-  git commit -am "Automated Deployment $(date -u)"
-  git push origin gh-pages
+
+  if [[ -n $(git status -s) ]]; then
+    git add -A
+    git commit -am "Automated Deployment $(date -u)"
+    git push origin gh-pages
+  else
+    echo >&2 "There are no changes to deploy."
+    exit 1
+  fi
 else
   echo >&2 "Please commit of stash your changes."
   exit 1

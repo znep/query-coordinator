f9bd3aa947b29b9b3e2b0f0a3af9c8e3791d983d
Trey Cordova <trey.cordova@socrata.com>
2015-11-18 23:48:16 -0800

    Full deployment script;

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 if [[ -z $(git status -s) ]]; then
   bundle exec middleman build
   git checkout gh-pages
-  ls | grep -v "build"
+  ls | grep -v "build" | xargs -o rm -rf
+  mv build/* .
+  rm -rf build/
+  git add -A
+  git commit -am "Automated Deployment $(date -u)"
+  git push origin gh-pages
 else
   echo >&2 "Please commit of stash your changes."
   exit 1

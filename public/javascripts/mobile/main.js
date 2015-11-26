abd81948141fd00ef0b27553143c3da6e998b378
Omer Buyukoglu <buyomer@gmail.com>
2015-11-26 16:13:24 +0200

    CORE-7522 Hook up DLMobile into SODA and metadata APIs

--- public/javascripts/mobile/main.js
+++ public/javascripts/mobile/main.js
@@ -1,3 +1,92 @@
+(function(root) {
+  'use strict';
+
+  var DOMAIN = 'https://dataspace.demo.socrata.com/metadata/v1/';
+  var DATASET_UID = location.pathname.split('/')[2];
+
+  var _cards;
+  var _datasetId;
+  var _pageName;
+  var _pageId;
+  var _cardView;
+
+  var _$cardContainder;
+
+  function getPageData() {
+    return $.get(DOMAIN + '/page/' + DATASET_UID);
+  }
+
+  function getPageDataset() {
+    return $.get(DOMAIN + '/dataset/' + DATASET_UID);
+  }
+
+  function setupPage() { 
+    // Card container template
+    _$cardContainder = $(
+      [
+        '<div class="component-container">',
+          '<article class="intro-text">',
+            '<h5>Column Chart</h5>',
+            '<p class="intro padding hidden">',
+              '<span class="desc"></span>',
+              '<span class="text-link">more</span>',
+            '</p>',
+            '<div class="all hidden">',
+              '<p class="padding">',
+                '<span class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis magni dolor veritatis saepe quis cum assumenda. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis magni dolor veritatis saepe quis cum assumenda. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis magni dolor veritatis saepe quis cum</span>',
+                '<span class="text-link">less</span>',
+              '</p>',
+            '</div>',
+          '</article>',
+          '<div id="column-chart"></div>',
+        '</div>'
+      ].join('')
+    );
+
+    // Get page data
+    getPageData().success(function(data) {
+      _cards = data.cards;
+      _datasetId = data.datasetId;
+      _pageName = data.name;
+
+      // Set page title
+      document.title = _pageName;
+
+      // Get dataset
+      getPageDataset(_datasetId).done(function(){
+        renderCards(_cards);  
+      });
+    });
+  }
+
+  setupPage();
+
+  
+
+  function renderCards(cards) {
+    var newDate;
+
+    $(cards).each(function(i, card) {
+      switch (card.cardType) {
+        case 'timeline':
+          break;
+        case 'feature':
+          break;
+        case 'column':
+          // window.columnChart();
+
+          break;
+        default:
+          break
+      };
+    });
+  }
+
+ 
+})(window);
+
+
+
 $(function () {
   'use strict';
 

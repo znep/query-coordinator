39c018e45f43b7c705584a8479188bdce6a4ad1b
Cate Uselton <cathode.use@gmail.com>
2015-12-03 11:46:10 -0800

    adds dataset tests for isLayered and getDownloadType

--- karma/oldUx/util/datasetTest.js
+++ karma/oldUx/util/datasetTest.js
@@ -125,6 +125,109 @@ describe('blist.dataset', function () {
     });
   });
 
+  describe('isLayered', function() {
+    var dataset;
+
+    beforeEach(function() {
+      dataset = new Dataset({id: 'test-test'});
+    });
+
+    describe('if the dataset has no layers', function() {
+
+      it('should return false', function() {
+        dataset.metadata = { geo: { layers: 'four-four' }};
+        expect(dataset.isLayered()).to.equal(false);
+      });
+
+    });
+
+    describe('if the view is a derived view with no added datasets or layers', function() {
+
+      it('should return false', function() {
+        dataset.displayFormat = { viewDefinitions: [ {uid: 'self' }, {uid: 'self'} ] }
+        expect(dataset.isLayered()).to.equal(false);
+      });
+
+    });
+
+    describe('if the dataset has direct layers', function() {
+
+      it('should return true', function() {
+        dataset.metadata = { geo: { layers: 'four-four,meow-meow' }};
+        expect(dataset.isLayered()).to.equal(true);
+      });
+
+    });
+
+    describe('if the view has layers from an added dataset', function() {
+
+      it('should return true', function() {
+        dataset.displayFormat = { viewDefinitions: [ {uid: 'self' }, {uid: 'meow-meow'} ] }
+        expect(dataset.isLayered()).to.equal(true);
+      });
+    });
+  });
+
+  describe('getDownloadType', function() {
+
+    beforeEach(function() {
+      dataset = new Dataset({id: 'four-four'});
+    });
+
+    describe('if the dataset is in the old backend and not geospatial', function() {
+
+      it('should have a download type of obe_normal', function() {
+        dataset.newBackend = false;
+        expect(dataset.getDownloadType()).to.equal('obe_normal');
+      });
+    });
+
+    describe('if the dataset is in the new backend and not geospatial', function() {
+
+      it('should have a download type of nbe_normal', function() {
+        dataset.newBackend = true;
+        expect(dataset.getDownloadType()).to.equal('nbe_normal');
+      });
+    });
+
+    describe('if the dataset is in the old backend and is geospatial, with no layers', function() {
+
+      it('should have a download type of obe_monolayer_geo', function() {
+        dataset.newBackend = false;
+        dataset.metadata = { geo: {} };
+        expect(dataset.getDownloadType()).to.equal('obe_monolayer_geo');
+      });
+    });
+
+    describe('if the dataset is in the new backend and is geospatial, with no layers', function() {
+
+      it('should have a download type of nbe_monolayer_geo', function() {
+        dataset.newBackend = true;
+        dataset.metadata = { geo: {} };
+        expect(dataset.getDownloadType()).to.equal('nbe_monolayer_geo');
+      });
+    });
+
+    describe('if the dataset is in the old backend and is geospatial, with some layers', function() {
+
+      it('should have a download type of obe_multilayer_geo', function() {
+        dataset.newBackend = false;
+        dataset.metadata = { geo: { layers: 'test-test,meow-meow' } };
+        expect(dataset.getDownloadType()).to.equal('obe_multilayer_geo');
+      });
+    });
+
+    describe('if the dataset is in the new backend and is geospatial, with some layers', function() {
+
+      it('should have a download type of nbe_multilayer_geo', function() {
+        dataset.newBackend = true;
+        dataset.metadata = { geo: { layers: 'test-test,meow-meow' } };
+        expect(dataset.getDownloadType()).to.equal('nbe_multilayer_geo');
+      });
+    });
+  });
+
+
   function generateRejectedPromise(payload) {
     var deferred = $.Deferred();
 

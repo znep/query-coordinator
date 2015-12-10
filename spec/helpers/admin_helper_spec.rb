e7aeddc8f3b1d92aee3b6bc36dc69f97659906f4
Randy Antler <randy.antler@socrata.com>
2015-12-10 15:04:55 -0800

    EN-813 Fix nil reference error when metadata_fields "fields" is not present

--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -7,4 +7,19 @@ describe AdminHelper do
       expect(helper.form_button('/', 'text')).to match(/^<form/)
     end
   end
+
+  describe 'a11y_metadata_fieldset_summary' do
+    it 'returns an a11y_summary with non-empty "fields" value' do
+      metadata_fields = { 'fields' => [ { 'name' => 'anemone' } ] }
+      columns = []
+      expect(helper.a11y_metadata_fieldset_summary(metadata_fields, columns))
+    end
+
+    it 'returns an a11y_summary with empty "fields" value' do
+      metadata_fields = { }
+      columns = []
+      expect(helper.a11y_metadata_fieldset_summary(metadata_fields, columns))
+    end
+  end
+
 end

ae78c7dab57ab7a71786a8b7fcfd305c570fe81f
Chris Laidlaw <chris.laidlaw@socrata.com>
2015-12-15 16:01:17 -0800

    Fix "TypeError: Cannot read property 'digest' of null".

--- app/assets/javascripts/editor/components/StoryPublicationStatus.js
+++ app/assets/javascripts/editor/components/StoryPublicationStatus.js
@@ -1,5 +1,5 @@
 /*
- * A component that renders a story title.
+ * A component that renders the publication status of a story.
  */
 (function($, root) {
 
@@ -14,13 +14,13 @@
     function render() {
       var permissions = storyteller.storyStore.getStoryPermissions(storyUid);
       var storyIsDirty = storyteller.storySaveStatusStore.isStoryDirty();
-      var havePublishedAndDraftDiverged;
+      var publishedAndDraftDiverged;
       var translationKey;
 
       if (permissions && permissions.isPublic) {
-        havePublishedAndDraftDiverged = _havePublishedAndDraftDiverged();
+        publishedAndDraftDiverged = _havePublishedAndDraftDiverged();
 
-        if (storyIsDirty || havePublishedAndDraftDiverged) {
+        if (storyIsDirty || publishedAndDraftDiverged) {
           translationKey = 'status.draft';
         } else {
           translationKey = 'status.published';
@@ -29,14 +29,33 @@
         translationKey = 'status.draft';
       }
 
-      $this.text(I18n.t('editor.settings_panel.publishing_section.{0}'.format(translationKey)));
+      $this.text(
+        I18n.t('editor.settings_panel.publishing_section.{0}'.
+          format(translationKey)
+        )
+      );
     }
 
     function _havePublishedAndDraftDiverged() {
-      var publishedStory = storyteller.storyStore.getStoryPublishedStory(storyUid) || root.publishedStory;
+      var publishedStory =
+        storyteller.storyStore.getStoryPublishedStory(storyUid) ||
+        root.publishedStory;
       var digest = storyteller.storyStore.getStoryDigest(storyUid);
+      var publishedAndDraftDiverged = false;
 
-      return publishedStory.digest !== digest;
+      // Only stories that have been published can have their published and
+      // draft versions diverge. If a story has never been published, storyStore
+      // will return undefined for .getStoryPublishedStory() and the
+      // publishedStory object embedded in the page by the Rails app will be set
+      // to null. Because of the '|| root.publishedStory;' conditional
+      // assignment to publishedStory above, we can be reasonably confident that
+      // we will only ever encounter a JSON representation of the published
+      // story or null.
+      if (publishedStory !== null && publishedStory.hasOwnProperty('digest')) {
+        publishedAndDraftDiverged = publishedStory.digest !== digest;
+      }
+
+      return publishedAndDraftDiverged;
     }
 
     storyteller.storySaveStatusStore.addChangeListener(render);

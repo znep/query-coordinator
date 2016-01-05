264144be4a95a157d1037296872d09a719d88a2b
AEscobarCruz <alejandro.escobar@socrata.com>
2016-01-05 13:49:28 -0800

    logger changed to include proper format for sumo

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,5 +1,5 @@
 Rails.application.config.log_tags = [
-  lambda { |req| Time.now.utc.iso8601 },
+  lambda { |req| Time.now.utc.strftime " %Y-%m-%d %H:%M:%S,%L "},
   lambda { |req| req.host || 'unknown' },
   :uuid,
   lambda { |req| "PID-%.5d" % Process.pid }

55cd36ed9af20f2cf888d05906fd872b7a2a1123
Anita <asovay@gmail.com>
2016-01-13 11:49:35 -0800

    EN-968: Variable name change, custom_themes -> custom_theme_configs

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -113,9 +113,9 @@ class StoriesController < ApplicationController
     if @story
       @inspiration_category_list = InspirationCategoryList.new.to_parsed_json
       theme_list = ThemeList.new
-      @standard_themes = theme_list.standard_theme_list.sort_by { |key| key["title"] }
-      @custom_themes = theme_list.custom_theme_list.sort_by { |key| key["title"] }
-      @custom_theme_configs = theme_list.custom_themes
+      @standard_theme_configs = theme_list.standard_theme_list.sort_by { |key| key["title"] }
+      @custom_theme_configs = theme_list.custom_theme_list.sort_by { |key| key["title"] }
+      @custom_themes = theme_list.custom_themes
       @published_story = PublishedStory.find_by_uid(params[:uid])
 
       respond_to do |format|

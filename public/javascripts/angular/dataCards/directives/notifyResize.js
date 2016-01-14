b3ff099b016930c95d0e91ea6a21fe36a86ad874
Andrew Harrison <andrew.harrison@socrata.com>
2016-01-14 14:48:52 -0800

    EN-1264: Disable use-strict pragma check in dev mode
    This is a temporary workaround because ESLint is on the verge of
    supporting rules associated with file globs.
    See https://github.com/eslint/eslint/issues/3611 for details.

--- public/javascripts/angular/dataCards/directives/notifyResize.js
+++ public/javascripts/angular/dataCards/directives/notifyResize.js
@@ -7,8 +7,6 @@ const angular = require('angular');
 // If the div is resized, myDivResized will be broadcast with the new size as the arguments to the event
 // contained within an object of the form: { height: N, width: N } -- not including outer margins.
 angular.module('dataCards.directives').directive('notifyResize', function() {
-  'use strict';
-
   return {
     restrict: 'A',
     link: function($scope, element, attrs) {

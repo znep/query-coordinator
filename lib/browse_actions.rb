4b2517ed3fe2eb879c39bbea30098ad76ecfa9ba
Jason Kroll <jason.kroll@socrata.com>
2016-02-10 12:23:34 -0800

    Move federation logic into Federation model

--- lib/browse_actions.rb
+++ lib/browse_actions.rb
@@ -119,42 +119,8 @@ module BrowseActions
     }
   end
 
-  def federations
-    @federations ||= Federation.find.select do |f|
-      f.targetDomainCName ==
-        CurrentDomain.cname &&
-        f.lensName.empty? &&
-        f.acceptedUserId.present? &&
-        f.sourceDomainCName != CurrentDomain.cname # protect against degenerate case
-    end
-  end
-
-  def federated_search_boosts
-    federations.each_with_object({}) do |f, hash|
-      hash[f.sourceDomainCName] = f.searchBoost
-    end
-  end
-
-  # Look up the domain cnames for Cetera
-  def federated_domain_cnames(federation_id)
-    home = [CurrentDomain.cname]
-
-    if federation_id.blank?
-      # All domains in the federation
-      home + federations.map(&:sourceDomainCName)
-
-    elsif federation_id.to_i == CurrentDomain.domain.id
-      # Just the home domain
-      home
-
-    else
-      # Just a federated domain--potentially empty
-      federations.find_all { |fed| fed.id == federation_id.to_i }.map(&:sourceDomainCName)
-    end
-  end
-
   def federated_facet
-    all_feds = federations.sort_by(&:text).map do |fed|
+    all_feds = Federation.federations.sort_by(&:text).map do |fed|
       cname = fed.sourceDomainCName
       {
         text: cname,
@@ -434,14 +400,14 @@ module BrowseActions
 
             # Domain ids have to be translated to domain cnames for Cetera
             fed_id = browse_options[:search_options][:federation_filter]
-            browse_options[:search_options][:domains] = federated_domain_cnames(fed_id)
+            browse_options[:search_options][:domains] = Federation.federated_domain_cnames(fed_id)
 
             # If you try to federate from a domain that didn't approve it, no federation for you!
             unless browse_options[:search_options][:domains].present?
               raise "Invalid federated domain id: #{fed_id} for domain #{CurrentDomain.cname}"
             end
 
-            browse_options[:search_options][:domain_boosts] = federated_search_boosts
+            browse_options[:search_options][:domain_boosts] = Federation.federated_search_boosts
             browse_options[:search_options][:categories] = selected_category_and_any_children(browse_options)
             Cetera.search_views(browse_options[:search_options])
           else

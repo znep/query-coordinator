c33c01035d0ed0321b12297b9f6c76f510e1268d
Anita Wang <anita.wang@socrata.com>
2016-02-18 17:24:36 -0800

    EN-3077: Google font loading with every change in Firefox

--- app/assets/javascripts/editor/RichTextEditor.js
+++ app/assets/javascripts/editor/RichTextEditor.js
@@ -136,9 +136,19 @@
       return _lastContentHeight;
     };
 
+
+    /**
+     * Applies the Google Font embed code to the rich text editor.
+     * Will only add this code if it has not already been added.
+     */
     this.applyThemeFont = function(theme) {
       var headElement = _editorElement[0].contentDocument.querySelector('head');
-      $(headElement).append($(theme));
+      var $theme = $(theme);
+      var href = $theme.attr('href');
+
+      if ($(headElement).find('link[href="{0}"]'.format(href)).length === 0) {
+        $(headElement).append($(theme));
+      }
     };
 
     // Add a `themeName` class to the html root of the iframe

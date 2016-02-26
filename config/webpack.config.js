feba03b1381525868e58749f8a440af262dbe096
isotluravioli <cagri.ekin@gmail.com>
2016-02-26 14:49:23 +0200

    Mobile qfb component configuration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -224,6 +224,30 @@ function generateDataLensMobileConfig() {
     output: {
       filename: isProduction() ? 'mobile/[name]-[hash].js' : 'mobile/[name].js'
     },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel',
+          query: {
+            presets: ['react', 'es2015']
+          }
+        },
+        {
+          test: /\.scss$/,
+          loader: 'style!css!autoprefixer-loader!sass'
+        },
+        {
+          test: /\.svg/,
+          loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        },
+        {
+          test: /\.png$/,
+          loader: 'url-loader?limit=100000'
+        }
+      ]
+    },
     plugins: [
       new ManifestPlugin({
         fileName: 'data-lens-mobile-manifest.json'

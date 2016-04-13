53f1820379230f954b533020adc0845138ad3ea6
bjorn <tie.372@gmail.com>
2016-04-13 16:15:54 -0700

    EN-4870: Make webpack great again;
    (cherry picked from commit 0693c1aca5b6457d8cb95068ef1a1d12f4b71b27)

---
+++ config/webpack/dataset-landing-page.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var fs = require('fs');
+var _ = require('lodash');
+
+var common = require('./common');
+var identifier = path.basename(__filename, '.config.js');
+
+module.exports = _.defaultsDeep({
+  context: path.resolve(common.root, 'public/javascripts/datasetLandingPage'),
+  entry: './main',
+  output: common.getOutput(identifier),
+  resolve: {
+    alias: {
+      dotdotdot: 'dotdotdot/src/js/jquery.dotdotdot.min.js'
+    }
+  },
+  plugins: common.plugins.concat(common.getManifestPlugin(identifier))
+}, require('./base'));

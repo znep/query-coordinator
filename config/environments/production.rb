86e20cb78312ab33918488acb63837ee15757e2f
Randy Antler <randy.antler@socrata.com>
2016-04-18 15:18:56 -0700

    ENG: Add missing cache_store
    Signed-off-by: Randy Antler <randy.antler@socrata.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.cache_store = :mem_cache_store, '127.0.0.1:22121', { :namespace => 'webapp', :check_size => false }
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?

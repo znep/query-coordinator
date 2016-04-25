6d05cb01f7cd1fb919e9a27906c7abce1d92265d
Randy Antler <randy.antler@socrata.com>
2016-04-25 10:14:13 -0700

    Clean up license - remove unused files

--- engine/lib/tasks/chrome_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :chrome do
-#   # Task goes here
-# end
--- engine/spec/chrome_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChromeTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Chrome
-  end
-end

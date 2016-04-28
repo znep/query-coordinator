da5e9bda1e49c9ecb8a6a488514ebe79fba3fa48
Kaida Masaki <kaida.masaki@socrata.com>
2016-04-28 15:58:39 -0700

    Fix message in dev server start script

--- bin/start_dev_server.sh
+++ bin/start_dev_server.sh
@@ -4,5 +4,5 @@ set -e
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BASEDIR="$(dirname "${REALPATH}")/.."
 
-echo "Running on port 80 requires sudo, using 'sudo' to start nginx." >&2
+echo "Running on port 443 requires sudo, using 'sudo' to start nginx." >&2
 sudo nginx -c "${BASEDIR}/dev-server/nginx.conf" -g 'error_log /dev/stderr info;'

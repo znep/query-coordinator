7705bb26c495e24b692e9926ffb37887c52986ae
Dylan Bussone <dylan.bussone@socrata.com>
2016-05-13 15:14:31 -0700

    Remove final references to site_chrome_admin

--- app/helpers/site_chrome_admin_helper.rb
+++
@@ -1,9 +0,0 @@
-module SiteChromeAdminHelper
-  def tab_link_classnames(index)
-    "tab-link#{' current' if index == 0}"
-  end
-
-  def tab_section_classnames(index)
-    "tab-content#{' current' if index == 0}"
-  end
-end
--- app/helpers/site_chrome_helper.rb
+++ app/helpers/site_chrome_helper.rb
@@ -1,7 +1,11 @@
 # Helper for SiteChromeController and its views
 module SiteChromeHelper
-  def site_chrome_default_properties
-    {
-    }
+  def tab_link_classnames(index)
+    "tab-link#{' current' if index == 0}"
   end
+
+  def tab_section_classnames(index)
+    "tab-content#{' current' if index == 0}"
+  end
+
 end

21ac914099ebc49b5bb3db4e0e7c66534fb702dd
Dylan Bussone <dylan.bussone@socrata.com>
2016-05-17 11:18:12 -0700

    Fix lint error

--- public/javascripts/screens/site-chrome.js
+++ public/javascripts/screens/site-chrome.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
   $('ul.tabs li').click(function() {
     var clickedTabId = $(this).data('tab-id');
 
-    $('ul.tabs li').removeClass('current');
-    $('.tab-content').removeClass('current');
-
+    $('ul.tabs li, .tab-content').removeClass('current');
     $(this).addClass('current');
+
     $('.tab-content[data-tab-id="{0}"]'.format(clickedTabId)).addClass('current');
-  })
+  });
 });

e1e29cfb7ca4d6186560d531193e7e0eeffde7e2
Nate Moore <nathaniel.moore@socrata.com>
2016-05-17 14:59:31 -0700

    Add spec for whats_new_helper

---
+++ spec/helpers/whats_new_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe WhatsNewHelper do
+  it 'should parse dates properly' do
+    news = {}
+    news['articles'] = [
+      { 'updated_at' => '2016-05-01T00:00:00Z' },
+      { 'updated_at' => '2250-01-01T16:20:00Z' },
+      { 'updated_at' => '1992-03-23T10:10:10Z' }
+    ]
+
+    WhatsNewHelper.parse_dates(news)
+
+    expect(news['articles'][0]['parsed_datetime']).to eq '1 May 2016'
+    expect(news['articles'][1]['parsed_datetime']).to eq '1 January 2250'
+    expect(news['articles'][2]['parsed_datetime']).to eq '23 March 1992'
+  end
+end

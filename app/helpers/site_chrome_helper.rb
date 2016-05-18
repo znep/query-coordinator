f91a8dee1fd3c1c18348e246df838d1567679566
Dylan Bussone <dylan.bussone@socrata.com>
2016-05-18 15:09:39 -0700

    EN-4378: Remember tabs when we switch them for reloading

--- app/helpers/site_chrome_helper.rb
+++ app/helpers/site_chrome_helper.rb
@@ -1,13 +1,5 @@
 # Helper for SiteChromeController and its views
 module SiteChromeHelper
-  def tab_link_classnames(index)
-    "tab-link#{' current' if index == 0}"
-  end
-
-  def tab_section_classnames(index)
-    "tab-content#{' current' if index == 0}"
-  end
-
   def social_share_link(type)
     @site_chrome.content['general']['social_shares'].detect{ |x| x['type'] == type }['url']
   end
--- public/javascripts/screens/site-chrome.js
+++ public/javascripts/screens/site-chrome.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
+  // Show appropriate tab given the url hash. Ex: /site_chrome#tab=general
+  var urlHash = document.location.hash.substring(1);
+  var activeTabId = urlHash.substr(urlHash.indexOf('tab=')).split('&')[0].split('=')[1];
+  if (activeTabId) {
+    showTab(activeTabId);
+  } else {
+    // TODO - No tab specified, show first tab
+    var firstTabId = $('ul.tabs li').first().data()['tabId'] || 'general';
+    showTab(firstTabId);
+  }
+
+  // Change active tab and tab-content on click
   $('ul.tabs li').click(function() {
     var clickedTabId = $(this).data('tab-id');
-
-    $('ul.tabs li, .tab-content').removeClass('current');
-    $(this).addClass('current');
-
-    $('.tab-content[data-tab-id="{0}"]'.format(clickedTabId)).addClass('current');
+    showTab(clickedTabId);
   });
 });
+
+function showTab(tabId) {
+  $('ul.tabs li, .tab-content').removeClass('current');
+  $('.tab-content[data-tab-id="{0}"], .tab-link[data-tab-id="{0}"]'.
+    format(tabId)).addClass('current');
+};

e7607764b1887dbfc5b0e58c12bad32ca2fed169
Randy Antler <randy.antler@socrata.com>
2016-06-01 15:49:27 -0700

    ENG: en-6330 fix broken minitest unit tests
    Signed-off-by: Randy Antler <randy.antler@socrata.com>

---
+++ spec/lib/cetera_spec.rb
@@ -0,0 +1,197 @@
+require 'rails_helper'
+
+describe Cetera do
+  include TestHelperMethods
+
+  it 'should handle cetera soql params as query' do
+    params = Cetera.cetera_soql_params(sample_search_options)
+
+    expected_cetera_params.each do |key, expected|
+      actual = params[key]
+      expect(actual).to eq(expected)
+    end
+  end
+
+  it 'should test_cetera_limit_type_translator' do
+    frontend_to_cetera = {
+      'data_lens' => 'datalenses',
+      'story' => 'stories',
+      'pulse' => 'pulses',
+      'tables' => 'datasets',
+      'charts' => 'charts',
+      'maps' => 'maps',
+      'calendars' => 'calendars',
+      'filters' => 'filters',
+      'href' => 'links',
+      'blob' => 'files',
+      'forms' => 'forms',
+      'apis' => 'apis'
+    }
+
+    frontend_to_cetera.each do |frontend_type, cetera_type|
+      expect(Cetera.translate_display_type(frontend_type, 'dataset')).to eq(cetera_type)
+    end
+
+    expect(Cetera.translate_display_type('tables', 'view')).to eq('filters')
+  end
+
+  describe 'Displays' do
+    it 'should test_dataset' do
+      dataset = Cetera::Displays::Dataset
+      expect(dataset.name).to eq('table')
+      expect(dataset.title).to eq('Table')
+      expect(dataset.type).to eq('blist')
+    end
+
+    it 'should test_file' do
+      file = Cetera::Displays::File
+      expect(file.name).to eq('non-tabular file or document')
+      expect(file.type).to eq('blob')
+    end
+
+    it 'should test_link' do
+      link = Cetera::Displays::Link
+      expect(link.name).to eq('external dataset')
+      expect(link.type).to eq('href')
+    end
+
+    it 'should test_map' do
+      map = Cetera::Displays::Map
+      expect(map.name).to eq('map')
+      expect(map.type).to eq('map')
+    end
+  end
+
+  describe 'CeteraResultRow' do
+    let(:sample_result_row) do
+      hash = JSON.parse(File.read("#{Rails.root}/test/fixtures/cetera_row_results.json"))
+      Cetera::CeteraResultRow.new(hash)
+    end
+
+    it 'should test_cetera_result_row_delegators' do
+      expect(sample_result_row.id).to eq('r8ea-mq83')
+      expect(sample_result_row.link).to eq('https://data.cityofchicago.org/d/r8ea-mq83')
+      expect(sample_result_row.name).to eq('Performance Metrics - Innovation & Technology - Site Availability')
+      expect(sample_result_row.description).to match(/availability metrics/)
+      expect(sample_result_row.type).to eq('dataset')
+      expect(sample_result_row.categories).to eq(['Administration & Finance'])
+      expect(sample_result_row.tags).to eq(['administration', 'finance'])
+    end
+
+    it 'should test_cetera_result_row_other_methods' do
+      expect(sample_result_row.default_page).to eq('1234-abcd')
+      expect(sample_result_row.display_class).to eq('Blist')
+      expect(sample_result_row.display_title).to eq('Table')
+      expect(sample_result_row.domainCName).to eq('data.cityofchicago.org')
+      expect(sample_result_row.federated?).to eq(true)
+      expect(sample_result_row.icon_class).to eq('icon')
+      expect(sample_result_row.story?).to eq(false)
+    end
+  end
+
+  it 'should test_cetera_sort_order_translator' do
+    frontend_to_cetera = {
+      nil => 'relevance',
+      'relevance' => 'relevance',
+      'most_accessed' => 'page_views_total',
+      'alpha' => 'name',
+      'newest' => 'createdAt',
+      'oldest' => 'createdAt ASC',
+      'last_modified' => 'updatedAt'
+    }
+
+    frontend_to_cetera.each do |frontend_type, cetera_type|
+      expect(Cetera.translate_sort_by(frontend_type)).to eq(cetera_type)
+    end
+
+    ['bogus', 'sort by', 'orders'].each do |bogus|
+      expect { Cetera.translate_sort_by(bogus) }.to raise_error(KeyError)
+    end
+  end
+
+  describe 'search_views' do
+    it 'should test_cetera_search_views_raises_timeout_error_on_timeout' do
+      query = { domains: ['example.com'] }
+      stub_request(:get, APP_CONFIG.cetera_host + '/catalog/v1').
+        with(query: Cetera.cetera_soql_params(query)).
+        to_timeout
+
+      expect { Cetera.search_views(query, {}, 'Unvailable') }.to raise_error(Timeout::Error)
+    end
+
+    it 'should test_cookies_and_request_id_get_passed_along_to_cetera' do
+      query = { domains: ['example.com'] }
+      cookies = { 'i am a cookie' => 'of oatmeal and raisins',
+                  'i am also a cookie' => 'of chocolate chips' }
+      request_id = 'iAmProbablyUnique'
+
+      stub_request(:get, APP_CONFIG.cetera_host + '/catalog/v1').
+        with(headers: { 'Cookie' => cookies.map { |key, val| "#{key}=#{val}" }.join('; '),
+                        'X-Socrata-RequestId' => request_id },
+             query: Cetera.cetera_soql_params(query)).
+        to_return(status: 500, body: 'I cannot be parsed!')
+
+      # Unsuccessful response returns false, just sayin'
+      expect(Cetera.search_views(query, cookies, request_id)).to eq(false)
+    end
+
+    it 'should test_for_user_param_gets_passed_along_to_cetera' do
+      domain = 'data.redmond.gov'
+      init_current_domain
+      allow(CurrentDomain).to receive(:cname).and_return(domain)
+      query = {
+        domains: [domain],
+        for_user: '7kqh-9s5a'
+      }
+
+      cetera_soql_params = Cetera.cetera_soql_params(query)
+      expect(cetera_soql_params).to include(:for_user)
+
+      response = {
+        'results' => [],
+        'resultSetSize' => 0,
+        'timings' => { 'serviceMillis' => 118, 'searchMillis' => [2, 2] }
+      }
+
+      stub_request(:get, APP_CONFIG.cetera_host + '/catalog/v1').
+        with(query: cetera_soql_params).
+        to_return(status: 200, body: response.to_json)
+
+      res = Cetera.search_views(query, {}, 'funnyId')
+
+      expect(res.data.to_hash).to eq(response)
+      expect(res.results).to be_empty
+    end
+  end
+
+  private
+
+  def sample_search_options
+    {
+      domains: ['data.cityofchicago.org', 'data.example.com', 'datacatalog.cookcountyil.gov'],
+      domain_boosts: { 'data.example.com' => 0.808, 'datacatalog.cookcountyil.gov' => 0.909 },
+      limitTo: 'tables',
+      q: 'giraffes are whack!&@*!',
+      limit: 10,
+      page: 4,
+      categories: ['Traffic, Parking, and Transportation', 'And another thing'],
+      metadata_tag: { 'Dataset-Information_Superhero' => 'Superman' }
+    }
+  end
+
+  # CGI.escape happens in search_views params.to_query
+  def expected_cetera_params
+    {
+      domains: 'data.cityofchicago.org,data.example.com,datacatalog.cookcountyil.gov',
+      # search_context (CurrentDomain.cname) changes with test context so is not tested
+      boostDomains: { 'data.example.com' => 0.808, 'datacatalog.cookcountyil.gov' => 0.909 },
+      only: 'datasets',
+      q: 'giraffes are whack!&@*!',
+      limit: 10,
+      offset: 30,
+      categories: ['Traffic, Parking, and Transportation', 'And another thing'],
+      'Dataset-Information_Superhero' => 'Superman'
+    }
+  end
+
+end

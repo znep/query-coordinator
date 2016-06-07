3052c9deb4bd97bf1c07e1e105820981da0553db
Randy Antler <randy.antler@socrata.com>
2016-06-07 13:57:50 -0700

    EN-6355 Make sure asset helper paths are correct

--- engine/lib/site_chrome_helper.rb
+++ engine/lib/site_chrome_helper.rb
@@ -7,11 +7,13 @@ module SiteChromeHelper
   end
 
   def site_chrome_stylesheet_tag
+    # Note: The _inclusion_ of the leading / is critcial to the helper generating the correct digest path
     stylesheet_link_tag('/chrome/themes/custom.css', media: 'all')
   end
 
   def site_chrome_javascript_tag
-    javascript_include_tag('/asset_pipeline/chrome/application')
+    # Note: The _lack_ of a leading / is critical to this helper generating the correct digest path
+    javascript_include_tag('chrome/application')
   end
 
 end

67a768043080bb4309eb876b6bb1ac256b5ccc7d
Randy Antler <randy.antler@socrata.com>
2016-06-09 17:49:01 -0700

    EN-6969 Add nginx config to support serving static assets
    Bring local docker file in sync with main file
    
    Add daemonized nginx to frontend startup sequence
    
    Incidentally add quotes to paths to protect against whitespace in the APP_DIR path.
    
    Add rake assets:precompile step to ensure that any assets provided by asset pipeline
    are properly digested and compressed.
    
    Change unicorn listen port to from 3000 to 3333
    
    Update readme with docker usage examples
    
    Add guard clause around starting nginx
    
    Fix invalid polaroid_hostname in default config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-listen Integer(ENV['UNICORN_LISTEN_PORT'] || 3000)
+listen Integer(ENV['UNICORN_LISTEN_PORT'] || 3333)
 worker_processes Integer(ENV['UNICORN_WORKER_PROCESSES'] || ENV['WORKER_PROCESSES'] || 4)
 timeout Integer(ENV['UNICORN_TIMEOUT'] || 300)
 

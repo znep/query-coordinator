5d16354ed6d8107fa8d80088fddc428c5c849728
Michael Brown <michaelb990@gmail.com>
2016-06-14 10:34:16 -0700

    EN-7063: update start_dev_server script to set user/group for mac os x (#2797)

--- bin/start_dev_server.sh
+++ bin/start_dev_server.sh
@@ -4,5 +4,12 @@ set -e
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BASEDIR="$(dirname "${REALPATH}")/.."
 
+CONFIG_DIRECTIVES='error_log /dev/stderr info;'
+if [[ $OSTYPE == darwin* ]]; then
+  # nginx needs to run with valid user/group or permission errors occur
+  # when writing to the proxy_temp directory on mac os x.
+  CONFIG_DIRECTIVES="$CONFIG_DIRECTIVES user $(whoami) staff;"
+fi
+
 echo "Running on port 443 requires sudo, using 'sudo' to start nginx." >&2
-sudo nginx -c "${BASEDIR}/dev-server/nginx.conf" -g 'error_log /dev/stderr info;'
+sudo nginx -c "${BASEDIR}/dev-server/nginx.conf" -g "$CONFIG_DIRECTIVES"

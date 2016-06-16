cabc0b2c742428cc3b9cd1c4cfe61335c80102e8
Dylan Bussone <dylan.bussone@socrata.com>
2016-06-15 17:27:02 -0700

    ENG - Remove styleguide css from public

--- engine/tools/postinstall.sh
+++ engine/tools/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p public/stylesheets/styleguide/css
+# mkdir -p public/stylesheets/styleguide/css
 mkdir -p public/stylesheets/styleguide/fonts
-cp node_modules/socrata-styleguide/dist/css/* public/stylesheets/styleguide/css
+# cp node_modules/socrata-styleguide/dist/css/* public/stylesheets/styleguide/css
 cp node_modules/socrata-styleguide/dist/fonts/* public/stylesheets/styleguide/fonts

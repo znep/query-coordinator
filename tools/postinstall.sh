800fd9909cdd5b4a04880d2670859e9fc5559d6e
bjorn <tie.372@gmail.com>
2016-06-15 17:36:35 -0700

    EN-7046: Prevent styleguide collisions with socrata_site_chrome;

--- tools/postinstall.sh
+++ tools/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p public/stylesheets/styleguide/css
-mkdir -p public/stylesheets/styleguide/fonts
-cp node_modules/socrata-styleguide/dist/css/* public/stylesheets/styleguide/css
-cp node_modules/socrata-styleguide/dist/fonts/* public/stylesheets/styleguide/fonts
+mkdir -p public/stylesheets/socrata-styleguide/css
+mkdir -p public/stylesheets/socrata-styleguide/fonts
+cp node_modules/socrata-styleguide/dist/css/* public/stylesheets/socrata-styleguide/css
+cp node_modules/socrata-styleguide/dist/fonts/* public/stylesheets/socrata-styleguide/fonts

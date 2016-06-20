a37f6420b2282056836bb4a8936e447ff797e9e7
Kristina Hjertberg <kristina.hjertberg@socrata.com>
2016-06-20 12:07:03 -0700

    EN-7242: Fix spatial lens javascript breaking
    (cherry picked from commit 214d136933e5b52b16aca3cd71648ea1222704c8)

--- public/javascripts/src/components/georegions/georegion-status.js
+++ public/javascripts/src/components/georegions/georegion-status.js
@@ -1,7 +1,11 @@
+// These statuses come from a variety of sources:
+// - 'disabled' and 'enabled' come from a specific curated region
+// - 'Failure' comes from ImportStatusService
+// - 'progress' comes from CuratedRegionJob
 const Status = {
   DISABLED: 'disabled',
   ENABLED: 'enabled',
-  FAILED: 'failed',
+  FAILED: 'Failure',
   PROGRESS: 'progress'
 };
 

ae1e57bc471408aa45cf6e91ab46aaa6e0b8132e
isotluravioli <cagri.ekin@gmail.com>
2016-06-24 10:25:12 +0300

    EN-7283 addressed suggestions

--- public/javascripts/adminGoals/components/Alert.js
+++ public/javascripts/adminGoals/components/Alert.js
@@ -6,24 +6,24 @@ import { connect } from 'react-redux';
 import { hideAlert } from '../actions/alertActions';
 
 class Alert extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    let alertClass = classNames('alert', this.props.label, {
-      hidden: _.isUndefined(this.props.label) || _.isEmpty(this.props.label)
-    });
-    let message = this.props.message || <span><b>Oh no!</b> Something went wrong and our team has been notified.
-        In the meantime, try your action again or refresh this page.</span>;
+    let alertProps = {
+      className: classNames('alert', this.props.label, {
+        hidden: _.isUndefined(this.props.label) || _.isEmpty(this.props.label)
+      }),
+      onClick: this.props.hide,
+      dangerouslySetInnerHTML: {
+        __html: this.props.message ||
+        this.props.translations.getIn(['admin', 'listing', 'default_alert_message'])
+      }
+    };
 
-    return <div className={ alertClass } onClick={ this.props.hide }>
-      { message }
-    </div>;
+    return <div { ...alertProps } />;
   }
 }
 
 const mapStateToProps = state => ({
+  translations: state.getIn(['goalTableData', 'translations']),
   label: state.getIn(['goalTableData', 'alert', 'label']),
   message: state.getIn(['goalTableData', 'alert', 'message'])
 });

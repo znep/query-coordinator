aad866d3926177e995b523418a612587094e2691
Andrew Harrison <andrew.harrison@socrata.com>
2016-06-28 12:33:39 -0700

    EN-3128: Refactor StorytellerService to use Diplomat

--- spec/services/storyteller_service_spec.rb
+++ spec/services/storyteller_service_spec.rb
@@ -2,23 +2,11 @@ require 'rails_helper'
 
 RSpec.describe StorytellerService do
   describe '#active?' do
-    let(:consul_service_uri) { 'http://some-consul-server.socrata.net' }
-    let(:active_version_url) { "#{consul_service_uri}/v1/kv/storyteller/active_version" }
-    let(:consul_status) { 200 }
-    let(:consul_response) { nil }
-
-    before do
-      allow(Rails.application.config).to receive(:consul_service_uri).and_return(consul_service_uri)
-
-      stub_request(:get, active_version_url).
-         to_return(status: consul_status, body: consul_response.to_json, headers: { 'Content-Type' => 'application/json' })
-    end
-
     let(:subject) { StorytellerService.active? }
 
     context 'when no connection to consul' do
       before do
-        allow(HTTParty).to receive(:get).and_raise
+        allow_any_instance_of(Diplomat::Kv).to receive(:get).and_raise(Faraday::ConnectionFailed.new(nil))
       end
 
       it 'is true' do
@@ -27,7 +15,9 @@ RSpec.describe StorytellerService do
     end
 
     context 'when consul does not contain key' do
-      let(:consul_status) { 404 }
+      before do
+        allow_any_instance_of(Diplomat::Kv).to receive(:get).and_raise(Diplomat::KeyNotFound.new)
+      end
 
       it 'is true' do
         expect(subject).to eq(true)
@@ -35,22 +25,11 @@ RSpec.describe StorytellerService do
     end
 
     context 'when key and value exist in consul' do
-      let(:active_version) { 'abc' }
-      let(:current_version) { 'abc' }
-      let(:consul_response) do
-        [
-          {
-            'CreateIndex' => 284415428,
-            'ModifyIndex' => 284736932,
-            'LockIndex' => 0,
-            'Key' => 'storyteller/active_version',
-            'Flags' => 0,
-            'Value' => Base64.encode64(active_version)
-          }
-        ]
-      end
+      let(:active_version) { nil }
+      let(:current_version) { nil }
 
       before do
+        allow_any_instance_of(Diplomat::Kv).to receive(:get).and_return(active_version)
         allow(Rails.application.config).to receive(:version).and_return(current_version)
       end
 
@@ -73,18 +52,8 @@ RSpec.describe StorytellerService do
       end
 
       context 'when value in consul is nil' do
-        let(:consul_response) do
-          [
-            {
-              'CreateIndex' => 284415428,
-              'ModifyIndex' => 284736932,
-              'LockIndex' => 0,
-              'Key' => 'storyteller/active_version',
-              'Flags' => 0,
-              'Value' => nil
-            }
-          ]
-        end
+        let(:active_version) { nil }
+        let(:current_version) { 'abc' }
 
         it 'is true' do
           expect(subject).to eq(true)

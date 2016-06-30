e96f627201fb91b4a9be155b7a2b75509afc6908
bjorn <tie.372@gmail.com>
2016-06-30 09:55:12 -0700

    EN-6549: Add missing property to APP_CONFIG;
    (cherry picked from commit 90e389fe3c5e4eb0da0c44b5bc280d1b49807b5a)

--- lib/app_config.rb
+++ lib/app_config.rb
@@ -57,6 +57,7 @@ class AppConfig < Hashie::Dash
   property :opendata_ga_tracking_code
   property :standard_ga_tracking_code
   property :airbrake_api_key
+  property :dataset_landing_page_airbrake_api_key
   property :recaptcha_2_site_key
   property :recaptcha_2_secret_token
   property :mixpanel_token

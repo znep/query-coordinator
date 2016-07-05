bc3f509af86ac38b4fdb86d9c910bb53a52b468a
Nate Moore <nathaniel.moore@socrata.com>
2016-07-05 12:13:19 -0700

    EN-6841: Use auth0 login when multiple connections are present

--- public/javascripts/screens/login-auth0.js
+++ public/javascripts/screens/login-auth0.js
@@ -7,12 +7,12 @@
   // Auth0Lock is exported by https://cdn.auth0.com/js/lock-7.14.min.js,
   // sourced in _login_form.html.erb — we can switch this to an import
   // when we get around to updating old UX code.
+  // eslint-disable-next-line no-undef
   var widget = new window.Auth0Lock(blist.configuration.auth0.id, blist.configuration.auth0.uri);
-  if (document.getElementById('auth0login')) {
-    // Only use_auth0 configuration.
-    // In this case, only use_auth0 is set internally, and no other options.
+
+  var showAuth0LoginWidget = function(container, closable) {
     widget.show({
-      container: 'auth0login',
+      container: container,
       callbackURL: blist.configuration.auth0.base_uri + '/auth/auth0/callback',
       focusInput: true,
       responseType: 'code',
@@ -21,8 +21,21 @@
         scope: 'openid profile'
       },
       resetLink: blist.configuration.auth0.base_uri + '/forgot_password',
-      signupLink: blist.configuration.auth0.base_uri + '/signup'
+      signupLink: blist.configuration.auth0.base_uri + '/signup',
+      closable: closable
     });
+
+    if (closable) {
+      widget.on('close', function() {
+        document.querySelector('.auth0login').style.display = 'block';
+      });
+    }
+  };
+
+  if (document.getElementById('auth0login')) {
+    // Only use_auth0 configuration.
+    // In this case, only use_auth0 is set internally, and no other options.
+    showAuth0LoginWidget('auth0login');
   } else {
     // Auth0 configuration with two or more buttons for selection.
     // Configuration is set to add enterprise login options.
@@ -32,6 +45,15 @@
           // The socrata.com connection is rendered locally.
           document.querySelector('.authProvider.socrata').style.display = 'block';
           document.querySelector('.auth0login').style.display = 'none';
+
+          if (document.getElementById('auth0login-socrata-id')) {
+            // if use_auth0_login_flow is on, use auth0 instead of local login
+            showAuth0LoginWidget('auth0login-socrata-id', true);
+          } else {
+            // show the local login flow
+            document.querySelector('.authProvider.socrata').style.display = 'block';
+          }
+
           return;
         } else if (connection) {
           // Redirect to the connection's authorization path.

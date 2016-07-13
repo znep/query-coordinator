1489bd7893844e0392974c971d47e62d37066e64
Dylan Bussone <dylanbussone@gmail.com>
2016-07-13 13:30:56 -0700

    EN-7500: Remove hyphens from Rails Logger request_id (#3007)

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,6 +1,6 @@
 Rails.application.config.log_tags = [
   lambda { |req| Time.now.utc.strftime " %Y-%m-%d %H:%M:%S,%L "},
   lambda { |req| req.host || 'unknown' },
-  :uuid,
+  lambda { |req| req.env['action_dispatch.request_id'].gsub('-', '') },
   lambda { |req| "PID-%.5d" % Process.pid }
 ]

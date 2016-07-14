080694746797af0adb1d43c9ae4e8f02472cb6b6
Dylan Bussone <dylan.bussone@socrata.com>
2016-07-14 13:02:53 -0700

    ENG: Refactor JS to not need a setTimeout

--- engine/app/assets/javascripts/socrata_site_chrome/site_chrome.js
+++ engine/app/assets/javascripts/socrata_site_chrome/site_chrome.js
@@ -1,24 +1,16 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var initialBodyOverflowY;
 
-setTimeout(function() {
-  $(document).ready(function() {
-    mobileMenuFlyout();
-  });
-}, 1000); // TODO HACK Don't do this.
+$(document).ready(function() {
+  initialBodyOverflowY = $('body').css('overflow-y');
+});
 
-function mobileMenuFlyout() {
+function mobileMenuToggle() {
   var $menu = $('#site-chrome-header .mobile-menu');
-  var $menuToggle = $('#site-chrome-header .menu-toggle');
-  var initialBodyOverflowY = $('body').css('overflow-y');
-
-  $menuToggle.click(function() {
-    $menu.toggleClass('active');
-    if ($menu.hasClass('active')) {
-      // Disable body from scrolling while menu is open
-      $('body').css('overflow-y', 'hidden');
-    } else {
-      $('body').css('overflow-y', initialBodyOverflowY);
-    }
-  });
+  $menu.toggleClass('active');
+  if ($menu.hasClass('active')) {
+    // Disable body from scrolling while menu is open
+    $('body').css('overflow-y', 'hidden');
+  } else {
+    $('body').css('overflow-y', initialBodyOverflowY || 'visible');
+  }
 }

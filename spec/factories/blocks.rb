2e1fdcea6c1d10454eb02423634bbceffc152d4a
Stan Rawrysz <stan.rawrysz@socrata.com>
2016-07-14 15:53:40 -0700

    EN-8099: fix copy story with legacy getty image components

--- spec/factories/blocks.rb
+++ spec/factories/blocks.rb
@@ -5,44 +5,46 @@ FactoryGirl.define do
     components { [ { type: 'html', value: 'Hello, world!' } ] }
     created_by 'test_user@socrata.com'
     presentable true
-  end
 
-  factory :block_with_image, class: Block do
-    layout '12'
-    components { [ { type: 'image', value: { documentId: 1, url: 'http://example.com/image.jpg' } } ] }
-    created_by 'test_user@socrata.com'
-  end
-
-  factory :block_with_hero, class: Block do
-    layout '12'
-    components { [ { type: 'hero', value: { documentId: 2, url: 'http://example.com/hero-image.jpg' } } ] }
-    created_by 'test_user@socrata.com'
-  end
-
-  factory :block_with_author, class: Block do
-    layout '12'
-    components do
-      [{
-        type: 'author',
-        value: {
-          image: {
-            documentId: 3,
-            url: 'http://example.com/hero-image.jpg'
-          }
-        }
-      }]
+    factory :block_with_image do
+      components { [ { type: 'image', value: { documentId: 1, url: 'http://example.com/image.jpg' } } ] }
     end
-    created_by 'test_user@socrata.com'
-  end
 
-  factory :block_with_getty_image, class: Block do
-    layout '12'
-    components {
-      [
-        { type: 'hero', value: { url: '/stories/api/v1/getty-images/2392020' } },
-        { type: 'author', value: { blurb: 'Hello!', image: { url: '/stories/api/v1/getty-images/203920', documentId: nil } } }
-      ]
-    }
-    created_by 'test_user@socrata.com'
+    factory :block_with_hero do
+      components { [ { type: 'hero', value: { documentId: 2, url: 'http://example.com/hero-image.jpg' } } ] }
+    end
+
+    factory :block_with_author do
+      components do
+        [{
+          type: 'author',
+          value: {
+            image: {
+              documentId: 3,
+              url: 'http://example.com/hero-image.jpg'
+            }
+          }
+        }]
+      end
+    end
+
+    factory :block_with_legacy_getty_image do
+      components {
+        [
+          { type: 'image', value: { documentId: nil, url: '/stories/api/v1/getty-images/things234' } },
+          { type: 'hero', value: { url: '/stories/api/v1/getty-images/2392020' } },
+          { type: 'author', value: { blurb: 'Hello!', image: { url: '/stories/api/v1/getty-images/203920', documentId: nil } } }
+        ]
+      }
+    end
+
+    factory :block_with_getty_image do
+      components {
+        [
+          { type: 'hero', value: { url: '/stories/api/v1/getty-images/2392020' } },
+          { type: 'author', value: { blurb: 'Hello!', image: { url: '/stories/api/v1/getty-images/203920', documentId: nil } } }
+        ]
+      }
+    end
   end
 end

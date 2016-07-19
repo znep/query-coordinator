a028e465da7e113980de5ce9014e854f8979ac80
isotluravioli <cagri.ekin@gmail.com>
2016-07-19 16:53:12 +0300

    EN-6637 OP - See appropriate form values based on form selections (Madlib)

--- karma/adminGoals/data/cachedGoals.js
+++ karma/adminGoals/data/cachedGoals.js
@@ -12,7 +12,7 @@ export default {
       "displayName": "cagri ekin",
       "publicTables": 0,
       "emailUnsubscribed": false,
-      "lastLogin": 1467376623,
+      "lastLogin": 1468830966,
       "numberOfFollowers": 0,
       "numberOfFriends": 0,
       "screenName": "cagri ekin",
@@ -22,19 +22,42 @@ export default {
       "email": "cagri@socrata.com"
     },
     "prevailing_measure": {
-      "start": "1988-01-01T00:00:00.000",
+      "start": "1988-02-01T00:00:00.000",
+      "unit": "kg",
       "target_delta_is_percent": false,
-      "target_type": "absolute",
-      "comparison": {"comparison_function": "<"},
+      "baseline": 2,
+      "target_type": "relative",
+      "comparison": {"comparison_function": ">"},
       "created_at": "2016-06-17T11:03:50.322+03:00",
-      "metric": {"metadata": "", "id": "fb43-rvc3"},
+      "target_delta": 0,
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": 1, "year": 1988},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "action_type": "maintain",
+          "target_type": "relative",
+          "maintain_type": ">"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"}
+        },
+        "name": "Speed Kills"
+      },
+      "metric": {"id": "fb43-rvc3"},
       "target": 20,
-      "version": "2016-06-17T11:03:50.322+03:00",
+      "updated_at": "2016-07-18T11:36:22.108+03:00",
+      "version": "2016-07-18T11:36:22.108+03:00",
       "end": "2016-12-31T23:59:59.000"
     },
     "datasetId": "6syt-4pv9",
-    "updated_at": "2016-06-30T13:36:22.074+03:00",
-    "version": "2016-06-30T13:36:22.074+03:00",
+    "updated_at": "2016-07-18T11:36:22.140+03:00",
+    "version": "2016-07-18T11:36:22.140+03:00",
     "prevailingMeasureProgress": "good",
     "id": "7ndm-ubkq",
     "category": {"id": "sxis-fnb2", "name": "AN OTHER CATEGORY", "version": "2016-06-17T11:02:29.873+03:00"},
@@ -44,7 +67,7 @@ export default {
   "63mm-ymcx": {
     "base_dashboard": "cfa5-i2ky",
     "created_at": "2016-06-17T10:30:50.697+03:00",
-    "name": "Make Goals Great Again",
+    "name": "Make Goals Again Edited",
     "created_by": {
       "publicViews": 0,
       "flags": ["admin"],
@@ -54,7 +77,7 @@ export default {
       "displayName": "cagri ekin",
       "publicTables": 0,
       "emailUnsubscribed": false,
-      "lastLogin": 1467376623,
+      "lastLogin": 1468831109,
       "numberOfFollowers": 0,
       "numberOfFriends": 0,
       "screenName": "cagri ekin",
@@ -67,21 +90,44 @@ export default {
       "start": "1978-01-01T00:00:00.000",
       "unit": "aws",
       "target_delta_is_percent": false,
-      "target_type": "absolute",
+      "baseline": 3,
+      "target_type": "none",
       "comparison": {"comparison_function": ">"},
       "created_at": "2016-06-17T10:47:38.678+03:00",
-      "metric": {"metadata": "", "id": "495y-kd4f"},
+      "target_delta": 5,
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 1978},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "action_type": "none",
+          "target_type": "absolute"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"},
+          "type": "static",
+          "value_edit": 3
+        },
+        "name": "awesomeness"
+      },
+      "metric": {"id": "495y-kd4f"},
       "target": 5000,
-      "version": "2016-06-17T10:47:38.678+03:00",
+      "updated_at": "2016-07-18T11:38:50.614+03:00",
+      "version": "2016-07-18T11:38:50.614+03:00",
       "end": "2016-12-31T23:59:59.000"
     },
     "datasetId": "6syt-4pv9",
-    "updated_at": "2016-07-01T16:22:07.722+03:00",
-    "version": "2016-07-01T16:22:07.722+03:00",
-    "prevailingMeasureProgress": "good",
+    "updated_at": "2016-07-18T11:38:50.614+03:00",
+    "version": "2016-07-18T11:38:50.614+03:00",
+    "prevailingMeasureProgress": "no_judgement",
     "id": "63mm-ymcx",
     "category": {"id": "sxis-fnb2", "name": "AN OTHER CATEGORY", "version": "2016-06-17T11:02:29.873+03:00"},
-    "is_public": true,
+    "is_public": false,
     "dashboardName": "Oh My Dashboard !"
   },
   "ykke-a4sz": {
@@ -97,7 +143,7 @@ export default {
       "displayName": "cagri ekin",
       "publicTables": 0,
       "emailUnsubscribed": false,
-      "lastLogin": 1467376623,
+      "lastLogin": 1468833006,
       "numberOfFollowers": 0,
       "numberOfFriends": 0,
       "screenName": "cagri ekin",
@@ -111,22 +157,46 @@ export default {
       "unit": "ton",
       "target_delta_is_percent": false,
       "target_type": "absolute",
-      "comparison": {"comparison_function": ">"},
+      "comparison": {"comparison_function": "<"},
       "created_at": "2016-06-13T13:34:51.823+03:00",
       "target_delta": 2000,
-      "metric": {"metadata": "", "id": "yay8-dxvt"},
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 1985},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "action_type": "reduce"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {
+            "metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}],\"valueType\":\"one\"}}",
+            "dataset": "6syt-4pv9",
+            "column1": "train_speed",
+            "date_column": "date",
+            "compute": {"aggregation_function": "most_recent"}
+          },
+          "type": "scratch"
+        },
+        "name": "A NUMBER"
+      },
+      "metric": {"id": "yay8-dxvt"},
       "target": 1,
-      "updated_at": "2016-06-16T17:40:26.418+03:00",
-      "version": "2016-06-16T17:40:26.418+03:00",
+      "updated_at": "2016-07-18T12:10:20.116+03:00",
+      "version": "2016-07-18T12:10:20.116+03:00",
       "end": "2016-12-31T23:59:59.000"
     },
     "datasetId": "rrgq-keyp",
-    "updated_at": "2016-07-01T15:19:31.058+03:00",
-    "version": "2016-07-01T15:19:31.058+03:00",
-    "prevailingMeasureProgress": "bad",
+    "updated_at": "2016-07-18T12:10:20.127+03:00",
+    "version": "2016-07-18T12:10:20.127+03:00",
+    "prevailingMeasureProgress": "good",
     "id": "ykke-a4sz",
     "category": {"id": "sxis-fnb2", "name": "AN OTHER CATEGORY", "version": "2016-06-17T11:02:29.873+03:00"},
-    "is_public": true,
+    "is_public": false,
     "dashboardName": "Oh My Dashboard !"
   },
   "g34u-2aa5": {
@@ -158,7 +228,23 @@ export default {
       "target_type": "absolute",
       "comparison": {"comparison_function": ">"},
       "created_at": "2016-06-13T13:18:27.078+03:00",
-      "metric": {"metadata": "", "id": "29ck-sy8b"},
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 2016},
+          "end": {"date": "end", "month": "year", "year": 2016}
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"}
+        },
+        "name": "Subject Subject Subject"
+      },
+      "metric": {"id": "29ck-sy8b"},
       "target": 5,
       "version": "2016-06-13T13:18:27.078+03:00",
       "end": "2016-12-31T23:59:59.000"
@@ -175,7 +261,7 @@ export default {
   "vefh-4ihb": {
     "base_dashboard": "cfa5-i2ky",
     "created_at": "2016-06-10T14:10:25.121+03:00",
-    "name": "My Über Goal Edited",
+    "name": "My Über Goal Edited Again",
     "created_by": {
       "publicViews": 0,
       "flags": ["admin"],
@@ -185,7 +271,7 @@ export default {
       "displayName": "cagri ekin",
       "publicTables": 0,
       "emailUnsubscribed": false,
-      "lastLogin": 1467376623,
+      "lastLogin": 1468829604,
       "numberOfFollowers": 0,
       "numberOfFriends": 0,
       "screenName": "cagri ekin",
@@ -198,18 +284,39 @@ export default {
       "start": "2016-01-01T00:00:00.000",
       "unit": "tonnes",
       "target_delta_is_percent": false,
-      "target_type": "absolute",
+      "baseline": 10,
+      "target_type": "relative",
       "comparison": {"comparison_function": ">"},
       "created_at": "2016-06-13T13:15:30.453+03:00",
-      "metric": {"metadata": "", "id": "46xe-b9sf"},
+      "target_delta": 20,
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 2016},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "target_type": "relative"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"}
+        },
+        "name": "Osmanite",
+        "use_progress_override": false,
+        "progress_override": "good"
+      },
+      "metric": {"id": "46xe-b9sf"},
       "target": 20000,
-      "updated_at": "2016-06-13T13:28:03.832+03:00",
-      "version": "2016-06-13T13:28:03.832+03:00",
+      "updated_at": "2016-07-18T11:13:42.682+03:00",
+      "version": "2016-07-18T11:13:42.682+03:00",
       "end": "2016-12-31T23:59:59.000"
     },
     "datasetId": "6syt-4pv9",
-    "updated_at": "2016-07-01T16:22:13.055+03:00",
-    "version": "2016-07-01T16:22:13.055+03:00",
+    "updated_at": "2016-07-18T11:13:42.686+03:00",
+    "version": "2016-07-18T11:13:42.686+03:00",
     "prevailingMeasureProgress": "needs_more_data",
     "id": "vefh-4ihb",
     "category": {"id": "imuv-ja3w", "name": "GREAT NEW CATEGORY", "version": "2016-06-13T13:36:03.459+03:00"},
@@ -229,7 +336,7 @@ export default {
       "displayName": "cagri ekin",
       "publicTables": 0,
       "emailUnsubscribed": false,
-      "lastLogin": 1467376623,
+      "lastLogin": 1468829847,
       "numberOfFollowers": 0,
       "numberOfFriends": 0,
       "screenName": "cagri ekin",
@@ -242,28 +349,47 @@ export default {
       "start": "1990-01-01T00:00:00.000",
       "unit": "fish",
       "target_delta_is_percent": false,
-      "target_type": "absolute",
-      "comparison": {"comparison_function": ">"},
+      "baseline": 10,
+      "target_type": "maintain",
+      "comparison": {"comparison_function": "<"},
       "created_at": "2016-06-13T13:14:14.182+03:00",
-      "metric": {"metadata": "", "id": "m933-w2uc"},
+      "target_delta": 20,
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 1990},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "action_type": "maintain"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"}
+        },
+        "name": "Something"
+      },
+      "metric": {"id": "m933-w2uc"},
       "target": 1,
-      "updated_at": "2016-06-16T17:38:51.185+03:00",
-      "version": "2016-06-16T17:38:51.185+03:00",
+      "updated_at": "2016-07-18T11:28:11.907+03:00",
+      "version": "2016-07-18T11:28:11.907+03:00",
       "end": "2016-12-31T23:59:59.000"
     },
     "datasetId": "rrgq-keyp",
-    "updated_at": "2016-07-01T18:38:57.680+03:00",
-    "version": "2016-07-01T18:38:57.680+03:00",
-    "prevailingMeasureProgress": "bad",
+    "updated_at": "2016-07-18T11:28:11.907+03:00",
+    "version": "2016-07-18T11:28:11.907+03:00",
+    "prevailingMeasureProgress": "good",
     "id": "59yh-53jg",
     "category": {"id": "imuv-ja3w", "name": "GREAT NEW CATEGORY", "version": "2016-06-13T13:36:03.459+03:00"},
-    "is_public": false,
+    "is_public": true,
     "dashboardName": "Oh My Dashboard !"
   },
   "jf7f-i9h8": {
     "base_dashboard": "24vs-rw4z",
     "created_at": "2016-06-16T17:42:11.317+03:00",
-    "name": "A very new goal Edited",
+    "name": "A very new goal Edited Action",
     "created_by": {
       "publicViews": 0,
       "flags": ["admin"],
@@ -273,7 +399,7 @@ export default {
       "displayName": "cagri ekin",
       "publicTables": 0,
       "emailUnsubscribed": false,
-      "lastLogin": 1467376623,
+      "lastLogin": 1468830770,
       "numberOfFollowers": 0,
       "numberOfFriends": 0,
       "screenName": "cagri ekin",
@@ -284,19 +410,41 @@ export default {
     },
     "prevailing_measure": {
       "start": "1991-01-01T00:00:00.000",
+      "unit": "kilo",
       "target_delta_is_percent": false,
-      "target_type": "absolute",
-      "comparison": {"comparison_function": ">"},
+      "baseline": 10,
+      "target_type": "relative",
+      "comparison": {"comparison_function": "<"},
       "created_at": "2016-06-16T17:44:09.205+03:00",
-      "metric": {"metadata": "", "id": "gqa6-8x5q"},
+      "target_delta": 0,
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 1991},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "action_type": "maintain",
+          "maintain_type": "<"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"}
+        },
+        "name": "Something"
+      },
+      "metric": {"id": "gqa6-8x5q"},
       "target": 1,
-      "version": "2016-06-16T17:44:09.205+03:00",
+      "updated_at": "2016-07-18T11:33:06.693+03:00",
+      "version": "2016-07-18T11:33:06.693+03:00",
       "end": "2016-12-31T23:59:59.000"
     },
     "datasetId": "6syt-4pv9",
-    "updated_at": "2016-07-01T16:53:31.218+03:00",
-    "version": "2016-07-01T16:53:31.218+03:00",
-    "prevailingMeasureProgress": "bad",
+    "updated_at": "2016-07-18T11:33:06.710+03:00",
+    "version": "2016-07-18T11:33:06.710+03:00",
+    "prevailingMeasureProgress": "good",
     "id": "jf7f-i9h8",
     "category": {
       "id": "pk8x-sf3m",
@@ -304,7 +452,69 @@ export default {
       "color": "#67f700",
       "version": "2016-06-16T17:42:11.482+03:00"
     },
-    "is_public": true,
+    "is_public": false,
     "dashboardName": "My Dashboard"
+  },
+  "sgv4-zzaj": {
+    "base_dashboard": "cfa5-i2ky",
+    "created_at": "2016-07-18T12:13:09.111+03:00",
+    "name": "Lousy New Goal",
+    "created_by": {
+      "publicViews": 0,
+      "flags": ["admin"],
+      "profileLastModified": 1440569029,
+      "login": "superuser",
+      "oid": 2,
+      "displayName": "cagri ekin",
+      "publicTables": 0,
+      "emailUnsubscribed": false,
+      "lastLogin": 1468833258,
+      "numberOfFollowers": 0,
+      "numberOfFriends": 0,
+      "screenName": "cagri ekin",
+      "rights": ["create_datasets", "edit_others_datasets", "edit_sdp", "edit_site_theme", "moderate_comments", "manage_users", "chown_datasets", "edit_nominations", "approve_nominations", "feature_items", "federations", "manage_stories", "manage_approval", "change_configurations", "view_domain", "view_others_datasets", "edit_pages", "create_pages", "view_goals", "view_dashboards", "edit_goals", "edit_dashboards", "create_dashboards", "manage_provenance", "view_all_dataset_status_logs", "create_story", "edit_story_title_desc", "create_story_copy", "delete_story", "manage_story_collaborators", "manage_story_visibility", "manage_story_public_version", "edit_story", "view_unpublished_story", "view_story"],
+      "id": "ytbe-ixad",
+      "createdAt": 1440569028,
+      "email": "cagri@socrata.com"
+    },
+    "prevailing_measure": {
+      "start": "1990-01-01T00:00:00.000",
+      "unit": "kg",
+      "target_delta_is_percent": false,
+      "baseline": 10,
+      "target_type": "relative",
+      "comparison": {"comparison_function": "<"},
+      "created_at": "2016-07-18T12:14:27.984+03:00",
+      "target_delta": 20,
+      "metadata": {
+        "edit": {
+          "start": {"date": "start", "month": "year", "year": 1990},
+          "end": {"date": "end", "month": "year", "year": 2016},
+          "action_type": "reduce",
+          "target_type": "relative"
+        },
+        "baseline": {
+          "squishy": {
+            "start": {"date": "start", "month": "year", "year": 2016},
+            "end": {"date": "start", "month": "year", "year": 2016}
+          },
+          "start": "2016-01-01T00:00:00.000",
+          "end": "2016-01-01T23:59:59.000",
+          "metric": {"metadata": "{\"wizard\":{\"filter_ast\":[{\"values\":[{}]}]}}"}
+        },
+        "name": "Aliexpress Order"
+      },
+      "metric": {"id": "tby8-sda4"},
+      "version": "2016-07-18T12:14:27.984+03:00",
+      "end": "2016-12-31T23:59:59.000"
+    },
+    "datasetId": "ay6s-62x8",
+    "updated_at": "2016-07-18T12:14:28.079+03:00",
+    "version": "2016-07-18T12:14:28.079+03:00",
+    "prevailingMeasureProgress": "bad",
+    "id": "sgv4-zzaj",
+    "category": {"id": "imuv-ja3w", "name": "GREAT NEW CATEGORY", "version": "2016-07-18T12:13:09.182+03:00"},
+    "is_public": true,
+    "dashboardName": "Oh My Dashboard !"
   }
 };

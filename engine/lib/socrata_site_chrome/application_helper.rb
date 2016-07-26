745c80aa18d650fd52e22831f0cafbf9c6683772
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2016-07-26 13:51:15 -0700

    Address CR comment

--- engine/lib/socrata_site_chrome/application_helper.rb
+++ engine/lib/socrata_site_chrome/application_helper.rb
@@ -43,11 +43,7 @@ module SocrataSiteChrome
 
     def copyright
       copy_with_year = "\u00A9 #{Time.now.year}"
-      if (footer_title && !footer_title.empty?) then
-        "#{copy_with_year}, #{footer_title}"
-      else
-        copy_with_year
-      end
+      footer_title.present? ? "#{copy_with_year}, #{footer_title}" : copy_with_year
     end
 
     def show_copyright?

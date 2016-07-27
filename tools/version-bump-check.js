d03b204a8c214ca917fff40fa8a40b07aa7ff491
Andrew Harrison <andrew.harrison@socrata.com>
2016-07-27 10:29:42 -0700

    Update version bump check to match frontend-visualizations

--- tools/version-bump-check.js
+++ tools/version-bump-check.js
@@ -15,29 +15,42 @@ var currentBranchPackageJSON = require(packagePath);
 pushd(baseDirectory, {silent: true});
 
 var isNotMaster = exec('git rev-parse --abbrev-ref HEAD', {silent: true}).stdout !== 'master';
-var stashed = exec('git stash -u', {silent: true}).stdout.indexOf('No local changes to save') === -1;
+var stash = exec('git stash -u', {silent: true});
+var stashed = stash.stdout.indexOf('No local changes to save') === -1;
 
-exec('git checkout master', {silent: true});
-
-// Invalidate the cache for package.json;
-delete require.cache[require.resolve(packagePath)];
-masterPackageJSON = require(packagePath);
-
-var exitCode = 0;
-var lessThanMasterVersion = semver.lt(currentBranchPackageJSON.version, masterPackageJSON.version);
-var equalToMasterVersion = currentBranchPackageJSON.version === masterPackageJSON.version;
-
-if (isNotMaster && (lessThanMasterVersion || equalToMasterVersion)) {
-  echo('Error: Version discrepancy: ' + currentBranchPackageJSON.version + ' (yours), ' + masterPackageJSON.version + ' (master)');
-  echo('=> You must bump the version.');
-  echo('=> Version bumps can be completed by editing .version in package.json and bower.json (if it exists).');
-  exitCode = 1;
-} else {
-  echo('Your version is good to go!');
-  exitCode = 0;
+if (stash.code !== 0) {
+  exit(1);
 }
 
-exec('git checkout -', {silent: true});
+var fetch = exec('git fetch --all', {silent: true});
+
+if (fetch.code === 0) {
+  var checkout = exec('git checkout origin/master', {silent: true});
+
+  if (checkout.code === 0) {
+
+    // Invalidate the cache for package.json;
+    delete require.cache[require.resolve(packagePath)];
+    masterPackageJSON = require(packagePath);
+
+    var exitCode = 0;
+    var lessThanMasterVersion = semver.lt(currentBranchPackageJSON.version, masterPackageJSON.version);
+    var equalToMasterVersion = currentBranchPackageJSON.version === masterPackageJSON.version;
+
+    if (isNotMaster && (lessThanMasterVersion || equalToMasterVersion)) {
+      echo('Error: Version discrepancy: ' + currentBranchPackageJSON.version + ' (yours), ' + masterPackageJSON.version + ' (master)');
+      echo('=> You must bump the version.');
+      echo('=> Version bumps can be completed by editing .version in package.json.');
+      exitCode = 1;
+    } else {
+      echo('Your version is good to go!');
+      exitCode = 0;
+    }
+
+    exec('git checkout -', {silent: true});
+  }
+}
+
 if (stashed) { exec('git stash pop', {silent: true}); }
 
 popd();

9c4a3e508752f036d85b98ddc5843ce6cb23a708
Randy Antler <randy.antler@socrata.com>
2016-08-05 14:06:52 -0700

    EN-6353: Integrate styleguide into catalog/browse2 (#3117)
    This is a a first-pass effort only which integrates the inclusion of the styleguide onto the catalog page and addresses any styling issues that arose as a result. The DOM structure of the existing catalog output has not been modified to match the catalog as defined in the styleguide. This will be addressed when we convert the catalog into a reusable component.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -237,7 +237,7 @@ module ApplicationHelper
     end
   end
 
-# styles
+# STYLES
   def should_render_individual_styles?
     Rails.env.development? && FeatureFlags.derive(@view, request).use_merged_styles != true
   end
@@ -292,6 +292,10 @@ module ApplicationHelper
         end
     end
     javascript_include_tag src
+  end
+
+  def socrata_styleguide
+    stylesheet_link_tag('/stylesheets/socrata-styleguide/css/styleguide.css', media: 'all')
   end
 
 # TOP OF PAGE

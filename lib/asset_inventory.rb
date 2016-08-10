d07ff8073b35a2d0ea81744cd3ddf4d6cebaf3e2
Taylor Cramer <cramertaylorj@gmail.com>
2016-08-10 15:30:43 -0700

    EN-9006 Prefer correctly named asset inventories (#3166)

--- lib/asset_inventory.rb
+++ lib/asset_inventory.rb
@@ -2,10 +2,19 @@ module AssetInventory
   def self.find
     views = Clytemnestra.search_cached_views(
       { :nofederate => true, :limitTo => 'assetinventory', :admin => true }).results
-      .sort_by { |view| view.createdAt }
 
-    if views.length > 0
-      views.last
+    views_with_ai_name = views.select{|view|
+      (view.name == 'Dataset of Datasets') || (view.name == 'Asset Inventory')
+    }
+
+    if views_with_ai_name.length > 0
+      views_with_ai_name
+        .sort_by{ |view| view.createdAt }
+        .last
+    elsif views.length > 0
+      views
+        .sort_by{ |view| view.rowsUpdatedAt }
+        .last
     else
       nil
     end

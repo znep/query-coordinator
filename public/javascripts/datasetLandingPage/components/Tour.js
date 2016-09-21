e40390995889655477600b520ece1e1ea1e514f0
Bjorn Swenson <tie.372@gmail.com>
2016-09-20 19:01:01 -0700

    EN-9723: Hide DSLP tour on blobs and hrefs; (#3409)
    * EN-9723: Hide DSLP tour on blobs and hrefs;
    
    * EN-9723: Fix lint;
    
    * EN-9723: Rename cookie function;

--- public/javascripts/datasetLandingPage/components/Tour.js
+++ public/javascripts/datasetLandingPage/components/Tour.js
@@ -4,48 +4,53 @@ import { connect } from 'react-redux';
 import { emitMixpanelEvent } from '../actions/mixpanel';
 
 export var Tour = (props) => {
-  var { onClickDone, onCloseTour } = props;
-
+  var { view, onClickDone, onCloseTour } = props;
+  var { isBlobby, isHref } = view;
   var { enableDatasetLandingPageTour } = window.serverConfig.featureFlags;
   var hasCookie = utils.getCookie('dslpTourClosed');
 
-  if (enableDatasetLandingPageTour && !hasCookie) {
-    var setCookie = () => window.document.cookie = 'dslpTourClosed=1';
-
-    window.document.addEventListener(
-      'SOCRATA_STYLEGUIDE_TOUR_COMPLETE',
-      () => {
-        onClickDone();
-        setCookie();
-      }
-    );
-
-    window.document.addEventListener(
-      'SOCRATA_STYLEGUIDE_TOUR_CLOSED',
-      () => onCloseTour()
-    );
-
-    return (
-      <div data-tour data-tour-name="grid-view-button-tour" data-tour-done={I18n.tour.done}>
-        <div
-          data-tour-step
-          data-step-number="1"
-          data-attach-to-element="#tour-anchor"
-          data-attach-to-position="bottom">
-          <h3>{I18n.tour.title}</h3>
-          <p>{I18n.tour.description}</p>
-        </div>
-      </div>
-    );
-  } else {
+  if (!enableDatasetLandingPageTour || hasCookie || isBlobby || isHref) {
     return <div />;
   }
+
+  var setTourClosedCookie = () => window.document.cookie = 'dslpTourClosed=1';
+
+  window.document.addEventListener(
+    'SOCRATA_STYLEGUIDE_TOUR_COMPLETE',
+    () => {
+      onClickDone();
+      setTourClosedCookie();
+    }
+  );
+
+  window.document.addEventListener(
+    'SOCRATA_STYLEGUIDE_TOUR_CLOSED',
+    () => onCloseTour()
+  );
+
+  return (
+    <div data-tour data-tour-name="grid-view-button-tour" data-tour-done={I18n.tour.done}>
+      <div
+        data-tour-step
+        data-step-number="1"
+        data-attach-to-element="#tour-anchor"
+        data-attach-to-position="bottom">
+        <h3>{I18n.tour.title}</h3>
+        <p>{I18n.tour.description}</p>
+      </div>
+    </div>
+  );
 };
 
 Tour.propTypes = {
+  view: PropTypes.object,
   onClickDone: PropTypes.func,
   onCloseTour: PropTypes.func
 };
+
+function mapStateToProps(state) {
+  return _.pick(state, 'view');
+}
 
 function mapDispatchToProps(dispatch) {
   function tourPayload() {
@@ -77,4 +82,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(null, mapDispatchToProps)(Tour);
+export default connect(mapStateToProps, mapDispatchToProps)(Tour);

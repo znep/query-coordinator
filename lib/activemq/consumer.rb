be94dfd5959f9b9f0f449a7308c86bdabaede306
Andrew Harrison <andrew.harrison@socrata.com>
2016-10-04 16:00:22 -0700

    EN-8773: Register per-host ActiveMQ listeners

--- lib/activemq/consumer.rb
+++ lib/activemq/consumer.rb
@@ -16,33 +16,43 @@ module ActiveMQ
 
     # Retrieves messages for processing and acknowledges them.
     def subscribe(&block)
-      client = Stomp::Client.open(
-        hosts: @hosts,
-        randomize: true
-      )
+      # NOTE: Our "Eurybates" ActiveMQ architecture can be thought of as a
+      # firehose with multiple nodes: every queue receives every message, but
+      # each queue exists on one or more nodes (and each node gets different
+      # messages). Because the firehose may be spread across multiple nodes,
+      # we need one process for every node; otherwise we will receive some
+      # messages but not others.
+      #
+      # The hosts property of Stomp::Client is used for failover, which is not
+      # the same use of multiple nodes as described above. We basically assume
+      # (from Storyteller's point of view) that the death of an ActiveMQ node
+      # is someone else's problem.
+      clients = @hosts.map do |host|
+        client = Stomp::Client.open(hosts: [host])
 
-      client.subscribe(@queue, :ack => 'client', 'activemq.prefetchSize' => 1) do |message|
-        begin
-          parsed_message = self.class.parse(message.body)
-          yield(parsed_message) unless parsed_message.nil?
-        rescue StandardError => ex
-          Rails.logger.error("Unexpected error in ActiveMQ::Consumer: #{ex.message}")
-          ::AirbrakeNotifier.report_error(ex, message: 'Unexpected error in ActiveMQ::Consumer.')
-        ensure
-          client.acknowledge(message)
+        client.subscribe(@queue, :ack => 'client', 'activemq.prefetchSize' => 1) do |message|
+          begin
+            parsed_message = self.class.parse(message.body)
+            yield(parsed_message) unless parsed_message.nil?
+          rescue StandardError => ex
+            Rails.logger.error("Unexpected error in ActiveMQ::Consumer: #{ex.message}")
+            ::AirbrakeNotifier.report_error(ex, message: 'Unexpected error in ActiveMQ::Consumer.')
+          ensure
+            client.acknowledge(message)
+          end
         end
       end
 
       begin
         Rails.logger.info('ActiveMQ::Consumer awaiting messages from queue...')
-        client.join
+        clients.each(&:join)
       rescue SystemExit, Interrupt, SignalException
         Rails.logger.info('Shutdown signal received...')
       rescue StandardError => ex
         Rails.logger.error("ActiveMQ::Consumer received error from Stomp::Client; re-raising...")
         raise ex
       ensure
-        client.close
+        clients.each(&:close)
         Rails.logger.info('Closed Stomp::Client.')
       end
     end

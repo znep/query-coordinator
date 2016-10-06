ebdae84778ed218822b7a7e728b1d7d297d8df14
Pete Vilter <peter.vilter@socrata.com>
2016-10-06 12:35:19 -0700

    add doc note

--- public/javascripts/importWizard/components/flashMessage.js
+++ public/javascripts/importWizard/components/flashMessage.js
@@ -1,5 +1,11 @@
 import React, { PropTypes } from 'react';
 
+/** sometimes you want to pass in an error message w/ HTML in it (e.g. a link)
+ * but we don't want to dangerouslySetInnerHTML here, because sometimes error messages
+ * accidentally have HTML in them (e.g. nginx returning an HTML 502 page from an API
+ * that usually returns JSON); this puts the onus on the caller to escape any HTML
+ * that might be in their error message.
+ */
 function flashMessage({ flashType, children }) {
   return (
     <div className={'flash-alert ' + flashType}>

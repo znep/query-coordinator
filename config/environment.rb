d6621be9d3977504d1e6b7e812b51e252a8c5d3f
Dylan Bussone <dylan.bussone@socrata.com>
2016-11-08 14:18:57 -0800

    EN-11291: Add request in call to site_chrome_favicon_tag

--- config/environment.rb
+++ config/environment.rb
@@ -28,3 +28,7 @@ end
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# TODO: fix this path?
+site_chrome_views_pathset = ActionView::PathSet.new(["#{Rails.root}/../socrata_site_chrome/engine/app/views/site_chrome"])
+ActionController::Base.view_paths += site_chrome_views_pathset

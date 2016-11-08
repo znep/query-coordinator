278ea7de6f8f2e3b55a1329c25dd992396beb971
Randy Antler <randy.antler@socrata.com>
2016-11-08 13:45:18 -0800

    EN-11820: Fix current_user bug. Add test coverage (#3676)
    Good news! Adding test coverage revealed another bug!

--- lib/json_with_airbrake.rb
+++ lib/json_with_airbrake.rb
@@ -4,21 +4,26 @@ class JSONWithAirbrake
       JSON.parse(*args)
     rescue JSON::ParserError => e
       options = args.last.is_a?(Hash) ? args.pop : {}
-      Airbrake.notify(
+      notification_args = {
         :error_class => 'JSON Parser',
         :error_message => "Parse error: #{e.to_s}",
         :session => {
           :current_domain => CurrentDomain.cname,
-          :current_user_id => User.current_user.id
-        },
-        :parameters => {
-          :request => {
-            :path => options[:request].path,
-            :method => options[:request].method,
-            :body => options[:request].body
-          }
+          :current_user_id => User.current_user.try(:id) || 'Anonymous'
         }
-      )
+      }
+      if options[:request].present?
+        notification_args.merge!(
+          :parameters => {
+            :request => {
+              :path => options[:request].path,
+              :method => options[:request].method,
+              :body => options[:request].body
+            }
+          }
+        )
+      end
+      Airbrake.notify(notification_args)
       raise e
     end
   end

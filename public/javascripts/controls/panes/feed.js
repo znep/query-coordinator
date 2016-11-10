0242bfeadbef1674678ab7eb42775cb585022d9e
NathalieKaligirwa <nathalie.kaligirwa@socrata.com>
2016-11-10 13:53:09 -0800

    EN-5453: Refresh page when a new comment is added (#3687)
    * refresh page when a new comment is added
    
    fix indentation
    
    * refactor code to be more readable
    
    * EN-5453: Small style changes;
    
    (cherry picked from commit 927990eee9c7c3d446dfb3c5d94123dc1ecc3b56)

--- public/javascripts/controls/panes/feed.js
+++ public/javascripts/controls/panes/feed.js
@@ -27,33 +27,14 @@
             var comments;
             var views = [];
 
-            // This is part of the fix for ticket EN-5453
-            // The grid view discuss pane has the option to see the activity around a dataset (such as derived views creation and modification)
-            // On datasets with a lot of derived views, loading the discuss pane takes a long time
-            // due to the slowness of core's api/views?getByTableId endpoint.
-            // A workaround is to remove the option of viewing derived views from the discuss pane through the use of a feature flag.
-            // For customers that have feature flag `remove_views_from_discuss_pane` set to true, the discuss pane will only show comments.
-            // This feature flag is false by default and will be turned on only for data.cityofnewyork.us
+            var currentView = cpObj._view;
+            var callback;
 
-            // We hard code the number of times doRender (line 51) has to wait before rendering.
-            // The reason is that doRender is not idempotent, and if called multiple times it will re-render items.
-            // It is safe to hard code this value given that the number of requests is static and not defined by the user.
-            // On line 48, we adjust the count to 1 if the feature flag is turned on
-            // because we only need to wait for one request (to fetch comments) before rendering
-            // as opposed to three requests (for getting comments, derived views, and the main view)
-            var requestCount = 3;
-
-            // on feature flag rollback, this line of code has to be removed (EN-5453)
-            if (window.blist.feature_flags.remove_views_from_discuss_pane  === true) {
-               requestCount = 1;
-            }
-
-            var doRender = _.after(requestCount, function() {
+            function renderDiscuss() {
               cpObj._finishProcessing();
-
               cpObj.$dom().find('.feedList .feed').feedList({
                 comments: comments,
-                mainView: cpObj._view,
+                mainView: currentView,
                 views: views,
                 addCommentCallback: function(view) {
                   $('.controlPane.about .numberOfComments').text(view.numberOfComments);
@@ -62,29 +43,53 @@
                   }
                 }
               });
-            });
+            }
 
-            cpObj._view.getComments(function(responseData) {
-              comments = responseData;
-              doRender();
-            });
-
-            // When `remove_views_from_discuss_pane` is to true, we do not need to get the parent and related views
-            // On feature flag rollback:
-            // the condition that checks the value of `remove_views_from_discuss_pane` has to be removed
-            // derived views and the view will be loaded for all domains.
-            if (window.blist.feature_flags.remove_views_from_discuss_pane === false) {
-              cpObj._view.getRelatedViews(function(relatedViews) {
-                views = views.concat(relatedViews);
-                doRender();
+            function getComments(cb) {
+              currentView.getComments(function(responseData) {
+                comments = responseData;
+                cb();
               });
-              cpObj._view.getParentDataset(function(parDS) {
-              if (!$.isBlank(parDS)) {
-                views = views.concat(parDS);
-              }
-              doRender();
-            });
-          }
+            }
+
+            function getParent(cb) {
+              currentView.getParentDataset(function(parDS) {
+                if (!$.isBlank(parDS)) {
+                  views = views.concat(parDS);
+                }
+                cb();
+              });
+            }
+
+            function getRelatedViews(cb) {
+              currentView.getRelatedViews(function(relatedViews) {
+                views = views.concat(relatedViews);
+                cb();
+              });
+            }
+
+            // This is part of the fix for ticket EN-5453
+            // The grid view discuss pane has the option to see the activity around a dataset (such as derived views creation and modification)
+            // On datasets with a lot of derived views, loading the discuss pane takes a long time
+            // due to the slowness of core's api/views?getByTableId endpoint.
+            // A workaround is to remove the option of viewing derived views from the discuss pane through the use of a feature flag.
+            // For customers that have feature flag `remove_views_from_discuss_pane` set to true, the discuss pane will only show comments.
+            // This feature flag is false by default and will be turned on only for data.cityofnewyork.us
+
+            // Note that renderDiscuss is not idempotent -- it is important to only call it once
+            // after all the requests have completed.
+
+            // on feature flag rollback, the if else statement has to be removed and replaced with the body of "else"
+            if (window.blist.feature_flags.remove_views_from_discuss_pane === true) {
+              callback = _.after(2, renderDiscuss);
+              getComments(callback);
+              getParent(callback);
+            } else {
+              callback = _.after(3, renderDiscuss);
+              getComments(callback);
+              getParent(callback);
+              getRelatedViews(callback);
+            }
           }
         }
       }];

09c886b389f1f640771c01a6b2125e7e0348afb3
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2016-11-11 17:38:04 -0800

    EN-10787: Add classic goal edit paths.

--- spec/controllers/odysseus_controller_spec.rb
+++ spec/controllers/odysseus_controller_spec.rb
@@ -27,14 +27,18 @@ describe OdysseusController do
 
   describe 'goal-related routes' do
     let(:dashboard_id) { 'dash-dash' }
-    let(:category_id) { 'cate-cate' }
+    let(:category_id) { 'cate-gory' }
     let(:goal_id) { 'goal-goal' }
 
     let(:canonical_view_path) { "/stat/goals/#{dashboard_id}/#{category_id}/#{goal_id}" }
+    let(:canonical_edit_path) { "/stat/goals/#{dashboard_id}/#{category_id}/#{goal_id}/edit" }
     let(:classic_view_path) { "/stat/goals/#{dashboard_id}/#{category_id}/#{goal_id}/view" }
+    let(:classic_edit_path) { "/stat/goals/#{dashboard_id}/#{category_id}/#{goal_id}/edit-classic" }
 
     let(:canonical_single_view_path) { "/stat/goals/single/#{goal_id}" }
+    let(:canonical_single_edit_path) { "/stat/goals/single/#{goal_id}/edit" }
     let(:classic_single_view_path) { "/stat/goals/single/#{goal_id}/view" }
+    let(:classic_single_edit_path) { "/stat/goals/single/#{goal_id}/edit-classic" }
 
     describe 'get #classic_single_goal' do
       context 'goal has no published narrative in storyteller' do
@@ -98,6 +102,60 @@ describe OdysseusController do
               get :classic_goal, :dashboard_id => dashboard_id, :category_id => category_id, :goal_id => goal_id
             end.to raise_error(RuntimeError)
           end
+        end
+      end
+    end
+
+    describe 'get #classic_goal_edit' do
+      before do
+        rspec_stub_feature_flags_with(:open_performance_narrative_editor => flag_value)
+      end
+      context 'open_performance_narrative_editor is set to classic' do
+        let(:flag_value) { 'classic' }
+
+        it 'renders path on odysseus' do
+          expect(subject).to receive(:render_odysseus_path) do |path, options|
+            expect(options).to be_nil
+            expect(path).to eq(classic_edit_path)
+          end
+          get :classic_goal_edit, :dashboard_id => dashboard_id, :category_id => category_id, :goal_id => goal_id
+        end
+      end
+
+      context 'open_performance_narrative_editor is set to storyteller' do
+        let(:flag_value) { 'storyteller' }
+
+        it 'redirects to the canonical view url' do
+          expect(subject).to_not receive(:render_odysseus_path)
+          get :classic_goal_edit, :dashboard_id => dashboard_id, :category_id => category_id, :goal_id => goal_id
+          subject.should redirect_to(canonical_edit_path)
+        end
+      end
+    end
+
+    describe 'get #classic_single_goal_edit' do
+      before do
+        rspec_stub_feature_flags_with(:open_performance_narrative_editor => flag_value)
+      end
+      context 'open_performance_narrative_editor is set to classic' do
+        let(:flag_value) { 'classic' }
+
+        it 'renders path on odysseus' do
+          expect(subject).to receive(:render_odysseus_path) do |path, options|
+            expect(options).to be_nil
+            expect(path).to eq(classic_single_edit_path)
+          end
+          get :classic_single_goal_edit, :goal_id => goal_id
+        end
+      end
+
+      context 'open_performance_narrative_editor is set to storyteller' do
+        let(:flag_value) { 'storyteller' }
+
+        it 'redirects to the canonical view url' do
+          expect(subject).to_not receive(:render_odysseus_path)
+          get :classic_single_goal_edit, :goal_id => goal_id
+          subject.should redirect_to(canonical_single_edit_path)
         end
       end
     end

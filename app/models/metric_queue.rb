80365d1058ba6c05ee2b5c410b77df9fc4211367
Andrew "Peaches" Gall <housepage@gmail.com>
2016-11-29 17:16:23 -0800

    EN-11001: More verbose logging of flushing of metrics (#3787)

--- app/models/metric_queue.rb
+++ app/models/metric_queue.rb
@@ -59,6 +59,7 @@ class MetricQueue
       now -= now % 120
       now *= 1_000
       filename = "#{targetdir}#{sprintf('/metrics2012.%016x.data', now)}"
+      Rails.logger.debug("Flushing #{current_requests.length} metrics to file #{filename}")
       File.open(filename, 'ab') do |metricfile|
         current_requests.each do |request|
           write_start_of_record(metricfile)

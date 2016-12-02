e5a4c8b3f551c8678087ade63b217e59e9405a0c
Kristina Hjertberg <hjert.k@gmail.com>
2016-12-01 18:55:03 -0800

    EN-10742: See EditBar on vis canvas

--- public/javascripts/visualizationCanvas/App.js
+++ public/javascripts/visualizationCanvas/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { VelocityComponent } from 'velocity-react';
+import EditBar from './components/EditBar';
 import InfoPane from './components/InfoPane';
 import VisualizationContainer from './components/VisualizationContainer';
 import Table from './components/Table';
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <VelocityComponent animation={{ opacity: 1 }} runOnMount duration={275}>
       <div style={{ opacity: 0 }}>
+        <EditBar />
         <div className="container">
           <InfoPane />
           <VisualizationContainer />

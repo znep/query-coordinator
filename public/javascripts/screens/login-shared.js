462a83ec72d366bd9ef25a2977d816741cf1773e
Nate Moore <nathaniel.moore@socrata.com>
2016-12-02 16:45:36 -0800

    EN-12385: Fix social link page js (#3812)
    Previously, this javascript would switch the login and sign up forms but now that we have a new login page, we just want to redirect. The javascript was removed so that the "SIgn In" link in the top right would just redirect, but apparently the javascript was a little overloaded.

--- public/javascripts/screens/login-shared.js
+++ public/javascripts/screens/login-shared.js
@@ -13,4 +13,14 @@ $(function() {
     });
     $('.modalContentBox').removeClass('loginModal').addClass('signupModal');
   });
+
+  // this only pertains to the signup screen to switch between creating an account
+  // and linkin an existing one
+  // (when clicking the Sign In button in the top-right,
+  //  you're just redirected to /login for now)
+  $('.signInLink[href=#login]').click(function(event) {
+    event.preventDefault();
+    $('.signupForm').hide();
+    $('.loginForm').show();
+  });
 });

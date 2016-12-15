e4c0d01bc27bf5f079c1319ab7b18e07806cf97a
Randy Antler <randy.antler@socrata.com>
2016-12-14 18:06:30 -0800

    ENG: Export FeatureFlags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var utils = require('./utils');
 var Analytics = require('./Analytics');
+var FeatureFlags = require('./FeatureFlags');
 
 utils.Analytics = Analytics;
+utils.FeatureFlags = FeatureFlags;
+
 module.exports = utils;

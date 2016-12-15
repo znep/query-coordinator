7e442e4264ae6649d59171cfc4568bef2d09204a
Nate Moore <nathaniel.moore@socrata.com>
2016-12-15 12:08:26 -0800

    Test cache when domain is nil
    (cherry picked from commit bf6b023c791b61c38be1d989c3c8c534c8054768)

--- spec/models/configuration_spec.rb
+++ spec/models/configuration_spec.rb
@@ -42,12 +42,28 @@ describe Configuration do
           Rails.cache = ActiveSupport::Cache::MemoryStore.new
           Rails.cache.clear
           allow(CurrentDomain).to receive(:configUpdatedAt).and_return(1477332984)
-          allow(CoreServer::Base.connection).to receive(:get_request).once.and_return(site_chrome_response)
+          expect(CoreServer::Base.connection).to receive(:get_request).once.and_return(site_chrome_response)
           config = subject.class.find_by_type('site_chrome', default_only = true, CurrentDomain.cname)
           expect(config.type).to eq('site_chrome')
           subject.class.find_by_type('site_chrome', default_only = true, CurrentDomain.cname)
           subject.class.find_by_type('site_chrome', default_only = true, CurrentDomain.cname)
           subject.class.find_by_type('site_chrome', default_only = true, CurrentDomain.cname)
+
+          Rails.cache.clear
+        ensure
+          Rails.cache = ActiveSupport::Cache::NullStore.new
+        end
+      end
+
+      it 'does not cache when cname is nil', :verify_stubs => false do
+        begin
+          Rails.cache = ActiveSupport::Cache::MemoryStore.new
+          Rails.cache.clear
+          allow(CurrentDomain).to receive(:configUpdatedAt).and_return(1477332984)
+          expect(CoreServer::Base.connection).to receive(:get_request).twice.and_return(site_chrome_response)
+          config = subject.class.find_by_type('site_chrome', default_only = true, nil)
+          expect(config.type).to eq('site_chrome')
+          subject.class.find_by_type('site_chrome', default_only = true, nil)
 
           Rails.cache.clear
         ensure

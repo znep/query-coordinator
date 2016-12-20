d882cda492ace7fd2b5837dd4a10380ed734108b
Dylan Bussone <dylan.bussone@socrata.com>
2016-12-19 16:16:05 -0800

    EN-12756: Use Site Chrome test helpers for stubs

--- spec/support/site_chrome_stubs.rb
+++ spec/support/site_chrome_stubs.rb
@@ -1,6 +1,9 @@
 module SiteChromeStubs
+  include SocrataSiteChrome::Test::Helpers
+
   def stub_site_chrome
-    @request.env[SocrataSiteChrome::Middleware::SOCRATA_SITE_CHROME_ENV_KEY] = SocrataSiteChrome::Middleware.new(@app)
+    stub_site_chrome_instance # SocrataSiteChrome::Test::Helpers
+
     stub_request(:get, 'http://localhost:8080/configurations.json?defaultOnly=true&type=site_chrome').
       with(:headers => {'X-Socrata-Host'=>'test.host'}).
       to_return(:status => 200, :body => '[{}]', :headers => {})

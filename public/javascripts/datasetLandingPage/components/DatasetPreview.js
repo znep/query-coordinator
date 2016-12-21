009f398126180485ea27ef80b11bfcf5a204df51
Bjorn Swenson <tie.372@gmail.com>
2016-12-21 12:49:48 -0800

    EN-12681: Only use first table sort in Primer; (#3858)

--- public/javascripts/datasetLandingPage/components/DatasetPreview.js
+++ public/javascripts/datasetLandingPage/components/DatasetPreview.js
@@ -33,7 +33,7 @@ export const DatasetPreview = React.createClass({
 
     return {
       configuration: {
-        order: view.sortOrder,
+        order: _.take(view.sortOrder), // The table breaks if more than one sort is present
         viewSourceDataLink: false
       },
       datasetUid: view.id,

bfae2c4c982add588d31d6f1fc8e80274a46ce55
Kaida Masaki <kaida.masaki@socrata.com>
2016-12-29 15:40:00 -0800

    Add modal boilerplate to databasing page

--- public/javascripts/datasetManagementUI/components/ShowUpsertJob.js
+++ public/javascripts/datasetManagementUI/components/ShowUpsertJob.js
@@ -1,6 +1,11 @@
 import _ from 'lodash';
 import React, { PropTypes } from 'react';
+import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import { Modal, ModalHeader, ModalContent, ModalFooter } from 'socrata-components';
+import * as Links from '../links';
 
 function query(db, upsertJobId) {
   const upsertJob = _.find(db.upsert_jobs, { id: upsertJobId });
@@ -9,17 +14,51 @@ function query(db, upsertJobId) {
   };
 }
 
-function ShowUpsertJob({ upsertJob }) {
+function ShowUpsertJob({ upsertJob, onDismiss }) {
+  function goToDataPage() {
+    console.log(upsertJob);
+    console.log("Going to show schema!");
+  }
+
+  const modalProps = {
+    fullScreen: true,
+    onDismiss: goToDataPage
+  };
+  const headerProps = {
+    title: (
+      <span>
+        <Link to={Links.uploads}>{I18n.home_pane.data}</Link> &gt;&nbsp;
+        {/* TODO: Internationalize! */}
+        Upsert Jobs
+      </span>
+    ),
+    onDismiss: onDismiss
+  };
+
   return (
     <div className="show-upsert-job">
-      <p>Job:</p>
-      <pre>{JSON.stringify(upsertJob, null, 4)}</pre>
+      <Modal {...modalProps}>
+        <ModalHeader {...headerProps} />
+
+        <ModalContent>
+          <p>Job:</p>
+          <pre>{JSON.stringify(upsertJob, null, 4)}</pre>
+        </ModalContent>
+        <ModalFooter>
+          <div className="modal-footer-actions">
+            <button id="done" className="btn btn-primary" onClick={onDismiss}>
+              Done {/* TODO: Internationalize! */}
+            </button>
+          </div>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
 
 ShowUpsertJob.propTypes = {
-  upsertJob: PropTypes.object.isRequired
+  upsertJob: PropTypes.object.isRequired,
+  onDismiss: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -27,4 +66,12 @@ function mapStateToProps(state, ownProps) {
   return query(state.db, _.toNumber(params.upsertJobId));
 }
 
-export default connect(mapStateToProps)(ShowUpsertJob);
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onDismiss: () => {
+      dispatch(push(Links.home(ownProps.location)));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowUpsertJob);

f0660f9ea4c2f24846e72dff646ef803b4505398
Kaida Masaki <kaida.masaki@socrata.com>
2017-01-04 15:50:54 -0800

    First attempt at crude spinner logic + styling

--- public/javascripts/datasetManagementUI/components/ColumnStatus.js
+++ public/javascripts/datasetManagementUI/components/ColumnStatus.js
@@ -22,6 +22,7 @@ export const ColumnStatus = React.createClass({
         </th>
       );
     } else if (column.num_transform_errors) {
+      // TODO: Have a message for when errors are trickling in.
       return (
         <th key={column.id} className="col-errors">
           <div>
@@ -30,12 +31,22 @@ export const ColumnStatus = React.createClass({
           </div>
         </th>
       );
-    } else {
+    } else if (column.contiguous_rows_processed) {
+      // TODO: Only show this when all rows are good.
       return (
         <th key={column.id} className="col-errors">
           <div>
             <span className="err-info success socrata-icon-checkmark3" />
             {I18n.show_output_schema.column_header.no_errors_exist}
+          </div>
+        </th>
+      );
+    } else {
+      return (
+        <th key={column.id} className="col-errors">
+          <div>
+            <span className="err-info spinner-default" />
+            {I18n.show_output_schema.column_header.scanning}
           </div>
         </th>
       );

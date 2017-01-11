33ae26ec57f313703b22c66863c07339032013ff
Bjorn Swenson <tie.372@gmail.com>
2017-01-11 12:31:49 -0800

    EN-12646: Localize DSLP dates; (#3883)
    * EN-12646: Localize DSLP dates;
    
    * EN-12646: Less global momentum;
    
    * EN-12646: Remove unused import;

--- public/javascripts/datasetLandingPage/lib/formatDate.js
+++ public/javascripts/datasetLandingPage/lib/formatDate.js
@@ -2,5 +2,6 @@ import moment from 'moment';
 
 // Formats an ISO8601 date to something pretty like January 1, 1970.
 export default function formatDate(date) {
-  return moment(date, moment.ISO_8601).format('MMMM D, YYYY');
+  const locale = _.defaultTo(serverConfig.locale, 'en');
+  return moment(date, moment.ISO_8601).locale(locale).format('MMMM D, YYYY');
 }

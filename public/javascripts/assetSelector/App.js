20e4c345c85a65f2e7bbe1c582c39b61d8a83e67
Dylan Bussone <dylan.bussone@socrata.com>
2017-01-11 16:25:41 -0800

    EN-12424: Add asset selector header

--- public/javascripts/assetSelector/App.js
+++ public/javascripts/assetSelector/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BackButton from './components/BackButton';
+import Header from './components/Header';
 import ResultsContainer from './components/ResultsContainer';
 
 export class App extends Component {
@@ -25,6 +26,7 @@ export class App extends Component {
     return (
       <div
         className={this.state.modalIsOpen ? '' : 'hidden'}>
+        <Header />
         <BackButton onClick={this.closeModal} />
         <ResultsContainer />
       </div>
---
+++ public/javascripts/assetSelector/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => (
+  <div className="asset-selector-header">
+    <h1>Select Featured Content in category</h1>{/* TODO: localization */}
+  </div>
+);
+
+export default Header;

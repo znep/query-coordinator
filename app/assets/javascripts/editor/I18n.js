aecd0917c7b1adff436a0688fcb3ee142b56826e
Giacomo Ferrari <progman32@gmail.com>
2017-01-17 17:37:52 -0800

    EN-13265: Fix editor publication status display (#1052)

--- app/assets/javascripts/editor/I18n.js
+++ app/assets/javascripts/editor/I18n.js
@@ -3,6 +3,10 @@ import Environment from '../StorytellerEnvironment';
 
 export default {
   t: function t(translationKeys) {
+    if (!_.has(Environment.TRANSLATIONS, translationKeys) && _.has(window, 'console')) {
+      console.warn(`Missing translation: ${translationKeys}`);
+    }
+
     return _.get(Environment.TRANSLATIONS, translationKeys);
   }
 };

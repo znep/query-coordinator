0a24c784698e488a51f64957b412ea48aa336b98
Robert J. Macomber <robert.macomber@socrata.com>
2017-01-18 09:10:53 -0800

    EN-13269: Limit conversion dropdown to types where conversions exist
    We have a place for properties of types, so add a "can be the target
    of a conversion" field to it and filter the dropdown by that field.

--- public/javascripts/datasetManagementUI/lib/soqlTypes.js
+++ public/javascripts/datasetManagementUI/lib/soqlTypes.js
@@ -1,11 +1,32 @@
 export const soqlProperties = {
-  'SoQLText': { canonicalName: 'text' },
-  'SoQLNumber': { canonicalName: 'number' },
-  'SoQLBoolean': { canonicalName: 'boolean' },
-  'SoQLFixedTimestamp': { canonicalName: 'fixed_timestamp' },
-  'SoQLFloatingTimestamp': { canonicalName: 'floating_timestamp' },
-  'SoQLLocation': { canonicalName: 'location' },
-  'SoQLPoint': { canonicalName: 'point' }
+  'SoQLText': {
+    canonicalName: 'text', // The standard SODA2 API name
+    conversionTarget: true // Whether a simple to_$canonicalname function exists
+  },
+  'SoQLNumber': {
+    canonicalName: 'number',
+    conversionTarget: true
+  },
+  'SoQLBoolean': {
+    canonicalName: 'boolean',
+    conversionTarget: true
+  },
+  'SoQLFixedTimestamp': {
+    canonicalName: 'fixed_timestamp',
+    conversionTarget: true
+  },
+  'SoQLFloatingTimestamp': {
+    canonicalName: 'floating_timestamp',
+    conversionTarget: true
+  },
+  'SoQLLocation': {
+    canonicalName: 'location',
+    conversionTarget: false
+  },
+  'SoQLPoint': {
+    canonicalName: 'point',
+    conversionTarget: false
+  }
 };
 
 export const soqlTypes = Object.keys(soqlProperties);

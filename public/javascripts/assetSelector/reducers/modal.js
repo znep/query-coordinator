15b467b108d0613c08c20a1503ca55b9136ef40d
Dylan Bussone <dylan.bussone@socrata.com>
2017-01-24 11:58:21 -0800

    EN-12424: Use body class instead of modifying style directly

--- public/javascripts/assetSelector/reducers/modal.js
+++ public/javascripts/assetSelector/reducers/modal.js
@@ -17,11 +17,11 @@ export default (state = initialState(), action = {}) => {
   state = _.cloneDeep(state);
   switch (action.type) {
     case 'OPEN_MODAL':
-      $('body').css('overflow-y', 'hidden'); /* How terrible is this? */
+      $('body').addClass('modal-open');
       state.modalIsOpen = true;
       return state;
     case 'CLOSE_MODAL':
-      $('body').css('overflow-y', 'auto');
+      $('body').removeClass('modal-open');
       state.modalIsOpen = false;
       return state;
     case 'OPEN_EXTERNAL_RESOURCE_CONTAINER':

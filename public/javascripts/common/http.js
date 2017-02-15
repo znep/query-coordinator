e7f4d291e3e2377ae052b58cc0a7db2a0248dd4a
Trey Cordova <treycordova@hotmail.com>
2017-02-14 16:31:04 -0800

    EN-13785: Removes undefined headers from defaultHeaders. (#4058)
    Several services don't appreciate the presence of a header that has a
    value of "undefined". Let's turn those frowns upside down.

--- public/javascripts/common/http.js
+++ public/javascripts/common/http.js
@@ -1,9 +1,11 @@
-export const defaultHeaders = {
+import _ from 'lodash';
+
+export const defaultHeaders =  _.omitBy({
   'Accept': 'application/json',
   'Content-Type': 'application/json',
   'X-CSRF-Token': window.serverConfig.csrfToken,
   'X-App-Token': window.serverConfig.appToken
-};
+}, _.isUndefined);
 
 // Used to throw errors from non-200 responses when using fetch.
 export function checkStatus(response) {

34a180b8f1e6bda7cdf61b4ac453e80f2d5fe498
Dylan Bussone <dylan.bussone@socrata.com>
2017-02-21 11:59:50 -0800

    EN-13475: PR feedback

--- engine/lib/socrata_site_chrome/cache_key.rb
+++ engine/lib/socrata_site_chrome/cache_key.rb
@@ -4,9 +4,21 @@ module SocrataSiteChrome
     # cache_key method in consuming applications. For example in the frontend, this is defined in the
     # frontend/app/models/configuration.rb class.
     def self.cache_key_string(domain_config, config_type)
+      request_config = domain_config.try(:request_config)
+      raise 'Invalid domain_config for Site Chrome cache key' if [
+        domain_config,
+        request_config,
+        request_config.try(:[], :application),
+        request_config.try(:[], :cache_key_prefix),
+        domain_config.try(:cname),
+        domain_config.try(:config_updated_at)
+      ].any?(&:blank?)
+
+      raise 'config_type must be present for Site Chrome cache key' if config_type.blank?
+
       [
-        domain_config.request_config[:application],
-        domain_config.request_config[:cache_key_prefix],
+        request_config[:application],
+        request_config[:cache_key_prefix],
         'domain',
         domain_config.cname,
         domain_config.config_updated_at,
--- engine/spec/socrata_site_chrome/cache_key_spec.rb
+++ engine/spec/socrata_site_chrome/cache_key_spec.rb
@@ -31,5 +31,24 @@ describe SocrataSiteChrome::CacheKey do
 
       expect(result).to eq('frontend:deadbeef:domain:data.michigan.gov:1477076931:configurations:feature_set')
     end
+
+    it 'raises if domain_config is invalid' do
+      expect { helper.cache_key_string(nil, 'feature_set') }.
+        to raise_error('Invalid domain_config for Site Chrome cache key')
+
+      invalid_domain_config = OpenStruct.new(
+        :request_config => nil,
+        :cname => 'test.com',
+        :config_updated_at => Time.now
+      )
+      expect { helper.cache_key_string(invalid_domain_config, 'feature_set') }.
+        to raise_error('Invalid domain_config for Site Chrome cache key')
+    end
+
+    it 'raises if config_type is invalid' do
+      domain_config = SocrataSiteChrome::DomainConfig.new(domain)
+      expect { helper.cache_key_string(domain_config, nil) }.
+        to raise_error('config_type must be present for Site Chrome cache key')
+    end
   end
 end

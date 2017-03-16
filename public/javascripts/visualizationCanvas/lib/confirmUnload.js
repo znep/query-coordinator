94ef5d17cdb35f0734e7433352f83fa272808cd3
Bjorn Swenson <tie.372@gmail.com>
2017-03-16 14:51:14 -0700

    EN-14624: No unsaved changes popup when in view mode; (#4228)

--- public/javascripts/visualizationCanvas/lib/confirmUnload.js
+++ public/javascripts/visualizationCanvas/lib/confirmUnload.js
@@ -1,11 +1,14 @@
+import { ModeStates } from './constants';
+
 const message = window.I18n.unsaved_changes;
 
 export default (store) =>
   (event) => {
-    const { isDirty, isEditMenuActive, authoringWorkflow } = store.getState();
-    const shouldPreventNavigation = isDirty || isEditMenuActive || authoringWorkflow.isActive;
+    const { isDirty, isEditMenuActive, authoringWorkflow, mode } = store.getState();
+    const hasChanged = isDirty || isEditMenuActive || authoringWorkflow.isActive;
+    const isViewMode = mode === ModeStates.VIEW;
 
-    if (shouldPreventNavigation) {
+    if (hasChanged && !isViewMode) {
       event.returnValue = message;
       return message;
     }

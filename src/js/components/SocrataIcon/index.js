1817f0e920e3bf0a2c696fdb14c6f94af147ceb3
Nate Moore <nathaniel.moore@socrata.com>
2017-03-16 14:05:56 -0700

    Add bell and logo icons (#263)
    * Add bell and logo icons
    
    * Bump version to 11.8.0
    
    * Add styles for coloring logo with SocrataIcon

--- src/js/components/SocrataIcon/index.js
+++ src/js/components/SocrataIcon/index.js
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 
-export function SocrataIcon({ name }) {
+export function SocrataIcon({ name, className }) {
   const attributes = {
-    className: `socrata-icon socrata-icon-${name}`,
+    className: `socrata-icon socrata-icon-${name} ${className}`,
     dangerouslySetInnerHTML: {
       __html: require(`src/fonts/svg/${name}.svg`) // eslint-disable-line global-require
     }
@@ -12,7 +12,12 @@ export function SocrataIcon({ name }) {
 }
 
 SocrataIcon.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+SocrataIcon.defaultProps = {
+  className: ''
 };
 
 export default SocrataIcon;

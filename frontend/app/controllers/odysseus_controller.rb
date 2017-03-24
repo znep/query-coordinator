bc45496a219004291cf4484488fb3cc2a2a4ebdf
Andrew Harrison <andrew.harrison@socrata.com>
2017-03-24 15:08:43 -0700

    EN-14883: Add request ID when proxying to Odysseus

--- frontend/app/controllers/odysseus_controller.rb
+++ frontend/app/controllers/odysseus_controller.rb
@@ -140,6 +140,7 @@ class OdysseusController < ApplicationController
     req['X-Socrata-Host'] = req['Host'] = CurrentDomain.cname
     req['X-Socrata-Locale'] = I18n.locale
     req['X-Socrata-Default-Locale'] = CurrentDomain.default_locale
+    req['X-Socrata-RequestId'] = request_id
     req['Cookie'] = request.headers['Cookie']
 
     begin

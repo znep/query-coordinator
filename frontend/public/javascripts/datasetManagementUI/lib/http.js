67a139726642c476d45faa930f84062bef3fd31c
Kaida Masaki <kaida.masaki@socrata.com>
2017-03-28 14:23:45 -0700

    Add logic around retrying upserts

--- frontend/public/javascripts/datasetManagementUI/lib/http.js
+++ frontend/public/javascripts/datasetManagementUI/lib/http.js
@@ -33,6 +33,22 @@ export function checkStatus(response) {
   throw error;
 }
 
+export function checkStatusForPoll(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else if (response.status === 503) {
+    return null; // We want to retry these but not actually try to process them.
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
 export function getJson(resp) {
-  return resp.json();
+  if (resp) {
+    return resp.json();
+  } else {
+    return resp;
+  }
 }

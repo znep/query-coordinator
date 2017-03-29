2be2f18d3220777087f328db42b7db2d1c868d21
Pete Vilter <peter.vilter@socrata.com>
2017-03-29 16:06:36 -0700

    remove nprogress stuff

--- frontend/public/javascripts/datasetManagementUI/components/App.js
+++ frontend/public/javascripts/datasetManagementUI/components/App.js
@@ -1,14 +1,12 @@
 import React, { PropTypes } from 'react';
 import AppBar from './AppBar';
 import NotificationList from './NotificationList';
-import ThinTopProgressBar from './ThinTopProgressBar';
 import styles from 'styles/App.scss';
 
 const classNames = `dataset-management-ui ${styles.datasetManagementUi}`;
 
 const App = ({ children }) =>
   <div className={classNames}>
-    <ThinTopProgressBar />
     <AppBar />
     {children}
     <NotificationList />

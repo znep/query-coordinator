ab1ef0b22cb123f7d935eb8f80a1da07e1f0de45
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2017-03-29 17:48:43 -0700

    EN-14559: Fix frontend common tests.

--- frontend/karma/common/karma.conf.js
+++ frontend/karma/common/karma.conf.js
@@ -1,8 +1,11 @@
+var webpackCommon = require('../../config/webpack/common');
+
 var karmaConfig = require('../helpers/karma_config');
 var webpackConfig = require('../helpers/webpack').karmaWebpackConfig(
   'base',
   [ 'public/javascripts/common', 'karma/common' ]
 );
+webpackConfig.module.loaders = [ webpackCommon.getBabelLoader() ];
 
 module.exports = function (karma) {
   karma.set(karmaConfig({

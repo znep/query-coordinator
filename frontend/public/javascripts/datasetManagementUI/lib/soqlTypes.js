91a96648b7ead66b9bc080c7a4906716f1d87975
Robert J. Macomber <robert.macomber@socrata.com>
2017-04-05 13:19:58 -0700

    EN-15328: Provide a separate type property on soql types for CSS
    So that the CSS can fit the prevailing style

--- frontend/public/javascripts/datasetManagementUI/lib/soqlTypes.js
+++ frontend/public/javascripts/datasetManagementUI/lib/soqlTypes.js
@@ -3,31 +3,37 @@
 export const soqlProperties = {
   SoQLText: {
     canonicalName: 'text', // The standard SODA2 API name
+    cssName: 'text', // The name used in CSS classes
     conversionTarget: true, // Whether a simple to_$canonicalname function exists
     icon: 'text'
   },
   SoQLNumber: {
     canonicalName: 'number',
+    cssName: 'number',
     conversionTarget: true,
     icon: 'number'
   },
   SoQLBoolean: {
     canonicalName: 'boolean',
+    cssName: 'boolean',
     conversionTarget: true,
     icon: 'boolean'
   },
   SoQLFloatingTimestamp: {
     canonicalName: 'floating_timestamp',
+    cssName: 'floatingTimestamp',
     conversionTarget: true,
     icon: 'date'
   },
   SoQLLocation: {
     canonicalName: 'location',
+    cssName: 'location',
     conversionTarget: false,
     sodaType: 'location'
   },
   SoQLPoint: {
     canonicalName: 'point',
+    cssName: 'point',
     conversionTarget: false,
     icon: 'map'
   }

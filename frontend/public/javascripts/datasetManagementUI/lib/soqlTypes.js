znep/query-coordinator

commit c505d5a9e1963e16460f81258d90efe1b33d81fd

EN-14441 css modules for DSMUI (#4202)
* EN-14020: Update rails -> 4.2.8

Release Notes:
http://weblog.rubyonrails.org/2017/2/21/Rails-4-2-8-has-been-released/

Primarily changes in ActiveSupport and Rails core around
Date/Time/Timezone items

* Fix failing rspec test around missing translation

I'm not sure if this is the correct way to fix this, but I ran the
`bin/pull-translations` commmand and it updated this file and then rspec
passed.

* [EN-14441] added css module support for dsmui

* [EN-14441] added way for modules to coexist with global library

* [EN-14441] modularized showupdate

* [EN-14441] fixed metadata firefox bug declaratively

* [EN-14441] modularized css for schemapreview and datasetpreview

* [EN-14441] finished componentizing css for ShowUpdate

* [EN-14441] replaced hardcoded icons with react icon component

* [EN-14441] modularizedd styles for several components

* [EN-14441] modularized textInput styles

* [EN-14441] modularized textArea and select styles

* [EN-14441] modularized metadataeditor styles

* [EN-14441] modularized metadata edit components styles

* [EN-14441] modularized metadacontent styles

* [EN-14441] modularized styles from manageMetadata

* [EN-14441] updated savebtn styles

* [EN-14441] modularized notification styles

* [EN-14441] module styles for ShowUpload

* [EN-14441] modularized table styles

* [EN-14441] modularized table styles

* [EN-14441] modularized styles for remaining components

* [EN-14441] cleanup

* [EN-14441] cleanup from merge

* [EN-14441] configured karma to work with cssmodules

* [EN-14441] fixed broken tests

* [EN-14441] fixed style erros in outputschema table

* [EN-14441] fixed progresbars in outputschema

frontend/public/javascripts/datasetManagementUI/lib/soqlTypes.js
@@ -17,7 +17,7 @@ export const soqlProperties = {
     icon: 'boolean'
   },
   SoQLFloatingTimestamp: {
-    canonicalName: 'floating_timestamp',
+    canonicalName: 'floatingTimestamp',
     conversionTarget: true,
     icon: 'date'
   },

frontend/public/javascripts/datasetManagementUI/styles/TypeIcon.scss
@@ -1,0 +1,28 @@
+@import 'node_modules/bourbon/app/assets/stylesheets/functions/shade';
+@import 'node_modules/bourbon/app/assets/stylesheets/functions/tint';
+@import 'app/styles/styleguide-colors';
+
+.date {
+  color: #B26B00;
+}
+.text {
+  color: #204490;
+}
+.number {
+  color: #0098C8;
+}
+.boolean {
+  color: #894BAA;
+}
+.fixedTimestamp {
+  color: #B26B00;
+}
+.floatingTimestamp {
+  color: #B26B00;
+}
+.location {
+  color: #006A01;
+}
+.point {
+  color: #006A01;
+}

commit a522041b9aa3a5b50eb1c037e0a4e9796cac0947

EN-15328: Change soql type's canonical names back to their canonical names
Was broken by c505d5a9e1963e16460f81258d90efe1b33d81f which changed
from snake case to camel case for some reason.

frontend/public/javascripts/datasetManagementUI/lib/soqlTypes.js
@@ -17,7 +17,7 @@ export const soqlProperties = {
     icon: 'boolean'
   },
   SoQLFloatingTimestamp: {
-    canonicalName: 'floatingTimestamp',
+    canonicalName: 'floating_timestamp',
     conversionTarget: true,
     icon: 'date'
   },

frontend/public/javascripts/datasetManagementUI/styles/TypeIcon.scss
@@ -14,10 +14,10 @@
 .boolean {
   color: #894BAA;
 }
-.fixedTimestamp {
+.fixed_timestamp {
   color: #B26B00;
 }
-.floatingTimestamp {
+.floating_timestamp {
   color: #B26B00;
 }
 .location {
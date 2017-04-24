77bc2876cb1d8d6b333d77d98f45ea46c01e74ed
Dylan Bussone <dylan.bussone@socrata.com>
2017-04-24 15:21:25 -0700

    Update helpful comment

--- engine/app/assets/javascripts/socrata_site_chrome/application.js
+++ engine/app/assets/javascripts/socrata_site_chrome/application.js
@@ -20,5 +20,5 @@
 
   If you add new files to the tree, or require new node_modules, you ALSO need to manually add those items
   to the custom site chrome case, if applicable.
-  Do this in the `site_chrome_javascript_tag` method in engine/lib/site_chrome_helper.rb
+  Do this in the `site_chrome_javascript_tag` method in engine/lib/site_chrome_consumer_helpers.rb
 */

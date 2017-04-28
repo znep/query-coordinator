db0e7477a89301ff1be8604abcd6a2da6bfbd91a
Randy Antler <randy.antler@socrata.com>
2017-04-28 09:25:49 -0700

    EN-15606: Fix bug that deleted the wrong connector

--- frontend/app/helpers/administration/connector_helper.rb
+++ frontend/app/helpers/administration/connector_helper.rb
@@ -2,7 +2,7 @@ module Administration
   module ConnectorHelper
 
     def esri_arcgis?
-      params[:type] == 'esri_arcgis'
+      params[:type] == 'esri_arcgis' || params[:server_backend] == 'esri_crawler'
     end
 
  end

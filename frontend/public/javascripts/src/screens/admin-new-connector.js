d024852dbfefce32d2c696cc225dadf404085c3c
Randy Antler <randy.antler@socrata.com>
2017-05-01 13:36:05 -0700

    EN-16028: eslint

--- frontend/public/javascripts/src/screens/admin-new-connector.js
+++ frontend/public/javascripts/src/screens/admin-new-connector.js
@@ -3,6 +3,6 @@ $(function() {
     var esriArcgis = this.options[this.selectedIndex].value === 'esri_arcgis';
 
     $('.new-connector-form .esri-arcgis')[esriArcgis ? 'show' : 'hide']().attr('disabled', !esriArcgis);
-    $('.new-connector-form .data-json')[esriArcgis ? 'hide' : 'show']().attr('disabled', esriArcgis)
+    $('.new-connector-form .data-json')[esriArcgis ? 'hide' : 'show']().attr('disabled', esriArcgis);
   });
 });

dc58e840b07d64c488005031d96c366e7626334e
Brandon Webster <brandon.webster@shiftboard.com>
2017-05-19 15:16:47 -0700

    add back removed os subscription to bootstrap

--- frontend/public/javascripts/datasetManagementUI/bootstrap.js
+++ frontend/public/javascripts/datasetManagementUI/bootstrap.js
@@ -1,11 +1,8 @@
 import _ from 'lodash';
-import {
-  upsertFromServer,
-  batch
-} from 'actions/database';
+import { upsertFromServer, batch } from 'actions/database';
 import { setFourfour } from 'actions/routing';
 import { showModal } from 'actions/modal';
-import { insertAndSubscribeToUpload } from 'actions/manageUploads';
+import { insertAndSubscribeToUpload, insertChildrenAndSubscribeToOutputSchema } from 'actions/manageUploads';
 import { parseDate } from 'lib/parseDate';
 import * as ApplyRevision from 'actions/applyRevision';
 import { createCombinedValidationRules, createInitialModel } from 'components/ManageMetadata/DatasetForm';
@@ -40,46 +37,58 @@ const millis = 1000;
 export function bootstrap(store, initialView, initialRevision, customMetadata) {
   const operations = [];
   store.dispatch(setFourfour(initialView.id));
-  operations.push(upsertFromServer('views', {
-    id: initialView.id,
-    name: initialView.name,
-    description: initialView.description,
-    category: initialView.category,
-    owner: initialView.owner,
-    lastUpdatedAt: new Date(initialView.viewLastModified * millis), // TODO: not sure about this one
-    dataLastUpdatedAt: new Date(initialView.rowsUpdatedAt * millis),
-    metadataLastUpdatedAt: new Date(initialView.viewLastModified * millis),
-    createdAt: new Date(initialView.createdAt * millis),
-    viewCount: initialView.viewCount,
-    downloadCount: initialView.downloadCount,
-    license: initialView.license || {},
-    licenseId: initialView.licenseId,
-    attribution: initialView.attribution,
-    attributionLink: initialView.attributionLink,
-    schema: calculateInitialSchema(initialView, customMetadata),
-    tags: initialView.tags || [],
-    privateMetadata: initialView.privateMetadata || {},
-    attachments: _.get(initialView, 'metadata.attachments', []),
-    metadata: initialView.metadata || {},
-    customMetadataFields: window.initialState.customMetadata || []
-  }));
-  operations.push(upsertFromServer('revisions', {
-    id: initialRevision.id,
-    fourfour: initialView.id,
-    revision_seq: _.toNumber(initialRevision.revision_seq),
-    created_at: parseDate(initialRevision.inserted_at || initialRevision.created_at),
-    created_by: initialRevision.created_by
-  }));
-  initialRevision.uploads.forEach((upload) => {
+  operations.push(
+    upsertFromServer('views', {
+      id: initialView.id,
+      name: initialView.name,
+      description: initialView.description,
+      category: initialView.category,
+      owner: initialView.owner,
+      lastUpdatedAt: new Date(initialView.viewLastModified * millis), // TODO: not sure about this one
+      dataLastUpdatedAt: new Date(initialView.rowsUpdatedAt * millis),
+      metadataLastUpdatedAt: new Date(initialView.viewLastModified * millis),
+      createdAt: new Date(initialView.createdAt * millis),
+      viewCount: initialView.viewCount,
+      downloadCount: initialView.downloadCount,
+      license: initialView.license || {},
+      licenseId: initialView.licenseId,
+      attribution: initialView.attribution,
+      attributionLink: initialView.attributionLink,
+      schema: calculateInitialSchema(initialView, customMetadata),
+      tags: initialView.tags || [],
+      privateMetadata: initialView.privateMetadata || {},
+      attachments: _.get(initialView, 'metadata.attachments', []),
+      metadata: initialView.metadata || {},
+      customMetadataFields: window.initialState.customMetadata || []
+    })
+  );
+  operations.push(
+    upsertFromServer('revisions', {
+      id: initialRevision.id,
+      fourfour: initialView.id,
+      revision_seq: _.toNumber(initialRevision.revision_seq),
+      created_at: parseDate(initialRevision.inserted_at || initialRevision.created_at),
+      created_by: initialRevision.created_by
+    })
+  );
+
+  initialRevision.uploads.forEach(upload => {
+    _.each(_.flatMap(upload.schemas, is => is.output_schemas), os => {
+      store.dispatch(insertChildrenAndSubscribeToOutputSchema(os));
+    });
+
     insertAndSubscribeToUpload(store.dispatch, upload);
   });
-  initialRevision.upsert_jobs.forEach((upsertJob) => {
-    operations.push(upsertFromServer('upsert_jobs', {
-      ...upsertJob,
-      created_at: parseDate(upsertJob.inserted_at || upsertJob.created_at),
-      finished_at: upsertJob.finished_at ? parseDate(upsertJob.finished_at) : null,
-      created_by: upsertJob.created_by
-    }));
+
+  initialRevision.upsert_jobs.forEach(upsertJob => {
+    operations.push(
+      upsertFromServer('upsert_jobs', {
+        ...upsertJob,
+        created_at: parseDate(upsertJob.inserted_at || upsertJob.created_at),
+        finished_at: upsertJob.finished_at ? parseDate(upsertJob.finished_at) : null,
+        created_by: upsertJob.created_by
+      })
+    );
     if (upsertJob.status === ApplyRevision.UPSERT_JOB_IN_PROGRESS) {
       store.dispatch(ApplyRevision.pollForUpsertJobProgress(upsertJob.id));
     }

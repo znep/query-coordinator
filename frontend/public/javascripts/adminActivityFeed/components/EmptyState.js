e829bb43d04f8c02f84fe1b0bc36633ee690835e
Rifat Cagri Ekin <cagri.ekin@gmail.com>
2017-05-29 13:54:53 +0300

    EN-16421 Admin > Activity Log > Empty table message after filters

--- frontend/public/javascripts/adminActivityFeed/components/EmptyState.js
+++ frontend/public/javascripts/adminActivityFeed/components/EmptyState.js
@@ -1,11 +1,18 @@
+import { fromJS } from 'immutable';
 import React from 'react';
 import connectLocalization from './Localization/connectLocalization';
 import { connect } from 'react-redux';
+import LocalizedText from './Localization/LocalizedText';
 
 import './EmptyState.scss';
 
 class EmptyState extends React.Component {
-  renderMessage() {
+  // Don't re-render component if loading in progress.
+  shouldComponentUpdate(nextProps) {
+    return !nextProps.inLoadingStage;
+  }
+
+  getEmptyTableMessage() {
     const { localization } = this.props;
 
     const message = localization.translate('empty_message', {
@@ -15,13 +22,35 @@ class EmptyState extends React.Component {
     return <div className="emptyState" dangerouslySetInnerHTML={{__html: message}} />;
   }
 
+  getFilteredEmptyTableMessage() {
+    return (
+      <div className="emptyState">
+        <LocalizedText localeKey='empty_message_filtered'/>
+      </div>
+    );
+  }
+
+  renderMessage() {
+    return this.props.isNotFiltered ?
+      this.getEmptyTableMessage() :
+      this.getFilteredEmptyTableMessage();
+  }
+
   render() {
     return this.props.isStateEmpty ? this.renderMessage() : null;
   }
 }
 
-const mapStateToProps = (state) => ({
-  isStateEmpty: state.get('activities').count() === 0
-});
+const mapStateToProps = (state) => {
+  const filtersEmptyState = {event: 'All', status: 'All', dateFrom: null, dateTo: null};
+  const isNotFiltered = state.get('filter').equals(fromJS(filtersEmptyState));
+  const inLoadingStage = state.get('loading');
+
+  return {
+    isStateEmpty: state.get('activities').count() === 0,
+    isNotFiltered,
+    inLoadingStage
+  };
+};
 
 export default connectLocalization(connect(mapStateToProps)(EmptyState));

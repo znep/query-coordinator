66bd7a0d626f7ca86aa42751580809ee1ea83677
Giacomo Ferrari <giacomo.ferrari@socrata.com>
2017-06-01 16:26:32 -0700

    EN-16246: Re-babelify frontend's common tests.

--- frontend/karma/common/karma.conf.js
+++ frontend/karma/common/karma.conf.js
@@ -5,7 +5,7 @@ var webpackConfig = require('../helpers/webpack').karmaWebpackConfig(
   'base',
   [ 'public/javascripts/common', 'karma/common' ]
 );
-webpackConfig.module.loaders = [ webpackCommon.getStandardLoaders() ];
+webpackConfig.module.loaders = webpackCommon.getStandardLoaders();
 
 module.exports = function (karma) {
   karma.set(karmaConfig({

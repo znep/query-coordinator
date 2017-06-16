81c5e94d728d70b239d8efae947fa3bca8b292ee
Randy Antler <randy.antler@socrata.com>
2017-06-16 15:06:24 -0700

    EN-17039: Don't mixpanel without mixpanelContext (#4919)
    (cherry picked from commit 5d2b10df04f7ea5eb839a8c2d6dcb2c44135f922)

--- frontend/public/javascripts/common/cetera_utils.js
+++ frontend/public/javascripts/common/cetera_utils.js
@@ -125,14 +125,15 @@ export const ceteraUtils = (() => {
       const fetchUrl = `${CETERA_RESULTS_PATH}?${queryString}`;
 
       const reportToMixpanel = (json) => {
-        console.debug(`reportToMixpanel: "${mixpanelContext.eventName}"`);
-        mixpanel.sendPayload(
-          mixpanelContext.eventName,
-          {
-            'Result Count': json.results.length,
-            ...mixpanelContext.params
-          }
-        );
+        if (mixpanelContext) {
+          mixpanel.sendPayload(
+            mixpanelContext.eventName,
+            {
+              'Result Count': json.results.length,
+              ...mixpanelContext.params
+            }
+          );
+        }
         return json;
       };
 

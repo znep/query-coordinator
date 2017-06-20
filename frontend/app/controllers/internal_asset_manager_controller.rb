7b781a234cfe034f3a689474461990e269ffdb36
Randy Antler <randy.antler@socrata.com>
2017-06-19 18:11:43 -0700

    EN-16948: Add more asset type counts (#4926)
    * EN-16948: Add more asset type counts
    
    * EN-16807: eslint
    
    * EN-19948: Use correct view type names
    
    * EN-16948: Sort asset counts by name
    
    * EN-16948: Add more singular/plural tests

--- frontend/app/controllers/internal_asset_manager_controller.rb
+++ frontend/app/controllers/internal_asset_manager_controller.rb
@@ -20,20 +20,18 @@ class InternalAssetManagerController < ApplicationController
       show_visibility: true
     }
 
-    cetera_response =
-      begin
-        AssetInventoryService::InternalAssetManager.find(request_id, cookie, search_options).to_h
-      rescue => e
-        report_error("Error fetching Cetera results: #{e.inspect}")
-        { 'results' => [], 'resultSetSize' => 0 }
-      end
+    cetera_response = begin
+      AssetInventoryService::InternalAssetManager.find(request_id, cookie, search_options).to_h
+    rescue => e
+      report_error("Error fetching Cetera results: #{e.inspect}")
+      { 'results' => [], 'resultSetSize' => 0 }
+    end
 
     @catalog_results = cetera_response['results'].to_a
     @catalog_result_set_size = cetera_response['resultSetSize'].to_i
 
-    cetera_user_search_client = Cetera::Utils.user_search_client
     begin
-      roled_users = cetera_user_search_client.find_all_with_roles(request_id, forwardable_session_cookies)
+      roled_users = Cetera::Utils.user_search_client.find_all_with_roles(request_id, forwardable_session_cookies)
       user_results = Cetera::Results::UserSearchResult.new(roled_users).results
     rescue => e
       report_error("Error fetching Cetera user results: #{e.inspect}")
@@ -42,36 +40,32 @@ class InternalAssetManagerController < ApplicationController
     @users_list = user_results.sort_by(&:sort_key)
 
     cetera_facet_search_client = Cetera::Utils.facet_search_client
+    @domain_categories = begin
+      cetera_facet_search_client.get_categories_of_views(
+        request_id, forwardable_session_cookies, domains: CurrentDomain.cname
+      ).to_h['results'].to_a.pluck('domain_category').reject(&:empty?)
+    rescue => e
+      report_error("Error fetching Cetera domain categories: #{e.inspect}")
+      []
+    end
 
-    @domain_categories =
-      begin
-        cetera_facet_search_client.get_categories_of_views(
-          request_id, forwardable_session_cookies, domains: CurrentDomain.cname
-        ).to_h['results'].to_a.pluck('domain_category').reject(&:empty?)
-      rescue => e
-        report_error("Error fetching Cetera domain categories: #{e.inspect}")
-        []
-      end
-
-    @domain_tags =
-      begin
-        cetera_facet_search_client.get_tags_of_views(
-          request_id, forwardable_session_cookies, domains: CurrentDomain.cname
-        ).to_h['results'].to_a.pluck('domain_tag').reject(&:empty?)
-      rescue => e
-        report_error("Error fetching Cetera domain tags: #{e.inspect}")
-        []
-      end
+    @domain_tags = begin
+      cetera_facet_search_client.get_tags_of_views(
+        request_id, forwardable_session_cookies, domains: CurrentDomain.cname
+      ).to_h['results'].to_a.pluck('domain_tag').reject(&:empty?)
+    rescue => e
+      report_error("Error fetching Cetera domain tags: #{e.inspect}")
+      []
+    end
 
     # Note: asset_types should match those listed in the `asset_counts` component and reducer
-    asset_types = %w(datasets charts maps stories)
-    @asset_counts =
-      begin
-        Cetera::Utils.get_asset_counts(asset_types, request_id, forwardable_session_cookies)
-      rescue => e
-        report_error("Error fetching Cetera asset counts: #{e.inspect}")
-        {}
-      end
+    asset_types = %w(charts datalenses datasets hrefs filters maps stories)
+    @asset_counts = begin
+      Cetera::Utils.get_asset_counts(asset_types, request_id, forwardable_session_cookies)
+    rescue => e
+      report_error("Error fetching Cetera asset counts: #{e.inspect}")
+      {}
+    end
   end
 
   private

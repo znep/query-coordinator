3fc6eb11325432e2d5459acb9a0da5b0b789270a
Randy Antler <randy.antler@socrata.com>
2017-07-12 18:30:11 -0700

    EN-17188: Add aria helper method to site_chrome (#5094)
    (cherry picked from commit 2d5d9f0fbc96c513abbd0937008c331028db71c6)

--- storyteller/app/helpers/application_helper.rb
+++ storyteller/app/helpers/application_helper.rb
@@ -39,6 +39,12 @@ module ApplicationHelper
     content_tag('script', '', :type => 'text/javascript', :src => uri)
   end
 
+  def icon_with_aria_text(text, opts = {})
+    content_tag(:span, :class => opts.fetch(:class, 'icon')) do
+      content_tag(:span, text, :class  => 'aria-not-displayed')
+    end
+  end
+
   def page_title
     page_title_parts = []
     page_title_parts << @story_metadata.title

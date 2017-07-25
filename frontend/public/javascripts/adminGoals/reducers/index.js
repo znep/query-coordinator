4791002d614adf17900d314627b05f3ed1d00dc1
Andrew Harrison <andrew.harrison@socrata.com>
2017-07-25 10:56:45 -0700

    EN-17498: Remove feedback panel from vetted components

--- frontend/public/javascripts/adminGoals/reducers/index.js
+++ frontend/public/javascripts/adminGoals/reducers/index.js
@@ -1,13 +1,11 @@
 import * as Immutable from 'immutable';
 import * as ReduxImmutable  from 'redux-immutablejs';
 import * as Sections from '../sections';
-import * as Feedback from '../components/feedback';
 import notification from './notification';
 
 export default ReduxImmutable.combineReducers({
   translations: state => state || Immutable.fromJS(window.translations || {}),
   notification,
   goals: Sections.Goals.rootReducer,
-  shared: Sections.Shared.rootReducer,
-  feedback: Feedback.Flannel.rootReducer
+  shared: Sections.Shared.rootReducer
 });

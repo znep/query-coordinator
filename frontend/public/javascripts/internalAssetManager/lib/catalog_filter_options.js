80c9fa8df7a5168cce7b4416f8a9f37e9f543284
Dylan Bussone <dylanbussone@gmail.com>
2017-07-31 21:44:26 +0100

    EN-17431: SIAM - add icons to asset type filter dropdown (#5224)

--- frontend/public/javascripts/internalAssetManager/lib/catalog_filter_options.js
+++ frontend/public/javascripts/internalAssetManager/lib/catalog_filter_options.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { SocrataIcon } from 'common/components';
 
+const calendarIcon = <SocrataIcon name="date" />;
+const chartIcon = <SocrataIcon name="bar-chart" />;
+const datasetIcon = <SocrataIcon name="dataset" />;
+const datalensIcon = <SocrataIcon name="cards" />;
+const externalIcon = <SocrataIcon name="external" />;
+const fileIcon = <SocrataIcon name="attachment" />;
+const filteredIcon = <SocrataIcon name="filter" />;
+const formIcon = <SocrataIcon name="list2" />;
+const mapIcon = <SocrataIcon name="map" />;
+const storyIcon = <SocrataIcon name="story" />;
+const workingIcon = <SocrataIcon name="working-copy" />;
+
 const communityIcon = <SocrataIcon name="community" />;
 const officialIcon = <SocrataIcon name="official2" />;
 
@@ -8,17 +20,17 @@ const getTranslation = (key) => _.get(I18n, `filters.${key}`);
 
 export const assetTypeOptions = [
   { title: getTranslation('asset_types.options.all'), value: null },
-  { title: getTranslation('asset_types.options.calendars'), value: 'calendars' },
-  { title: getTranslation('asset_types.options.charts'), value: 'charts' },
-  { title: getTranslation('asset_types.options.datasets'), value: 'datasets' },
-  { title: getTranslation('asset_types.options.datalenses'), value: 'datalenses' },
-  { title: getTranslation('asset_types.options.external'), value: 'hrefs' },
-  { title: getTranslation('asset_types.options.files'), value: 'files' },
-  { title: getTranslation('asset_types.options.filtered'), value: 'filters' },
-  { title: getTranslation('asset_types.options.forms'), value: 'forms' },
-  { title: getTranslation('asset_types.options.maps'), value: 'maps' },
-  { title: getTranslation('asset_types.options.stories'), value: 'stories' },
-  { title: getTranslation('asset_types.options.working_copies'), value: 'workingCopies' }
+  { title: getTranslation('asset_types.options.calendars'), value: 'calendars', icon: calendarIcon },
+  { title: getTranslation('asset_types.options.charts'), value: 'charts', icon: chartIcon },
+  { title: getTranslation('asset_types.options.datasets'), value: 'datasets', icon: datasetIcon },
+  { title: getTranslation('asset_types.options.datalenses'), value: 'datalenses', icon: datalensIcon },
+  { title: getTranslation('asset_types.options.external'), value: 'hrefs', icon: externalIcon },
+  { title: getTranslation('asset_types.options.files'), value: 'files', icon: fileIcon },
+  { title: getTranslation('asset_types.options.filtered'), value: 'filters', icon: filteredIcon },
+  { title: getTranslation('asset_types.options.forms'), value: 'forms', icon: formIcon },
+  { title: getTranslation('asset_types.options.maps'), value: 'maps', icon: mapIcon },
+  { title: getTranslation('asset_types.options.stories'), value: 'stories', icon: storyIcon },
+  { title: getTranslation('asset_types.options.working_copies'), value: 'workingCopies', icon: workingIcon }
 ];
 
 export const authorityOptions = [

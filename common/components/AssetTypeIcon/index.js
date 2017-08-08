93a6a50e2c8e7223345b2d73fa9ab0eca77eb21b
Michael Chui <michael.chui@socrata.com>
2017-08-08 16:14:48 -0700

    EN-17007: Generalize an AssetTypeIcon for SIAM.

---
+++ common/components/AssetTypeIcon/index.js
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import React, { PropTypes } from 'react';
+import { getIconNameForDisplayType } from 'common/displayTypeMetadata';
+import SocrataIcon from '../SocrataIcon';
+
+class AssetTypeIcon extends React.Component {
+  getSvgName() {
+    const { displayType, isPublished } = this.props;
+    return getIconNameForDisplayType(displayType, isPublished);
+  }
+
+  render() {
+    const { displayType, className, tooltip } = this.props;
+    const name = this.getSvgName();
+
+    const otherAttributes = {
+      'data-type': displayType,
+      title: tooltip
+    }
+
+    return <SocrataIcon
+      name={name}
+      className={`asset-type-icon ${className}`}
+      otherAttributes={otherAttributes} />;
+  }
+};
+
+AssetTypeIcon.propTypes = {
+  displayType: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  isPublished: PropTypes.bool,
+  tooltip: PropTypes.string
+};
+
+AssetTypeIcon.defaultProps = {
+  className: '',
+  isPublished: true
+};
+
+export default AssetTypeIcon;

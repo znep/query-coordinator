6addd4634ebe1616b0b42a6dfdd11806041f10bf
Brandon Webster <brandon.webster@socrata.com>
2017-09-01 14:15:15 -0700

    EN-15668 made recent actions less insane

---
+++ frontend/public/javascripts/datasetManagementUI/components/RecentActionItems/RecentActionItems.js
@@ -0,0 +1,136 @@
+import React, { PropTypes } from 'react';
+import moment from 'moment';
+import { Link } from 'react-router';
+import SocrataIcon from '../../../common/components/SocrataIcon';
+import * as Links from 'links';
+import styles from './RecentActionItems.scss';
+
+const RecentActionsTimestamp = ({ date }) =>
+  <span className={styles.timestamp}>
+    {moment.utc(date).fromNow()}
+  </span>;
+
+RecentActionsTimestamp.propTypes = {
+  date: PropTypes.object.isRequired
+};
+
+export const RevisionActivity = ({ details }) =>
+  <div className={styles.activity} data-activity-type="update">
+    <div className={styles.timeline}>
+      <SocrataIcon name="plus2" className={styles.icon} />
+    </div>
+    <div>
+      <p>
+        <span className={styles.createdBy}>{details.createdBy}</span> opened a revision
+      </p>
+      <RecentActionsTimestamp date={details.createdAt} />
+    </div>
+  </div>;
+
+RevisionActivity.propTypes = {
+  details: PropTypes.shape({
+    createdAt: PropTypes.object,
+    createdBy: PropTypes.string
+  }).isRequired
+};
+
+export const SourceActivity = ({ details }) =>
+  <div className={styles.activity} data-activity-type="source">
+    <div className={styles.timeline}>
+      <SocrataIcon name="data" className={styles.icon} />
+    </div>
+    <div>
+      <p>
+        <span className={styles.createdBy}>{details.createdBy}</span> uploaded a file
+      </p>
+      <RecentActionsTimestamp date={details.createdAt} />
+    </div>
+  </div>;
+
+SourceActivity.propTypes = {
+  details: PropTypes.shape({
+    createdAt: PropTypes.object,
+    createdBy: PropTypes.string
+  }).isRequired
+};
+
+export const OutputSchemaActivity = ({ details, params }) =>
+  <div className={styles.activity} data-activity-type="outputschema">
+    <div className={styles.timeline}>
+      <SocrataIcon name="edit" className={styles.icon} />
+    </div>
+    <div>
+      <p>
+        <span className={styles.createdBy}>{details.createdBy}</span> changed the&nbsp;
+        <Link to={Links.showOutputSchema(params, details.sourceId, details.isid, details.osid)}>schema</Link>
+      </p>
+      <RecentActionsTimestamp date={details.createdAt} />
+    </div>
+  </div>;
+
+OutputSchemaActivity.propTypes = {
+  details: PropTypes.shape({
+    createdAt: PropTypes.object,
+    createdBy: PropTypes.string,
+    sourceId: PropTypes.number,
+    isid: PropTypes.number,
+    osid: PropTypes.number
+  }).isRequired,
+  params: PropTypes.object.isRequired
+};
+
+export const TaskSetActivity = ({ details }) =>
+  <div className={styles.activity} data-activity-type="taskSet">
+    <div className={styles.timeline}>
+      <SocrataIcon name="dataset" className={styles.icon} />
+    </div>
+    <div>
+      <p>
+        <span className={styles.createdBy}>{details.createdBy}</span> started data processing
+      </p>
+      <RecentActionsTimestamp date={details.createdAt} />
+    </div>
+  </div>;
+
+TaskSetActivity.propTypes = {
+  details: PropTypes.shape({
+    createdAt: PropTypes.object,
+    createdBy: PropTypes.string
+  }).isRequired
+};
+
+export const TaskSetFinishedActivity = ({ details }) =>
+  <div className={styles.activity} data-activity-type="taskSetcompleted">
+    <div className={styles.timeline}>
+      <SocrataIcon name="checkmark3" className={styles.icon} />
+    </div>
+    <div>
+      <p>Data processing successfully finished</p>
+      <RecentActionsTimestamp date={details.createdAt} />
+    </div>
+  </div>;
+
+TaskSetFinishedActivity.propTypes = {
+  details: PropTypes.shape({
+    createdAt: PropTypes.object,
+    createdBy: PropTypes.string
+  }).isRequired
+};
+
+export const TaskSetFailedActivity = ({ details }) =>
+  <div className={styles.activity} data-activity-type="taskSetfailed">
+    <div className={styles.timeline}>
+      <SocrataIcon name="failed" className={styles.icon} />
+    </div>
+    <div>
+      <p>Data processing failed</p>
+      <RecentActionsTimestamp date={details.createdAt} />
+    </div>
+  </div>;
+
+TaskSetFailedActivity.propTypes = {
+  details: PropTypes.shape({
+    createdAt: PropTypes.object,
+    createdBy: PropTypes.string
+  }).isRequired
+};
--- frontend/public/javascripts/datasetManagementUI/containers/RecentActionsContainer.js
+++ frontend/public/javascripts/datasetManagementUI/containers/RecentActionsContainer.js
@@ -1,9 +1,137 @@
+/* eslint new-cap: 0 */
+/* eslint no-confusing-arrow: 0 */
+import daggy from 'lib/daggy';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { withRouter } from 'react-router';
 import RecentActions from 'components/RecentActions/RecentActions';
+import * as ApplyRevision from 'reduxStuff/actions/applyRevision';
 
-const mapStateToProps = ({ entities }) => ({
-  entities
+const Activity = daggy.taggedSum('Activity', {
+  Revision: ['details'],
+  Source: ['details'],
+  OutputSchema: ['details'],
+  TaskSet: ['details'],
+  FinishedTaskSet: ['details'],
+  FailedTaskSet: ['details'],
+  Empty: []
+});
+
+// shapeRevision :: [Revisions] -> Int -> [Activity]
+const shapeRevisions = (revisions, revisionSeq) => {
+  const currentRevision = revisions.find(revision => revision.revision_seq === revisionSeq);
+
+  if (!currentRevision) {
+    return [Activity.Empty];
+  }
+
+  const createdAt = currentRevision.created_at || 'hmm';
+  const createdBy = currentRevision.created_by.display_name || 'Unknown';
+
+  return [
+    Activity.Revision({
+      createdAt,
+      createdBy
+    })
+  ];
+};
+
+// shapeSources :: [Sources] -> [Activity]
+const shapeSources = sources =>
+  sources.length === 0
+    ? [Activity.Empty]
+    : sources.map(source =>
+        Activity.Source({
+          createdAt: source.created_at,
+          createdBy: source.created_by.display_name
+        })
+      );
+
+// shapeOutputSchemas :: [OutputSchema] -> [InputSchema] -> [Sources]  -> [Activity]
+const shapeOutputSchemas = (oss, iss, ss) =>
+  oss.length === 0
+    ? [Activity.Empty]
+    : oss.map(os => {
+      const inputSchema = iss.find(is => is.id === os.input_schema_id);
+      const source = ss.find(s => s.id === inputSchema.source_id);
+
+      return Activity.OutputSchema({
+        createdAt: os.created_at,
+        createdBy: os.created_by.display_name,
+        sourceId: source.id,
+        isid: inputSchema.id,
+        osid: os.id
+      });
+    });
+
+// shapeTaskSets :: [TaskSet] -> [Activity]
+const shapeTaskSets = tss =>
+  tss.length === 0
+    ? [Activity.Empty]
+    : tss.map(ts =>
+        Activity.TaskSet({
+          createdAt: ts.created_at,
+          createdBy: ts.created_by.display_name
+        })
+      );
+
+// shapeFailedTaskSets :: [TaskSet] -> [Activity]
+const shapeFailedTaskSets = tss => {
+  const failed = tss
+    .filter(ts => !!ts.finished_at)
+    .filter(ts => ts.status === ApplyRevision.TASK_SET_FAILURE);
+
+  return failed.length === 0
+    ? [Activity.Empty]
+    : failed.map(ts =>
+        Activity.FailedTaskSet({
+          createdAt: ts.finished_at,
+          createdBy: ts.created_by.display_name
+        })
+      );
+};
+
+// shapeFinishedTaskSets :: [TaskSet] -> [Activity]
+const shapeFinishedTaskSets = tss => {
+  const finished = tss
+    .filter(ts => !!ts.finished_at)
+    .filter(ts => ts.status === ApplyRevision.TASK_SET_SUCCESSFUL);
+
+  return finished.length === 0
+    ? [Activity.Empty]
+    : finished.map(ts =>
+        Activity.FinishedTaskSet({
+          createdAt: ts.finished_at,
+          createdBy: ts.created_by.display_name
+        })
+      );
+};
+
+// createActivities :: Entities -> Params -> [Activity]
+const createActivities = (entities, params) => {
+  const activities = [
+    ...shapeRevisions(_.values(entities.revisions), _.toNumber(params.revisionSeq)),
+    ...shapeSources(_.values(entities.sources)),
+    ...shapeOutputSchemas(
+      _.values(entities.output_schemas),
+      _.values(entities.input_schemas),
+      _.values(entities.sources)
+    ),
+    ...shapeTaskSets(_.values(entities.task_sets)),
+    ...shapeFailedTaskSets(_.values(entities.task_sets)),
+    ...shapeFinishedTaskSets(_.values(entities.task_sets))
+  ];
+
+  return _.chain(activities)
+    .filter(activity => !Activity.Empty.is(activity))
+    .sortBy(activity => activity.details.createdAt)
+    .reverse()
+    .value();
+};
+
+const mapStateToProps = ({ entities }, { params }) => ({
+  activities: createActivities(entities, params),
+  params
 });
 
 export default withRouter(connect(mapStateToProps)(RecentActions));

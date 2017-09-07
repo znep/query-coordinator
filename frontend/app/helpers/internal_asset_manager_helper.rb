2666d10137228c0a110e7c2134245cbe012bd00a
Michael Chui <michael.chui@socrata.com>
2017-09-07 14:20:55 -0700

    EN-18610 (added fix): Only send for_user when on myAssets tab.

--- frontend/app/helpers/internal_asset_manager_helper.rb
+++ frontend/app/helpers/internal_asset_manager_helper.rb
@@ -159,8 +159,12 @@ module InternalAssetManagerHelper
       options.merge!(published: false, only: 'datasets') if params[:assetTypes] == 'workingCopies'
       options.merge!(published: true) if params[:assetTypes] == 'datasets'
       # EN-15849
-      options.merge!(for_user: current_user.id) if feature_flag?(:enable_internal_asset_manager_my_assets, request)
+      options.merge!(for_user: current_user.id) if current_tab_is_my_assets && feature_flag?(:enable_internal_asset_manager_my_assets, request)
     end
+  end
+
+  def current_tab_is_my_assets
+    params.fetch(:tab, 'myAssets') == 'myAssets'
   end
 
 end

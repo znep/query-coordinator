1bd21a6ca2833e9082690e8211120702be4fb1c1
Nate Moore <nathaniel.moore@socrata.com>
2017-09-21 13:47:33 -0700

    EN-18397: Add new rights to enums (#5525)

--- frontend/lib/user_rights.rb
+++ frontend/lib/user_rights.rb
@@ -1,58 +1,64 @@
 # Enum of the possible rights that are assigned directly to a user.
 module UserRights
-
-  # NOTE: This should reflect the Rights enum from Core at
-  # unobtainium/src/main/java/com/blist/models/account/User.java
-  # and the js export list at
-  # frontend/public/javascript/common/rights.js
-  SOURCE_ARRAY = %w{
-    approve_nominations
-    change_configurations
-    chown_datasets
-    create_dashboards
-    create_datasets
-    create_pages
-    create_story
-    create_story_copy
-    delete_story
-    edit_dashboards
-    edit_goals
-    edit_nominations
-    edit_others_datasets
-    edit_pages
-    edit_sdp
-    edit_site_theme
-    edit_story
-    edit_story_title_desc
-    feature_items
-    federations
-    manage_approval
-    manage_provenance
-    manage_stories
-    manage_story_collaborators
-    manage_story_public_version
-    manage_story_visibility
-    manage_users
-    moderate_comments
-    short_session
-    use_data_connectors
-    view_all_dataset_status_logs
-    view_dashboards
-    view_domain
-    view_goals
-    view_others_datasets
-    view_story
-    view_unpublished_story
-    edit_others_stories
-    view_stories_stats
-  }
-
-  SOURCE_ARRAY.each do |right|
-    const_set(right.upcase, right)
+  
+    # NOTE: This should reflect the Rights enum from Core at
+    # unobtainium/src/main/java/com/blist/models/account/User.java
+    # and the js export list at
+    # frontend/public/javascript/common/rights.js
+    SOURCE_ARRAY = %w{
+      approve_nominations
+      change_configurations
+      chown_datasets
+      create_dashboards
+      create_datasets
+      create_pages
+      edit_dashboards
+      edit_goals
+      edit_nominations
+      edit_others_datasets
+      edit_pages
+      edit_sdp
+      edit_site_theme
+      feature_items
+      federations
+      manage_approval
+      manage_goals
+      manage_provenance
+      manage_spatial_lens
+      manage_stories
+      manage_users
+      moderate_comments
+      short_session
+      use_data_connectors
+      view_activity_log
+      view_all_dataset_status_logs
+      view_dashboards
+      view_domain
+      view_goals
+      view_others_datasets
+      create_data_lens
+  
+      create_story
+      create_story_copy
+      delete_story
+      edit_others_stories
+      edit_story
+      edit_story_title_desc
+      manage_story_collaborators
+      manage_story_public_version
+      manage_story_visibility
+      view_story
+      view_stories_stats
+      view_unpublished_story
+    }
+  
+    SOURCE_ARRAY.each do |right|
+      const_set(right.upcase, right)
+    end
+  
+    def self.to_h
+      SOURCE_ARRAY.reduce({}) { |acc, right| acc[right.upcase] = right ; acc }
+    end
+  
   end
-
-  def self.to_h
-    SOURCE_ARRAY.reduce({}) { |acc, right| acc[right.upcase] = right ; acc }
-  end
-
-end
+  

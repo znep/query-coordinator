b102bd8ff13b1bce7565e258facc659472f60f65
Constantine Vetoshev <constantine.vetoshev@socrata.com>
2017-10-05 15:06:35 -0700

    EN-16805: Use the new helpers to add month sorting support.

--- common/visualizations/dataProviders/UngroupedCategoricalDataManager.js
+++ common/visualizations/dataProviders/UngroupedCategoricalDataManager.js
@@ -1,8 +1,9 @@
-// Vendor Imports
-const _ = require('lodash');
-// Project Imports
-const I18n = require('common/i18n').default;
-const makeSocrataCategoricalDataRequest = require('./makeSocrataCategoricalDataRequest');
+import _ from 'lodash';
+
+import I18n from 'common/i18n';
+import * as MPH from 'common/visualizations/dataProviders/MonthPredicateHelper';
+import makeSocrataCategoricalDataRequest from './makeSocrataCategoricalDataRequest';
+
 
 function getData(vif, options) {
   const dataRequests = vif.series.map((series, seriesIndex) => {
@@ -115,7 +116,23 @@ function getData(vif, options) {
       compact().
       every((val) => !_.isNaN(_.toNumber(val)));
 
-    const compareValues = makeValueComparator(orderBySort, doSortNumeric);
+    // Determine whether all values for ordering are months in the same language.
+    const language = MPH.detectLanguage(_.get(dataTable, `rows.0.${sortValueIndex}`, null));
+    const doSortMonths =
+      !doSortNumeric &&
+      language &&
+      _.every(dataTable.rows,
+              (row) => !_.isUndefined(MPH.monthIndex(row[sortValueIndex], language)));
+
+    let transformer = _.identity;
+    if (doSortNumeric) {
+      transformer = _.toNumber;
+    }
+    else if (doSortMonths) {
+      transformer = (x) => _.toNumber(MPH.monthIndex(x, language));
+    }
+
+    const compareValues = makeValueComparator(orderBySort, transformer);
     const compareCategoriesToNullAndOther = (a, b) => {
       if (a === otherCategoryName) {
         return 1;
@@ -194,15 +211,14 @@ function getData(vif, options) {
 // NOTE: There's a possibility that we can move this logic further down into
 // makeSocrataCategoricalDataRequest, but at time of writing that module isn't
 // covered by tests, so the change is too risky to attempt.
-function makeValueComparator(direction, numeric) {
+function makeValueComparator(direction, transformer = _.identity) {
   const _compare = (direction === 'asc') ? _.gt : _.lt;
-  const _transform = numeric ? _.toNumber : _.identity;
 
   return (a, b) =>  {
     if (a === b) {
       return 0;
     } else {
-      return _compare(_transform(a), _transform(b)) ? 1 : -1;
+      return _compare(transformer(a), transformer(b)) ? 1 : -1;
     }
   };
 }

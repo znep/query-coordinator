3b87e075773ad89f85ff5fe55589f8ea95de83c6
Rifat Cagri Ekin <cagri.ekin@gmail.com>
2017-10-18 03:34:26 -0700

    Revert "EN-18783 DSLP > Menu consistency"

--- frontend/public/javascripts/datasetLandingPage/components/InfoPaneButtons.js
+++ frontend/public/javascripts/datasetLandingPage/components/InfoPaneButtons.js
@@ -5,13 +5,25 @@ import { isUserRoled } from '../../common/user';
 import { localizeLink } from 'common/locale';
 import { FeatureFlags } from 'common/feature_flags';
 
-import DownloadFlyout from './DownloadFlyout';
-import ApiFlyout from './ApiFlyout';
-import ShareFlyout from './ShareFlyout';
-
 export default class InfoPaneButtons extends Component {
   componentDidMount() {
     window.addEventListener('resize', this.alignActionButtons);
+  }
+
+  componentWillUpdate(nextProps) {
+    const { isMobile, isTablet } = nextProps;
+    const apiBtn = document.querySelector('.entry-actions .api');
+    const apiLink = document.querySelector('.entry-actions .api-link');
+
+    if (apiBtn && apiLink) {
+      if (isMobile || isTablet) {
+        apiBtn.classList.add('hidden');
+        apiLink.classList.remove('hidden');
+      } else {
+        apiBtn.classList.remove('hidden');
+        apiLink.classList.add('hidden');
+      }
+    }
   }
 
   componentWillUnmount() {
@@ -175,8 +187,115 @@ export default class InfoPaneButtons extends Component {
     );
   }
 
+  renderDownloadLink(format, callback) {
+    const { view } = this.props;
+    let extension = format;
+    let type = format.toUpperCase();
+    const params = {
+      accessType: 'DOWNLOAD'
+    };
+
+    if (format === 'csv_for_excel') {
+      extension = 'csv';
+      type = 'CSV for Excel';
+      params.bom = 'true';
+      params.format = 'true';
+    }
+
+    if (format === 'csv_for_excel_europe') {
+      extension = 'csv';
+      type = 'CSV for Excel (Europe)';
+      params.bom = 'true';
+      params.format = 'true';
+      params.delimiter = ';';
+    }
+
+    if (format === 'tsv_for_excel') {
+      extension = 'tsv';
+      type = 'TSV for Excel';
+      params.bom = 'true';
+    }
+
+    // Construct the query string, url, and label
+    const queryString = $.param(params);
+    const url = `/api/views/${view.id}/rows.${extension}?${queryString}`;
+    const label = I18n.download[format] || format.toUpperCase();
+
+    return (
+      <li key={format}>
+        <a role="menuitem" href={url} data-type={type} onClick={callback}>
+          {label}
+        </a>
+      </li>
+    );
+  }
+
+  renderDownloadDropdown() {
+    const { onDownloadData, view } = this.props;
+    const overrideLink = _.get(view.metadata, 'overrideLink');
+
+    // Special cases for different view types and download overrides
+    if (view.isHref) {
+      return null;
+    } else if (view.isBlobby) {
+      return (
+        <a
+          href={`/api/views/${view.id}/files/${view.blobId}?filename=${view.blobFilename}`}
+          className="btn btn-simple btn-sm unstyled-link download"
+          target="_blank">
+          {I18n.action_buttons.download}
+        </a>
+      );
+    } else if (overrideLink) {
+      return (
+        <a href={overrideLink} className="btn btn-simple btn-sm unstyled-link download">
+          {I18n.action_buttons.download}
+        </a>
+      );
+    } else {
+      const exportLinks = _.map(view.exportFormats, (format) =>
+        this.renderDownloadLink(format, onDownloadData)
+      );
+
+      return (
+        <div
+          className="dropdown btn btn-simple download btn-sm"
+          data-dropdown
+          data-orientation="bottom">
+          <span aria-hidden>{I18n.action_buttons.download}</span>
+          <ul role="menu" aria-label={I18n.action_buttons.download} className="dropdown-options">
+            {exportLinks}
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  renderApiButton() {
+    const { view } = this.props;
+    const isBlobbyOrHref = view.isBlobby || view.isHref;
+
+    if (isBlobbyOrHref) {
+      return null;
+    }
+
+    return (
+      <button className="btn btn-simple btn-sm api" data-modal="api-modal">
+        {I18n.action_buttons.api}
+      </button>
+    );
+  }
+
+  renderShareButton() {
+    return (
+      <button className="btn btn-simple btn-sm share" data-modal="share-modal">
+        {I18n.action_buttons.share}
+      </button>
+    );
+  }
+
   renderMoreActions() {
-    const { view, onWatchDatasetFlagClick, isMobile, isTablet } = this.props;
+    const { view, onWatchDatasetFlagClick } = this.props;
     const isBlobbyOrHref = view.isBlobby || view.isHref;
 
     const contactFormLink = !view.disableContactDatasetOwner ? (
@@ -206,9 +325,8 @@ export default class InfoPaneButtons extends Component {
       );
     }
 
-    const shouldApiLinkRender = isMobile || isTablet;
     let apiLink = null;
-    if (shouldApiLinkRender && !isBlobbyOrHref) {
+    if (!isBlobbyOrHref) {
       apiLink = (
         <li>
           <a tabIndex="0" role="button" className="option api-link" data-modal="api-modal">
@@ -257,9 +375,9 @@ export default class InfoPaneButtons extends Component {
       <div className="btn-group">
         {this.renderExploreDataDropdown()}
         {this.renderManageButton()}
-        <DownloadFlyout {...this.props} />
-        <ApiFlyout {...this.props} />
-        <ShareFlyout {...this.props} />
+        {this.renderDownloadDropdown()}
+        {this.renderApiButton()}
+        {this.renderShareButton()}
         {this.renderMoreActions()}
       </div>
     );

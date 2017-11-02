98702f86e5cf82a4dca69e448e69911af0b81ace
Roark Brewster <roarkb@users.noreply.github.com>
2017-11-02 16:03:09 -0700

    EN-19679 - Storyteller: write cheetahRevision to version endpoint (#5808)
    EN-19679 - write cheetahRevision to version endpoint when Storyteller gets built

--- storyteller/spec/controllers/version_controller_spec.rb
+++ storyteller/spec/controllers/version_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe VersionController, type: :controller do
       stub_const('Storyteller::REVISION_NUMBER', 'therevisionnumber')
       stub_const('Storyteller::BUILD_TIMESTAMP', 12345678)
       stub_const('Storyteller::BOOTED_TIMESTAMP', 98765432)
+      stub_const('Storyteller::CHEETAH_REVISION_NUMBER', 'thecheetahrevisionnumber')
 
       get :show
     end
@@ -30,6 +31,10 @@ RSpec.describe VersionController, type: :controller do
 
     it 'renders semantic version' do
       expect(result['version']).to eq(SemVer.find.format('%M.%m.%p'))
+    end
+
+    it 'renders cheetah revision number' do
+      expect(result['cheetahRevision']).to eq 'thecheetahrevisionnumber'
     end
   end
 end

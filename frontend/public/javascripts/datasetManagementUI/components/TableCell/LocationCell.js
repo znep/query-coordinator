4b5355dfb0e1c17924ab133599a6aacb30e7693b
Chris Duranti <chrisd1891@gmail.com>
2017-11-28 16:40:06 -0800

    EN-19498 Location Cell formatting (#5985)
    * Make it match other UI components

--- frontend/public/javascripts/datasetManagementUI/components/TableCell/LocationCell.js
+++ frontend/public/javascripts/datasetManagementUI/components/TableCell/LocationCell.js
@@ -4,8 +4,18 @@ import TypedCell from './TypedCell';
 
 class LocationCell extends Component {
   render() {
-    const { latitude, longitude } = this.props.value;
-    const text = `Location(${latitude}, ${longitude})`;
+    const {
+      latitude,
+      longitude,
+      human_address: ha
+    } = this.props.value;
+
+    const address = ha.address || '';
+    const city = ha.city || '';
+    const state = ha.state || '';
+    const zip = ha.zip || '';
+
+    const text = `${address} ${city} ${state} ${zip} (${latitude}, ${longitude})`.trim();
 
     return (<TypedCell value={text} format={this.props.format} />);
   }

5908ce942773c823026f12f68178b0996667f81c
Brandon Webster <brandon.webster@socrata.com>
2017-12-05 10:54:37 -0800

    EN-17527 added new selectors and tests

---
+++ frontend/karma/datasetManagementUI/components/NewFormTest.js
@@ -0,0 +1,128 @@
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+import NewForm, {
+  getOutputSchemaCols,
+  getRevision
+} from 'components/NewForm/NewForm';
+
+describe.only('NewForm', () => {
+  describe('getRevision', () => {
+    const revision1 = {
+      id: 600,
+      fourfour: 'abcd-efgh',
+      revision_seq: 0
+    };
+
+    const revision2 = {
+      id: 601,
+      fourfour: 'abcd-efgh',
+      revision_seq: 1
+    };
+
+    const revisions = {
+      [revision1.id]: revision1,
+      [revision2.id]: revision2
+    };
+
+    it('finds a revision by sequence number if it exists in revisions table', () => {
+      const result = getRevision(revisions, 0);
+      assert.deepEqual(result, revision1);
+    });
+
+    it('returns undefined if revision sequence param is not a number', () => {
+      const result = getRevision(revisions, '1');
+      assert.isUndefined(result);
+    });
+
+    it('returns undefined if revision sequence param is NaN', () => {
+      const result = getRevision(revisions, NaN);
+      assert.isUndefined(result);
+    });
+
+    it('returns undefined if the revisions table is undefined', () => {
+      const result = getRevision(undefined, 0);
+      assert.isUndefined(result);
+    });
+  });
+
+  describe('getOutputSchemaCols', () => {
+    const output_schema_columns = {
+      '4-1234': {
+        id: '4-1234',
+        output_schema_id: 4,
+        output_column_id: 1234,
+        is_primary_key: false
+      },
+      '4-1235': {
+        id: '4-1235',
+        output_schema_id: 4,
+        output_column_id: 1235,
+        is_primary_key: false
+      },
+      '5-1236': {
+        id: '5-1236',
+        output_schema_id: 5,
+        output_column_id: 1236,
+        is_primary_key: false
+      }
+    };
+
+    const output_schemas = {
+      '4': {
+        id: 4
+      },
+      '5': {
+        id: 5
+      }
+    };
+
+    const output_columns = {
+      '1234': {
+        id: 1234,
+        position: 1,
+        transform_id: 7
+      },
+      '1235': {
+        id: 1235,
+        position: 2,
+        transform_id: 7
+      },
+      '1236': {
+        id: 1236,
+        position: 1,
+        transform_id: 7
+      }
+    };
+
+    const transforms = {
+      '7': {
+        id: 7
+      }
+    };
+
+    const entities = {
+      transforms,
+      output_schemas,
+      output_columns,
+      output_schema_columns
+    };
+
+    it('returns an array of output columns for an output schema', () => {
+      const result = getOutputSchemaCols(entities, 4);
+      assert.isArray(result);
+      assert.lengthOf(result, 2);
+      assert.sameMembers(result.map(res => res.id), [1234, 1235]);
+    });
+
+    it('returns undefined if outputSchemaId param is not a number', () => {
+      const result = getOutputSchemaCols(entities, '4');
+      assert.isUndefined(result);
+    });
+
+    it('returns undefined if outputSchemaId param is NaN', () => {
+      const result = getOutputSchemaCols(entities, NaN);
+      assert.isUndefined(result);
+    });
+  });
+});
---
+++ frontend/public/javascripts/datasetManagementUI/components/NewForm/Newform.js
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as Selectors from 'selectors';
+
+class NewForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      datasetForm: {},
+      columnForm: {}
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      datasetForm: this.props.datasetMetadata,
+      columnForm: this.props.outputSchemaColumns
+    });
+  }
+
+  render() {
+    return <pre style={{ whiteSpace: 'pre-wrap', width: 500 }}>{JSON.stringify(this.state)}</pre>;
+  }
+}
+
+// isNumber :: Any -> Boolean
+function isNumber(x) {
+  return typeof x === 'number' && !isNaN(x);
+}
+
+// getOutputSchemaCols :: Entities -> Number -> Array OutputColumn | undefined
+export function getOutputSchemaCols(entities, outputSchemaId) {
+  let cols;
+
+  if (isNumber(outputSchemaId)) {
+    cols = Selectors.columnsForOutputSchema(entities, outputSchemaId);
+  }
+
+  return cols;
+}
+
+//  getRevision :: Revisions -> Number -> Revision | undefined
+// Attempts to find a revision by its revision sequence number; if it fails to
+// find a revision, it returns undefined
+export function getRevision(revisions = {}, revisionSeq) {
+  let rev;
+
+  if (isNumber(revisionSeq)) {
+    rev = Object.values(revisions).find(r => r.revision_seq === revisionSeq);
+  }
+
+  return rev;
+}
+
+const mapStateToProps = ({ entities }, { params }) => {
+  const revisionSeq = Number(params.revisionSeq);
+  const outputSchemaId = Number(params.outputSchemaId);
+  const revision = getRevision(entities.revisions, revisionSeq);
+  const datasetMetadata = revision && revision.metadata ? Selectors.datasetMetadata(revision.metadata) : {};
+  const outputSchemaColumns = getOutputSchemaCols(entities, outputSchemaId) || {};
+
+  return {
+    datasetMetadata,
+    outputSchemaColumns
+  };
+};
+
+export default connect(mapStateToProps)(NewForm);

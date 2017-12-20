241ce0a0a63181d0e9c7decc38d501f42ff58f47
Thomas Brigham <thomas@thomasbrigham.me>
2017-12-20 13:40:55 -0800

    EN-20845: Adds fix for user_profile column

--- frontend/karma/userProfile/index.js
+++ frontend/karma/userProfile/index.js
@@ -2,6 +2,8 @@ import 'script!jquery';
 
 window.serverConfig = require('./data/mock_server_config').default;
 window.socrata = require('./data/mock_initial_state').default;
+window.sessionData = require('./data/mock_mixpanel_config').default;
+window.mixpanelConfig = require('./data/mock_session_data').default;
 
 function requireAll(context) {
   context.keys().forEach(context);

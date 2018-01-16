dfa73fc9e4ca2feaebacfb200c1a4744cc546071
Marc Slemko <marc.slemko@socrata.com>
2018-01-15 23:18:54 -0800

    fixup! EN-21473: Fix remote code execution vulnerability in RemotePartialsController.
    typo in comment

--- frontend/app/controllers/remote_partials_controller.rb
+++ frontend/app/controllers/remote_partials_controller.rb
@@ -1,7 +1,7 @@
 class RemotePartialsController < ApplicationController
   skip_before_filter :require_user
 
-  # NOTE: It is not safe to pass request parameters directly into into the partial as locals, as of
+  # NOTE: It is not safe to pass request parameters directly into the partial as locals, as of
   # 2018/01/15 rails versions prior to 5.1.0 have a security hole that allows code injection if you
   # allow arbitrary keys.
   def templates

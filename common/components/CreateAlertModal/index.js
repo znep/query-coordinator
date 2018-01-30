c496b7e979f0333e062b4daec8d8e013303e50ae
haibeeb <haiton@gmail.com>
2018-01-30 11:45:25 -0800

    Elumitas/en 21901/alert grammar changes (#6511)
    * EN-21091: Update grammar strings for alerts advanced search
    
    * EN-21901: Change key name to be more grammatically correct

--- common/components/CreateAlertModal/index.js
+++ common/components/CreateAlertModal/index.js
@@ -226,8 +226,8 @@ class CreateAlertModal extends Component {
   renderAdvanceTabContent() {
 
     const { rawSoqlQuery } = this.state;
-    const alertTitle = I18n.t('alert_title', { scope: 'shared.components.create_alert_modal.advance_search' });
-    const description = I18n.t('description', { scope: 'shared.components.create_alert_modal.advance_search' });
+    const alertTitle = I18n.t('alert_title', { scope: 'shared.components.create_alert_modal.advanced_search' });
+    const description = I18n.t('description', { scope: 'shared.components.create_alert_modal.advanced_search' });
 
     return (
       <div styleName="advance-alert-section">
@@ -239,7 +239,7 @@ class CreateAlertModal extends Component {
 
         <div>
           <label styleName="raw-query-title">
-            {I18n.t('text_box_description', { scope: 'shared.components.create_alert_modal.advance_search' })}
+            {I18n.t('text_box_description', { scope: 'shared.components.create_alert_modal.advanced_search' })}
           </label>
           <textarea
             value={rawSoqlQuery}

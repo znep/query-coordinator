cd4fde4a3b3be703452b7c29f292d843c5437c0b
Brandon Webster <brandon.webster@socrata.com>
2018-01-30 10:50:45 -0800

    EN-21576 add status text

--- frontend/public/javascripts/datasetManagementUI/components/ManageMetadataSidebar/ManageMetadataSidebar.js
+++ frontend/public/javascripts/datasetManagementUI/components/ManageMetadataSidebar/ManageMetadataSidebar.js
@@ -2,9 +2,46 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router';
 import * as Links from 'datasetManagementUI/links/links';
+import {
+  SAVED,
+  UNSAVED,
+  ERRORED,
+  INITIALIZED
+} from 'datasetManagementUI/components/ManageMetadata/ManageMetadata';
 import StatusIndicator from 'datasetManagementUI/components/StatusIndicator/StatusIndicator';
 
 const datasetMetadataEnabled = !window.serverConfig.featureFlags.usaid_features_enabled;
+
+const FormStatusText = ({ formStatus }) => {
+  let statusText;
+
+  switch (formStatus) {
+    case INITIALIZED:
+      statusText = 'No changes';
+      break;
+    case SAVED:
+      statusText = 'Saved changes';
+      break;
+    case UNSAVED:
+      statusText = 'Unsaved changes';
+      break;
+    case ERRORED:
+      statusText = 'Error';
+      break;
+    default:
+      statusText = '';
+  }
+
+  return (
+    <span className={formStatus === ERRORED ? 'dsmp-status-text-error' : 'dsmp-status-text'}>
+      {statusText}
+    </span>
+  );
+};
+
+FormStatusText.propTypes = {
+  formStatus: PropTypes.string
+};
 
 const ManageMetadataSidebar = ({
   params,
@@ -23,6 +60,8 @@ const ManageMetadataSidebar = ({
           className="dsmp-tab"
           activeClassName="selected">
           {I18n.metadata_manage.dataset_metadata_label}
+          <br />
+          <FormStatusText formStatus={datasetFormStatus} />
           <StatusIndicator formStatus={datasetFormStatus} />
         </Link>
       ) : (
@@ -35,6 +74,8 @@ const ManageMetadataSidebar = ({
           className="dsmp-tab"
           activeClassName="selected">
           {I18n.metadata_manage.column_metadata_label}
+          <br />
+          <FormStatusText formStatus={columnFormStatus} />
           <StatusIndicator formStatus={columnFormStatus} />
         </Link>
       ) : (

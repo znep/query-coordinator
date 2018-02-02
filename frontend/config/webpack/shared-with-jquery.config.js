5f2cb58861d77ed671740fb907904bc1b18b43b3
Brandon Webster <brandon.webster@socrata.com>
2018-02-02 09:29:48 -0800

    Revert "Merge pull request #6569 from socrata/branweb/EN-22023-aaf-fix"
    This reverts commit b6250e09efabc1b903b7c127a3ff4dfdbd4149a7, reversing
    changes made to d3a7f7bbec3bb52f757f87ac53bdbb022703d2d9.

--- frontend/config/webpack/shared-with-jquery.config.js
+++ frontend/config/webpack/shared-with-jquery.config.js
@@ -11,7 +11,6 @@ var identifier = path.basename(__filename, '.config.js');
 module.exports = _.defaultsDeep({
   context: path.resolve(webpackHelpers.frontendRoot, 'public/javascripts'),
   entry: webpackHelpers.withHotModuleEntries({
-    'adminActivityFeed': 'adminActivityFeed/main.js',
     'gridViewSocrataVisualizations': 'grid_view_socrata_visualizations/main.js',
     'visualizationEmbedLoader': 'visualization_embed/loader.js',
     'visualizationEmbedMain': 'visualization_embed/main.js'

0aa5499ca88a8801ee68868ab23dc8e008f28eca
Brandon Webster <brandon.webster@socrata.com>
2018-02-08 11:23:44 -0800

    EN-22162 hide label on activity feed

--- common/components/FilterBar/FilterItem.js
+++ common/components/FilterBar/FilterItem.js
@@ -296,6 +296,7 @@ export class FilterItem extends Component {
     //       way to do this.
     return (
       <div className="filter-bar-filter">
+        <label className="filter-control-label">{this.props.column.name}</label>
         <div
           className="filter-control-container"
           ref={(ref) => this.controlContainer = ref}
